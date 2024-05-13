f84e0d595addc667a4e51b5204c7b24dc48411e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 14:03:38 +0000

    Add package RoboticConsole/0.1/RoboticConsole-0.1.tar.gz

---
+++ packages/RoboticConsole/RoboticConsole-0.1.tar.gz/RoboticConsole-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RoboticConsole",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  
+    entry_points={},
+    author="kunal bhande",
+    author_email="bhandekunal16@gmail.com",
+    description="this is replacement of current terminal with easy to access thing.",
+    url="https://github.com/Bhandekunal16/Robotic",
+)
