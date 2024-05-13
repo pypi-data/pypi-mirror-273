f9ed66e91e763da8806630277564a9864b191e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 15:03:50 +0000

    Add package lxs-SimpleTools/1.0.0/lxs_simpletools-1.0.0.tar.gz

---
+++ packages/lxs-SimpleTools/lxs_simpletools-1.0.0.tar.gz/lxs_simpletools-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lxs-SimpleTools',
+    version='1.0.0',
+    author='liuxiansong',
+    author_email='326427540@qq.com',
+    description='处理字符串和时间简单工具函数',
+    packages=find_packages(),  # 自动查找并包含所有的包
+    install_requires=[
+
+    ],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
