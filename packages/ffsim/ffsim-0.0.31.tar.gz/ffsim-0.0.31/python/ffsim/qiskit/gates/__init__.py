8fddb7a7d6289f3a97dbd349b622dc8a422e55c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 14:03:59 +0000

    Add package ffsim/0.0.31/ffsim-0.0.31.tar.gz

---
+++ packages/ffsim/ffsim-0.0.31.tar.gz/ffsim-0.0.31/python/ffsim/qiskit/gates/__init__.py
@@ -0,0 +1,27 @@
+# (C) Copyright IBM 2024.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Qiskit fermionic quantum gates."""
+
+from ffsim.qiskit.gates.diag_coulomb import DiagCoulombEvolutionJW
+from ffsim.qiskit.gates.orbital_rotation import OrbitalRotationJW
+from ffsim.qiskit.gates.slater_determinant import (
+    PrepareHartreeFockJW,
+    PrepareSlaterDeterminantJW,
+)
+from ffsim.qiskit.gates.ucj import UCJOperatorJW
+
+__all__ = [
+    "DiagCoulombEvolutionJW",
+    "OrbitalRotationJW",
+    "PrepareHartreeFockJW",
+    "PrepareSlaterDeterminantJW",
+    "UCJOperatorJW",
+]
