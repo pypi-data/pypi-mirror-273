6036238969067b25816081846eafb10605b2ecf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 15:02:55 +0000

    Add package funcnodes-opencv/0.1.5/funcnodes_opencv-0.1.5-py3-none-any.whl

---
+++ packages/funcnodes-opencv/funcnodes_opencv-0.1.5-py3-none-any.whl/funcnodes_opencv/__init__.py
@@ -0,0 +1,23 @@
+from .imageformat import OpenCVImageFormat
+from . import image_operations, masks, colormodes, filter, components
+import funcnodes as fn
+import funcnodes_numpy as fnnp  # noqa: F401 # for type hinting
+
+__all__ = ["OpenCVImageFormat", "image_operations", "masks"]
+
+
+__version__ = "0.1.5"
+
+
+NODE_SHELF = fn.Shelf(
+    name="OpenCV",
+    description="OpenCV image processing nodes.",
+    subshelves=[
+        image_operations.NODE_SHELF,
+        masks.NODE_SHELF,
+        colormodes.NODE_SHELF,
+        filter.NODE_SHELF,
+        components.NODE_SHELF,
+    ],
+    nodes=[],
+)
