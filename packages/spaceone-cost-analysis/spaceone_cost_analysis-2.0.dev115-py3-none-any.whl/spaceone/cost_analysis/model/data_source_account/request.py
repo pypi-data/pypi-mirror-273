8073f375a437ce7dd198477131d18f859b42cf39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 15:03:03 +0000

    Add package spaceone-cost-analysis/2.0.dev115/spaceone_cost_analysis-2.0.dev115-py3-none-any.whl

---
+++ packages/spaceone-cost-analysis/spaceone_cost_analysis-2.0.dev115-py3-none-any.whl/spaceone/cost_analysis/model/data_source_account/request.py
@@ -0,0 +1,45 @@
+from typing import Union
+from pydantic import BaseModel
+
+__all__ = [
+    "DataSourceAccountUpdateRequest",
+    "DataSourceAccountResetRequest",
+    "DataSourceAccountGetRequest",
+    "DataSourceAccountSearchQueryRequest",
+    "DataSourceAccountStatQueryRequest",
+]
+
+
+class DataSourceAccountUpdateRequest(BaseModel):
+    data_source_id: str
+    account_id: str
+    workspace_id: Union[str, None] = None
+    domain_id: str
+
+
+class DataSourceAccountResetRequest(BaseModel):
+    data_source_id: str
+    account_id: Union[str, None] = None
+    workspace_id: Union[str, None] = None
+    domain_id: str
+
+
+class DataSourceAccountGetRequest(BaseModel):
+    data_source_id: str
+    account_id: str
+    workspace_id: Union[str, None] = None
+    domain_id: str
+
+
+class DataSourceAccountSearchQueryRequest(BaseModel):
+    query: Union[dict, None] = None
+    data_source_id: Union[str, None] = None
+    account_id: Union[str, None] = None
+    workspace_id: Union[str, None] = None
+    domain_id: str
+
+
+class DataSourceAccountStatQueryRequest(BaseModel):
+    query: dict
+    workspace_id: Union[str, None] = None
+    domain_id: str
