0ad8dec57b0ffd64f592038e394acf7f0fc0b165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 15:03:22 +0000

    Add package sky130/0.11.1/sky130-0.11.1-py3-none-any.whl

---
+++ packages/sky130/sky130-0.11.1-py3-none-any.whl/sky130/config.py
@@ -0,0 +1,44 @@
+"""Store configuration."""
+
+__all__ = ["PATH"]
+
+import pathlib
+
+home = pathlib.Path.home()
+cwd = pathlib.Path.cwd()
+cwd_config = cwd / "config.yml"
+
+home_config = home / ".config" / "sky130.yml"
+config_dir = home / ".config"
+config_dir.mkdir(exist_ok=True)
+module_path = pathlib.Path(__file__).parent.absolute()
+repo_path = module_path.parent
+
+
+class Path:
+    module = module_path
+    repo = repo_path
+    lyp = module_path / "klayout" / "layers.lyp"
+    lyp_yaml = module_path / "klayout" / "layers.yaml"
+    libs = module_path / "sky130A"
+    sparameters = module_path / "sparameters"
+
+    libs_tech = libs / "libs.tech"
+    libs_ref = libs / "libs.ref"
+    libs_ngspice = libs_tech / "ngspice"
+    spice = module_path / "spice"
+
+    libhd = libs / "sky130hd"
+    libhs = libs / "sky130hs"
+    libhvl = libs / "sky130hvl"
+
+    gds = module_path / "gds"
+    gdshd = libhd / "gds" / "sky130_fd_sc_hd.gds"
+    gdshs = libhs / "gds" / "sky130_fd_sc_hs.gds"
+    gdshvl = libhvl / "gds" / "sky130_fd_sc_hvl.gds"
+
+
+PATH = Path()
+
+if __name__ == "__main__":
+    print(PATH)
