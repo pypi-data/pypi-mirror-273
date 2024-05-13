bb478d3fb00c81624082e3edeb20f7c68ea2e976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 14:04:02 +0000

    Add package nonebot-tsugu/0.0.2/nonebot_tsugu-0.0.2.tar.gz

---
+++ packages/nonebot-tsugu/nonebot_tsugu-0.0.2.tar.gz/nonebot_tsugu-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+
+with open('README.md', 'r', encoding='utf-8') as readme:
+    long_description = readme.read()
+
+setup(
+    name='nonebot_tsugu',
+    version='0.0.2',
+    author='otae',
+    author_email='otae1204@qq.com',
+    description='基于tsugu-bangdream-bot项目实现的nonebot2插件',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/otae-1204/NoneBot-Tsugu',
+    include_package_data=False,
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.10',
+)
