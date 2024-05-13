c6c2ec08ca2b656b17dd28cfdb8bd376f249df54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 16:03:35 +0000

    Add package Hakware-py/0.5.0/Hakware-py-0.5.0.tar.gz

---
+++ packages/Hakware-py/Hakware-py-0.5.0.tar.gz/Hakware-py-0.5.0/Hakware-py/__init__.py
@@ -0,0 +1,8 @@
+# Import key functions and classes
+from HakwareObserverpy import InitiateCollecection
+
+# Optional: Define a version number for the package
+__version__ = "0.5.0"
+
+# Optional: Define the package's public interface
+__all__ = ["InitiateCollecection"]
