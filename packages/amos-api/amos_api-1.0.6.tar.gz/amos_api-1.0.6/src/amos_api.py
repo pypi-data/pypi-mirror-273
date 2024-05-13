6e20825bcd44db59396b21e36e2a188cce3e83a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 15:03:10 +0000

    Add package amos-api/1.0.6/amos_api-1.0.6.tar.gz

---
+++ packages/amos-api/amos_api-1.0.6.tar.gz/amos_api-1.0.6/src/amos_api.py
@@ -0,0 +1,21 @@
+from utils import (
+    HttpCode,
+    CheckAssertionError,
+    CheckValueError,
+    CheckTypeError,
+    UnknownError,
+    ResponseOK,
+    ResponseValidationError,
+    ResponseAlgorithmError
+)
+
+__all__ = [
+    "HttpCode",
+    "CheckAssertionError",
+    "CheckValueError",
+    "CheckTypeError",
+    "UnknownError",
+    "ResponseOK",
+    "ResponseValidationError",
+    "ResponseAlgorithmError"
+]
