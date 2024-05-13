03b7d35fa37d7f09c4bab19c7fabbd6d204a41eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 14:02:42 +0000

    Add package botrun-gemini/4.5.132/botrun_gemini-4.5.132.tar.gz

---
+++ packages/botrun-gemini/botrun_gemini-4.5.132.tar.gz/botrun_gemini-4.5.132/sh/go.sh
@@ -0,0 +1,9 @@
+source venv/bin/activate
+
+#python botrun_gemini.py --project_id scoop-386004 \
+#--location us-central1 \
+#--model_name gemini-1.5-pro-preview-0409 \
+#--text_prompt "請跟我講一個簡短的故事。" \
+#--system_prompt "妳是講故事高手"
+
+python botrun_gemini.py
