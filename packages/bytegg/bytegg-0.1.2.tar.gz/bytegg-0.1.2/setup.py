661db51ed16ff83f421db5fa642e70eb3616024f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 14:03:15 +0000

    Add package bytegg/0.1.2/bytegg-0.1.2.tar.gz

---
+++ packages/bytegg/bytegg-0.1.2.tar.gz/bytegg-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='bytegg',
+    version='0.1.2',    
+    description='A Python package that bypasses advertisements to streamline user experiences with content.',
+    url='https://github.com/KirbyHacks/ByteGG',
+    author='! rL⌀w',
+    author_email='bytebvrd@gmail.com',
+    license='MIT',
+    packages=['bytegg'],
+    install_requires=['urllib3',
+                      'aiohttp',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
