d332392b1030ab87663448c72a4fdfa4f5d816de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 16:03:21 +0000

    Add package llm-jupyter/0.1.0/llm_jupyter-0.1.0-py3-none-any.whl

---
+++ packages/llm-jupyter/llm_jupyter-0.1.0-py3-none-any.whl/llm_jupyter.py
@@ -0,0 +1,22 @@
+import click
+import llm
+import subprocess
+
+
+@llm.hookimpl
+def register_commands(cli):
+    @cli.command(context_settings=dict(ignore_unknown_options=True))
+    @click.argument("args", nargs=-1, type=click.UNPROCESSED)
+    def ipython(args):
+        """
+        Run IPython interpreter, passing through any arguments
+        """
+        subprocess.run(["ipython", *args])
+
+    @cli.command(context_settings=dict(ignore_unknown_options=True))
+    @click.argument("args", nargs=-1, type=click.UNPROCESSED)
+    def notebook(args):
+        """
+        Run Jupyter Notebook, passing through any arguments
+        """
+        subprocess.run(["jupyter", "notebook", *args])
