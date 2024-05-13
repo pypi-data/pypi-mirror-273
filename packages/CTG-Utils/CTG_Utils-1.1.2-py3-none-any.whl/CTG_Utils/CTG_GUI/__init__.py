ad74c3f0f17fc98c1f3832c057ac961722e5f102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 15:03:46 +0000

    Add package CTG-Utils/1.1.2/CTG_Utils-1.1.2-py3-none-any.whl

---
+++ packages/CTG-Utils/CTG_Utils-1.1.2-py3-none-any.whl/CTG_Utils/CTG_GUI/__init__.py
@@ -0,0 +1,6 @@
+__author__  = 'BiblioMeter team'
+__license__ = 'MIT'
+
+from CTG_Utils.CTG_GUI.GUI_Globals import *
+from CTG_Utils.CTG_GUI.Useful_Functions import *
+from CTG_Utils.CTG_GUI.Page_Classes import *
