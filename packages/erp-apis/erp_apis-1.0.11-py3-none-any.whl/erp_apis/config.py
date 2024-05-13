176695588e91a04690c8c40fde1add1c4a917672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 14:03:54 +0000

    Add package erp-apis/1.0.11/erp_apis-1.0.11-py3-none-any.whl

---
+++ packages/erp-apis/erp_apis-1.0.11-py3-none-any.whl/erp_apis/config.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# Project：erp_out_of_stock
+# File：config.py
+# Author：李福成
+# Date ：2024-04-29 10:04
+# IDE：PyCharm
+
+Erp321BaseUrl = 'https://www.erp321.com'
+ErpApiBaseUrl = 'https://api.erp321.com'
+
+
+
+DEFAULT_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/237.84.2.178 Safari/537.36 Edg/109.0.1518.78',
+}
