f91d46910fd63e7bdaf48bbac0a0f9c63abf3cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 14:02:42 +0000

    Add package adbmanager/0.0.2/adbmanager-0.0.2.tar.gz

---
+++ packages/adbmanager/adbmanager-0.0.2.tar.gz/adbmanager-0.0.2/adbmanager/setup.py
@@ -0,0 +1,12 @@
+# from setuptools import setup, find_packages
+
+# setup(
+#     name='adbmanager',
+#     version='0.0.1',
+#     packages=find_packages(),
+#     package_data={'keyboardsEvents': ['keyboard_events.json']},
+#     author='Frankelly Franco',
+#     author_email='francarium@gmail.com',
+#     description='Un paquete rapido para la gestion abd mediante python.',
+#     url='https://francarium.com',
+# )
