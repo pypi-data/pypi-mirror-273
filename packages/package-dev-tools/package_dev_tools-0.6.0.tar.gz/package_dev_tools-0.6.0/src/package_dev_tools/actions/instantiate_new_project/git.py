099c5cf0c3c72e84fc0da586dc61e9a9531bf402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 15:02:49 +0000

    Add package package-dev-tools/0.6.0/package_dev_tools-0.6.0.tar.gz

---
+++ packages/package-dev-tools/package_dev_tools-0.6.0.tar.gz/package_dev_tools-0.6.0/src/package_dev_tools/actions/instantiate_new_project/git.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass, field
+
+import cli
+
+from ...models import Path
+
+
+@dataclass
+class GitInterface:
+    path: Path = field(default_factory=Path.cwd)
+    git_name: str = "Quinten"
+    git_email: str = "quinten.roets@gmail.com"
+    commit_message: str = "Instantiate new project"
+
+    def commit(self) -> None:
+        self.get("add -A")
+        self.configure()
+        self.get("commit --no-verify -m", self.commit_message)
+
+    def get(self, *args: str | Path | int) -> str:
+        git_arg = f"git {args[0]}"
+        return cli.capture_output(git_arg, *args[1:], cwd=self.path)
+
+    def configure(self) -> None:
+        git_configuration = {"name": self.git_name, "email": self.git_email}
+        for attribute, value in git_configuration.items():
+            self.get(f"config user.{attribute} {value}")
