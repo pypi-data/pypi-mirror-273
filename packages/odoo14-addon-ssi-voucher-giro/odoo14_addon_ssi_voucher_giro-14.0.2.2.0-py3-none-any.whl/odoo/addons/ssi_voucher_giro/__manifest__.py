c5c691680d3c56409c4e5887e858442d7f80c36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 16:03:19 +0000

    Add package odoo14-addon-ssi-voucher-giro/14.0.2.2.0/odoo14_addon_ssi_voucher_giro-14.0.2.2.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-voucher-giro/odoo14_addon_ssi_voucher_giro-14.0.2.2.0-py3-none-any.whl/odoo/addons/ssi_voucher_giro/__manifest__.py
@@ -0,0 +1,37 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Giro Accounting Voucher",
+    "version": "14.0.2.2.0",
+    "category": "Accounting",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_voucher_bank_cash",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir_rule_data.xml",
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "data/account_voucher_type_data.xml",
+        "data/ir_windows_action_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_giro_payment_data.xml",
+        "data/sequence_template_giro_receipt_data.xml",
+        "data/approval_template_giro_payment_data.xml",
+        "data/approval_template_giro_receipt_data.xml",
+        "data/policy_template_giro_payment_data.xml",
+        "data/policy_template_giro_receipt_data.xml",
+        "views/account_giro_voucher_views.xml",
+        "views/account_giro_receipt_views.xml",
+        "views/account_giro_payment_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
