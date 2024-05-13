129232c7b7fbc38e8f66237994bb51df8862110a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 15:03:33 +0000

    Add package binpolar/0.3/binpolar-0.3.tar.gz

---
+++ packages/binpolar/binpolar-0.3.tar.gz/binpolar-0.3/binpolar/polarity_detector.py
@@ -0,0 +1,41 @@
+import re
+import tensorflow as tf
+from transformers import DistilBertTokenizer
+
+# Define global variables for the model and tokenizer
+_model = None
+_tokenizer = None
+
+# Lazily loads and returns the model and tokenizer
+def get_model():
+    global _model
+    if _model is None:
+        _model = tf.keras.models.load_model('distilbert_ocean')
+    return _model
+
+def get_tokenizer():
+    global _tokenizer
+    if _tokenizer is None:
+        _tokenizer = DistilBertTokenizer.from_pretrained('distilbert_ocean/tokenizer')
+    return _tokenizer
+
+def preprocess_text(text):
+    text = re.sub(r'[^\w\s]', '', text).lower()
+    return text
+
+def detect_polarity(input):
+    # 1 is positive 0 is negative
+    model = get_model()
+    tokenizer = get_tokenizer()
+
+    text = preprocess_text(input)
+    sentiment_en = tokenizer(text, truncation=True, padding=True, return_tensors='tf')
+    s_input_ids = sentiment_en['input_ids']
+    s_attention_mask = sentiment_en['attention_mask']
+    s_output = model.predict({'input_ids': s_input_ids, 'attention_mask': s_attention_mask})
+
+    prob = s_output[0]
+    print("pos_probabilities:", prob)
+    isPos = (prob > 0.5).astype(int)
+
+    return isPos
---
+++ packages/binpolar/binpolar-0.3.tar.gz/binpolar-0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.3'
+DESCRIPTION = 'Binary polarity detection'
+LONG_DESCRIPTION = 'A fine-tuned DistilBERT model for binary polarity detection in text.'
+
+# Setting up
+setup(
+    name="binpolar",
+    version=VERSION,
+    author="Joyce Lee",
+    author_email="<leejoy1610@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['transformers'],
+    keywords=['python', 'text', 'polarity'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
