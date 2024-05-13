2a9b7f838c8eb9ce11b01cec57cbeafa3951e368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 14:03:15 +0000

    Add package bytegg/0.1.3/bytegg-0.1.3.tar.gz

---
+++ packages/bytegg/bytegg-0.1.3.tar.gz/bytegg-0.1.3/bytegg/__init__.py
@@ -0,0 +1,11 @@
+"""
+ByteGG: A Python library for bypassing ad links and Roblox services.
+
+This library provides functions for bypassing ad links and Roblox services,
+making it easier to access content without the hassle of dealing with
+annoying ads and restrictions.
+"""
+
+from .core import bypass
+
+__version__ = "0.1.3"
