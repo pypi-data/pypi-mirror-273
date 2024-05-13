d04ab97ed80821d20d10067ff60840921a0421d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 16:03:20 +0000

    Add package antakia/0.4.5/antakia-0.4.5.tar.gz

---
+++ packages/antakia/antakia-0.4.5.tar.gz/antakia-0.4.5/src/antakia/__init__.py
@@ -0,0 +1,9 @@
+"""
+This is antakia package documentation.
+"""
+
+import importlib.metadata
+from antakia.antakia import AntakIA
+
+__version__ = importlib.metadata.version("antakia")
+__author__ = "AI-vidence"
