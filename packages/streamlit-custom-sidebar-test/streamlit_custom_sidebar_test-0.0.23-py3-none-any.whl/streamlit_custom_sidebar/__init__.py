db037841e1f527e459db6a305d386770d15eb529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 16:03:26 +0000

    Add package streamlit-custom-sidebar-test/0.0.23/streamlit_custom_sidebar_test-0.0.23-py3-none-any.whl

---
+++ packages/streamlit-custom-sidebar-test/streamlit_custom_sidebar_test-0.0.23-py3-none-any.whl/streamlit_custom_sidebar/__init__.py
@@ -0,0 +1,7 @@
+IS_RELEASE = True 
+
+from .Streamlit_template import CustomSidebarDefault
+from .profile_sidebar import CustomSidebarProfile
+from .slim_expand_sidebar import HoverExpandSidebarTemplate
+
+
