dd859458f07674c265e7fa645e57900929a55751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 16:03:20 +0000

    Add package cyclic-classes/0.0.2/cyclic_classes-0.0.2.tar.gz

---
+++ packages/cyclic-classes/cyclic_classes-0.0.2.tar.gz/cyclic_classes-0.0.2/cyclic_classes/decorators.py
@@ -0,0 +1,28 @@
+"""
+Cyclic Classes - Decorators
+"""
+
+from __future__ import annotations
+
+from .classes import get_registered_class
+
+
+def register(cls):
+    """Register a class with the cyclic_classes space"""
+    # Get registered class which we'll register under
+    registered_name = cls.__qualname__
+    name = cls.__module__ + "." + registered_name
+    registered = get_registered_class(name=name, qualname=registered_name)
+
+    # Create a new class that will now be registered under the registered_class and return that instead
+    new_cls = type(
+        cls.__qualname__,
+        (
+            cls,
+            registered,
+        ),
+        dict(cls.__dict__),
+        class_name=registered_name,
+    )
+    new_cls.__registered__ = cls
+    return new_cls
