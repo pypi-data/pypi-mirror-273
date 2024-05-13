2ca03dcad2c54a87bf58ad0ce3c5f9d85f004513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 14:04:03 +0000

    Add package PypiInstruments/1.0.10/PypiInstruments-1.0.10-py3-none-any.whl

---
+++ packages/PypiInstruments/PypiInstruments-1.0.10-py3-none-any.whl/instruments/SMU_Examples.py
@@ -0,0 +1,39 @@
+from smu_keithley_2450 import *
+import hid
+from instruments.instruments_name import *
+
+"""使用SMU测试的例子"""
+from instruments.instruments_name import *
+
+smuid = smu_2460_id
+
+smu = SmuKeithley(pyvisa.ResourceManager(), smuid)
+
+# smu.smu2450_reset()
+
+# smu.sum2450_force_volt_sens_cur_init(3.3,1,5,1,0.01)
+
+# smu.smu2450_set_volt_output(5)
+
+# smu.smu2450_read_sense_curr(1)
+
+# print(smu.smu_2450_meas_v())
+
+# print(smu.smu_meas_v())
+
+"""以下是测试用例"""
+"""强制电流测电压初始化并且改变提供电流并且测量电压"""
+smu.sum2450_force_cur_sens_volt_init(-0.1, 5,2, 5, 0.1) # 初始化
+#
+# smu.smu_sour_ii(-0.6,1) # 提供电流 1是能提供的最大电流
+# print(smu.smu_2450_meas_v(1)) # 测量电压 四线
+
+
+
+"""只测量电压"""
+""" 两线 且应该先将SMU设置为电压表模式 """
+# print(smu.smu_2450_meas_v(0))
+
+"""测量电流"""
+""" 两线 且应该先将SMU设置为电压表模式 """
+# print(smu.smu_2450_meas_i(0))
---
+++ packages/PypiInstruments/PypiInstruments-1.0.10-py3-none-any.whl/instruments/device_scan.py
@@ -0,0 +1,14 @@
+import pyvisa
+from instruments.instruments_name import *
+# 创建VISA资源管理器
+
+def device_scan():
+    rm = pyvisa.ResourceManager()
+
+    # 获取所有连接的设备列表
+    instruments = rm.list_resources()
+
+    # 打印出所有设备
+    print("Connected VISA devices:")
+    for instrument in instruments:
+        print(instrument)
