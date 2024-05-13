3aeaa7e11c8a3e0bd2c77fbe4987aa2d2da17c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 14:03:06 +0000

    Add package anabih-python-test/19.0.0/anabih_python_test-19.0.0-py3-none-any.whl

---
+++ packages/anabih-python-test/anabih_python_test-19.0.0-py3-none-any.whl/my_package/greetings.py
@@ -0,0 +1,15 @@
+
+class Greetings:
+    def say_hi(self):
+        junit_xml = """<?xml version="1.0" encoding="utf-8"?>
+            <testsuites time="5.548">
+               <testsuite errors="0" failures="0" name="my-test-suite" skipped="0" tests="1" time="0.342">
+                  <testcase classname="my classname" name="my name" time="0.143"/>
+               </testsuite>
+            </testsuites>
+        """
+        print(junit_xml)
+
+        f = open("junit-output.xml", "w")
+        f.write(junit_xml)
+        f.close()
