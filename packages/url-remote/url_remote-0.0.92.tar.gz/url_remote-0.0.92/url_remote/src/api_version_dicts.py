cc5f8d1937e857885c02e6b1713aedacbaeec3ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 14:02:46 +0000

    Add package url-remote/0.0.92/url_remote-0.0.92.tar.gz

---
+++ packages/url-remote/url_remote-0.0.92.tar.gz/url_remote-0.0.92/url_remote/src/api_version_dicts.py
@@ -0,0 +1,15 @@
+AUTHENTICATION_API_VERSION_DICT = {
+    "play1": 1,
+    "dvlp1": 2
+}
+
+EVENT_API_VERSION_DICT = {
+    "play1": 1,
+    "dvlp1": 1
+}
+
+# TODO Please also add SMARTLINK_API_VERSION_DICT
+SMARTLINK_API_VERSION_DICT = {
+    "play1": 1,
+    "dvlp1": 1
+}
