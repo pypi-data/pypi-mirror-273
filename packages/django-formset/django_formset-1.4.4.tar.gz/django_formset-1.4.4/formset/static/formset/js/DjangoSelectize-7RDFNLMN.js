3ffe7ab278b86a54f1a0c09e25715fbe38029019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 16:03:16 +0000

    Add package django-formset/1.4.4/django_formset-1.4.4.tar.gz

---
+++ packages/django-formset/django_formset-1.4.4.tar.gz/django_formset-1.4.4/formset/static/formset/js/DjangoSelectize-7RDFNLMN.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-IUS6Z5AU.js";import"./chunk-SERXULES.js";import"./chunk-W5RPWA2M.js";import"./chunk-FDUUONAQ.js";import"./chunk-65OJRBX6.js";import"./chunk-P2VM2T3G.js";export{a as DjangoSelectize,b as DjangoSelectizeElement};
---
+++ packages/django-formset/django_formset-1.4.4.tar.gz/django_formset-1.4.4/formset/static/formset/js/SortableSelect-UDJCCTN4.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-3FM5H3DO.js";import"./chunk-NOGHTXP6.js";import"./chunk-65OJRBX6.js";import"./chunk-P2VM2T3G.js";export{b as SortableOptGroupElement,a as SortableSelectElement};
