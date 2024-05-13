10a6b6f11b012afbaf227808cac702c638421d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 16:02:57 +0000

    Add package zhlyr/3.8/zhlyr-3.8.tar.gz

---
+++ packages/zhlyr/zhlyr-3.8.tar.gz/zhlyr-3.8/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup , find_packages
+with open('README.md','r') as file:
+    setup(
+        name = 'zhlyr',
+        version = '3.8',
+        packages = find_packages(),
+        requires = ['requests','shazamio','shazam'],
+        long_description =file.read(),
+        long_description_content_type='text/markdown',
+        description = 'Python library for music handling',
+        author = 'Mtsky',
+        author_email = 'secon2636@gmail.com',
+        url = 'https://gaoc3.github.io/zhlyr/',
+        license = 'MIT',
+        classifiers=[
+            "Development Status :: 1 - Planning",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Unix",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ],
+        keywords= ['lyrics','music','shazam','serialize','serializer','recognize'],
+        python_requires=">=3.9"
+    )
