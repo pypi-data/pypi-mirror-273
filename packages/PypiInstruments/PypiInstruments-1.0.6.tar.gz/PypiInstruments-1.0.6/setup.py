0a022d9ca7e0e7e72d596829e39b9f587612a01d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 14:03:59 +0000

    Add package PypiInstruments/1.0.6/PypiInstruments-1.0.6.tar.gz

---
+++ packages/PypiInstruments/PypiInstruments-1.0.6.tar.gz/PypiInstruments-1.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name="PypiInstruments",  # package name
+    version="1.0.6",  # package version
+    description='my package',  # package description
+    packages=find_packages(),
+    zip_safe=False,
+)
