843ab2a74aef14d2ae50122d78d22b7c666a7048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 14:02:55 +0000

    Add package adolphz/1.1/adolphz-1.1.tar.gz

---
+++ packages/adolphz/adolphz-1.1.tar.gz/adolphz-1.1/setup.py
@@ -0,0 +1,46 @@
+from distutils.core import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+    name = 'adolphz',
+    packages = ['adolphz'],
+    version = '1.1',
+    license='MIT',
+    description = 'adolphz',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author = 'Mirukutea',
+    author_email = 'tea@masrizky.com',
+    url = 'https://github.com/mirukutea/adolphz',
+    download_url = 'https://github.com/mirukutea/adolphz/archive/v_01.tar.gz',
+    project_urls={
+        'Documentation': 'https://github.com/mirukutea/adolphz',
+        'Funding': 'https://donate.pypi.org',
+        'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/mirukutea/adolphz/',
+        'Tracker': 'https://github.com/mirukutea/adolphz/issues',
+    },
+    keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],
+    install_requires=[
+            'validators',
+            'beautifulsoup4',
+            'discordautochat',
+            'discorudo',
+        ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+     dependencies= [
+        'discordautochat',
+        'discorudo'
+    ]
+)
