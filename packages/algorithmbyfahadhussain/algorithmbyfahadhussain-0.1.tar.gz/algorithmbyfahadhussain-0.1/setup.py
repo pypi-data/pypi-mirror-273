eaac643fd7918a04517977145bdd1cd0173083f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 16:03:21 +0000

    Add package algorithmbyfahadhussain/0.1/algorithmbyfahadhussain-0.1.tar.gz

---
+++ packages/algorithmbyfahadhussain/algorithmbyfahadhussain-0.1.tar.gz/algorithmbyfahadhussain-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='algorithmbyfahadhussain',
+    version='0.1',
+    packages=find_packages(),
+    author='fahadhussain',
+    author_email='bsdsf21m014@pucit.edu.pk',
+    description='Description of your package',
+    long_description='Searching & Sorting algorithms functions',
+    url='https://github.com/fahadcr14/algorithms',
+    install_requires=[
+        # List of your package dependencies
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        # Add more classifiers as needed
+    ],
+)
