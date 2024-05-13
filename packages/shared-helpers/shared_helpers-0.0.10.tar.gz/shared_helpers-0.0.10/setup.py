df5e2773b7b887a05c512182b416fe144f1667fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 16:03:22 +0000

    Add package shared-helpers/0.0.10/shared_helpers-0.0.10.tar.gz

---
+++ packages/shared-helpers/shared_helpers-0.0.10.tar.gz/shared_helpers-0.0.10/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='shared_helpers',
+    packages=find_packages(),
+    version='0.0.10',
+    description='User workplace shared library',
+    author='vladimir.dolzhikov1@leroymerlin.ru',
+)
