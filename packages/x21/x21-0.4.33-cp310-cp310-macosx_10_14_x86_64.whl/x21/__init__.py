e513f647f1ea461ecccd5bc9e2c3613bf350aec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 16:03:07 +0000

    Add package x21/0.4.33/x21-0.4.33-cp310-cp310-macosx_10_14_x86_64.whl

---
+++ packages/x21/x21-0.4.33-cp310-cp310-macosx_10_14_x86_64.whl/x21/__init__.py
@@ -0,0 +1,17 @@
+# backwards compat:
+from ._main import (
+    __dex_22b__,
+    __dex_22c__,
+    __dex_23a__,
+    __dex_23b__,
+)
+from ._x21 import dex_23c, dex_24a
+
+__all__ = [
+    "__dex_22b__",
+    "__dex_22c__",
+    "__dex_23a__",
+    "__dex_23b__",
+    "dex_23c",
+    "dex_24a",
+]
