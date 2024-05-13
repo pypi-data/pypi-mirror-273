b68062f27114a8b23382f74be3858413c7c0c429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-13 16:03:12 +0000

    Add package flake8-async/24.5.1/flake8_async-24.5.1.tar.gz

---
+++ packages/flake8-async/flake8_async-24.5.1.tar.gz/flake8_async-24.5.1/tests/eval_files/async101.py
@@ -0,0 +1,74 @@
+# ASYNCIO_NO_ERROR - nursery/cancelscope in asyncio is called taskgroup/timeout
+# Note: this one *shouldn't* be giving the same errors for anyio (but it currently does)
+# since it has TaskGroups instead of nurseries. #TODO #215
+# type: ignore
+import contextlib
+import contextlib as bla
+from contextlib import asynccontextmanager, contextmanager, contextmanager as blahabla
+
+import trio
+
+
+def foo0():
+    with trio.open_nursery() as _:
+        yield 1  # error: 8
+
+
+async def foo1():
+    async with trio.open_nursery() as _:
+        yield 1  # error: 8
+
+
+@contextmanager
+def foo2():
+    with trio.open_nursery() as _:
+        yield 1  # safe
+
+
+async def foo3():
+    async with trio.CancelScope() as _:
+        yield 1  # error: 8
+
+
+@asynccontextmanager
+async def foo4():
+    async with trio.open_nursery() as _:
+        yield 1  # safe
+
+
+async def foo5():
+    async with trio.open_nursery():
+        yield 1  # error: 8
+
+        def foo6():
+            yield 1  # safe
+
+
+@contextlib.asynccontextmanager
+async def foo7():
+    async with trio.open_nursery() as _:
+        yield 1  # safe
+
+
+@bla.contextmanager
+def foo8():
+    with trio.open_nursery() as _:
+        yield 1  # safe
+
+
+@blahabla
+def foo9():
+    with trio.open_nursery() as _:
+        yield 1  # error: 8
+
+
+@pytest.fixture()
+def foo_false_alarm():
+    with trio.open_nursery() as _:
+        yield 1
+
+
+@pytest.fixture
+def foo_false_alarm_2():
+    with trio.open_nursery() as _:
+        yield 1
