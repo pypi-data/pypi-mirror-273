764ee938e0d94bb002edc741896b713c52af58c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 15:03:27 +0000

    Add package RabbitSpider/1.6/rabbitspider-1.6.tar.gz

---
+++ packages/RabbitSpider/rabbitspider-1.6.tar.gz/rabbitspider-1.6/README.md
@@ -0,0 +1,48 @@
+基于rabbitmq 做消息队列开发的分布式异步爬虫框架，支持web监控，定时任务，运行模式 auto 先生产后消费，m 只生产，w 只消费
+
+安装pip install RabbitSpider==1.5
+
+    from jsonpath import jsonpath
+    from RabbitSpider.core.engine import Engine
+    from RabbitSpider.http.request import Request
+    from RabbitSpider.http.response import Response
+    from RabbitSpider.utils.rabbit_go import go
+    from RabbitSpider.core.download import AiohttpDownload, CurlDownload
+
+
+    class Test(Engine):
+
+        def __init__(self, sync):
+            super().__init__(sync)
+            # pip 安装需要添加rabbitmq redis 配置
+            self.settings.set('RABBIT_HOST', '121.36.225.245')
+            self.settings.set('RABBIT_PORT', 5672)
+            self.settings.set('RABBIT_USERNAME', 'yuntom')
+            self.settings.set('RABBIT_PASSWORD', '123456')
+            self.settings.set('RABBIT_VIRTUAL_HOST', '/')
+            self.settings.set('REDIS_QUEUE_HOST', '127.0.0.1')
+            self.settings.set('REDIS_QUEUE_PORT', 6379)
+            self.settings.set('REDIS_QUEUE_DB', 1)
+            self.settings.set('LOG_LEVEL', 'ERROR')
+    
+        async def start_requests(self):
+            for i in range(50):
+                data = '{"token":"","pn":0,"rn":10,"sdt":"","edt":"","wd":"","inc_wd":"","exc_wd":"","fields":"","cnum":"","sort":"{\\"webdate\\":\\"0\\",\\"id\\":\\"0\\"}","ssort":"","cl":10000,"terminal":"","condition":[{"fieldName":"categorynum","equal":"002006","notEqual":null,"equalList":null,"notEqualList":null,"isLike":true,"likeType":2}],"time":[{"fieldName":"webdate","startTime":"2024-01-25 00:00:00","endTime":"2024-02-24 23:59:59"}],"highlights":"","statistics":null,"unionCondition":[],"accuracy":"","noParticiple":"1","searchRange":null,"noWd":true}'
+                yield Request(url='https://www.jxsggzy.cn/XZinterface/rest/esinteligentsearch/getFullTextDataNew',
+                              data=data, method='post', dupe_filter=False)
+    
+        async def parse(self, request: Request, response: Response):
+            url = 'https://www.jxsggzy.cn/' + jsonpath(response.json(), expr='$..linkurl')[0]
+            yield Request(url=url, dupe_filter=False, callback=self.parse_item)
+    
+        async def parse_item(self, request: Request, response: Response):
+            item = {'title': response.xpath('//p[@class="title"]/text()').get()}
+            yield item
+    
+        async def process_item(self, item: dict):
+            """入库逻辑"""
+            print(item)
+
+    if __name__ == '__main__':
+        go(Test, 'auto', 10)
+
---
+++ packages/RabbitSpider/rabbitspider-1.6.tar.gz/rabbitspider-1.6/RabbitSpider/core/engine.py
@@ -0,0 +1,185 @@
+import asyncio
+import os
+import pickle
+import sys
+from collections.abc import AsyncGenerator, Coroutine
+from traceback import print_exc
+from typing import Optional
+from aio_pika.abc import AbstractIncomingMessage
+from loguru import logger
+from RabbitSpider.http.request import Request
+from RabbitSpider.http.response import Response
+from RabbitSpider.utils.control import TaskManager
+from RabbitSpider.utils.control import SettingManager
+from RabbitSpider.utils.dupefilter import RFPDupeFilter
+from RabbitSpider.utils.expections import RabbitExpect
+from RabbitSpider.core.scheduler import Scheduler
+from RabbitSpider.core.download import CurlDownload
+from aio_pika.exceptions import QueueEmpty, ChannelClosed
+
+
+class Engine(object):
+    name = os.path.basename(sys.argv[0])
+    allow_status_code: list = [200]
+    max_retry: int = 5
+    http_version = 1
+    tls = "chrome120"
+
+    def __init__(self, sync):
+        self._filter = None
+        self._scheduler = None
+        self._connection = None
+        self._channel = None
+        self._download = None
+        self._task_manager = None
+        self._sync = sync
+        self.logger = None
+        self.session = None
+        self._future = None
+        self.settings = SettingManager()
+        self._task_manager = TaskManager(self._sync)
+        self._download = CurlDownload(http_version=self.http_version, impersonate=self.tls)
+
+    async def start_spider(self):
+        await self._scheduler.create_queue(self._channel, self.name)
+        start_request = self.start_requests()
+        await self.routing(start_request)
+
+    async def open_spider(self):
+        """初始化数据库"""
+        self.logger.info(f'{self.name}任务开始！')
+
+    async def start_requests(self):
+        """初始请求"""
+        pass
+
+    async def parse(self, request: Request, response: Response):
+        """默认回调"""
+        pass
+
+    async def process_item(self, item: dict):
+        """入库逻辑"""
+        pass
+
+    async def close_spider(self):
+        self.logger.info(f'{self.name}任务结束！')
+
+    def before_request(self, ret):
+        """请求前"""
+        return ret
+
+    async def routing(self, result):
+        if isinstance(result, AsyncGenerator):
+            async for req in result:
+                if isinstance(req, Request):
+                    ret = pickle.dumps(req.model_dump())
+                    if req.dupe_filter:
+                        if self._filter.request_seen(ret):
+                            self.logger.info(f'生产数据：{req.model_dump()}')
+                            await self._scheduler.producer(self._channel, queue=self.name, body=ret)
+                    else:
+                        self.logger.info(f'生产数据：{req.model_dump()}')
+                        await self._scheduler.producer(self._channel, queue=self.name, body=ret)
+
+                elif isinstance(req, dict):
+                    await self.process_item(req)
+        elif isinstance(result, Coroutine):
+            await result
+        else:
+            raise RabbitExpect('回调函数返回类型错误！')
+
+    async def crawl(self):
+        self.session = await self._download.new_session()
+        while True:
+            try:
+                incoming_message: Optional[AbstractIncomingMessage] = await self._scheduler.consumer(self._channel,
+                                                                                                     queue=self.name)
+            except QueueEmpty:
+                if self._task_manager.all_done():
+                    await self._download.exit(self.session)
+                    await self._scheduler.delete_queue(self._channel, self.name)
+                    await self._channel.close()
+                    await self._connection.close()
+                    break
+                else:
+                    continue
+            except Exception:
+                break
+            if incoming_message:
+                await self._task_manager.semaphore.acquire()
+                self._task_manager.create_task(self.deal_resp(incoming_message))
+            else:
+                print(incoming_message)
+
+    async def consume(self):
+        self.session = await self._download.new_session()
+        await self._scheduler.consumer(self._channel, queue=self.name, callback=self.deal_resp,
+                                       prefetch=self._sync)
+        self._future = asyncio.Future()
+        await self._future
+
+    async def deal_resp(self, incoming_message):
+        ret = self.before_request(pickle.loads(incoming_message.body))
+        self.logger.info(f'消费数据：{ret}')
+        try:
+            response = await self._download.fetch(self.session, ret)
+        except Exception as e:
+            self.logger.error(f'请求失败：{e}')
+            print_exc()
+            response = None
+        if response and response.status in self.allow_status_code:
+            try:
+                callback = getattr(self, ret['callback'])
+                result = callback(Request(**ret), response)
+                if result:
+                    await self.routing(result)
+            except Exception as e:
+                self.logger.error(f'请求失败：{e}')
+                print_exc()
+                for task in asyncio.all_tasks():
+                    task.cancel()
+        else:
+            if ret['retry'] < self.max_retry:
+                ret['retry'] += 1
+                await self._scheduler.producer(self._channel, queue=self.name, body=pickle.dumps(ret),
+                                               priority=ret['retry'])
+            else:
+                self.logger.error(f'请求失败：{ret}')
+        try:
+            await self._channel.declare_queue(name=self.name, passive=True)
+            await incoming_message.ack()
+        except Exception:
+            self._future.set_result('done') if self._future else None
+            self.logger.info(f'队列{self.name}已删除！')
+
+    async def load_setting(self):
+        logger.add("../rabbit_log/rabbit_{time:YYYY-MM-DD}.log", level=self.settings.get('LOG_LEVEL'), rotation="1 day",
+                   retention="1 week",
+                   format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {extra[scope]} | {name}:{line} - {message}")
+        self.logger = logger.bind(scope=self.name)
+
+        self._filter = RFPDupeFilter(self.name,
+                                     self.settings.get('REDIS_QUEUE_HOST'),
+                                     self.settings.get('REDIS_QUEUE_PORT'),
+                                     self.settings.get('REDIS_QUEUE_DB'))
+
+        self._scheduler = Scheduler(self.settings.get('RABBIT_USERNAME'),
+                                    self.settings.get('RABBIT_PASSWORD'),
+                                    self.settings.get('RABBIT_HOST'),
+                                    self.settings.get('RABBIT_PORT'),
+                                    self.settings.get('RABBIT_VIRTUAL_HOST'))
+        self._connection, self._channel = await self._scheduler.connect()
+
+    async def run(self, mode):
+        await self.load_setting()
+        await self.open_spider()
+        if mode == 'auto':
+            await self.start_spider()
+            await self.crawl()
+        elif mode == 'm':
+            await self.start_spider()
+        elif mode == 'w':
+            await self.consume()
+        else:
+            raise RabbitExpect('执行模式错误！')
+        await self.close_spider()
