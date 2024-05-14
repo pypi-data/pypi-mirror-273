b1618fdf57c7e248809dad18785274984b4a5a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 16:05:29 +0000

    Add package popoll-backend/0.0.51/popoll_backend-0.0.51-py3-none-any.whl

---
+++ packages/popoll-backend/popoll_backend-0.0.51-py3-none-any.whl/popoll_backend/query/update_date.py
@@ -0,0 +1,28 @@
+import datetime
+import sqlite3
+from typing import Optional
+
+from popoll_backend.model import Payload
+from popoll_backend.query import Query
+from popoll_backend.query.get_date import GetDate
+
+
+class UpdateDate(Query):
+    
+    id: int
+    title: str
+    date: datetime.date
+    time: Optional[datetime.time]
+    
+    def __init__(self, poll: str, id: int, title: str, date: datetime.date, time: Optional[datetime.time]):
+        super().__init__(poll)
+        self.id = id
+        self.title = title
+        self.date = date
+        self.time = time
+        
+    def process(self, cursor: sqlite3.Cursor):
+        cursor.execute('UPDATE dates SET title=?, date=?, time=? WHERE id=?', (self.title, self.date, self.time, self.id))
+    
+    def buildResponse(self, cursor: sqlite3.Cursor) -> Payload:
+        return GetDate(self.poll, self.id, False).run(cursor)
