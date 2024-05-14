18f6865aafd39afc4d24bbd65c86729408075031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 06:03:19 +0000

    Add package ItsMagic/0.3/itsmagic-0.3.tar.gz

---
+++ packages/ItsMagic/itsmagic-0.3.tar.gz/itsmagic-0.3/setup.py
@@ -0,0 +1,15 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='ItsMagic',
+    version='0.3',
+    description='Uma biblioteca simples para facilitar criar bots discord',
+    py_modules=['itsmagic'],
+    author='OSkyDev',
+    author_email='skydagringa1@gmail.com',
+    license='MIT',
+    install_requires=[
+        'discord.py'
+    ],
+)
