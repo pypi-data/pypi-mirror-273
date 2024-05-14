c23c71b5b0020d8fc841e504642ee50033e1075f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 06:03:28 +0000

    Add package mcbootflash/9.0.0/mcbootflash-9.0.0-py3-none-any.whl

---
+++ packages/mcbootflash/mcbootflash-9.0.0-py3-none-any.whl/mcbootflash/__init__.py
@@ -0,0 +1,39 @@
+"""Flash firmware to devices running Microchip's 16-bit bootloader."""
+
+from .error import (
+    BadAddress,
+    BadLength,
+    BootloaderError,
+    UnsupportedCommand,
+    VerifyFail,
+)
+from .flash import (
+    checksum,
+    erase_flash,
+    get_boot_attrs,
+    reset,
+    self_verify,
+    write_flash,
+)
+from .types import BootAttrs, Chunk, Command
+from .util import chunked
+
+__all__ = [
+    "BadAddress",
+    "BadLength",
+    "BootloaderError",
+    "Chunk",
+    "Command",
+    "UnsupportedCommand",
+    "VerifyFail",
+    "BootAttrs",
+    "checksum",
+    "chunked",
+    "erase_flash",
+    "get_boot_attrs",
+    "reset",
+    "self_verify",
+    "write_flash",
+]
+
+__version__ = "9.0.0"
