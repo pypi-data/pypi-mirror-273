fac1a5591a4b2a9e48c69f3b6ba599fa852061fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 05:04:16 +0000

    Add package pyfuckfurry/1.0/pyfuckfurry-1.0.tar.gz

---
+++ packages/pyfuckfurry/pyfuckfurry-1.0.tar.gz/pyfuckfurry-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="pyfuckfurry",
+    version="1.0",
+    description="libraly blyat.",
+    author="ANUSIXA66383",
+    author_email="whonix777890@proton.me",
+    packages=find_packages(),
+)
