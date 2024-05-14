f63f0f33e45121de3358889106043d38aa598bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 05:05:30 +0000

    Add package ob-metaflow-stubs/3.3/ob_metaflow_stubs-3.3-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-3.3-py2.py3-none-any.whl/metaflow-stubs/mflog/mflog.pyi
@@ -0,0 +1,22 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.11.14.1+ob(v1)                                                   #
+# Generated on 2024-05-13T17:42:14.269524                                        #
+##################################################################################
+
+from __future__ import annotations
+
+import typing
+if typing.TYPE_CHECKING:
+    import datetime
+
+VERSION: bytes
+
+RE: bytes
+
+ISOFORMAT: str
+
+MISSING_TIMESTAMP: datetime.datetime
+
+MISSING_TIMESTAMP_STR: str
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-3.3-py2.py3-none-any.whl/metaflow-stubs/plugins/aws/step_functions/production_token.pyi
@@ -0,0 +1,18 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.11.14.1+ob(v1)                                                   #
+# Generated on 2024-05-13T17:42:14.302504                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+def new_token(token_prefix, prev_token = None):
+    ...
+
+def load_token(token_prefix):
+    ...
+
+def store_token(token_prefix, token):
+    ...
+
