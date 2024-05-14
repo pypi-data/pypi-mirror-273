33c067e18523402780b413464007b3ba3bd3983a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 15:03:36 +0000

    Add package janim/1.0.2/janim-1.0.2.tar.gz

---
+++ packages/janim/janim-1.0.2.tar.gz/janim-1.0.2/janim/gui/glwidget.py
@@ -0,0 +1,34 @@
+
+import moderngl as mgl
+from PySide6.QtCore import Signal
+from PySide6.QtOpenGLWidgets import QOpenGLWidget
+from PySide6.QtWidgets import QWidget
+
+from janim.anims.timeline import TimelineAnim
+
+
+class GLWidget(QOpenGLWidget):
+    '''
+    窗口中央的渲染界面
+    '''
+    rendered = Signal()
+
+    def __init__(self, anim: TimelineAnim, parent: QWidget | None = None) -> None:
+        super().__init__(parent)
+        self.anim = anim
+
+    def set_time(self, time: float) -> None:
+        self.anim.anim_on(time)
+        self.update()
+
+    def initializeGL(self) -> None:
+        self.ctx = mgl.create_context()
+        self.ctx.enable(mgl.BLEND)
+        self.qfuncs = self.context().functions()
+
+        self.ctx.clear(*self.anim.cfg.background_color.rgb)
+
+    def paintGL(self) -> None:
+        self.qfuncs.glClear(0x00004000 | 0x00000100)    # GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT
+        self.anim.render_all(self.ctx)
+        self.rendered.emit()
