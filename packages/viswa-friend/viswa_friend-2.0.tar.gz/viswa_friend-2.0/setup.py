a550134460bb7e93ebdefa28ccca8c0e83029cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:03:17 +0000

    Add package viswa-friend/2.0/viswa_friend-2.0.tar.gz

---
+++ packages/viswa-friend/viswa_friend-2.0.tar.gz/viswa_friend-2.0/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="viswa_friend", version="2.0", packages=find_packages(), description="viswa 🤝 is a loyal friend who always has your back. He's the kind of friend who will drop everything to help you out, no questions asked. 🎉 He's the friend who makes every moment together unforgettable.")
