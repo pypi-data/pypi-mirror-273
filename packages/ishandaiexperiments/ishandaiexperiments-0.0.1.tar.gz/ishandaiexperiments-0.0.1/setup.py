423e1d580ff4e38616cfe2b24403118d99aeb98e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 05:04:13 +0000

    Add package ishandaiexperiments/0.0.1/ishandaiexperiments-0.0.1.tar.gz

---
+++ packages/ishandaiexperiments/ishandaiexperiments-0.0.1.tar.gz/ishandaiexperiments-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='ishandaiexperiments',
+  version='0.0.1',
+  description='dai experiments',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='S Ishan',
+  author_email='is9678@srmist.edu.in',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='dai', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
