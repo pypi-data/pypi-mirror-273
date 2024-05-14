b924bef1bfb8b845e5a1ff85ecd7c048e5335a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:03:25 +0000

    Add package unite-toolbox/0.1.9/unite_toolbox-0.1.9.tar.gz

---
+++ packages/unite-toolbox/unite_toolbox-0.1.9.tar.gz/unite_toolbox-0.1.9/tests/test_entropy.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+
+import numpy as np
+
+from unite_toolbox.bin_estimators import (
+    calc_bin_entropy,
+    calc_qs_entropy,
+    calc_uniform_bin_entropy,
+    estimate_ideal_bins,
+)
+from unite_toolbox.kde_estimators import calc_kde_entropy
+from unite_toolbox.knn_estimators import calc_knn_entropy
+from unite_toolbox.utils.bootstrapping import one_sample_bootstrap
+
+data_path = Path("tests/data/test_data.npy")
+data = np.load(data_path)
+data = data[:, :, 0]
+
+def test_differential_entropy() -> None:
+    """Test differential entropy.
+
+    Test for the implemented methods to estimate differential entropy. Each
+    estimate should be close to the expected result given a fixed set of
+    samples from the Normal distribution.
+    """
+    # Estimates
+    h_nonuniform_bin, _ = calc_bin_entropy(data, [10, 10])
+    bins = estimate_ideal_bins(data, counts=False)["scott"]
+    h_uniform_bin, _ = calc_uniform_bin_entropy(data, bins)
+    h_qs = calc_qs_entropy(data, seed=42)
+    h_knn = calc_knn_entropy(data)
+    h_kde = calc_kde_entropy(data)
+    h_ikde = calc_kde_entropy(data, mode="integral")
+
+    # Assertions
+    assert np.isclose(h_nonuniform_bin, 1.31, atol=0.01)
+    assert np.isclose(h_uniform_bin, 1.44, atol=0.01)
+    assert np.isclose(h_qs, 0.79, atol=0.01)
+    assert np.isclose(h_knn, 1.97, atol=0.01)
+    assert np.isclose(h_kde, 1.62, atol=0.01)
+    assert np.isclose(h_ikde, 1.63, atol=0.01)
+
+
+def test_one_sample_bootstrap() -> None:
+    """Test for one sample bootstrap.
+
+    Test for the implementation of bootstrapping for one sample (typically
+    for entropy). Uses the kNN estimator to check if the option to add noise
+    is also working correctly, although any estimator could be used.
+    """
+    bs_h, bs_h_ci = one_sample_bootstrap(
+        data, calc_knn_entropy, 100, 0.05, 42, add_noise=True, k=3,
+    )
+    assert np.isclose(bs_h, 1.47, atol=0.01)
+    assert np.isclose(bs_h_ci[0], 1.24, atol=0.01)
+    assert np.isclose(bs_h_ci[1], 1.73, atol=0.01)
---
+++ packages/unite-toolbox/unite_toolbox-0.1.9.tar.gz/unite_toolbox-0.1.9/tests/test_utils.py
@@ -0,0 +1,169 @@
+from pathlib import Path
+
+import numpy as np
+
+from tests.aux_functions import pdf_mnorm, rng
+from unite_toolbox.bin_estimators import calc_vol_array, estimate_ideal_bins
+from unite_toolbox.knn_estimators import vol_lp_ball
+from unite_toolbox.utils.data_validation import (
+    add_noise_to_data,
+    find_repeats,
+    validate_array,
+)
+from unite_toolbox.utils.marginal_scores import calc_marginal_scores, power_set
+from unite_toolbox.utils.sampling import get_samples
+
+data_path = Path("tests/data/test_data.npy")
+data = np.load(data_path)
+samples = data[:, :, 0]
+labels = ["a", "b", "c"]
+
+
+def test_ideal_bins() -> None:
+    """Test for rules-of-thumb.
+
+    Checks the estimated rules-of-thumb against previously calculated number
+    of bins for a fixed data set.
+    """
+    rules = ["fd", "scott", "sturges", "doane"]  # numpy.histogram_bin_edges
+    true_ideal_bins = [[10, 10], [8, 9], [9, 9], [9, 11]]
+    res_dict = {r: b for (r, b) in zip(rules, true_ideal_bins, strict=True)}
+    ideal_bins = estimate_ideal_bins(samples.copy())
+    assert ideal_bins == res_dict
+
+
+def test_vol_array() -> None:
+    """Test for area (volume) of non-uniform array.
+
+    Calculates the area of each cell in a 2D array defined by the location of
+    its bin edges. Compares the result against the manually computed bin areas.
+    """
+    edges = [[0.0, 1.0, 3.0, 7.0, 12.0], [4.0, 8.0, 10.0]]
+    area_array = calc_vol_array(edges)
+    true_area_array = np.array(
+        [[4.0, 2.0], [8.0, 4.0], [16.0, 8.0], [20.0, 10.0]]
+    )
+    assert (area_array == true_area_array).all()
+
+
+def test_vol_ball() -> None:
+    """Test for volume of lp ball.
+
+    Calculates the volume of two lp balls using different norms and compares
+    against the theoretical result.
+    """
+    radius = 2.0
+    assert np.isclose(vol_lp_ball(radius, d=2, p_norm=2), 12.57, atol=0.01)
+    assert np.isclose(vol_lp_ball(radius, d=3, p_norm=np.inf), 64.0, atol=0.01)
+
+
+def test_validate_array() -> None:
+    """Test for basic array validation.
+
+    Creates a copy of the `samples` array and transforms it into a list to test
+    the input as an ArrayLike in the validate_array function.
+    """
+    arr = samples.copy().tolist()
+    unite_arr = validate_array(arr)
+    assert type(unite_arr) == np.ndarray
+    assert unite_arr.ndim == 2
+
+
+def test_repeats() -> None:
+    """Test for finding repeated rows in array.
+
+    Repeats 5 random rows in the `samples` array and tests that the
+    `find_repeats` function finds the correct row indices for the repeated
+    rows.
+    """
+    arr = samples.copy()
+    repeats = rng.choice(range(1, samples.shape[0]), size=5, replace=False)
+    arr[repeats] = arr[0]
+    repeat_matches = find_repeats(arr).nonzero()[0][1:]
+    assert np.array_equal(sorted(repeat_matches), sorted(repeats))
+
+
+def test_add_noise_to_samples() -> None:
+    """Test for adding noise to repeated rows.
+
+    Repeats 4 random rows in the `samples` array, adds noise to the repeated
+    rows and creates a new array in which noise has been added to the repeated
+    rows. Checks if the number of unique rows in the `noisy_arr` is the same as
+    in the original array.
+    """
+    arr = samples.copy()
+    repeats = rng.choice(range(1, samples.shape[0]), size=5, replace=False)
+    arr[repeats] = arr[0]
+    noisy_arr = add_noise_to_data(arr)
+    assert np.unique(noisy_arr, axis=0).shape[0] == samples.shape[0]
+
+
+def test_power_set() -> None:
+    """Test for creating a power set.
+
+    Creates a power set for a predefined list of labels.
+    """
+    res = [
+        (),
+        ("a",),
+        ("b",),
+        ("c",),
+        ("a", "b"),
+        ("a", "c"),
+        ("b", "c"),
+        ("a", "b", "c"),
+    ]
+    assert power_set(labels) == res
+
+
+def test_marginal_scores() -> None:
+    """Test for calculating a marginal score.
+
+    Calculates marginal scores for a predefined list of labels and given scores
+    for combinations of them.
+    """
+    scores = [0.00, 0.20, 0.25, 0.10, 0.70, 0.50, 0.55, 1.00]
+    scores = dict(zip(power_set(labels), scores, strict=True))
+    true_marginal_scores = {"a": 0.36, "b": 0.41, "c": 0.23}
+    marginal_scores = calc_marginal_scores(scores)
+    for feature in labels:
+        assert np.isclose(
+            marginal_scores[feature], true_marginal_scores[feature], atol=0.005
+        )
+
+
+def test_get_samples() -> None:
+    """Test for rejection sampling.
+
+    Obtains samples from a predefined 2D normal mixture distribution and checks
+    that they are the same as a fixed set of samples.
+    """
+    mnorm1_params = [
+        [[-2, 0], [[1, -0.5], [-0.5, 1]], 0.5],
+        [[2, 0], [[1, 0.5], [0.5, 1]], 0.5],
+    ]
+    mnorm_lims = [[-10, 10], [-10, 10]]
+
+    samples = get_samples(
+        func=pdf_mnorm,
+        limits=mnorm_lims,
+        n_samples=10,
+        seed=42,
+        params=mnorm1_params,
+    )
+    fixed_samples = np.array(
+        [
+            [-1.92, -0.52],
+            [-2.79, 0.25],
+            [1.73, -0.13],
+            [-2.67, 0.79],
+            [1.81, -2.06],
+            [-0.90, 0.32],
+            [-1.61, 0.29],
+            [0.40, -1.84],
+            [-2.42, 0.34],
+            [2.34, 0.85],
+        ]
+    )
+    tol = 0.01
+    assert (np.abs(samples - fixed_samples) < tol).all()
