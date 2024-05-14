4f46ca0a9ce8b80183c5a21f4663affcdf48f64c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 14:03:45 +0000

    Add package basic-webserver/0.1/basic_webserver-0.1.tar.gz

---
+++ packages/basic-webserver/basic_webserver-0.1.tar.gz/basic_webserver-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='basic-webserver',
+    version='0.1',
+    description=' A basic web server that can render HTML or plain text.',
+    author='kaimv',
+    author_email='kaimv.hi@gmail.com',
+    url='https://github.com/kaimv/basic-webserver',
+    packages=find_packages(),
+)
