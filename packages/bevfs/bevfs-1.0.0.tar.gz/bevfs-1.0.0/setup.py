52efd1bc234b3f3e64681c80f496989ce2a06318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 06:03:36 +0000

    Add package bevfs/1.0.0/bevfs-1.0.0.tar.gz

---
+++ packages/bevfs/bevfs-1.0.0.tar.gz/bevfs-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='bevfs',
+    version='1.0.0',
+    author='Mohammed Bilal Shakeel',
+    author_email='mohammedbilalshakeel@gmail.com',
+    description='A package for feature selection in high dimensional data.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Bilal39/Bird-Eye-View/tree/main',
+    packages=find_packages(),  # Automatically discover and include all packages
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Define minimum Python version required
+    install_requires=[
+        'pandas>=1.3.0',
+        'numpy>=1.21.0',
+        'scikit-learn>=0.24.0',
+    ],
+)
---
+++ packages/bevfs/bevfs-1.0.0.tar.gz/bevfs-1.0.0/test/test.py
@@ -0,0 +1,3 @@
+from bev import bev_algorithm
+
+bev_algorithm("/Users/mohammedbilalshakeel/Desktop/bev/test/samples_data_modi.txt")
