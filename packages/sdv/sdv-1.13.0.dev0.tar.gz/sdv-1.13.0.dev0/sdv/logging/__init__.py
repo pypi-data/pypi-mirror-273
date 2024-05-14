19c000ee5a5ee21685e32c7cf8eb3c1899d7fc76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 14:03:29 +0000

    Add package sdv/1.13.0.dev0/sdv-1.13.0.dev0.tar.gz

---
+++ packages/sdv/sdv-1.13.0.dev0.tar.gz/sdv-1.13.0.dev0/sdv/logging/__init__.py
@@ -0,0 +1,10 @@
+"""Module for configuring loggers within the SDV library."""
+
+from sdv.logging.logger import get_sdv_logger
+from sdv.logging.utils import disable_single_table_logger, get_sdv_logger_config
+
+__all__ = (
+    'disable_single_table_logger',
+    'get_sdv_logger',
+    'get_sdv_logger_config',
+)
---
+++ packages/sdv/sdv-1.13.0.dev0.tar.gz/sdv-1.13.0.dev0/sdv/logging/utils.py
@@ -0,0 +1,51 @@
+"""Utilities for configuring logging within the SDV library."""
+
+import contextlib
+import logging
+import shutil
+from pathlib import Path
+
+import platformdirs
+import yaml
+
+
+def get_sdv_logger_config():
+    """Return a dictionary with the logging configuration."""
+    store_path = Path(platformdirs.user_data_dir('sdv', 'sdv-dev'))
+    store_path.mkdir(parents=True, exist_ok=True)
+    config_path = Path(__file__).parent / 'sdv_logger_config.yml'
+
+    if (store_path / 'sdv_logger_config.yml').exists():
+        config_path = store_path / 'sdv_logger_config.yml'
+    else:
+        shutil.copyfile(config_path, store_path / 'sdv_logger_config.yml')
+
+    with open(config_path, 'r') as f:
+        logger_conf = yaml.safe_load(f)
+
+    for logger in logger_conf.get('loggers', {}).values():
+        handler = logger.get('handlers', {})
+        if handler.get('filename') == 'sdv_logs.log':
+            handler['filename'] = store_path / handler['filename']
+
+    return logger_conf
+
+
+@contextlib.contextmanager
+def disable_single_table_logger():
+    """Temporarily disables logging for the single table synthesizers.
+
+    This context manager temporarily removes all handlers associated with
+    the ``SingleTableSynthesizer`` logger, disabling logging for that module
+    within the current context. After the context exits, the
+    removed handlers are restored to the logger.
+    """
+    # Logging without ``SingleTableSynthesizer``
+    single_table_logger = logging.getLogger('SingleTableSynthesizer')
+    handlers = single_table_logger.handlers
+    single_table_logger.handlers = []
+    try:
+        yield
+    finally:
+        for handler in handlers:
+            single_table_logger.addHandler(handler)
