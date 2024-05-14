d570f53f46fbe54e049ae7ad5e7a8842e381e092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 16:04:18 +0000

    Add package omegi-python-instrumentation/0.1.1/omegi_python_instrumentation-0.1.1-py3-none-any.whl

---
+++ packages/omegi-python-instrumentation/omegi_python_instrumentation-0.1.1-py3-none-any.whl/omegi/__init__.py
@@ -0,0 +1,60 @@
+import logging
+import os
+from typing import Collection
+
+from opentelemetry import trace
+from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
+from opentelemetry.instrumentation.utils import unwrap
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from .exporter.OmegiSpanExporter import OmegiKafkaSpanExporter
+from .util.OmegiDependencyInstrument import instrument_dependencies
+from .util.OmegiTracingSetup import wrap_functions
+
+
+class OmegiInstrumentor(BaseInstrumentor):
+    def __init__(self, app=None, project_root=None):
+        self.app = app
+        self.project_root = project_root
+
+    def instrumentation_dependencies(self) -> Collection[str]:
+        return []
+
+    def instrument(self):
+        self._instrument()
+
+    def _instrument(self):
+        """
+        SETTING UP INSTRUMENTATION
+        1. Figure Custom TraceExporter and SpanProcessor
+        TraceExporter exports spans to Kafka.
+        Kafka server urls can be figured through environment variables
+        2. Figure Other Instrumentations
+        By installed libraries this library detects fast api, django, flask, elasticsearch
+        detected library's instrumentation is enabled automatically
+        3. Set Function tracing
+        detect module's functions and wrap it in decorator to enable tracing
+        """
+        if self.project_root is None:
+            logging.error("Please set Project root")
+        # Setup Custom Exporter, SpanProcessor
+        custom_exporter = OmegiKafkaSpanExporter()
+        span_processor = BatchSpanProcessor(custom_exporter)
+        trace.set_tracer_provider(TracerProvider(
+            resource=Resource.create({"service.name": os.getenv("OMEGI_SERVICE_NAME")}),
+        ))
+        trace.get_tracer_provider().add_span_processor(span_processor)
+        tracer = trace.get_tracer(__name__)
+        # Setup Instrumentations
+        if self.app is not None:
+            self._start_depending_instumentation(app=self.app)
+        # Setup Tracing Functions
+        wrap_functions(tracer, self.project_root)
+
+    def _uninstrument(self, **kwargs):
+        return unwrap(kwargs)
+
+    def _start_depending_instumentation(self, app):
+        instrument_dependencies(self.app)
