87bc9a3685065b7825a0832ecaa62dd8c1de3527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 15:03:48 +0000

    Add package wrongbutusefulsbi/0.0.4/wrongbutusefulsbi-0.0.4-py3-none-any.whl

---
+++ packages/wrongbutusefulsbi/wrongbutusefulsbi-0.0.4-py3-none-any.whl/wrongbutusefulsbi/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+
+"""TODO: PACKAGE DESCRIPTION
+"""
+
+from wrongbutusefulsbi.examples.contaminated_normal import *
+from wrongbutusefulsbi.examples.contaminated_slcp import *
+from wrongbutusefulsbi.examples.misspec_ma1 import *
+from wrongbutusefulsbi.examples.sir import *
+from wrongbutusefulsbi.examples.toad import *
