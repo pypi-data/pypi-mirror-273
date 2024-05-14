688b32d123f37b041a6c558e9357f973548065cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 06:03:37 +0000

    Add package isic-metadata/1.7.0/isic_metadata-1.7.0-py3-none-any.whl

---
+++ packages/isic-metadata/isic_metadata-1.7.0-py3-none-any.whl/tests/test_batch.py
@@ -0,0 +1,53 @@
+from pydantic import ValidationError
+import pytest
+
+from isic_metadata.metadata import MetadataBatch, MetadataRow
+
+
+def test_batch():
+    MetadataBatch(
+        items=[
+            MetadataRow.model_validate({"diagnosis": "melanoma"}),
+            MetadataRow.model_validate({"diagnosis": "melanoma"}),
+        ]
+    )
+
+
+def test_lesions_belong_to_same_patient():
+    with pytest.raises(ValidationError) as excinfo:
+        MetadataBatch(
+            items=[
+                MetadataRow(lesion_id="foo", patient_id="foopatient"),
+                MetadataRow(lesion_id="foo", patient_id="barpatient"),
+            ]
+        )
+    assert len(excinfo.value.errors()) == 1
+    assert "belong to multiple patients" in excinfo.value.errors()[0]["msg"]
+
+
+def test_blank_lesions_dont_belong_to_same_patient():
+    MetadataBatch(
+        items=[
+            MetadataRow(lesion_id="", patient_id="foopatient"),
+            MetadataRow(lesion_id="", patient_id="barpatient"),
+        ]
+    )
+
+
+def test_rcm_case_has_at_most_one_macroscopic_image():
+    with pytest.raises(ValidationError) as excinfo:
+        MetadataBatch(
+            items=[
+                MetadataRow(image_type="RCM: macroscopic", rcm_case_id="foo"),
+                MetadataRow(image_type="RCM: macroscopic", rcm_case_id="foo"),
+            ]
+        )
+    assert len(excinfo.value.errors()) == 1
+    assert "have multiple macroscopic images" in excinfo.value.errors()[0]["msg"]
+
+    MetadataBatch(
+        items=[
+            MetadataRow(image_type="RCM: macroscopic", rcm_case_id="foo"),
+            MetadataRow(image_type="RCM: macroscopic", rcm_case_id="bar"),
+        ]
+    )
