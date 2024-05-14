43f5ddc393bff5c7e23462ebf98db8c7733b779b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 04:03:08 +0000

    Add package ooui/0.6.0/ooui-0.6.0.tar.gz

---
+++ packages/ooui/ooui-0.6.0.tar.gz/ooui-0.6.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ooui',
+    description='Open Object User Interface',
+    author='GISCE',
+    author_email='devel@gisce.net',
+    url='https://github.com/gisce/python-ooui',
+    version='0.6.0',
+    license='MIT',
+    long_description='''Open Object User Interface for GISCE-ERP''',
+    provides=['ooui'],
+    install_requires=[
+        'lxml',
+        'python-dateutil',
+        'six',
+        'simpleeval<0.9.12',
+    ],
+    tests_require=[
+        'mamba',
+        'expects',
+    ],
+    packages=find_packages()
+)
