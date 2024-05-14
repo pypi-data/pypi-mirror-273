4c6641758ea085ff3ff524a14b6d5a4556572257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 16:04:38 +0000

    Add package local_tuya_ceiling_fan/2.0.0/local_tuya_ceiling_fan-2.0.0.tar.gz

---
+++ packages/local_tuya_ceiling_fan/local_tuya_ceiling_fan-2.0.0.tar.gz/local_tuya_ceiling_fan-2.0.0/README.md
@@ -0,0 +1,58 @@
+# local-tuya-ceiling-fan
+
+[![tests](https://github.com/gpajot/local-tuya-ceiling-fan/actions/workflows/test.yml/badge.svg?branch=main&event=push)](https://github.com/gpajot/local-tuya-ceiling-fan/actions/workflows/test.yml?query=branch%3Amain+event%3Apush)
+[![version](https://img.shields.io/pypi/v/local_tuya_ceiling_fan?label=stable)](https://pypi.org/project/local_tuya_ceiling_fan/)
+[![python](https://img.shields.io/pypi/pyversions/local_tuya_ceiling_fan)](https://pypi.org/project/local_tuya_ceiling_fan/)
+
+Control a Tuya Ceiling fan over LAN.
+
+## Features
+- asynchronous methods and transport
+- persistent communication to the device
+- automatic remote device state updates (remotes can still be used)
+- configurable buffering for subsequent updates
+- constraints between device commands
+- Domoticz plugin using a dedicated thread
+
+## Usage
+See [local tuya requirements](https://github.com/gpajot/local-tuya#requirements) first to find device information.
+
+Example usage:
+```python
+from local_tuya import DeviceConfig, ProtocolConfig
+from local_tuya_ceiling_fan import FanDevice, FanSpeed
+
+
+async with FanDevice(DeviceConfig(ProtocolConfig("{id}", "{address}", b"{key}"))) as device:
+    await device.switch(True)
+    await device.set_speed(FanSpeed.L2)
+    await device.switch(False)
+```
+
+> 💡 There is a safety mechanism that turns off the fan and waits 30s before changing the direction.
+
+## Domoticz plugin
+The plugin requires having fetched device information using instructions above.
+Make sure to read [plugin instructions](https://www.domoticz.com/wiki/Using_Python_plugins) first.
+> 💡 The Domoticz version should be `2022.1` or higher.
+
+```shell
+python -m pip install --upgrade local-tuya-ceiling-fan[domoticz]
+python -m local_tuya_ceiling_fan.domoticz.install
+```
+Domoticz path defaults to `~/domoticz` but you can pass a `-p` option to the second command to change that:
+```shell
+python -m local_tuya_ceiling_fan.domoticz.install -p /some/other/path
+```
+
+Restart Domoticz and create a new Hardware using `Tuya Ceiling Fan`. Fill in device information and add.
+The hardware will create up to 5 devices to control the fan (all prefixed with hardware name):
+- `power`: turn the fan on or off
+- `speed`: set the speed
+- `direction`: set direction
+- `light`: turn the light on or off
+- `mode`: set the operating mode
+
+You can customize the devices you want added in the hardware page.
+
+All device names and levels can be changed once added as only IDs are used internally.
---
+++ packages/local_tuya_ceiling_fan/local_tuya_ceiling_fan-2.0.0.tar.gz/local_tuya_ceiling_fan-2.0.0/local_tuya_ceiling_fan/domoticz/install.py
@@ -0,0 +1,130 @@
+from typing import Dict
+
+from local_tuya import DeviceConfig, ProtocolConfig
+from local_tuya_domoticz_tools import (
+    PluginMetadata,
+    UnitId,
+    UnitManager,
+    install_plugin,
+    selector_switch_unit,
+    switch_unit,
+)
+
+from local_tuya_ceiling_fan.device import (
+    FanDevice,
+    FanDirection,
+    FanMode,
+    FanSpeed,
+    FanState,
+)
+
+
+def _get_metadata() -> PluginMetadata:
+    return PluginMetadata(
+        name="Tuya Ceiling Fan",
+        package="local_tuya_ceiling_fan",
+        description={
+            "p": [
+                {"h2": "Tuya Ceiling Fan"},
+                {
+                    "h3": "Features",
+                    "ul": {
+                        "li": [
+                            "Control a Tuya ceiling fan over LAN",
+                            "Automatically receive new device state (compatible with remote usage)",
+                        ]
+                    },
+                },
+                {
+                    "h3": "Devices",
+                    "ul": {
+                        "li": [
+                            "power - Turn the fan on or off",
+                            "speed - Set the speed",
+                            "direction - Set direction (fan is properly stopped before)",
+                            "light - Turn the light on or off",
+                            "mode - Set the operating mode",
+                        ]
+                    },
+                },
+            ]
+        },
+    )
+
+
+class FanUnitId(UnitId):
+    POWER = 1
+    SPEED = 2
+    DIRECTION = 3
+    LIGHT = 4
+    MODE = 5
+
+
+def on_start(
+    protocol_config: ProtocolConfig,
+    _: Dict[str, str],
+    manager: UnitManager[FanState],
+) -> FanDevice:
+    device = FanDevice(
+        config=DeviceConfig(
+            protocol=protocol_config,
+        ),
+    )
+    manager.register(
+        switch_unit(
+            id_=FanUnitId.POWER,
+            name="power",
+            image=9,
+            command_func=device.switch,
+        ),
+        lambda s: s.power,
+    )
+    manager.register(
+        selector_switch_unit(
+            id_=FanUnitId.SPEED,
+            name="speed",
+            image=7,
+            enum=FanSpeed,
+            command_func=device.set_speed,
+        ),
+        lambda s: s.speed,
+    )
+    manager.register(
+        selector_switch_unit(
+            id_=FanUnitId.DIRECTION,
+            name="direction",
+            image=7,
+            enum=FanDirection,
+            command_func=device.set_direction,
+        ),
+        lambda s: s.direction,
+    )
+    manager.register(
+        switch_unit(
+            id_=FanUnitId.LIGHT,
+            name="light",
+            image=0,
+            command_func=device.switch_light,
+        ),
+        lambda s: s.light,
+    )
+    manager.register(
+        selector_switch_unit(
+            id_=FanUnitId.MODE,
+            name="mode",
+            image=19,
+            enum=FanMode,
+            command_func=device.set_mode,
+        ),
+        lambda s: s.mode,
+    )
+    return device
+
+
+if __name__ == "__main__":
+    install_plugin(
+        _get_metadata(),
+        on_start,
+        "local_tuya_ceiling_fan.domoticz.install",
+        FanUnitId,
+    )
