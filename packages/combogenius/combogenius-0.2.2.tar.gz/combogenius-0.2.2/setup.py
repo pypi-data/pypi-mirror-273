a2353c9dbac3018e021e21adb76a1bc534f67517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 05:04:54 +0000

    Add package combogenius/0.2.2/combogenius-0.2.2.tar.gz

---
+++ packages/combogenius/combogenius-0.2.2.tar.gz/combogenius-0.2.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory=Path(__file__).parent
+long_description=(this_directory / "README.md").read_text()
+
+setup(
+    author='Anna Movsisyan, Lusine Aghinyan, Ararat Kazarian, Hovhannes Hovhannisyan, Eduard Petrosyan',
+    name='combogenius',
+    description='A package designed to efficiently generate new product combinations using check information, and deliver combo suggestions to business partners via email.',
+    version='0.2.2',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(include=['combogenius','combogenius.*']),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',]
+) 
