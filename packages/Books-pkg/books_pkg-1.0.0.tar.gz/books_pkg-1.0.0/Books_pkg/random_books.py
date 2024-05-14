7726fb16aa392f145b0aa2037afa9524107ffc86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 15:03:36 +0000

    Add package Books-pkg/1.0.0/books_pkg-1.0.0.tar.gz

---
+++ packages/Books-pkg/books_pkg-1.0.0.tar.gz/books_pkg-1.0.0/Books_pkg/random_books.py
@@ -0,0 +1,55 @@
+import pkg_resources
+import pandas as pd
+
+class RandomBooks:
+    FILE_PATH = pkg_resources.resource_filename(__name__,'books.csv')
+
+    def __init__(self):
+        self._file = pd.read_csv(RandomBooks.FILE_PATH)
+        self._books = None
+        self._title = None
+        self._author = None
+        self._genre = None
+        self._height = None
+        self._publisher = None
+
+    def generate_random(self):
+        self._books = self._file.sample()
+        self._title = self._books["Title"].values[0]
+        self._author = self._books["Author"].values[0]
+        self._genre = self._books["Genre"].values[0]
+        self._height = self._books["Height"].values[0]
+        self._publisher = self._books["Publisher"].values[0]
+
+    def buscar_title(self):
+        file = "books.csv"
+        datos = pd.read_csv(file)
+        buscar = datos[datos["Title"] == self]
+        if buscar.size == 0:
+            return buscar
+        else:
+            return "No se encontro el Titulo: "
+
+    def buscar_author(self):
+        file = "books.csv"
+        datos = pd.read_csv(file)
+        buscar = datos[datos["Author"] == self]
+        if buscar.size == 0:
+            return buscar
+        else:
+            return "No se encontro el Titulo: "
+
+    def getTitle(self):
+        return self._title
+
+    def getAuthor(self):
+        return self._author
+
+    def getGenre(self):
+        return self._genre
+
+    def getHeight(self):
+        return self._height
+
+    def getPublisher(self):
+        return self._publisher
