0b1d71ace499936c1a5ca609e87788bc1f0b799c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 04:03:44 +0000

    Add package database-creator/0.0/database_creator-0.0.tar.gz

---
+++ packages/database-creator/database_creator-0.0.tar.gz/database_creator-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='database_creator',
+      version='0.0',
+      description='',
+      packages=['database_creator'],
+      author_email='hirikayato@mail.ru',
+      zip_safe=False)
