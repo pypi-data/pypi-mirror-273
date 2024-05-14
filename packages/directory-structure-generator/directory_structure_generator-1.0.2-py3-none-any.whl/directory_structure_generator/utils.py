50127aef4efe7672a0e085029fdc6f0e2b96986b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 04:03:51 +0000

    Add package directory-structure-generator/1.0.2/directory_structure_generator-1.0.2-py3-none-any.whl

---
+++ packages/directory-structure-generator/directory_structure_generator-1.0.2-py3-none-any.whl/directory_structure_generator/utils.py
@@ -0,0 +1,101 @@
+import argparse
+import os
+
+from .colors import Colors
+from .icons import Icons
+
+
+def get_from_file(file_name):
+    with open(file_name, "r") as file:
+        return file.read().splitlines()
+
+
+def save_to_file(file_name, data):
+    with open(file_name, "w") as file:
+        for line in data:
+            file.write(line + "\n")
+
+
+def add_headers(file_name, structure, language):
+    structure.insert(0, f"# {file_name} folder structure\n")
+    structure.insert(1, f"```{language}")
+    structure.insert(2, f"{Icons.folder} {file_name}")
+
+
+def add_footers(structure):
+    structure.append(f"```")
+
+
+def get_max_column_length(structure):
+    max_length = 0
+    for line in structure:
+        if len(line) > max_length:
+            max_length = len(line)
+    return max_length
+
+
+def remove_extras(structure):
+    reversed_structure = structure[::-1]
+    max_column_width = get_max_column_length(structure)
+    for column in range(max_column_width):
+        for line in reversed_structure:
+            line_list = list(line)
+            # if the current column is empty, skip it
+            if column >= len(line_list):
+                continue
+
+            # make sure the line is not the last one
+            if line_list[column] != "│":
+                break
+            else:
+                line_list[column] = " "
+
+            # update the line
+            reversed_structure[reversed_structure.index(line)] = "".join(line_list)
+
+    return reversed_structure[::-1]
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Generate a folder structure from a given path."
+    )
+    parser.add_argument(
+        "--path",
+        dest="path",
+        default=os.getcwd(),
+        help="The path to the folder to analyze.",
+        required=False,
+        type=str,
+    )
+    parser.add_argument(
+        "--exclude_dirs",
+        dest="exclude_dirs",
+        default=[],
+        help="The directories to exclude from the structure separated by spaces.",
+        nargs="+",
+        required=False,
+    )
+    parser.add_argument(
+        "--language",
+        dest="language",
+        default="plaintext",
+        help="The language to use for the folder structure.",
+        required=False,
+        type=str,
+    )
+    parser.add_argument(
+        "--sort",
+        dest="sort",
+        default="False",
+        help="Sort the directories and files alphabetically.",
+        required=False,
+        type=str,
+    )
+    print(f"{Colors.yellow}{Icons.loading} Parsing the arguments...{Colors.reset}")
+    args = parser.parse_args()  # Parse the arguments
+
+    # make all the directories to exclude lowercase
+    args.exclude_dirs = [d.lower() for d in args.exclude_dirs]
+
+    return args.path, args.exclude_dirs, args.language, args.sort
