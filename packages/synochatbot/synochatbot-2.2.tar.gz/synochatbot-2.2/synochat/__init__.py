619a4e23a4e0c8f0548988bfb0d31b2c1757c113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 05:03:55 +0000

    Add package synochatbot/2.2/synochatbot-2.2.tar.gz

---
+++ packages/synochatbot/synochatbot-2.2.tar.gz/synochatbot-2.2/synochat/__init__.py
@@ -0,0 +1,34 @@
+#from .instance import instance
+from .bot import app, send_message, run_bot
+import threading
+import time
+time.sleep(1)
+
+class SynoBot:
+    def __init__(self, prefix):
+        self.prefix = prefix
+        self.alias_to_func = {}
+        self.on_unknown_message1 = False
+
+    def message(self, alias, arguments=0):
+        def decorator(func):
+            self.alias_to_func[alias] = self.prefix + func
+            return func
+        return decorator
+    
+    def on_unknown_message(self, func):
+        def wrapper(*args, **kwargs):
+            # Code to handle unknown messages goes here
+            print("Unknown message.")
+            self.on_unknown_message1 = True
+        
+            # Call the original function
+            return func(*args, **kwargs)
+    
+        return wrapper
+
+    def run(self, **kwargs):
+        flask_thread = threading.Thread(target=app.run, kwargs=kwargs)
+        flask_thread.start()
+def instance(prefix):
+    return SynoBot(prefix)
