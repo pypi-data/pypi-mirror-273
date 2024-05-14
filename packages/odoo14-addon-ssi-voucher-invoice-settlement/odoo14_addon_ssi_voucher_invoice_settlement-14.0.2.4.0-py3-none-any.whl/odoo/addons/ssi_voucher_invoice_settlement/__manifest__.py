38dad6a9c555f3bdba1788d6fa049db0aa1eb4da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:03:03 +0000

    Add package odoo14-addon-ssi-voucher-invoice-settlement/14.0.2.4.0/odoo14_addon_ssi_voucher_invoice_settlement-14.0.2.4.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-voucher-invoice-settlement/odoo14_addon_ssi_voucher_invoice_settlement-14.0.2.4.0-py3-none-any.whl/odoo/addons/ssi_voucher_invoice_settlement/__manifest__.py
@@ -0,0 +1,39 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Invoice Settlement Voucher",
+    "version": "14.0.2.4.0",
+    "category": "Accounting",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_voucher_settlement_common",
+        "ssi_financial_accounting",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir_rule_data.xml",
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "data/account_voucher_type_data.xml",
+        "data/ir_windows_action_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "data/approval_template_data.xml",
+        "data/policy_template_data.xml",
+        "data/account_journal_data.xml",
+        "data/account_voucher_type_allowed_journal_data.xml",
+        "views/account_invoice_settlement_views.xml",
+    ],
+    "demo": [
+        "demo/account_journal_demo.xml",
+        "demo/account_voucher_type_allowed_journal_demo.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
---
+++ packages/odoo14-addon-ssi-voucher-invoice-settlement/odoo14_addon_ssi_voucher_invoice_settlement-14.0.2.4.0-py3-none-any.whl/odoo/addons/ssi_voucher_invoice_settlement/models/invoice_settlement.py
@@ -0,0 +1,156 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class InvoiceSettlement(models.Model):
+    _name = "account.invoice_settlement"
+    _inherit = "mixin.account.voucher"
+    _description = "Invoice Settlement"
+
+    @api.model
+    def _default_type_id(self):
+        return self.env.ref(
+            "ssi_voucher_invoice_settlement." "voucher_type_invoice_settlement"
+        ).id
+
+    type_id = fields.Many2one(
+        default=lambda self: self._default_type_id(),
+    )
+    company_currency_id = fields.Many2one(
+        string="Company Currency",
+        comodel_name="res.currency",
+        related="company_id.currency_id",
+        store=True,
+    )
+    line_ids = fields.One2many(
+        comodel_name="account.invoice_settlement_line",
+    )
+    line_summary_ids = fields.One2many(
+        comodel_name="account.invoice_settlement_line_summary",
+    )
+    line_dr_ids = fields.One2many(
+        comodel_name="account.invoice_settlement_line",
+    )
+    line_cr_ids = fields.One2many(
+        comodel_name="account.invoice_settlement_line",
+    )
+
+
+class InvoiceSettlementLine(models.Model):
+    _name = "account.invoice_settlement_line"
+    _inherit = "mixin.account.voucher.line"
+    _description = "Invoice Settlement Line"
+
+    voucher_id = fields.Many2one(
+        comodel_name="account.invoice_settlement",
+    )
+    tax_ids = fields.One2many(
+        comodel_name="account.invoice_settlement_line_tax",
+    )
+    currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        related="voucher_id.currency_id",
+        store=True,
+    )
+    company_currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        related="voucher_id.company_currency_id",
+        store=True,
+    )
+
+
+class InvoiceSettlementLineTax(models.Model):
+    _name = "account.invoice_settlement_line_tax"
+    _inherit = "mixin.account.voucher.line.tax"
+    _description = "Invoice Settlement Line Tax"
+
+    voucher_line_id = fields.Many2one(
+        comodel_name="account.invoice_settlement_line",
+    )
+    currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        related="voucher_line_id.currency_id",
+        store=True,
+    )
+    company_currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        related="voucher_line_id.company_currency_id",
+        store=True,
+    )
+
+
+class InvoiceSettlementLineSummary(models.Model):
+    _name = "account.invoice_settlement_line_summary"
+    _inherit = "mixin.account.voucher.line.summary"
+    _description = "Invoice Settlement Line Summary"
+    _auto = False
+
+    voucher_id = fields.Many2one(
+        comodel_name="account.invoice_settlement",
+    )
+
+    def _select(self):
+        select_str = """
+        SELECT
+            row_number() OVER() as id,
+            a.voucher_id AS voucher_id,
+            a.account_id AS account_id,
+            a.partner_id AS partner_id,
+            a.currency_id AS currency_id,
+            SUM(a.amount_before_tax) AS amount_before_tax,
+            SUM(a.amount_tax) AS amount_tax,
+            SUM(a.amount_after_tax) AS amount_after_tax
+        """
+        return select_str
+
+    def _get_from_table(self):
+        return "account_invoice_settlement_line"
+
+    def _from(self):
+        from_str = """
+        %s AS a
+        """ % (
+            self._get_from_table()
+        )
+        return from_str
+
+    def _where(self):
+        where_str = """
+        WHERE 1 = 1
+        """
+        return where_str
+
+    def _join(self):
+        join_str = """
+        """
+        return join_str
+
+    def _group_by(self):
+        group_str = """
+        GROUP BY a.voucher_id,a.account_id,a.partner_id,a.currency_id
+        """
+        return group_str
+
+    def init(self):
+        # tools.drop_view_if_exists(self._cr, self._table)
+        # pylint: disable=locally-disabled, sql-injection
+        self._cr.execute(
+            """CREATE or REPLACE VIEW %s as (
+            %s
+            FROM %s
+            %s
+            %s
+            %s
+        )"""
+            % (
+                self._table,
+                self._select(),
+                self._from(),
+                self._join(),
+                self._where(),
+                self._group_by(),
+            )
+        )
