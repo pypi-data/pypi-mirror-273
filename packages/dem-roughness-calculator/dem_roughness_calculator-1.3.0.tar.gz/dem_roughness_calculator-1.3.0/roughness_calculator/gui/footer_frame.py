0495bcb61058ee021648782dce7c5d916de3acd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 16:04:12 +0000

    Add package dem-roughness-calculator/1.3.0/dem_roughness_calculator-1.3.0.tar.gz

---
+++ packages/dem-roughness-calculator/dem_roughness_calculator-1.3.0.tar.gz/dem_roughness_calculator-1.3.0/roughness_calculator/gui/footer_frame.py
@@ -0,0 +1,171 @@
+import logging
+import logging.handlers
+import webbrowser
+from typing import Any
+import tkinter as tk
+import tkinter.scrolledtext as st
+
+import customtkinter as ctk
+
+from roughness_calculator.classes.defaults import Defaults
+from roughness_calculator.gui.defaults import DEFAULTS
+
+# Set up logging
+logger = logging.getLogger(__name__)
+
+
+class FooterFrame(ctk.CTkFrame):
+    """
+    A class used to create the footer frame of the GUI.
+
+    Attributes
+    ----------
+    main_gui : Any
+        The main GUI object.
+    help_button : WebsiteButton
+        The button that opens the documentation website.
+    info_label : ctk.CTkLabel
+        The label that displays the copyright information.
+    """
+
+    def __init__(self, parent: ctk.CTk, main_gui: Any, **kwargs: Any):
+        """
+        Constructs all the necessary attributes for the FooterFrame object.
+
+        Parameters
+        ----------
+            parent : ctk.CTk
+                The parent widget.
+            main_gui : Any
+                The main GUI object.
+            **kwargs : Any
+                Additional keyword arguments.
+        """
+        super().__init__(parent, **kwargs)
+        logger.info("Initializing FooterFrame")
+        self.main_gui = main_gui
+
+        # Configure the grid
+        self.grid_rowconfigure([0, 1], weight=1)
+        self.grid_columnconfigure([0, 1, 2, 3], weight=1)
+
+        # Create the help button
+        self.help_button = WebsiteButton(self,
+                                         "https://github.com/lbatschelet/dem-roughness-calculator",
+                                         text="Documentation")
+        self.help_button.grid(row=0,
+                              column=0,
+                              padx=(DEFAULTS.PADX, DEFAULTS.PADX * 0.5),
+                              pady=(DEFAULTS.PADY, DEFAULTS.PADY * 0.5),
+                              sticky="nsew")
+
+        # Create the log button
+        self.log_button = ctk.CTkButton(self, text="Show Logs", command=self.open_log_window)
+        self.log_button.grid(row=0,
+                             column=3,
+                             padx=(DEFAULTS.PADX * 0.5, DEFAULTS.PADX),
+                             pady=(DEFAULTS.PADY, DEFAULTS.PADY * 0.5),
+                             sticky="nsew")
+
+        # Create the info label
+        self.info_label = (
+            ctk.CTkLabel(self,
+                         text="Surface Roughness Calculator - © 2024 L. Batschelet, F. Mohaupt, S. Röthlisberger. "
+                              "Licensed under the MIT License.",
+                         font=self.main_gui.fonts['small']))
+        self.info_label.grid(row=1,
+                             column=0,
+                             columnspan=4,
+                             padx=DEFAULTS.PADX,
+                             pady=(DEFAULTS.PADY * 0.5, DEFAULTS.PADY),
+                             sticky="nsew")
+
+    def open_log_window(self):
+        """
+        Opens the log window.
+        """
+        LogWindow(self)
+
+
+class WebsiteButton(ctk.CTkButton):
+    """
+    A class used to create a button that opens a website.
+
+    Attributes
+    ----------
+    url : str
+        The URL of the website to open.
+    """
+
+    def __init__(self, parent: ctk.CTk, url: str, **kwargs: Any):
+        """
+        Constructs all the necessary attributes for the WebsiteButton object.
+
+        Parameters
+        ----------
+            parent : ctk.CTk
+                The parent widget.
+            url : str
+                The URL of the website to open.
+            **kwargs : Any
+                Additional keyword arguments.
+        """
+        super().__init__(parent, command=self.open_website, **kwargs)
+        logger.info("Initializing WebsiteButton with URL: %s", url)
+        self.url = url
+
+    def open_website(self):
+        """
+        Opens the website in a web browser.
+        """
+        logger.info("Opening website: %s", self.url)
+        webbrowser.open(self.url)
+
+
+class LogWindow(tk.Toplevel):
+    """
+    A class used to create a window that displays the logs.
+
+    Attributes
+    ----------
+    text_area : st.ScrolledText
+        The text area that displays the logs.
+    """
+
+    def __init__(self, parent: ctk.CTk):
+        super().__init__(parent)
+        self.title("Logs")
+        self.geometry("500x500")  # adjust as needed
+
+        # Create a scrollable text area
+        self.text_area = st.ScrolledText(self, wrap=tk.WORD)
+        self.text_area.pack(fill=tk.BOTH, expand=True)
+
+        # Get the log file handler from the root logger
+        root_logger = logging.getLogger()
+        self.file_handler = next((handler for handler in root_logger.handlers if isinstance(handler, logging.FileHandler)), None)
+
+        # Set the state to disabled to make the text read-only
+        self.text_area.config(state=tk.DISABLED)
+
+        # Start the log update loop
+        self.update_logs()
+
+    def update_logs(self):
+        """
+        Updates the logs in the text area.
+        """
+        if self.file_handler:
+            # Clear the text area
+            self.text_area.config(state=tk.NORMAL)
+            self.text_area.delete(1.0, tk.END)
+
+            # Read the log file and insert its contents into the text area
+            with open(self.file_handler.baseFilename, "r") as log_file:
+                self.text_area.insert(tk.END, log_file.read())
+
+            # Set the state to disabled to make the text read-only
+            self.text_area.config(state=tk.DISABLED)
+
+        # Schedule the next update
+        self.after(Defaults.LOG_UPDATE_INTERVAL, self.update_logs)
---
+++ packages/dem-roughness-calculator/dem_roughness_calculator-1.3.0.tar.gz/dem_roughness_calculator-1.3.0/roughness_calculator/gui/parameter_input.py
@@ -0,0 +1,176 @@
+import tkinter as tk
+
+import customtkinter as ctk
+
+from roughness_calculator.gui.analyze_and_optimize import AnalyzeAndOptimizeFrame
+from roughness_calculator.gui.defaults import DEFAULTS
+
+
+class ParameterFrame(ctk.CTkFrame):
+    def __init__(self, parent, main_gui, **kwargs):
+        super().__init__(parent, **kwargs)
+
+        self.main_gui = main_gui
+
+        # Make the GUI responsive
+        self.grid_columnconfigure([0, 1], weight=1)
+        self.grid_rowconfigure([0, 1], weight=1)
+
+        self.window_size_field = (
+            ParameterInput(self,
+                           "Window size",
+                           "The size of the window to use for processing. Defaults to 1.0.",
+                           self.main_gui))
+        self.window_size_field.grid(row=0,
+                                    column=0,
+                                    padx=(DEFAULTS.PADX, DEFAULTS.PADX * 0.5),
+                                    pady=(DEFAULTS.PADY, DEFAULTS.PADY * 0.5),
+                                    sticky="nsew")
+
+        self.category_thresholds_field = (
+            ParameterInput(self,
+                           "Category Thresholds",
+                           "List of thresholds for categorizing data. "
+                           "First category starts from first value. (Optional)",
+                           self.main_gui))
+        self.category_thresholds_field.grid(row=0,
+                                            column=1,
+                                            padx=(DEFAULTS.PADX * 0.5, DEFAULTS.PADX),
+                                            pady=(DEFAULTS.PADY, DEFAULTS.PADY * 0.5),
+                                            sticky="nsew")
+
+        self.band_number_field = (
+            ParameterInput(self,
+                           "Band Number",
+                           "The band number to use for processing. Defaults to 1.",
+                           self.main_gui))
+        self.band_number_field.grid(row=1,
+                                    column=0,
+                                    padx=(DEFAULTS.PADX, DEFAULTS.PADX * 0.5),
+                                    pady=(DEFAULTS.PADY * 0.5, DEFAULTS.PADY * 0.5),
+                                    sticky="nsew")
+
+        self.high_value_threshold_field = (
+            ParameterInput(self,
+                           "High value threshold",
+                           "Used to filter out high values at the borders of the data. Defaults to 10.",
+                           self.main_gui))
+        self.high_value_threshold_field.grid(row=1,
+                                             column=1,
+                                             padx=(DEFAULTS.PADX * 0.5, DEFAULTS.PADX),
+                                             pady=(DEFAULTS.PADY * 0.5, DEFAULTS.PADY * 0.5),
+                                             sticky="nsew")
+
+        # Create a new frame for the buttons
+        self.button_frame = ctk.CTkFrame(self)
+        self.button_frame.grid(row=2,
+                               column=0,
+                               columnspan=2,
+                               padx=DEFAULTS.PADX,
+                               pady=(DEFAULTS.PADY * 0.5, DEFAULTS.PADY),
+                               sticky="ew")
+        # Set equal weights for each column in the button frame
+        self.button_frame.grid_columnconfigure([0, 1, 2], weight=1)
+
+        self.start_processing_button = (
+            ctk.CTkButton(self.button_frame, text="Start Processing", command=self.main_gui.start_processing))
+        self.start_processing_button.grid(row=0,
+                                          column=0,
+                                          padx=(DEFAULTS.PADX, DEFAULTS.PADX * 0.5),
+                                          pady=DEFAULTS.PADY,
+                                          sticky="ew")
+
+        # Create a new button
+        self.analyze_and_optimize_button = ctk.CTkButton(self.button_frame,
+                                                         text="Analyze and optimize...",
+                                                         command=self.toggle_frame, state=tk.DISABLED)
+        self.analyze_and_optimize_button.grid(row=0,
+                                              column=1,
+                                              padx=(DEFAULTS.PADX * 0.5, DEFAULTS.PADX * 0.5),
+                                              pady=DEFAULTS.PADY,
+                                              sticky="ew")
+
+        self.save_file_button = (
+            ctk.CTkButton(self.button_frame, text="Save File", command=self.main_gui.save_image, state=tk.DISABLED))
+        self.save_file_button.grid(row=0,
+                                   column=2,
+                                   padx=(DEFAULTS.PADX * 0.5, DEFAULTS.PADX * 0.5),
+                                   pady=DEFAULTS.PADY,
+                                   sticky="ew")
+
+        # Create a new frame and initially hide it
+        self.analyze_and_optimize_frame = AnalyzeAndOptimizeFrame(self, main_gui)
+        self.analyze_and_optimize_frame.grid(row=3,
+                                             column=0,
+                                             columnspan=3,
+                                             padx=DEFAULTS.PADX,
+                                             pady=(0, DEFAULTS.PADY),
+                                             sticky="ew")
+        self.analyze_and_optimize_frame.grid_remove()
+
+    def toggle_frame(self):
+        if self.analyze_and_optimize_frame.winfo_viewable():
+            self.analyze_and_optimize_frame.grid_remove()
+        else:
+            self.analyze_and_optimize_frame.grid()
+
+    def get_parameters(self):
+        return {
+            "window_size": self.window_size_field.get() or None,
+            "category_thresholds": self.category_thresholds_field.get() or None,
+            "band_number": self.band_number_field.get() or None,
+            "high_value_threshold": self.high_value_threshold_field.get() or None,
+            "control_input_path": self.analyze_and_optimize_frame.control_input_path_field.get() or None
+        }
+
+
+class DescriptionField(ctk.CTkLabel):
+    def __init__(self, parent, text, **kwargs):
+        super().__init__(parent, text=text, **kwargs)
+
+
+class ParameterInput(ctk.CTkFrame):
+    def __init__(self, parent, name, description, main_gui, **kwargs):
+        super().__init__(parent, **kwargs)
+        self.main_gui = main_gui
+        self.grid_columnconfigure([0, 1], weight=1)
+
+        self.name_label = ctk.CTkLabel(self, text=name, font=self.main_gui.fonts['h3'])
+        self.name_label.grid(row=0,
+                             column=0,
+                             padx=(DEFAULTS.PADX, DEFAULTS.PADX * 0.25),
+                             pady=DEFAULTS.PADY,
+                             sticky="w")
+
+        self.description_button = ctk.CTkButton(self, text="Description", command=self.show_description)
+        self.description_button.grid(row=0,
+                                     column=1,
+                                     padx=(DEFAULTS.PADX * 0.25, DEFAULTS.PADX),
+                                     pady=DEFAULTS.PADY,
+                                     sticky="e")
+
+        self.entry = ctk.CTkEntry(self)
+        self.entry.grid(row=1,
+                        column=0,
+                        columnspan=2,
+                        padx=(DEFAULTS.PADX, DEFAULTS.PADX),
+                        pady=(0, DEFAULTS.PADY),
+                        sticky="ew")
+
+        self.description_field = DescriptionField(self, description)
+        self.description_field.grid(row=2,
+                                    column=0,
+                                    columnspan=2,
+                                    padx=(DEFAULTS.PADX, DEFAULTS.PADX),
+                                    pady=(0, DEFAULTS.PADY),
+                                    sticky="w")
+        self.description_field.grid_remove()
+
+    def show_description(self):
+        if self.description_field.winfo_viewable():
+            self.description_field.grid_remove()
+        else:
+            self.description_field.grid()
+
+    def get(self):
+        return self.entry.get()
