fd0e684d208b5140bcc3f8cc34712c8d99e99885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 14:03:39 +0000

    Add package wrapica/1.0.7/wrapica-1.0.7.tar.gz

---
+++ packages/wrapica/wrapica-1.0.7.tar.gz/wrapica-1.0.7/src/wrapica/project_analysis/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# Import everything
+from .functions.project_analyses_functions import (
+    # Project Analysis functions
+    get_project_analysis_inputs,
+    get_analysis_input_object_from_analysis_code,
+    get_outputs_object_from_analysis_id,
+    get_analysis_output_object_from_analysis_code,
+    get_cwl_outputs_json_from_analysis_id,
+    get_analysis_obj,
+    get_analysis_steps,
+    get_analysis_log_from_analysis_step,
+    write_analysis_step_logs,
+    abort_analysis
+)
+
+__all__ = [
+    # Functions
+    'get_project_analysis_inputs',
+    'get_analysis_input_object_from_analysis_code',
+    'get_outputs_object_from_analysis_id',
+    'get_analysis_output_object_from_analysis_code',
+    'get_cwl_outputs_json_from_analysis_id',
+    'get_analysis_obj',
+    'get_analysis_steps',
+    'get_analysis_log_from_analysis_step',
+    'write_analysis_step_logs',
+    'abort_analysis'
+]
