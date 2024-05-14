97ac8c1571ce3a5179d489d4d115ec742394bf5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 07:03:14 +0000

    Add package shelloracle/1.4.0/shelloracle-1.4.0-py3-none-any.whl

---
+++ packages/shelloracle/shelloracle-1.4.0-py3-none-any.whl/shelloracle/providers/openai.py
@@ -0,0 +1,33 @@
+from collections.abc import AsyncIterator
+
+from openai import AsyncOpenAI, APIError
+
+from . import Provider, ProviderError, Setting, system_prompt
+
+
+class OpenAI(Provider):
+    name = "OpenAI"
+
+    api_key = Setting(default="")
+    model = Setting(default="gpt-3.5-turbo")
+
+    def __init__(self):
+        if not self.api_key:
+            raise ProviderError("No API key provided")
+        self.client = AsyncOpenAI(api_key=self.api_key)
+
+    async def generate(self, prompt: str) -> AsyncIterator[str]:
+        try:
+            stream = await self.client.chat.completions.create(
+                model=self.model,
+                messages=[
+                    {"role": "system", "content": system_prompt},
+                    {"role": "user", "content": prompt}
+                ],
+                stream=True,
+            )
+            async for chunk in stream:
+                if chunk.choices[0].delta.content is not None:
+                    yield chunk.choices[0].delta.content
+        except APIError as e:
+            raise ProviderError(f"Something went wrong while querying OpenAI: {e}") from e
