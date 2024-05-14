1d59233c4a7b5a7e1a7ff8bad0eb91606baecc16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 06:03:33 +0000

    Add package socketsecurity/0.0.43/socketsecurity-0.0.43.tar.gz

---
+++ packages/socketsecurity/socketsecurity-0.0.43.tar.gz/socketsecurity-0.0.43/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='socketsecurity',
+   version='0.0.43',
+   packages=find_packages(),
+   install_requires=[
+      'requests',
+      'mdutils'
+      'prettytable',
+      'argparse'
+   ],
+   entry_points='''
+      [console_scripts]
+      socketcli=socketsecurity.socketcli:main
+      ''',
+)
