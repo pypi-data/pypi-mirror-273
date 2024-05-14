21cac1a26d8c71ac11216b12607eaac7657e4dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:03:15 +0000

    Add package nicotool/1.4.8.5/nicotool-1.4.8.5.tar.gz

---
+++ packages/nicotool/nicotool-1.4.8.5.tar.gz/nicotool-1.4.8.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nicotool",
+    version="1.4.8.5",
+    packages=find_packages(),
+    install_requires=[
+        "pytube",
+        "moviepy",
+        "requests"
+        ],
+    author="Nico",
+    description="A handy litle tool",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown"
+)
