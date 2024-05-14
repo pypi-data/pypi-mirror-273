792c7de5d0363d772989cdc1545a5c777a18c209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 06:03:08 +0000

    Add package sonatoki/0.2.1/sonatoki-0.2.1.tar.gz

---
+++ packages/sonatoki/sonatoki-0.2.1.tar.gz/sonatoki-0.2.1/src/sonatoki/Filters.py
@@ -0,0 +1,206 @@
+# STL
+import re
+from abc import ABC, abstractmethod
+from typing import Set
+from functools import lru_cache as cache  # cache comes in 3.9
+
+# PDM
+import regex
+from typing_extensions import override
+
+# LOCAL
+from sonatoki.constants import (
+    VOWELS,
+    NIMI_PU,
+    ALPHABET,
+    ALLOWABLES,
+    CONSONANTS,
+    NIMI_LINKU,
+    NIMI_PU_ALE,
+    POSIX_PUNCT,
+    UNICODE_PUNCT,
+    NIMI_LINKU_ALE,
+    ALL_PUNCT_RANGES,
+    NIMI_LINKU_SANDBOX,
+)
+
+regex.DEFAULT_VERSION = regex.VERSION1
+
+
+class Filter(ABC):
+    @classmethod
+    @abstractmethod
+    @cache(maxsize=None)
+    def filter(cls, token: str) -> bool:
+        raise NotImplementedError
+
+
+class RegexFilter(Filter):
+    pattern: "re.Pattern[str]"
+
+    @classmethod
+    @override
+    @cache(maxsize=None)
+    def filter(cls, token: str) -> bool:
+        return not not re.fullmatch(cls.pattern, token)
+
+
+class Regex1Filter(Filter):
+    pattern: "regex.Pattern[str]"
+
+    @classmethod
+    @override
+    @cache(maxsize=None)
+    def filter(cls, token: str) -> bool:
+        return not not regex.fullmatch(cls.pattern, token)
+
+
+class MemberFilter(Filter):
+    tokens: Set[str]
+
+    @classmethod
+    @override
+    @cache(maxsize=None)
+    def filter(cls, token: str) -> bool:
+        return token.lower() in cls.tokens
+
+
+class SubsetFilter(Filter):
+    tokens: Set[str]
+
+    @classmethod
+    @override
+    @cache(maxsize=None)
+    def filter(cls, token: str) -> bool:
+        return set(token.lower()).issubset(cls.tokens)
+
+
+class Miscellaneous(MemberFilter):
+    tokens = set(ALLOWABLES)
+
+
+class ProperName(Filter):
+    """Determines if a given token is a valid name (also called a loan word).
+    When Toki Pona is written with the Latin alphabet, names are generally
+    capitalized at their start. This filter identifies those tokens.
+
+    Note that this alone cannot determine if a token is a valid name, because
+    a standalone name is considered invalid in Toki Pona- names generally have head nouns.
+    This tool only examines one token at a time, so cannot detect names any better than identifying their capital letter.
+    """
+
+    @classmethod
+    @override
+    @cache(maxsize=None)
+    def filter(cls, token: str) -> bool:
+        return token == token.capitalize()
+        # TODO:  If the token is in a script which doesn't have a case distinction,
+        # this will errantly match.
+
+
+class NimiPu(MemberFilter):
+    tokens = set(NIMI_PU)
+
+
+class NimiPuAle(MemberFilter):
+    tokens = set(NIMI_PU_ALE)
+
+
+class NimiLinku(MemberFilter):
+    tokens = set(NIMI_LINKU)
+
+
+class NimiLinkuAle(MemberFilter):
+    tokens = set(NIMI_LINKU_ALE)
+
+
+class NimiLinkuSandbox(MemberFilter):
+    tokens = set(NIMI_LINKU_SANDBOX)
+
+
+class Phonotactic(RegexFilter):
+    """Determines if a given token is phonotactically valid Toki Pona (or `n`).
+    Excludes both consecutive nasals and the illegal syllables:
+    - "nm", "nn"
+    - "wu", "wo", "ji", "ti"
+
+    Note that if this validator is used after `Cleaners.ConsecutiveDuplicates`,
+    "nn" cannot be found."""
+
+    pattern = re.compile(
+        rf"^((^[{VOWELS}]|[klmnps][{VOWELS}]|[jt][aeou]|[w][aei])(n(?![mn]))?)+$|^n$",
+        # Can't split initial vowel group off like in Syllabics because of
+        # consecutive nasal detection; it is costly to duplicate
+        flags=re.IGNORECASE,
+    )
+
+
+class Syllabic(RegexFilter):
+    """Determines if a given token is syllabically valid Toki Pona (or `n`).
+    Words must have correctly ordered vowels and consonants, but the phonotactic
+    exceptions are not considered."""
+
+    # rf"^((^[{VOWELS}]|[{CONSONANTS}][{VOWELS}])n?)+$|^n$"
+    # Alterative I was exploring takes ~15% more steps
+    pattern = re.compile(
+        rf"^(?:^[{VOWELS}]n?)?(?:[{CONSONANTS}][{VOWELS}]n?)*$|^n$",
+        flags=re.IGNORECASE,
+    )
+
+
+class Alphabetic(SubsetFilter):
+    tokens = set(ALPHABET)
+
+
+class AlphabeticRe(RegexFilter):
+    pattern = re.compile(rf"[{ALPHABET}]+", flags=re.IGNORECASE)
+
+
+class Numeric(Filter):
+    """Determine if a given token is entirely numeric.
+    Covers all numeric symbols in Unicode.
+
+    This will fail to find numeric tokens such as "1.111" or "-42",
+    but if used with the aggressive tokenizer designed for `tok`, these will be
+    split into `["1", ".", "111"]` and `["-", "42"]` respectively. As such, the
+    numeric tokens will be split from their punctuation."""
+
+    @classmethod
+    @override
+    @cache(maxsize=None)
+    def filter(cls, msg: str) -> bool:
+        return msg.isnumeric()
+
+
+class Punctuation(SubsetFilter):
+    """Identify whether a token is entirely punctuation. Fastest implementation."""
+
+    tokens = set(POSIX_PUNCT + UNICODE_PUNCT)
+
+
+class PunctuationRe(RegexFilter):
+    """Faster implementation of `PunctuationRe1`.
+    Goes out of date compared to the `regex` library if UNICODE_PUNCT is not updated."""
+
+    pattern = re.compile(rf"[{ALL_PUNCT_RANGES}]+")
+
+
+class PunctuationRe1(Regex1Filter):
+    """Reference implementation for identifying tokens made entirely of punctuation."""
+
+    pattern = regex.compile(r"[\p{Punctuation}\p{posix_punct}]+")
+
+
+__all__ = [
+    "Alphabetic",
+    "NimiLinku",
+    "NimiLinkuAle",
+    "NimiLinkuSandbox",
+    "NimiPu",
+    "NimiPuAle",
+    "Numeric",
+    "Phonotactic",
+    "ProperName",
+    "Punctuation",
+    "Syllabic",
+]
---
+++ packages/sonatoki/sonatoki-0.2.1.tar.gz/sonatoki-0.2.1/tests/test_filters.py
@@ -0,0 +1,126 @@
+# STL
+import string
+
+# PDM
+import hypothesis.strategies as st
+from hypothesis import given, example
+
+# LOCAL
+from sonatoki.Filters import (
+    NimiPu,
+    Numeric,
+    Syllabic,
+    NimiLinku,
+    Alphabetic,
+    ProperName,
+    Phonotactic,
+    Punctuation,
+    AlphabeticRe,
+    PunctuationRe,
+    PunctuationRe1,
+)
+from sonatoki.Cleaners import ConsecutiveDuplicates
+from sonatoki.constants import NIMI_PU, NIMI_LINKU
+
+# FILESYSTEM
+from .test_utils import PROPER_NAME_RE
+
+
+@given(st.sampled_from(NIMI_PU))
+@example("lukin")
+@example("selo")
+@example("li")
+def test_NimiPu(s: str):
+    res = NimiPu.filter(s)
+    assert res, repr(s)
+
+
+@given(st.sampled_from(NIMI_LINKU))
+@example("pona")
+@example("tonsi")
+@example("kipisi")
+@example("n")
+def test_NimiLinku(s: str):
+    res = NimiLinku.filter(s)
+    assert res, repr(s)
+
+
+@given(st.sampled_from(NIMI_LINKU))
+def test_nimi_linku_properties(s: str):
+    assert ConsecutiveDuplicates.clean(s) == s, repr(s)
+    assert Alphabetic.filter(s), repr(s)
+    assert Syllabic.filter(s), repr(s)
+    assert Phonotactic.filter(s), repr(s)
+    # Passing phonotactic implies all of the above
+
+
+@given(st.from_regex(Phonotactic.pattern.pattern, fullmatch=True))
+@example("kijetesantakalu")
+@example("n")
+def test_Phonotactic(s: str):
+    res = Phonotactic.filter(s)
+    assert res, repr(s)
+
+
+@given(st.from_regex(Syllabic.pattern.pattern, fullmatch=True))
+@example("wuwojitiwunwonjintinmanna")
+def test_Syllabic(s: str):
+    res = Syllabic.filter(s)
+    assert res, repr(s)
+
+
+@given(st.from_regex(AlphabeticRe.pattern.pattern, fullmatch=True))
+@example("muems")
+@example("mpptp")
+@example("tptpt")
+def test_Alphabetic(s: str):
+    res_fn = Alphabetic.filter(s)
+    res_re = AlphabeticRe.filter(s)
+    assert res_fn == res_re, repr(s)
+
+
+@given(st.from_regex(AlphabeticRe.pattern.pattern, fullmatch=True))
+def test_AlphabeticRe(s: str):
+    res_re = AlphabeticRe.filter(s)
+    assert res_re, repr(s)
+
+
+@given(st.from_regex(PROPER_NAME_RE, fullmatch=True))
+def test_ProperName(s: str):
+    res = ProperName.filter(s)
+    assert res, repr(s)
+
+
+@given(st.from_regex(PunctuationRe.pattern.pattern, fullmatch=True))
+@example("[]")
+@example(r"\\")
+@example(r"\"")
+@example("⟨·⟩")
+@example("…")
+@example("「」")
+@example(string.punctuation)
+def test_PunctuationRe1(s: str):
+    res = PunctuationRe1.filter(s)
+    assert res, repr(s)
+
+
+@given(st.from_regex(PunctuationRe.pattern.pattern, fullmatch=True))
+def test_PunctuationRe(s: str):
+    res_re = PunctuationRe.filter(s)
+    res_re1 = PunctuationRe1.filter(s)
+    assert res_re == res_re1, repr(s)
+
+
+@given(st.from_regex(PunctuationRe.pattern.pattern, fullmatch=True))
+def test_Punctuation(s: str):
+    res_fn = Punctuation.filter(s)
+    res_re1 = PunctuationRe1.filter(s)
+    assert res_fn == res_re1, repr(s)
+
+
+@given(st.from_regex(r"\d+", fullmatch=True))
+@example("124125")
+@example("99990000")
+def test_Numeric(s: str):
+    res = Numeric.filter(s)
+    assert res, repr(s)
