24bb1b809fb7867193e94af0f2f71d8c2aa4bb0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:04:16 +0000

    Add package midas-mosaik/1.2.2/midas_mosaik-1.2.2-py3-none-any.whl

---
+++ packages/midas-mosaik/midas_mosaik-1.2.2-py3-none-any.whl/midas/api/__init__.py
@@ -0,0 +1,9 @@
+import logging
+
+LOG = logging.getLogger(__name__)
+
+from .fnc_analyze import analyze
+from .fnc_configure import configure
+from .fnc_download import download
+from .fnc_list import list_scenarios
+from .fnc_run import run
