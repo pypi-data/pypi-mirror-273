d1fb4f155e61a5fe22815f7d167f9ed51f62bb25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:03:37 +0000

    Add package yandex_api_client/0.0.2/yandex_api_client-0.0.2.tar.gz

---
+++ packages/yandex_api_client/yandex_api_client-0.0.2.tar.gz/yandex_api_client-0.0.2/yandex_api_client/api/__init__.py
@@ -0,0 +1,39 @@
+# flake8: noqa
+
+# import apis into api package
+from yandex_api_client.api.bids_api import BidsApi
+from yandex_api_client.api.business_offer_mappings_api import BusinessOfferMappingsApi
+from yandex_api_client.api.campaigns_api import CampaignsApi
+from yandex_api_client.api.categories_api import CategoriesApi
+from yandex_api_client.api.chats_api import ChatsApi
+from yandex_api_client.api.content_api import ContentApi
+from yandex_api_client.api.dbs_api import DbsApi
+from yandex_api_client.api.delivery_services_api import DeliveryServicesApi
+from yandex_api_client.api.express_api import ExpressApi
+from yandex_api_client.api.fbs_api import FbsApi
+from yandex_api_client.api.fby_api import FbyApi
+from yandex_api_client.api.feed_categories_api import FeedCategoriesApi
+from yandex_api_client.api.feedbacks_api import FeedbacksApi
+from yandex_api_client.api.feeds_api import FeedsApi
+from yandex_api_client.api.goods_stats_api import GoodsStatsApi
+from yandex_api_client.api.hidden_offers_api import HiddenOffersApi
+from yandex_api_client.api.models_api import ModelsApi
+from yandex_api_client.api.offer_mappings_api import OfferMappingsApi
+from yandex_api_client.api.offers_api import OffersApi
+from yandex_api_client.api.order_business_information_api import OrderBusinessInformationApi
+from yandex_api_client.api.order_delivery_api import OrderDeliveryApi
+from yandex_api_client.api.order_labels_api import OrderLabelsApi
+from yandex_api_client.api.orders_api import OrdersApi
+from yandex_api_client.api.orders_stats_api import OrdersStatsApi
+from yandex_api_client.api.outlet_licenses_api import OutletLicensesApi
+from yandex_api_client.api.outlets_api import OutletsApi
+from yandex_api_client.api.price_quarantine_api import PriceQuarantineApi
+from yandex_api_client.api.prices_api import PricesApi
+from yandex_api_client.api.regions_api import RegionsApi
+from yandex_api_client.api.reports_api import ReportsApi
+from yandex_api_client.api.returns_api import ReturnsApi
+from yandex_api_client.api.shipments_api import ShipmentsApi
+from yandex_api_client.api.stocks_api import StocksApi
+from yandex_api_client.api.tariffs_api import TariffsApi
+from yandex_api_client.api.warehouses_api import WarehousesApi
+
