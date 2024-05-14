98d6eccbefbbd35551f16804acdfca1729e18505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 15:03:42 +0000

    Add package dfapp-base/0.0.1a0/dfapp-base-0.0.1a0.tar.gz

---
+++ packages/dfapp-base/dfapp-base-0.0.1a0.tar.gz/dfapp-base-0.0.1a0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dfapp-base',
+    version='0.0.1a',
+    packages=find_packages(),
+    description='Create, Curate & Clean Datasets for Large Language Models.',
+    author='Dataformer AI',
+    author_email='contact@dataformer.ai',
+    url='https://github.com/BhabhaAI/dataformer-app',
+    install_requires=[
+        # Add dependencies here
+    ],
+)
