670cc6e01c0a13016efd3f183954e866a294e4cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 15:03:29 +0000

    Add package cancer-simulation/0.7.4/cancer_simulation-0.7.4-py3-none-any.whl

---
+++ packages/cancer-simulation/cancer_simulation-0.7.4-py3-none-any.whl/src/constants.py
@@ -0,0 +1,12 @@
+"""global constants"""
+
+SCREEN_HEIGHT = 900
+SCREEN_WIDTH = 1600
+
+BUTTON_HEIGHT = 100
+BUTTON_WIDTH = 100
+BUTTON_ADD = "./assets/icons8-add-100.png"
+BUTTON_START = "./assets/icons8-watermelon-100.png"
+BUTTON_STOP = "./assets/icons8-cancel-138.png"
+
+BLOCK_SIZE_DIVISIBLE = 500
