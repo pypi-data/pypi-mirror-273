1dbf04990e0dae2bdc22d3f50904d4150b973c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 04:03:35 +0000

    Add package exponentation/0.0.6/exponentation-0.0.6.tar.gz

---
+++ packages/exponentation/exponentation-0.0.6.tar.gz/exponentation-0.0.6/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='exponentation',
+  version='0.0.6',
+  author='katya_r',
+  author_email='katerina.riabova@gmail.com',
+  description='This is the simplest module for quick work with files.',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/katya_r/exponentation',
+  packages=find_packages(),
+  install_requires=[
+        'pandas',
+        'torch',
+        'numpy'
+  ],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='files speedfiles ',
+  project_urls={
+    'GitHub': 'https://github.com/your_username/your_project'
+  },
+  python_requires='>=3.6',
+  include_package_data=True,
+  package_data={'': ['data/*.csv'], '': ['data/*.pth']},
+)
