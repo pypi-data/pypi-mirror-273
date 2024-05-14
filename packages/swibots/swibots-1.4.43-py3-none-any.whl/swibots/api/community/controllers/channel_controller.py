597a0cd60087ca5adaa10bc821a6e000c1cac013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:03:28 +0000

    Add package swibots/1.4.43/swibots-1.4.43-py3-none-any.whl

---
+++ packages/swibots/swibots-1.4.43-py3-none-any.whl/swibots/api/community/controllers/channel_controller.py
@@ -0,0 +1,34 @@
+import logging
+from typing import TYPE_CHECKING, List
+
+from swibots.api.community.models import Channel
+
+
+if TYPE_CHECKING:
+    from swibots.api.community import CommunityClient
+
+log = logging.getLogger(__name__)
+
+BASE_PATH = "/v1/community/channel"
+
+
+class ChannelController:
+    def __init__(self, client: "CommunityClient"):
+        self.client = client
+
+    async def get_channel(self, channel_id: str):
+        """Get a channel by id"""
+        response = await self.client.get(f"{BASE_PATH}?channelId={channel_id}")
+        return self.client.build_object(Channel, response.data.get("result"))
+
+    async def create_channel(self, channel: Channel) -> str:
+        response = await self.client.post(BASE_PATH, data=channel.to_json_request())
+        return response.data.get("result", {}).get("channelId")
+
+    async def update_channel(self, channel: Channel) -> str:
+        response = await self.client.put(BASE_PATH, data=channel.to_json_request())
+        return response.data.get("result", {}).get("channelId")
+
+    async def get_all_channels(self, community_id: str) -> List[Channel]:
+        response = await self.client.get(f"{BASE_PATH}/all?communityId={community_id}")
+        return self.client.build_list(Channel, response.data.get("result"))
