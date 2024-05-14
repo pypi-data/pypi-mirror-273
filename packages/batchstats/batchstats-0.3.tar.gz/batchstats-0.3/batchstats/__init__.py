62d51e7f93adcb77eed03db6a1b281fa9dc9c821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 07:03:19 +0000

    Add package batchstats/0.3/batchstats-0.3.tar.gz

---
+++ packages/batchstats/batchstats-0.3.tar.gz/batchstats-0.3/batchstats/__init__.py
@@ -0,0 +1,7 @@
+from .nanstats import BatchNanMean, BatchNanStat, BatchNanSum
+from .stats import BatchCov, BatchMax, BatchMean, BatchMin, BatchStat, BatchSum, BatchVar
+
+__all__ = ['BatchCov', 'BatchMax', 'BatchMean', 'BatchMin', 'BatchStat', 'BatchSum', 'BatchVar',
+           'BatchNanMean', 'BatchNanStat', 'BatchNanSum']
+
+__version__ = '0.3'
