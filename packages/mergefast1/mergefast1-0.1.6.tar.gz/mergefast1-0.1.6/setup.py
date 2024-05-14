edc815f2672435110625918c628b9075f97c0ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:04:12 +0000

    Add package mergefast1/0.1.6/mergefast1-0.1.6.tar.gz

---
+++ packages/mergefast1/mergefast1-0.1.6.tar.gz/mergefast1-0.1.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mergefast1',
+    version='0.1.6',
+    py_modules=['mergefast'],
+    packages=find_packages(),
+    include_dirs=['mergefast'],
+    
+)
