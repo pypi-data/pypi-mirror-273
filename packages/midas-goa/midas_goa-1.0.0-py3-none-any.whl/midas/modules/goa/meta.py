9ae0f04b7de225ad520d65014f41b1f8c89ab9fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:03:58 +0000

    Add package midas-goa/1.0.0/midas_goa-1.0.0-py3-none-any.whl

---
+++ packages/midas-goa/midas_goa-1.0.0-py3-none-any.whl/midas/modules/goa/meta.py
@@ -0,0 +1,25 @@
+"""This module contains the mosaik_api simulator meta for the
+grid operator simulator.
+"""
+
+
+META = {
+    "type": "time-based",
+    "models": {
+        "GOA": {
+            "public": True,
+            "params": ["params"],
+            "any_inputs": True,
+            "attrs": [
+                "inbox",  # send messages to GOA
+                "outbox",  # receive messages from GOA
+                "health",  # Grid state
+                "error",  # Miss-estimation of the GOA
+                "max_overvoltage",
+                "min_undervoltage",
+                "num_voltage_violations",
+                "grid",
+            ],
+        }
+    },
+}
