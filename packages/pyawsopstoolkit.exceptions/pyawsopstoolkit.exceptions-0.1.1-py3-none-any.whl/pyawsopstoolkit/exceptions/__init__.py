f71e43d8e4629dc152aeb5c43a44260844b65d2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 16:04:00 +0000

    Add package pyawsopstoolkit.exceptions/0.1.1/pyawsopstoolkit.exceptions-0.1.1-py3-none-any.whl

---
+++ packages/pyawsopstoolkit.exceptions/pyawsopstoolkit.exceptions-0.1.1-py3-none-any.whl/pyawsopstoolkit/exceptions/__init__.py
@@ -0,0 +1,14 @@
+__all__ = [
+    "AssumeRoleError",
+    "ValidationError"
+]
+__name__ = "pyawsopstoolkit.exceptions"
+__description__ = """
+This package offers a collection of exception classes tailored for handling errors within the AWS Ops Toolkit.
+These exceptions are specifically designed to address different scenarios and errors that may occur during the
+execution of pyawsopstoolkit operations, providing comprehensive support for error handling and debugging within
+the toolkit.
+"""
+__version__ = "0.1.1"
+
+from pyawsopstoolkit.exceptions.__main__ import AssumeRoleError, ValidationError
