036af1c70b669371d28c3d658dbf1d42b47d7d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 05:04:18 +0000

    Add package pymobiledevice3/4.5.5/pymobiledevice3-4.5.5.tar.gz

---
+++ packages/pymobiledevice3/pymobiledevice3-4.5.5.tar.gz/pymobiledevice3-4.5.5/tests/services/test_springboard_services_relay.py
@@ -0,0 +1,13 @@
+from pymobiledevice3.services.springboard import SpringBoardServicesService
+
+PNG_HEADER = b'\x89\x50\x4e\x47\x0d\x0a\x1a\x0a'
+
+
+def test_get_icon_png_data(lockdown):
+    """
+    Test that getting icon's data returns a valid PNG.
+    :param pymobiledevice3.lockdown.LockdownClient lockdown: Lockdown client.
+    """
+    with SpringBoardServicesService(lockdown) as springboard:
+        icon_data = springboard.get_icon_pngdata("com.apple.weather")
+        assert icon_data.startswith(PNG_HEADER)
