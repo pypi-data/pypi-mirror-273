c375348fb4d6af113fdea2c59d74aa81bf5433c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 14:03:39 +0000

    Add package brdocs-validation/0.4.0/brdocs_validation-0.4.0-py3-none-any.whl

---
+++ packages/brdocs-validation/brdocs_validation-0.4.0-py3-none-any.whl/br_docs/__init__.py
@@ -0,0 +1,23 @@
+from pydantic.functional_validators import AfterValidator
+from typing_extensions import Annotated
+
+from br_docs.validators.cert import CERTv
+from br_docs.validators.cnh import CNHv
+from br_docs.validators.cnpj import CNPJv
+from br_docs.validators.cns import CNSv
+from br_docs.validators.cpf import CPFv
+from br_docs.validators.nis import NISv
+from br_docs.validators.renavam import RENAVAMv
+from br_docs.validators.te import TEv
+from br_docs.validators.sei import SEIv
+
+
+CPF = Annotated[str, AfterValidator(CPFv())]
+CNPJ = Annotated[str, AfterValidator(CNPJv())]
+CNH = Annotated[str, AfterValidator(CNHv())]
+NIS = Annotated[str, AfterValidator(NISv())]
+CNS = Annotated[str, AfterValidator(CNSv())]
+RENAVAM = Annotated[str, AfterValidator(RENAVAMv())]
+TE = Annotated[str, AfterValidator(TEv())]
+CERT = Annotated[str, AfterValidator(CERTv())]
+SEI = Annotated[str, AfterValidator(SEIv())]
---
+++ packages/brdocs-validation/brdocs_validation-0.4.0-py3-none-any.whl/br_docs/validators/cnh.py
@@ -0,0 +1,38 @@
+import re
+
+from br_docs.validators import CheckDigits
+
+
+class CNHv(CheckDigits):
+    Patterns = re.compile(r"^\d{11}$"),
+    CHECK_DIGITS = 2
+
+    @classmethod
+    def calculate_digits(cls, non_digits: list[int]) -> tuple[int, int]:
+        """ https://web.archive.org/web/20240223041941/https://siga0984.wordpress.com/2019/05/01/algoritmos-validacao-de-cnh/ """
+        largest_multipler, smallest_multiplier = 9, 1
+        digit_one, digit_two = 0, 0
+        is_digit_one_greater_than_nine = False
+
+        for nL in non_digits:
+            digit_one += nL * largest_multipler
+            digit_two += nL * smallest_multiplier
+            largest_multipler -= 1
+            smallest_multiplier += 1
+
+        digit_one %= 11
+        if digit_one > 9:
+            digit_one = 0
+            is_digit_one_greater_than_nine = True
+
+        digit_two %= 11
+        if is_digit_one_greater_than_nine:
+            if digit_two - 2 < 0:
+                digit_two += 9
+            elif digit_two - 2 >= 0:
+                digit_two -= 2
+
+        if digit_two > 9:
+            digit_two = 0
+
+        return digit_one, digit_two
