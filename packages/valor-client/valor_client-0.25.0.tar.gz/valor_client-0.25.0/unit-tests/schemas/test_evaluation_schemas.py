ab3cb2a734a2d03e5f713cb03f4f6a968898254a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 05:05:21 +0000

    Add package valor-client/0.25.0/valor_client-0.25.0.tar.gz

---
+++ packages/valor-client/valor_client-0.25.0.tar.gz/valor_client-0.25.0/unit-tests/schemas/test_evaluation_schemas.py
@@ -0,0 +1,16 @@
+from valor import enums, schemas
+
+
+def test_evaluation_request():
+    params = {
+        "model_names": ["md"],
+        "datum_filter": {
+            "dataset_names": ["ds"],
+        },
+        "parameters": {
+            "task_type": enums.TaskType.OBJECT_DETECTION.value,
+            "convert_annotations_to_type": enums.AnnotationType.BOX.value,
+        },
+        "meta": {},
+    }
+    schemas.EvaluationRequest(**params)
