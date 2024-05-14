8ce5ba7302f08894e5d4862a503d6d2a3efd43e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 14:03:27 +0000

    Add package gitops/0.10.6/gitops-0.10.6.tar.gz

---
+++ packages/gitops/gitops-0.10.6.tar.gz/gitops-0.10.6/gitops/main.py
@@ -0,0 +1,19 @@
+import pkg_resources
+from invoke import Collection, Program, Task
+
+from . import core, db, shorthands
+
+version = pkg_resources.require("gitops")[0].version
+
+namespace = Collection()
+
+# Load up some of our functions into the root namespace.
+for core_ns in [core, shorthands]:
+    tasks = filter(lambda x: isinstance(x, Task), vars(core_ns).values())
+    for task in tasks:
+        namespace.add_task(task)
+
+# Namespace the rarer ones.
+namespace.add_collection(db)
+
+program = Program(namespace=namespace, version=version)
