0751a5b0080280345974686fc7edd25902f24e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:04:21 +0000

    Add package vulp/2.3.0/vulp-2.3.0.tar.gz

---
+++ packages/vulp/vulp-2.3.0.tar.gz/vulp-2.3.0/vulp/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2023 Inria
+
+"""Real-time motion control for Python."""
+
+__version__ = "2.3.0"
