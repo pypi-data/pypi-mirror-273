9d71f29d483fd5a235097c079fa561ba9d85f230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 06:03:13 +0000

    Add package t0/4.0.9rc0/t0-4.0.9rc0.tar.gz

---
+++ packages/t0/t0-4.0.9rc0.tar.gz/t0-4.0.9rc0/setup_dependencies.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+dependencies = {
+    't0': {
+        'packages': ['T0+', 
+                     'T0Component+'],
+        'modules': [],
+        'systems': [],
+        'statics': []
+    }
+}
---
+++ packages/t0/t0-4.0.9rc0.tar.gz/t0-4.0.9rc0/src/python/T0/__init__.py
@@ -0,0 +1,8 @@
+"""
+_T0_
+
+Core libraries for Workload Management Packages
+
+"""
+__version__ = '4.0.9.pre'
+__all__ = []
