743237e26c24c1bda535f7e22c2aadc61f81c305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:02:57 +0000

    Add package fastapiwrapper/0.0.8/fastapiwrapper-0.0.8.tar.gz

---
+++ packages/fastapiwrapper/fastapiwrapper-0.0.8.tar.gz/fastapiwrapper-0.0.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.8'
+DESCRIPTION = 'Wrap an api that can fitch Deftable and timeseries table to make it more convenient. '
+LONG_DESCRIPTION = 'contain an api wrapper that helps to fetch gas cconsumption data.'
+
+# Setting up
+setup(
+    name="fastapiwrapper",
+    version=VERSION,
+    author="Ahmad Riad",
+    author_email="meuralengine@outlook.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description= LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pandas','requests'],
+    keywords=['python', 'Deftable', 'timeseries'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
