c15937af8d83a3ab53164ba59645c086a0480353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 05:04:10 +0000

    Add package tictactoewaas/0.1/tictactoewaas-0.1.tar.gz

---
+++ packages/tictactoewaas/tictactoewaas-0.1.tar.gz/tictactoewaas-0.1/README.md
@@ -0,0 +1,3 @@
+# Tic-Tac-Toe
+
+This is a simple implementation of the Tic-Tac-Toe game using Python.
---
+++ packages/tictactoewaas/tictactoewaas-0.1.tar.gz/tictactoewaas-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tictactoewaas',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # 11
+    ],
+    author='tiv',
+    description='game Taic-Tac-Toe',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Fauzian08/Tic-Tac-Toe-tic',  # gg
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
