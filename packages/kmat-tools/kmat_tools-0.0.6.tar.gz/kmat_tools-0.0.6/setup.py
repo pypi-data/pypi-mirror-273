e1e5093f5e5be97ef3eba3b0e86326f08538e2a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:03:59 +0000

    Add package kmat-tools/0.0.6/kmat_tools-0.0.6.tar.gz

---
+++ packages/kmat-tools/kmat_tools-0.0.6.tar.gz/kmat_tools-0.0.6/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup,Extension
+from Cython.Build import cythonize
+
+EXTENSION_PATH = "kmtools_cython"
+
+ext1 = Extension("km_basic_filter",
+                 sources=[f"{EXTENSION_PATH}/km_basic_filter.pyx", f"{EXTENSION_PATH}/lib/src/km_basic_filter.c"],
+                 include_dirs=[f"{EXTENSION_PATH}/lib/include"],
+                 extra_compile_args=['-Wall', '-Wno-unused-function', '-O3', '-pedantic'],
+                 libraries=['z', 'm'])
+
+ext2 = Extension("km_fasta",
+                 sources=[f"{EXTENSION_PATH}/km_fasta.pyx", f"{EXTENSION_PATH}/lib/src/km_fasta.c"],
+                 include_dirs=[f"{EXTENSION_PATH}/lib/include"],
+                 extra_compile_args=['-Wall', '-Wno-unused-function', '-O3', '-pedantic'],
+                 libraries=['z', 'm'])
+
+ext3 = Extension("km_unitig",
+                 sources=[f"{EXTENSION_PATH}/km_unitig.pyx", f"{EXTENSION_PATH}/lib/src/km_unitig.c"],
+                 include_dirs=[f"{EXTENSION_PATH}/lib/include"],
+                 extra_compile_args=['-Wall', '-Wno-unused-function', '-O3', '-pedantic'],
+                 libraries=['z', 'm'])
+
+ext4 = Extension("km_reset",
+                 sources=[f"{EXTENSION_PATH}/km_reset.pyx", f"{EXTENSION_PATH}/lib/src/km_reset.c"],
+                 include_dirs=[f"{EXTENSION_PATH}/lib/include"],
+                 extra_compile_args=['-Wall', '-Wno-unused-function', '-O3', '-pedantic'],
+                 libraries=['z', 'm'])
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="kmat_tools",
+    version="0.0.6",
+    author="Riccardo Vicedomini, Francesco Andreace, Camila Duitama",
+    author_email="cduitama@pasteur.fr",
+    include_package_data=True,
+    package_data={"": ["MANIFEST", "LICENSE"]},
+    ext_modules = cythonize([ext1,ext2,ext3,ext4]),
+    entry_points={
+        "console_scripts": [
+            "kmat_tools = kmat_tools.__main__:main",
+            'km_basic_filter = kmat_tools.kmtools_cython.km_tools_wrappers.km_basic_filter_wrapper:km_basic_filter_main',
+            'km_fasta = kmat_tools.kmtools_cython.km_tools_wrappers.km_fasta_wrapper:km_fasta_main',
+            'km_unitig = kmat_tools.kmtools_cython.km_tools_wrappers.km_unitig_wrapper:km_unitig_main'
+       ]
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CamilaDuitama/kmat_tools",
+    project_urls={
+        "Bug Tracker": "https://github.com/CamilaDuitama/kmat_tools/issues",
+    },
+        classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
