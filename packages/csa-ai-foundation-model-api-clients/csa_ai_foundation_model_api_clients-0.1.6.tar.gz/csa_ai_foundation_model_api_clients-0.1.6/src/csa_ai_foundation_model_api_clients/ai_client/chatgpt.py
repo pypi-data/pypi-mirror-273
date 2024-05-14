0d0a1e282efb608f00ffc6748d7f7cef46bb078b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 06:03:25 +0000

    Add package csa-ai-foundation-model-api-clients/0.1.6/csa_ai_foundation_model_api_clients-0.1.6.tar.gz

---
+++ packages/csa-ai-foundation-model-api-clients/csa_ai_foundation_model_api_clients-0.1.6.tar.gz/csa_ai_foundation_model_api_clients-0.1.6/src/csa_ai_foundation_model_api_clients/ai_client/chatgpt.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+import openai
+import datetime
+
+def generate_response(model_name, api_key, system_prompt, user_prompt, **kwargs):
+    TIME_START = datetime.datetime.now().isoformat()
+
+    openai.api_key = api_key
+
+    temperature = kwargs.get('temperature', 1)
+    max_tokens = kwargs.get('max_tokens', 4096)
+
+    completion = openai.chat.completions.create(
+        model=model_name,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ]
+    )
+
+    TIME_FINISHED = datetime.datetime.now().isoformat()
+
+    time_start = datetime.datetime.fromisoformat(TIME_START)
+    time_complete = datetime.datetime.fromisoformat(TIME_FINISHED)
+
+    duration = time_complete - time_start
+    TIME_TO_RUN = duration.total_seconds()
+
+    try:
+        tokens_input = completion.usage.prompt_tokens
+        tokens_output = completion.usage.completion_tokens
+        total_tokens = completion.usage.total_tokens
+    except AttributeError:
+        tokens_input = tokens_output = total_tokens = None
+    
+    serialized_completion = {
+        "id": getattr(completion, 'id', None),
+        "model": getattr(completion, 'model', None),
+        "created": getattr(completion, 'created', None),
+        "system_fingerprint": getattr(completion, 'system_fingerprint', None),
+        "choices": [
+            {
+                "finish_reason": choice.finish_reason,
+                "index": choice.index,
+                "message": {
+                    "content": getattr(choice.message, 'content', None),
+                    "role": getattr(choice.message, 'role', None)
+                }
+            } for choice in getattr(completion, 'choices', [])
+        ] if hasattr(completion, 'choices') else [],
+        "usage": {
+            "prompt_tokens": tokens_input,
+            "completion_tokens": tokens_output,
+            "total_tokens": total_tokens
+        }
+    }
+    
+    try:
+        response_message = completion.choices[0].message.content
+        status = "success"
+    except AttributeError:
+        response_message = None
+        status = "error"
+
+    ai_output = {
+        "$id": "csa-ai-toolkit-openai-chatgpt4-JSON-v1_00",
+        "metadata": {
+            "system": system_prompt,
+            "user-prompt": user_prompt,
+            "user-data": kwargs.get('user_data'),
+            "output": kwargs.get('output_file'),                        
+            "model_name": model_name,
+            "temperature": temperature,
+            "max_tokens": max_tokens,
+            "tokens_input": tokens_input,
+            "tokens_output": tokens_output,
+            "tokens_total": total_tokens,
+            "time_start": TIME_START,
+            "time_complete": TIME_FINISHED,
+            "time_to_run": TIME_TO_RUN
+        },
+        "extracted_data": response_message,
+        "completion": serialized_completion
+    }
+
+    api_response = {
+        "status": "success",
+        "ai_query_time": TIME_START,
+        "ai_response_time": TIME_FINISHED,
+        "ai_runtime": TIME_TO_RUN,
+        "tokens_input": tokens_input,
+        "tokens_output": tokens_output,
+        "tokens_total": total_tokens,
+        "ai_response_http_status_code": None,
+        "ai_response_stop_reason": None,
+        "ai_response_data": response_message
+    }
+
+
+    return api_response
