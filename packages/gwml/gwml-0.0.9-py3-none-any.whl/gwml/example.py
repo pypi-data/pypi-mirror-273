dd899b765548750baea35ba5f2e79ed67ffb7896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 16:05:31 +0000

    Add package gwml/0.0.9/gwml-0.0.9-py3-none-any.whl

---
+++ packages/gwml/gwml-0.0.9-py3-none-any.whl/gwml/example.py
@@ -0,0 +1,7 @@
+"""판다스 칼럼 조회하기"""
+
+import pandas as pd
+
+
+def getColumn(df, columnName):
+    return df.loc[:, [columnName]]
---
+++ packages/gwml/gwml-0.0.9-py3-none-any.whl/gwml/lib/example.py
@@ -0,0 +1,11 @@
+"""판다스 칼럼 조회하기"""
+
+import pandas as pd
+
+
+def getColumn(df, columnName):
+    return df.loc[:, [columnName]]
+
+
+def show():
+    print("!!!!!!!!!!!!!!")
