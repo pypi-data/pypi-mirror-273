6d74f1fb60c47cdeb1ad9396f73549c584ab28c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 15:03:40 +0000

    Add package scholarsync/0.0.1/scholarsync-0.0.1.tar.gz

---
+++ packages/scholarsync/scholarsync-0.0.1.tar.gz/scholarsync-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+setup(
+    name='scholarsync',
+    version='0.0.1',
+    packages=find_packages(),
+    author='AA',
+    author_email='idonthaveemail@mail.com',
+    description='The Literature mining and processing utility',
+    long_description=open('README.md').read(),
+    package_data={'scholarsync': ['data/*']},
+  # include_package_data=True,  # Include all package data
+
+    install_requires=[
+        'biopython',
+        'requests',
+        'beautifulsoup4',
+        'PyPDF2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'scholarsync = scholarsync.aio:main',
+        ],
+    },
+)
