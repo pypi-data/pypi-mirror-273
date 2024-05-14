0fa8f617fece7b64284440eed900fdb8e6235289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 04:04:08 +0000

    Add package rust-python-jaeger-reporter/0.1.23/rust_python_jaeger_reporter-0.1.23-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/rust-python-jaeger-reporter/rust_python_jaeger_reporter-0.1.23-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/rust_python_jaeger_reporter/__init__.py
@@ -0,0 +1,5 @@
+from .rust_python_jaeger_reporter import *
+
+__doc__ = rust_python_jaeger_reporter.__doc__
+if hasattr(rust_python_jaeger_reporter, "__all__"):
+    __all__ = rust_python_jaeger_reporter.__all__
