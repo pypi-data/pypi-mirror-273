2a8bfb170834ea4b7aee83cdfab55068e9583cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:03:04 +0000

    Add package Data-Checkmate/1.5/Data-Checkmate-1.5.tar.gz

---
+++ packages/Data-Checkmate/Data-Checkmate-1.5.tar.gz/Data-Checkmate-1.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# Reading the README.md file for long description in PyPI site
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='Data-Checkmate',
+    version='1.5',
+    author='Ketan Kirange',
+    author_email='k.kirange@reply.com',
+    description='A library for data quality validation using PyDeequ and to send email notification.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'pydeequ',
+        'boto3',
+        'pyyaml',  
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    keywords='data quality validation pydeequ data-checkmate',
+)
