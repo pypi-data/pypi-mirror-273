d70dc773147c92be32f7756e2eaab8c50a61c1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:03:45 +0000

    Add package paradi/0.1.0/paradi-0.1.0.tar.gz

---
+++ packages/paradi/paradi-0.1.0.tar.gz/paradi-0.1.0/README.md
@@ -0,0 +1,4 @@
+# Paradi
+### Python Abstract Restful Api Dialoger Interface
+
+This is an abstract class which purpose is to interface any Rest API using python's standard functionnalities.
---
+++ packages/paradi/paradi-0.1.0.tar.gz/paradi-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='paradi',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.31.0"
+    ],
+    python_requires='>=3.10',
+    author='Julien Crambes',
+    author_email='julien.crambes@gmail.com',
+)
