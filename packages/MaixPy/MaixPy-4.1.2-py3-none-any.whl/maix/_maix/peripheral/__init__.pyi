c4364042f73507413b2ad6a5ac4a4c54ea2b5249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 15:03:38 +0000

    Add package MaixPy/4.1.2/MaixPy-4.1.2-py3-none-any.whl

---
+++ packages/MaixPy/MaixPy-4.1.2-py3-none-any.whl/maix/_maix/peripheral/__init__.pyi
@@ -0,0 +1,14 @@
+"""
+Chip's peripheral driver
+"""
+from __future__ import annotations
+from . import adc
+from . import gpio
+from . import i2c
+from . import key
+from . import pwm
+from . import spi
+from . import timer
+from . import uart
+from . import wdt
+__all__ = ['adc', 'gpio', 'i2c', 'key', 'pwm', 'spi', 'timer', 'uart', 'wdt']
