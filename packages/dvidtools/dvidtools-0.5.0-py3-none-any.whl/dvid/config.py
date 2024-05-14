8f272bb7f5fc2a9f307a9295fae2ef0de49c2faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 16:04:57 +0000

    Add package dvidtools/0.5.0/dvidtools-0.5.0-py3-none-any.whl

---
+++ packages/dvidtools/dvidtools-0.5.0-py3-none-any.whl/dvid/config.py
@@ -0,0 +1,7 @@
+# This code is part of dvid-tools (https://github.com/flyconnectome/dvid_tools)
+# and is released under GNU GPL3
+
+# Change this according to your DVID server
+segmentation = 'segmentation'
+body_labels = 'segmentation'
+synapses = 'synapses'
