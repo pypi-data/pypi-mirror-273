0893023167cdd270fe5717d7132904b7124ace50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 14:03:34 +0000

    Add package otpcr/16/otpcr-16.tar.gz

---
+++ packages/otpcr/otpcr-16.tar.gz/otpcr-16/otpcr/modules/__init__.py
@@ -0,0 +1,30 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=W0406
+
+
+"modules"
+
+
+from . import cmd, err, mod, thr, irc, log, req, rst, rss, slg, tdo, tmr, udp
+
+
+def __dir__():
+    return (
+        'cmd',
+        'err',
+        'irc',
+        'log',
+        'mod',
+        'req',
+        'rss',
+        'rst',
+        'slg',
+        'tdo',
+        'thr',
+        'tmr',
+        'udp'
+    )
+
+
+__all__ = __dir__()
