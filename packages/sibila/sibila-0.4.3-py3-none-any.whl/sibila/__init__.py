ff5ec639037099ad26edb1c7717624056fd0373c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 04:03:06 +0000

    Add package sibila/0.4.3/sibila-0.4.3-py3-none-any.whl

---
+++ packages/sibila/sibila-0.4.3-py3-none-any.whl/sibila/__init__.py
@@ -0,0 +1,78 @@
+"""Structured data from local or remote LLM models."""
+
+__version__ = "0.4.3"
+
+__all__ = [
+    "Models",
+    "Model", "TextModel", "MessagesModel", "Tokenizer",
+    "GenConf", "GenRes", "GenError", "GenOut",
+    "AnthropicModel",
+    "FireworksModel",
+    "GroqModel",
+    "LlamaCppModel", "LlamaCppTokenizer",
+    "MistralModel",
+    "OpenAIModel", "OpenAITokenizer",
+    "TogetherModel",
+    "Thread", "MsgKind",
+    "Context", "Trim",
+    "JSchemaConf",
+    "TDesc"
+]
+
+__author__ = "Jorge Diogo"
+
+
+from .gen import (
+    GenConf,
+    GenRes,
+    GenError,
+    GenOut
+)
+
+from .thread import (
+    Thread,
+    MsgKind,
+)
+
+from .model import (
+    Model,
+    TextModel,
+    FormattedTextModel,
+    MessagesModel,
+    Tokenizer
+)
+
+
+from .anthropic import AnthropicModel
+
+from .llamacpp import (
+    LlamaCppModel,
+    LlamaCppTokenizer
+)
+
+from .mistral import MistralModel
+
+from .openai import (
+    OpenAIModel,
+    OpenAITokenizer
+)
+
+from .schema_format_openai import (
+    FireworksModel,
+    GroqModel,
+    TogetherModel
+)
+
+
+from .context import (
+    Context,
+    Trim
+)
+
+from .models import Models
+
+from .json_schema import JSchemaConf
+
+""" A shorter 'Annotated', meant to be used as: TDesc[type, "description"] """
+from typing import Annotated
+TDesc = Annotated
