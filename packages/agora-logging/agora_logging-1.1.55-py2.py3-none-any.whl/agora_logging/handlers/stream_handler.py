71dc1d45f7fd1d10894301576b58ab926a362e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 15:03:30 +0000

    Add package agora-logging/1.1.55/agora_logging-1.1.55-py2.py3-none-any.whl

---
+++ packages/agora-logging/agora_logging-1.1.55-py2.py3-none-any.whl/agora_logging/handlers/stream_handler.py
@@ -0,0 +1,31 @@
+import sys
+
+from datetime import datetime
+from .base_handler import  BaseHandler
+from ..version import __version__
+
+class StreamHandler(BaseHandler):
+    '''
+    Handles writing to streams
+    '''
+    def __init__(self,level):
+        super().__init__()
+        self.level=level
+        self.start_logging()
+
+    def start_logging(self):
+        '''
+        Begin logging with timestamp
+        '''
+        now = datetime.now()
+        timestamp=now.strftime("%m/%d/%Y, %H:%M:%S")
+        log_text=f"""---Log Start--- AEA Python-SDK {__version__} --- {timestamp}\n"""
+        self.write_to_stream(log_text)
+
+    def heading(self,frameinfo,message):
+        '''
+        Write heading
+        '''
+        data=self.text_to_heading(frameinfo,message)
+        stream=sys.stdout
+        self.write_to_stream(data,stream)
