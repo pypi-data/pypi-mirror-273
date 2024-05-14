adc5026773fc62def3524939f02ac7f2666da5e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 15:03:38 +0000

    Add package pygwalker/0.4.8.4/pygwalker-0.4.8.4-py3-none-any.whl

---
+++ packages/pygwalker/pygwalker-0.4.8.4-py3-none-any.whl/pygwalker/templates/dist/tools/loginTool.d.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+import type { ToolbarButtonItem } from "@kanaries/graphic-walker/components/toolbar/toolbar-button";
+export declare function getLoginTool(setMounted: React.Dispatch<React.SetStateAction<boolean>>, wrapRef: React.MutableRefObject<HTMLElement | null>): ToolbarButtonItem;
