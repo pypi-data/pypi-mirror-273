a40a9eb00884dcdd3f8473535f3f71da7c494786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 06:03:39 +0000

    Add package ItsMagic/0.1/itsmagic-0.1.tar.gz

---
+++ packages/ItsMagic/itsmagic-0.1.tar.gz/itsmagic-0.1/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='ItsMagic',
+    version='0.1',
+    description='Uma biblioteca simples',
+    py_modules=['itsmagic'],
+    author='SkyDev',
+    author_email='skydagringa1@gmail.com',
+    license='MIT',
+)
