6f140d35201cb202f41d96e333644c705e068ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 06:03:38 +0000

    Add package datafog/3.2.0b7/datafog-3.2.0b7.tar.gz

---
+++ packages/datafog/datafog-3.2.0b7.tar.gz/datafog-3.2.0b7/datafog/__init__.py
@@ -0,0 +1,27 @@
+from .main import (
+    DataFog,
+    ImageService,
+    OCRPIIAnnotator,
+    OperationType,
+    SparkService,
+    TextPIIAnnotator,
+    TextService,
+    DonutProcessor,
+    SpacyPIIAnnotator,
+    ImageDownloader,
+    PytesseractProcessor,
+)
+
+__all__ = [
+    "DataFog",
+    "ImageService",
+    "OCRPIIAnnotator",
+    "OperationType",
+    "SparkService",
+    "TextPIIAnnotator",
+    "TextService",
+    "DonutProcessor",
+    "SpacyPIIAnnotator",
+    "ImageDownloader",
+    "PytesseractProcessor",
+]
