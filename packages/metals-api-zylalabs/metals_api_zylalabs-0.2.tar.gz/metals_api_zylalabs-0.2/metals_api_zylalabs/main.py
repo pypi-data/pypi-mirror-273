b572a9d37927c64583012a6f73da1af943708364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 06:03:38 +0000

    Add package metals-api-zylalabs/0.2/metals_api_zylalabs-0.2.tar.gz

---
+++ packages/metals-api-zylalabs/metals_api_zylalabs-0.2.tar.gz/metals_api_zylalabs-0.2/metals_api_zylalabs/main.py
@@ -0,0 +1,21 @@
+import requests
+
+class MetalsAPI:
+    
+    def __init__(self, access_key:str):
+        self.access_key = access_key
+        
+    def get_latest(self, base:str, symbols):
+        URL = "https://metals-api.com/api/latest"        
+        params = {
+            "access_key": self.access_key,
+            "base": base,
+        }
+        if isinstance(symbols, list):
+            params["symbols"] = ",".join(symbols)
+        else:
+            params["symbols"] = symbols
+        
+        response = requests.get(URL, params=params)        
+        data = response.json()        
+        return data
---
+++ packages/metals-api-zylalabs/metals_api_zylalabs-0.2.tar.gz/metals_api_zylalabs-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md","r") as f:
+    description = f.read()
+
+setup(
+    name='metals_api_zylalabs',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
