500ad3176786f6dfb2af213bcb99e554cc1c7bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 15:03:32 +0000

    Add package daiexperiments/0.0.10/daiexperiments-0.0.10.tar.gz

---
+++ packages/daiexperiments/daiexperiments-0.0.10.tar.gz/daiexperiments-0.0.10/daiexperiments/__init__.py
@@ -0,0 +1,6 @@
+from .main import number_plate_detection
+from .main import genetic_algorithm
+from .main import face_detection
+from .main import three_cross_three_grid
+from .main import three_cross_three_grid_rl
+from .main import magic_square
