9746e3828a415f008e354075c4e675341aed7f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:03:02 +0000

    Add package odoo14-addon-ssi-voucher-advance-settlement/14.0.2.3.0/odoo14_addon_ssi_voucher_advance_settlement-14.0.2.3.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-voucher-advance-settlement/odoo14_addon_ssi_voucher_advance_settlement-14.0.2.3.0-py3-none-any.whl/odoo/addons/ssi_voucher_advance_settlement/__manifest__.py
@@ -0,0 +1,44 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Advance Settlement Voucher",
+    "version": "14.0.2.3.0",
+    "category": "Accounting",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_voucher_settlement_common",
+        "ssi_financial_accounting",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir_rule_data.xml",
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "data/account_voucher_type_data.xml",
+        "data/ir_windows_action_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_purchase_settlement_data.xml",
+        "data/sequence_template_sale_settlement_data.xml",
+        "data/approval_template_purchase_settlement_data.xml",
+        "data/approval_template_sale_settlement_data.xml",
+        "data/policy_template_purchase_settlement_data.xml",
+        "data/policy_template_sale_settlement_data.xml",
+        "data/account_journal_data.xml",
+        "data/account_voucher_type_allowed_journal_data.xml",
+        "views/account_sale_advance_settlement_views.xml",
+        "views/account_purchase_advance_settlement_views.xml",
+    ],
+    "demo": [
+        "demo/account_journal_demo.xml",
+        "demo/account_voucher_type_allowed_journal_demo.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
