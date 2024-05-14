ea0b76d535a787ba7c41da0876ac550dceb32b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 04:04:17 +0000

    Add package IvmInstruments/0.2.8/ivminstruments-0.2.8.tar.gz

---
+++ packages/IvmInstruments/ivminstruments-0.2.8.tar.gz/ivminstruments-0.2.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('./README.md', 'r') as file :
+    long_description = file.read()
+AUTHOR_USER_NAME = 'HarishKumarSedu'
+AUTHOR_EMAIL = 'harishkumarsedu@gmail.com'
+REPO_NAME = 'Instruments'
+setup(
+    name=f'Ivm{REPO_NAME}',
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    version='0.2.8',
+    py_modules=['Instruments'],
+    description=[ 'text/markdown','text/x-rst',],
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    long_description=long_description,
+    packages=find_packages(),
+    include_dirs=['Instruments'],
+    
+)
