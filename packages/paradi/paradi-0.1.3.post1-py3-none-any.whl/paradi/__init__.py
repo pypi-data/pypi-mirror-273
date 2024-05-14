afff6c83a7bb0a0772a5f6c93a40d08ab8a2c236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 16:05:02 +0000

    Add package paradi/0.1.3.post1/paradi-0.1.3.post1-py3-none-any.whl

---
+++ packages/paradi/paradi-0.1.3.post1-py3-none-any.whl/paradi/__init__.py
@@ -0,0 +1,11 @@
+"""
+Paradi
+Python Abstract Restful Api Dialoger Interface
+This is an abstract class which purpose is to interface any Rest API using python's standard functionalities.
+"""
+
+__version__ = "0.1.3.post1"
+__author__ = 'Julien Crambes'
+
+from . import logging_config
+from .paradi import Paradi
