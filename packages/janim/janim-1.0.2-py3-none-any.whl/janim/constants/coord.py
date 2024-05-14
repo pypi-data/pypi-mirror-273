b6de14df6b4bcb7a72d860b67355831a7ccfb2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 15:03:36 +0000

    Add package janim/1.0.2/janim-1.0.2-py3-none-any.whl

---
+++ packages/janim/janim-1.0.2-py3-none-any.whl/janim/constants/coord.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+ORIGIN = np.array((0., 0., 0.))
+UP = np.array((0., 1., 0.))
+DOWN = np.array((0., -1., 0.))
+RIGHT = np.array((1., 0., 0.))
+LEFT = np.array((-1., 0., 0.))
+IN = np.array((0., 0., -1.))
+OUT = np.array((0., 0., 1.))
+X_AXIS = np.array((1., 0., 0.))
+Y_AXIS = np.array((0., 1., 0.))
+Z_AXIS = np.array((0., 0., 1.))
+
+NAN_POINT = np.full(3, np.nan)
+
+# Useful abbreviations for diagonals
+UL = UP + LEFT
+UR = UP + RIGHT
+DL = DOWN + LEFT
+DR = DOWN + RIGHT
