02ccd721b313ee392414b1a3045f41f32b0f8673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 05:04:36 +0000

    Add package fenics-beat/0.0.5/fenics_beat-0.0.5-py3-none-any.whl

---
+++ packages/fenics-beat/fenics_beat-0.0.5-py3-none-any.whl/beat/__init__.py
@@ -0,0 +1,29 @@
+from . import (
+    monodomain_model,
+    monodomain_solver,
+    bidomain_model,
+    base_model,
+    cellmodels,
+    odesolver,
+    ecg,
+    utils,
+)
+
+from .monodomain_model import MonodomainModel
+from .bidomain_model import BidomainModel
+from .monodomain_solver import MonodomainSplittingSolver
+
+
+__all__ = [
+    "monodomain_model",
+    "cellmodels",
+    "bidomain_model",
+    "odesolver",
+    "base_model",
+    "MonodomainModel",
+    "BidomainModel",
+    "monodomain_solver",
+    "MonodomainSplittingSolver",
+    "ecg",
+    "utils",
+]
