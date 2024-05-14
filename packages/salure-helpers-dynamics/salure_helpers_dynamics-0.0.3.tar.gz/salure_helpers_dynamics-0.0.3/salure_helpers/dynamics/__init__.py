87b215458f1b30c3c1835b8761ccecd1f50d4831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 04:03:38 +0000

    Add package salure-helpers-dynamics/0.0.3/salure_helpers_dynamics-0.0.3.tar.gz

---
+++ packages/salure-helpers-dynamics/salure_helpers_dynamics-0.0.3.tar.gz/salure_helpers_dynamics-0.0.3/salure_helpers/dynamics/__init__.py
@@ -0,0 +1,1 @@
+from salure_helpers.dynamics.dynamics import Dynamics
---
+++ packages/salure-helpers-dynamics/salure_helpers_dynamics-0.0.3.tar.gz/salure_helpers_dynamics-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='salure_helpers_dynamics',
+    version='0.0.3',
+    description='Datev wrapper from Dynamics365',
+    long_description='Dynamics365 wrapper from Salure',
+    author='D&A Salure',
+    author_email='support@salureconnnect.com',
+    packages=["salure_helpers.dynamics"],
+    license='Salure License',
+    install_requires=[
+        'salure-helpers-salureconnect>=1',
+    ],
+    zip_safe=False,
+)
