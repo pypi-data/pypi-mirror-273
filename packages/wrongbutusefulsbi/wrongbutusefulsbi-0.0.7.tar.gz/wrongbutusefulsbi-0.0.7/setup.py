81cd1a427677e4327672a43b5a76ba8d8cd2bda8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 15:03:22 +0000

    Add package wrongbutusefulsbi/0.0.7/wrongbutusefulsbi-0.0.7.tar.gz

---
+++ packages/wrongbutusefulsbi/wrongbutusefulsbi-0.0.7.tar.gz/wrongbutusefulsbi-0.0.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+packages = ['wrongbutusefulsbi'] + ['wrongbutusefulsbi.' + p for p in find_packages('wrongbutusefulsbi')]
+
+with open("README.rst", encoding='utf8') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+# read version number
+__version__ = open('wrongbutusefulsbi/__init__.py').readlines()[-1].split(' ')[-1].strip().strip("'\"")
+
+setup(
+    name='wrongbutusefulsbi',
+    version=__version__,
+    packages=packages,
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    author='Ryan Kelly',
+    author_email='ryan@kiiii.com',
+    url='https://github.com/RyanJafefKelly/wrongbutusefulsbi',
+    install_requires=requirements,
+    license='GPLv3',
+    python_requires='>=3.7',
+)
