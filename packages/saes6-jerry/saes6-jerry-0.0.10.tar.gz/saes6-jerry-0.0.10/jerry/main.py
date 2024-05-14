af08baf0389afd38203b9f6124c2a11f181368f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 16:05:32 +0000

    Add package saes6-jerry/0.0.10/saes6-jerry-0.0.10.tar.gz

---
+++ packages/saes6-jerry/saes6-jerry-0.0.10.tar.gz/saes6-jerry-0.0.10/jerry/main.py
@@ -0,0 +1,12 @@
+import click
+
+@click.command()
+@click.argument("name")
+def main(name):
+    print(f'Hello {name}')
+
+
+@click.command()
+@click.argument("aa")
+def hello(aa):
+    print(f'aa {aa}')
