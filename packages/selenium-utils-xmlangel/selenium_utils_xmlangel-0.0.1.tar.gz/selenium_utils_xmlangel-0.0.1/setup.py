769b4e20a2943a81b557334ab4ae064494ce4686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 04:03:15 +0000

    Add package selenium-utils-xmlangel/0.0.1/selenium_utils_xmlangel-0.0.1.tar.gz

---
+++ packages/selenium-utils-xmlangel/selenium_utils_xmlangel-0.0.1.tar.gz/selenium_utils_xmlangel-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='selenium-utils-xmlangel',
+    version='0.0.1',
+    description='PYPI selenium-utils package creation written by xmlangel',
+    author='xmlangel',
+    author_email='kwangmyung.kim@gmail.com',
+    url='https://github.com/xmlangel/selenium-utils-xmlangel',
+    install_requires=['opencv-python', 'scikit-image', ],
+    packages=find_packages(exclude=[]),
+    keywords=['selenium', 'xmlangel', 'utils'],
+    python_requires='>=3.12',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
