b3abea9a862a8b2e0f0dc9c38174645e07d0892b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 03:03:46 +0000

    Add package crutil/2.1.0/crutil-2.1.0-py3-none-any.whl

---
+++ packages/crutil/crutil-2.1.0-py3-none-any.whl/crutil/utils.py
@@ -0,0 +1,50 @@
+"""
+This code defines some general utility functions, used across the project.
+"""
+
+# Standard imports.
+import traceback
+
+# Local constants.
+LUCKY_INT = 17
+TRUE_INT = 1
+FALSE_INT = 0
+
+###########
+# CLASSES #
+###########
+
+class CRUtilTypeMismatch(Exception):
+    """ A custom exception. """
+
+class CRUtilBadBooleanInteger(Exception):
+    """ A custom exception. """
+
+#############
+# FUNCTIONS #
+#############
+
+def refine_func(func, argtypes, restype):
+    """ Assign argument types and a return type to a ctypes function in one
+    line. """
+    func.argtypes = argtypes
+    func.restype = restype
+    return func
+
+def make_contact_template(library):
+    """ Prove that you can access the library in question. """
+    try:
+        func = refine_func(library.make_contact, [], None)
+        library.make_contact(LUCKY_INT)
+    except:
+        traceback.print_exc()
+        return False
+    return True
+
+def int_to_bool(integer):
+    """ Convert an integer representation of a boolean into a boolean. """
+    if integer == TRUE_INT:
+        return True
+    if integer == FALSE_INT:
+        return False
+    raise CRUtilBadBooleanInteger("Bad boolean integer: "+str(integer))
