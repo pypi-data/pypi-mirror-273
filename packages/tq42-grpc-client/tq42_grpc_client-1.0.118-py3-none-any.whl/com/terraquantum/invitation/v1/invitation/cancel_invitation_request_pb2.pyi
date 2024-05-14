40f914ea5d4c951232a14b6a3456124784eb22b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 16:04:02 +0000

    Add package tq42-grpc-client/1.0.118/tq42_grpc_client-1.0.118-py3-none-any.whl

---
+++ packages/tq42-grpc-client/tq42_grpc_client-1.0.118-py3-none-any.whl/com/terraquantum/invitation/v1/invitation/cancel_invitation_request_pb2.pyi
@@ -0,0 +1,13 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class CancelInvitationRequest(_message.Message):
+    __slots__ = ("invitation_id", "request_id")
+    INVITATION_ID_FIELD_NUMBER: _ClassVar[int]
+    REQUEST_ID_FIELD_NUMBER: _ClassVar[int]
+    invitation_id: str
+    request_id: str
+    def __init__(self, invitation_id: _Optional[str] = ..., request_id: _Optional[str] = ...) -> None: ...
