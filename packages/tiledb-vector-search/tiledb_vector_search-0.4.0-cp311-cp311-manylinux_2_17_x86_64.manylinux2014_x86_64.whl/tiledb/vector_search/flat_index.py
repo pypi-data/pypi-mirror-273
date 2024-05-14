aaabc30a78be44e00f8abf7615f064007470cdd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 05:05:25 +0000

    Add package tiledb-vector-search/0.4.0/tiledb_vector_search-0.4.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/tiledb-vector-search/tiledb_vector_search-0.4.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/tiledb/vector_search/flat_index.py
@@ -0,0 +1,216 @@
+from typing import Any, Mapping
+
+import numpy as np
+
+from tiledb.vector_search import index
+from tiledb.vector_search.module import *
+from tiledb.vector_search.storage_formats import STORAGE_VERSION
+from tiledb.vector_search.storage_formats import storage_formats
+from tiledb.vector_search.storage_formats import validate_storage_version
+from tiledb.vector_search.utils import add_to_group
+
+MAX_INT32 = np.iinfo(np.dtype("int32")).max
+MAX_UINT64 = np.iinfo(np.dtype("uint64")).max
+TILE_SIZE_BYTES = 128000000  # 128MB
+INDEX_TYPE = "FLAT"
+
+
+class FlatIndex(index.Index):
+    """
+    Open a flat index
+
+    Parameters
+    ----------
+    uri: str
+        URI of the index
+    config: Optional[Mapping[str, Any]]
+        config dictionary, defaults to None
+    """
+
+    def __init__(
+        self,
+        uri: str,
+        config: Optional[Mapping[str, Any]] = None,
+        timestamp=None,
+        **kwargs,
+    ):
+        self.index_type = INDEX_TYPE
+        super().__init__(uri=uri, config=config, timestamp=timestamp)
+        self._index = None
+        self.db_uri = self.group[
+            storage_formats[self.storage_version]["PARTS_ARRAY_NAME"]
+            + self.index_version
+        ].uri
+        schema = tiledb.ArraySchema.load(self.db_uri, ctx=tiledb.Ctx(self.config))
+        self.dimensions = schema.shape[0]
+        if self.base_size == -1:
+            self.size = schema.domain.dim(1).domain[1] + 1
+        else:
+            self.size = self.base_size
+
+        self.dtype = np.dtype(self.group.meta.get("dtype", None))
+        if (
+            storage_formats[self.storage_version]["IDS_ARRAY_NAME"] + self.index_version
+            in self.group
+        ):
+            self.ids_uri = self.group[
+                storage_formats[self.storage_version]["IDS_ARRAY_NAME"]
+                + self.index_version
+            ].uri
+        else:
+            self.ids_uri = ""
+        if self.size > 0:
+            self._db = load_as_matrix(
+                self.db_uri,
+                ctx=self.ctx,
+                config=config,
+                size=self.size,
+                timestamp=self.base_array_timestamp,
+            )
+            if self.dtype is None:
+                self.dtype = self._db.dtype
+            # Check for existence of ids array. Previous versions were not using external_ids in the ingestion assuming
+            # that the external_ids were the position of the vector in the array.
+            if self.ids_uri == "":
+                self._ids = StdVector_u64(np.arange(self.size).astype(np.uint64))
+            else:
+                self._ids = read_vector_u64(
+                    self.ctx, self.ids_uri, 0, self.size, self.base_array_timestamp
+                )
+
+    def get_dimensions(self):
+        return self.dimensions
+
+    def query_internal(
+        self,
+        queries: np.ndarray,
+        k: int = 10,
+        nthreads: int = 8,
+        **kwargs,
+    ):
+        """
+        Query a flat index
+
+        Parameters
+        ----------
+        queries: numpy.ndarray
+            ND Array of queries
+        k: int
+            Number of top results to return per query
+        nthreads: int
+            Number of threads to use for query
+        """
+        # TODO:
+        # - typecheck queries
+        # - add all the options and query strategies
+        if self.size == 0:
+            return np.full((queries.shape[0], k), index.MAX_FLOAT_32), np.full(
+                (queries.shape[0], k), index.MAX_UINT64
+            )
+
+        assert queries.dtype == np.float32
+
+        queries_m = array_to_matrix(np.transpose(queries))
+        d, i = query_vq_heap(self._db, queries_m, self._ids, k, nthreads)
+
+        return np.transpose(np.array(d)), np.transpose(np.array(i))
+
+
+def create(
+    uri: str,
+    dimensions: int,
+    vector_type: np.dtype,
+    group_exists: bool = False,
+    config: Optional[Mapping[str, Any]] = None,
+    storage_version: str = STORAGE_VERSION,
+    **kwargs,
+) -> FlatIndex:
+    validate_storage_version(storage_version)
+
+    index.create_metadata(
+        uri=uri,
+        dimensions=dimensions,
+        vector_type=vector_type,
+        index_type=INDEX_TYPE,
+        storage_version=storage_version,
+        group_exists=group_exists,
+        config=config,
+    )
+    with tiledb.scope_ctx(ctx_or_config=config):
+        group = tiledb.Group(uri, "w")
+        tile_size = TILE_SIZE_BYTES / np.dtype(vector_type).itemsize / dimensions
+        ids_array_name = storage_formats[storage_version]["IDS_ARRAY_NAME"]
+        parts_array_name = storage_formats[storage_version]["PARTS_ARRAY_NAME"]
+        updates_array_name = storage_formats[storage_version]["UPDATES_ARRAY_NAME"]
+        ids_uri = f"{uri}/{ids_array_name}"
+        parts_uri = f"{uri}/{parts_array_name}"
+        updates_array_uri = f"{uri}/{updates_array_name}"
+
+        ids_array_rows_dim = tiledb.Dim(
+            name="rows",
+            domain=(0, MAX_INT32),
+            tile=tile_size,
+            dtype=np.dtype(np.int32),
+        )
+        ids_array_dom = tiledb.Domain(ids_array_rows_dim)
+        ids_attr = tiledb.Attr(
+            name="values",
+            dtype=np.dtype(np.uint64),
+            filters=storage_formats[storage_version]["DEFAULT_ATTR_FILTERS"],
+        )
+        ids_schema = tiledb.ArraySchema(
+            domain=ids_array_dom,
+            sparse=False,
+            attrs=[ids_attr],
+            cell_order="col-major",
+            tile_order="col-major",
+        )
+        tiledb.Array.create(ids_uri, ids_schema)
+        add_to_group(group, ids_uri, ids_array_name)
+
+        parts_array_rows_dim = tiledb.Dim(
+            name="rows",
+            domain=(0, dimensions - 1),
+            tile=dimensions,
+            dtype=np.dtype(np.int32),
+        )
+        parts_array_cols_dim = tiledb.Dim(
+            name="cols",
+            domain=(0, MAX_INT32),
+            tile=tile_size,
+            dtype=np.dtype(np.int32),
+        )
+        parts_array_dom = tiledb.Domain(parts_array_rows_dim, parts_array_cols_dim)
+        parts_attr = tiledb.Attr(
+            name="values",
+            dtype=vector_type,
+            filters=storage_formats[storage_version]["DEFAULT_ATTR_FILTERS"],
+        )
+        parts_schema = tiledb.ArraySchema(
+            domain=parts_array_dom,
+            sparse=False,
+            attrs=[parts_attr],
+            cell_order="col-major",
+            tile_order="col-major",
+        )
+        tiledb.Array.create(parts_uri, parts_schema)
+        add_to_group(group, parts_uri, parts_array_name)
+
+        external_id_dim = tiledb.Dim(
+            name="external_id",
+            domain=(0, MAX_UINT64 - 1),
+            dtype=np.dtype(np.uint64),
+        )
+        dom = tiledb.Domain(external_id_dim)
+        vector_attr = tiledb.Attr(name="vector", dtype=vector_type, var=True)
+        updates_schema = tiledb.ArraySchema(
+            domain=dom,
+            sparse=True,
+            attrs=[vector_attr],
+            allows_duplicates=False,
+        )
+        tiledb.Array.create(updates_array_uri, updates_schema)
+        add_to_group(group, updates_array_uri, updates_array_name)
+
+        group.close()
+        return FlatIndex(uri=uri, config=config)
