b1f8bf42b395030a335bd8f4fc140c2ee41d5ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 07:03:20 +0000

    Add package my-road/1.0.0/my_road-1.0.0.tar.gz

---
+++ packages/my-road/my_road-1.0.0.tar.gz/my_road-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='my_road',
+    version='1.0.0',
+    description='Look at the road',
+    py_modules=['road'],
+    author='k0ng999',
+    author_email='baydar_14@mail.ru',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+)
