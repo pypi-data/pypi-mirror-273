39fc320bdc1aea4cb54f20652b3a189dc068bc58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 06:03:38 +0000

    Add package ishandaiexperiments/0.0.7/ishandaiexperiments-0.0.7.tar.gz

---
+++ packages/ishandaiexperiments/ishandaiexperiments-0.0.7.tar.gz/ishandaiexperiments-0.0.7/ishandaiexperiments/__init__.py
@@ -0,0 +1,2 @@
+from .main import number_plate_detection
+from .main import genetic_algorithm
---
+++ packages/ishandaiexperiments/ishandaiexperiments-0.0.7.tar.gz/ishandaiexperiments-0.0.7/ishandaiexperiments/main.py
@@ -0,0 +1,104 @@
+def number_plate_detection():
+    code = """
+            !pip install easyocr
+            !pip install imutils
+
+            import cv2
+            import matplotlib.pyplot as plt
+            import numpy as np
+            import easyocr
+            import imutils
+
+            image = cv2.imread("C:/Users/mahna/Downloads/car.png")
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            plt.imshow(cv2.cvtColor(gray,cv2.COLOR_BGR2RGB))
+
+            bfilter = cv2.bilateralFilter(gray, 11, 17, 17) #noise reduction
+            edged = cv2.Canny(bfilter, 30, 200)  # edge detection
+            plt.imshow(cv2.cvtColor(edged, cv2.COLOR_BGR2RGB))
+
+            keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            contours = imutils.grab_contours(keypoints)
+            contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
+
+            location = None
+            for contour in contours:
+                approx = cv2.approxPolyDP(contour, 10, True)
+                counter = 0
+                if len(approx) == 4:
+                        location = approx
+                        counter = counter+1
+                        if(counter == 3):
+                            print(location)
+                            break
+            
+            location
+
+            mask = np.zeros(gray.shape, np.uint8)
+            new_image = cv2.drawContours(mask, [location], 0, 255, -1)
+            new_image = cv2.bitwise_and(image, image, mask = mask)
+
+            plt.imshow(new_image)
+
+            (x,y) = np.where(mask==255)
+            (x1,y1) = (np.min(x), np.min(y))
+            (x2,y2) = (np.max(x), np.max(y))
+            cropped_image = gray[x1:x2+1, y1:y2+1]
+
+            plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
+
+            reader = easyocr.Reader(['en'])
+            result = reader.readtext(cropped_image)
+            result
+
+            result[0][1]
+            """
+    return code
+    
+def genetic_algorithm():
+    code = """
+            import numpy as np
+            num_genes = 10
+            population_size = 50
+            mutation_rate = 0.01
+            num_generations = 15
+            population = np.random.randint(0, 10, size=(population_size, num_genes))
+            print(population[:10])
+
+            def soft_tissue_characterization(chromosome):
+                return np.sum(chromosome)
+            def fitness_function(chromosome):
+                return -soft_tissue_characterization(chromosome)
+            
+            for generation in range(num_generations):
+                fitness_values = np.apply_along_axis(fitness_function, 1, population)
+
+
+                selected_indices = []
+                for _ in range(population_size):
+                    tournament_indices = np.random.choice(range(population_size), size=3, replace=False)
+                    tournament_fitness = fitness_values[tournament_indices]
+                    winner_index = tournament_indices[np.argmax(tournament_fitness)]
+                    selected_indices.append(winner_index)
+                selected_population = population[selected_indices]
+
+
+                crossover_points = np.random.randint(1, num_genes, size=population_size // 2)
+                offspring = np.empty_like(population)
+                for i in range(0, population_size, 2):
+                    parent1, parent2 = selected_population[i], selected_population[i + 1]
+                    crossover_point = crossover_points[i // 2]
+                    offspring[i, :] = np.concatenate((parent1[:crossover_point], parent2[crossover_point:]))
+                    offspring[i + 1, :] = np.concatenate((parent2[:crossover_point], parent1[crossover_point:]))
+
+
+                mutation_mask = np.random.random(size=offspring.shape) < mutation_rate
+                offspring ^= mutation_mask
+
+                population = offspring
+
+                best_fitness = np.max(fitness_values)
+                average_fitness = np.mean(fitness_values)
+                print(f"Generation {generation+1}, Best Fitness: {best_fitness}, Average Fitness: {average_fitness}")
+           """
+    return code
