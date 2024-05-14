1de9b050b598f9048201c430727c7d4d14d1883f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-14 05:05:43 +0000

    Add package newmysci/1.0.0/newmysci-1.0.0.tar.gz

---
+++ packages/newmysci/newmysci-1.0.0.tar.gz/newmysci-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="newmysci",
+    version="1.0.0",
+    description="A sample package",
+    author="Mhammed",
+    author_email="jeddou.ehtp@gmail.com",
+    packages=["newmysci"],
+    install_requires=[],
+)
