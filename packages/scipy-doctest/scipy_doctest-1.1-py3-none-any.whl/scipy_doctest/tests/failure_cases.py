2d5064a6b26ea0ecd88365b5aa80d0ffdfade6ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 16:04:21 +0000

    Add package scipy-doctest/1.1/scipy_doctest-1.1-py3-none-any.whl

---
+++ packages/scipy-doctest/scipy_doctest-1.1-py3-none-any.whl/scipy_doctest/tests/failure_cases.py
@@ -0,0 +1,17 @@
+__all__ = ['func9', 'func10']
+
+def func9():
+    """
+    Wrong output.
+    >>> import numpy as np
+    >>> np.array([1, 2, 3])
+    array([2, 3, 4])
+    """
+
+
+def func10():
+    """
+    NameError
+    >>> import numpy as np
+    >>> np.arraY([1, 2, 3])
+    """
