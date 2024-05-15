cfb5a0a91df38d439a920056cfc65dd9123822ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:04:05 +0000

    Add package ltm-process/1.7/ltm_process-1.7-py3-none-any.whl

---
+++ packages/ltm-process/ltm_process-1.7-py3-none-any.whl/ltm_process/process.pyi
@@ -0,0 +1,66 @@
+import torch
+import numpy as np
+import typing as _typing
+
+DataType: dict
+BlurKernel: torch.tensor
+BlurFilter: torch.nn.Conv2d
+Lut: torch.tensor
+
+@_typing.overload
+def image_warp(img: np.ndarray, m: np.ndarray, dst_shape: tuple)->np.ndarray:
+    """
+    flags: cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP
+    :param img: H,W,C or H,W
+    :param m: np.ndarray, size=(3,3)
+    :param dst_shape: (h,w)
+    :return: H,W,C or H,W
+    """
+    ...
+@_typing.overload
+def image_rotate(img: np.ndarray, angle:int)->np.ndarray:...
+@_typing.overload
+def transpose_angle(img_path: str)->int:...
+@_typing.overload
+def inter_linear(img: torch.tensor, curve: torch.tensor)->torch.tensor:
+    """
+    :param img: N,C,H,W
+    :param curve: N, length
+    :return: N,C,H,W
+    """
+    ...
+@_typing.overload
+def yuv_loader(path:str, dtype:str)->np.ndarray:...
+@_typing.overload
+def nps_out2ltm_in(nps_out_path: str, height:int=2304, width: int=4096)->np.ndarray: ...
+@_typing.overload
+def generate_mask0(ltm_in0: np.ndarray, mask1_path: str, max_min1_path: str, height: int=2304, width: int=4096)->np.ndarray:
+    """
+    :param ltm_in0: H,W,C
+    :param mask1_path: str
+    :param max_min1_path: str
+    :param height: int
+    :param width: int
+    :return: H,W
+    """
+    ...
+@_typing.overload
+def simulation_process_nps_out(real_shot_path: str, nps_out_path: str, mask1_path: str, max_min1_path: str, warp_matrix: np.ndarray=None, height=2304, width=4096, dst_shape=(1080, 1920))-> tuple[np.ndarray, np.ndarray]:
+    """
+    :param real_shot_path: str, for image rotate correction
+    :param nps_out_path: str
+    :param mask1_path: str
+    :param max_min1_path: str
+    :param warp_matrix: np.ndarray
+    :param height: int
+    :param width: int
+    :param dst_shape: tuple(h, w)
+    :return:ltm_in_pass0, mask_pass0
+    """
+    ...
+
+
+
+
+
+
