3c283467c950b5c864b7c68523cbbe0cd81c1d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:03:59 +0000

    Add package LearnHowToPackage-hsr/0.2/learnhowtopackage_hsr-0.2.tar.gz

---
+++ packages/LearnHowToPackage-hsr/learnhowtopackage_hsr-0.2.tar.gz/learnhowtopackage_hsr-0.2/README.md
@@ -0,0 +1,4 @@
+# This package is an experiment about learning how to package and upload to PyPI.  
+The module in this package:  
+- `example_module`, which include `add_one(n)`
+
---
+++ packages/LearnHowToPackage-hsr/learnhowtopackage_hsr-0.2.tar.gz/learnhowtopackage_hsr-0.2/setup.py
@@ -0,0 +1,33 @@
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+from distutils.core import setup
+setup(
+  name = 'LearnHowToPackage_hsr',         # How you named your package folder (MyLib)
+  packages = ['LearnHowToPackage_hsr'],   # Chose the same as "name"
+  version = '0.2',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'useless other than help me learn package',   # Give a short description about your library
+  author = 'Hu_need_help',                   # Type in your name
+  author_email = 'shiranhu_iesr22@163.com',      # Type in your E-Mail
+  url = 'https://github.com/HuNeedHelp?tab=repositories',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['hsr','learn how to upload package',],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'validators',       # 可以加上版本号,如validators=1.5.1   
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',   
+    'Programming Language :: Python :: 3.5', 
+    'Programming Language :: Python :: 3.6',
+  ],
+)
