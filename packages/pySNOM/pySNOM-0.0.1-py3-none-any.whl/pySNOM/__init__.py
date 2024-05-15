554580ee7e0d7582b736824627680580e526fd4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 03:05:20 +0000

    Add package pySNOM/0.0.1/pySNOM-0.0.1-py3-none-any.whl

---
+++ packages/pySNOM/pySNOM-0.0.1-py3-none-any.whl/pySNOM/__init__.py
@@ -0,0 +1,10 @@
+"""
+Module pySNOM
+=============
+Scanning Near-field Optical Microscopy (SNOM) analysis tools
+"""
+from .version import __version__
+__author__ = 'Gergely Nemeth, Ferenc Borondics'
+__credits__ = 'Synchrotron SOLEIL, Wigner Research Centre for Physics'
+
+from .pySNOM import *
