a72ba6b3153e3a58e8987c99767dfd6f85c6717b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 04:04:03 +0000

    Add package satsure-core-test/0.1.3/satsure_core_test-0.1.3.tar.gz

---
+++ packages/satsure-core-test/satsure_core_test-0.1.3.tar.gz/satsure_core_test-0.1.3/satelite/core/uploader.py
@@ -0,0 +1,24 @@
+import shutil
+from pathlib import Path, PosixPath
+from subprocess import Popen, PIPE
+
+
+def upload_folder(
+        local_folder: PosixPath, bucket_name: str, prefix: str,
+        exclude: str = "*", include: str = "*", delete_local: bool = False):
+    """
+        to upload file in AWS
+    :param local_folder:
+    :param bucket_name:
+    :param prefix:
+    :param exclude:
+    :param include:
+    :param delete_local:
+    :return:
+    """
+    command = f"""aws s3 cp "{local_folder}" "s3://{bucket_name}/{prefix}" --recursive --exclude "{exclude}" --include "{include}" --request-payer"""
+    process = Popen(command, shell=True, stdout=PIPE)
+    process.communicate()
+
+    if delete_local:
+        shutil.rmtree(Path(local_folder))
