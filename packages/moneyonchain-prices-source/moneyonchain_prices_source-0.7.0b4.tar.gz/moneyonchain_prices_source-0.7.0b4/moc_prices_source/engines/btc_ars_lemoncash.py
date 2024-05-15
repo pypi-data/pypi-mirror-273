2627febc834b9f49565c2be7596c8c38f1f10e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 04:04:09 +0000

    Add package moneyonchain-prices-source/0.7.0b4/moneyonchain_prices_source-0.7.0b4.tar.gz

---
+++ packages/moneyonchain-prices-source/moneyonchain_prices_source-0.7.0b4.tar.gz/moneyonchain_prices_source-0.7.0b4/moc_prices_source/engines/btc_ars_lemoncash.py
@@ -0,0 +1,28 @@
+from engine_base import Base, BTC_ARS
+from decimal import Decimal
+
+
+class Engine(Base):
+
+    _name        = Base._name_from_file(__file__)
+    _description = "Lemoncash"
+    _uri         = "https://api.lemoncash.com.ar/api/v1/exchange-rates-quotations-external"
+    _coinpair    = BTC_ARS
+
+    def _map(self, data):
+        value = {}
+        for i in data['results']:
+            if i['instrument']=='BTC-ARS':
+                value['price'] = (Decimal(i['purchase_price']['amount']) \
+                                  + Decimal(i['sale_price']['amount'])) / Decimal('2')
+                break       
+        return value
+
+
+if __name__ == '__main__':
+    print("File: {}, Ok!".format(repr(__file__)))
+    engine = Engine()
+    engine()
+    print(engine)
+    if engine.error:
+        print(engine.error)
