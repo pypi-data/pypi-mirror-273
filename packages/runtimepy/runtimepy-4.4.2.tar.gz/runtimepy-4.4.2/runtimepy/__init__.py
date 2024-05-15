80858f746184cef7742980f63fc720cb09ccb375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 16:03:56 +0000

    Add package runtimepy/4.4.2/runtimepy-4.4.2.tar.gz

---
+++ packages/runtimepy/runtimepy-4.4.2.tar.gz/runtimepy-4.4.2/runtimepy/__init__.py
@@ -0,0 +1,17 @@
+# =====================================
+# generator=datazen
+# version=3.1.4
+# hash=3d671d71c051dcb4a852a355aeda2674
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A framework for implementing Python services."
+PKG_NAME = "runtimepy"
+VERSION = "4.4.2"
+
+# runtimepy-specific content.
+METRICS_NAME = "metrics"
+DEFAULT_EXT = "yaml"
