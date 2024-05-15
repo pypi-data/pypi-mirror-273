81a32c5161cca720e0d2ece576d64c50a67243d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 04:03:25 +0000

    Add package thirdai/0.8.1/thirdai-0.8.1-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/thirdai/thirdai-0.8.1-cp39-cp39-macosx_11_0_arm64.whl/thirdai/bolt/__init__.py
@@ -0,0 +1,21 @@
+import thirdai._thirdai.bolt
+from thirdai._thirdai.bolt import *
+
+from .udt_modifications import modify_graph_udt, modify_mach_udt, modify_udt
+
+modify_udt()
+modify_graph_udt()
+modify_mach_udt()
+
+
+try:
+    # This is to prevent errors if torch or scipy are not installed.
+    # They are used in the seismic model but not the main thirdai package.
+    from .seismic_modifications import modify_seismic
+
+    modify_seismic()
+except ImportError:
+    pass
+
+__all__ = []
+__all__.extend(dir(thirdai._thirdai.bolt))
