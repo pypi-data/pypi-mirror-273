0877fb948c54c441dcb113c2332e9a3b69950826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 05:06:19 +0000

    Add package plingenn/1.0.0/plingenn-1.0.0.tar.gz

---
+++ packages/plingenn/plingenn-1.0.0.tar.gz/plingenn-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='plingenn',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Your Name',
+    author_email='your@email.com',
+    description='A brief description of your package',
+    url='https://github.com/yourusername/plingenn',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
