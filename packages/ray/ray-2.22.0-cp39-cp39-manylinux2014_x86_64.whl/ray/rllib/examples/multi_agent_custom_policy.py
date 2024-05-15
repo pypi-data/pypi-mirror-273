40fceb4b4a2965a58cf9be0df8c6f8474e9a1401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 14:04:12 +0000

    Add package ray/2.22.0/ray-2.22.0-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/ray/ray-2.22.0-cp39-cp39-manylinux2014_x86_64.whl/ray/rllib/examples/multi_agent_custom_policy.py
@@ -0,0 +1,6 @@
+msg = """
+This script has been moved to
+`ray.rllib.examples.multi_agent.custom_heuristic_rl_module.py`
+"""
+
+raise NotImplementedError(msg)
---
+++ packages/ray/ray-2.22.0-cp39-cp39-manylinux2014_x86_64.whl/ray/rllib/examples/parallel_evaluation_and_training.py
@@ -0,0 +1,6 @@
+msg = """
+This script has been moved to
+`ray.rllib.examples.evaluation.evaluation_parallel_to_training.py`
+"""
+
+raise NotImplementedError(msg)
