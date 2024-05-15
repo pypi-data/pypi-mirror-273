e902f3c4945e5ca0f51bca06bb02c60bf962c6ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 06:04:16 +0000

    Add package amolkit/1.0.6/amolkit-1.0.6-py3-none-any.whl

---
+++ packages/amolkit/amolkit-1.0.6-py3-none-any.whl/amolkit/misc.py
@@ -0,0 +1,10 @@
+def getExtension(filein):
+    extension = filein.split('.')[-1].lower()
+    return extension
+
+def FileStatus(filein): 
+    status = True
+    if not os.path.exists(filein): status=False
+    if os.stat(filein).st_size==0: status=False
+    if os.stat(filein).st_size/(1024 * 1024)>50: status=False
+    return status
