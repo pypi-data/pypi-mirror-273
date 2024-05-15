232e920f79a386df113e22a7f6ad5d8f601af5d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 04:03:35 +0000

    Add package assistant/1.1.2b1/assistant-1.1.2b1-py3-none-any.whl

---
+++ packages/assistant/assistant-1.1.2b1-py3-none-any.whl/assistant/listen/__init__.py
@@ -0,0 +1,14 @@
+try:
+    import listen
+except (ModuleNotFoundError, ImportError) as e:
+    print(e)
+    print("To speak with Assistant, you need to install the listen module.")
+    print("Run the following command:\n```shell\npip install -r stt-listen\n```")
+    sys.exit(1)
+
+if __name__ == '__main__':
+    from assistant.listen.main import main
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit(1)
