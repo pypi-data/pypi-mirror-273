0ba38c5f9e8ec3b5447f97d601db528c6941d075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:04:07 +0000

    Add package socketsecurity/0.0.54/socketsecurity-0.0.54-py3-none-any.whl

---
+++ packages/socketsecurity/socketsecurity-0.0.54-py3-none-any.whl/socketsecurity/core/exceptions.py
@@ -0,0 +1,31 @@
+__all__ = [
+    "APIFailure",
+    "APIKeyMissing",
+    "APIAccessDenied",
+    "APIInsufficientQuota",
+    "APIResourceNotFound"
+]
+
+
+class APIKeyMissing(Exception):
+    """Raised when the api key is not passed and the headers are empty"""
+
+
+class APIFailure(Exception):
+    """Raised when there is an error using the API"""
+    pass
+
+
+class APIAccessDenied(Exception):
+    """Raised when access is denied to the API"""
+    pass
+
+
+class APIInsufficientQuota(Exception):
+    """Raised when access is denied to the API"""
+    pass
+
+
+class APIResourceNotFound(Exception):
+    """Raised when access is denied to the API"""
+    pass
