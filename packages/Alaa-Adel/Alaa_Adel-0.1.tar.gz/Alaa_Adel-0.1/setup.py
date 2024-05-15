1ad517c0ba643cd9cf4930c0e8c09aebd134a2f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 05:04:06 +0000

    Add package Alaa-Adel/0.1/Alaa_Adel-0.1.tar.gz

---
+++ packages/Alaa-Adel/Alaa_Adel-0.1.tar.gz/Alaa_Adel-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup (
+    name = 'Alaa_Adel',
+    version = '0.1',
+    author = 'Alaa_Adel',
+    description = 'simple Python library for dragging files ',
+    packages = setuptools.find_packages(),
+    classifiers =[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
