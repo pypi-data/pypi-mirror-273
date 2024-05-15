6af8d2b875be765dfc25216d565e63e779b2572a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:03:42 +0000

    Add package kci-confluence-cli/0.2.1/kci_confluence_cli-0.2.1.tar.gz

---
+++ packages/kci-confluence-cli/kci_confluence_cli-0.2.1.tar.gz/kci_confluence_cli-0.2.1/confluence/page/subcommand.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import argparse
+from typing import Optional
+
+import confluence
+import confluence.page.get_page_body
+import confluence.page.update_page
+
+
+def add_arguments_to_parser(parser: argparse.ArgumentParser):  # noqa: ANN201
+    subparsers = parser.add_subparsers(dest="subcommand_name")
+
+    # サブコマンドの定義
+    confluence.page.get_page_body.add_parser(subparsers)
+    # 動かないのでコメントアウトする
+    # confluence.page.update_page.add_parser(subparsers)
+
+
+def add_parser(subparsers: Optional[argparse._SubParsersAction] = None) -> argparse.ArgumentParser:
+    subcommand_name = "page"
+    subcommand_help = "ページに関するサブコマンド"
+
+    parser = confluence.common.cli.add_parser(subparsers, subcommand_name, subcommand_help, description=subcommand_help, is_subcommand=False)
+    add_arguments_to_parser(parser)
+    return parser
