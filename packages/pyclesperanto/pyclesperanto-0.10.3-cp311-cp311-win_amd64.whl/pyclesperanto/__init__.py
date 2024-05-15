77541f5588e4d88c2f4073082ffe9d12d0e04346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 03:05:38 +0000

    Add package pyclesperanto/0.10.3/pyclesperanto-0.10.3-cp311-cp311-win_amd64.whl

---
+++ packages/pyclesperanto/pyclesperanto-0.10.3-cp311-cp311-win_amd64.whl/pyclesperanto/__init__.py
@@ -0,0 +1,31 @@
+from ._core import (
+    gpu_info,
+    select_backend,
+    select_device,
+    get_device,
+    wait_for_kernel_to_finish,
+    list_available_devices,
+    list_available_backends,
+    default_initialisation,
+    Device,
+)
+from ._array import Array, Image, is_image
+from ._memory import create, create_like, push, pull
+from ._functionalities import imshow, list_operations, execute, native_execute
+
+from ._tier1 import *
+from ._tier2 import *
+from ._tier3 import *
+from ._tier4 import *
+from ._tier5 import *
+from ._tier6 import *
+from ._tier7 import *
+from ._tier8 import *
+from ._interroperability import *
+
+from ._version import VERSION as __version__
+from ._version import CLIC_VERSION as __clic_version__
+from ._version import COMMON_ALIAS as __common_alias__
+
+
+default_initialisation()
