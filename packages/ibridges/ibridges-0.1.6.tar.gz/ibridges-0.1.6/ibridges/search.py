65134267b1d437ee70172d21126f4c2db4f2d020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 03:05:25 +0000

    Add package ibridges/0.1.6/ibridges-0.1.6.tar.gz

---
+++ packages/ibridges/ibridges-0.1.6.tar.gz/ibridges-0.1.6/ibridges/search.py
@@ -0,0 +1,81 @@
+"""Data query."""
+from __future__ import annotations
+
+from typing import Optional, Union
+
+from ibridges import icat_columns as icat
+from ibridges.path import IrodsPath
+from ibridges.session import Session
+
+
+def search_data(session: Session, path: Optional[Union[str, IrodsPath]] = None,
+           checksum: Optional[str] = None, key_vals: Optional[dict] = None) -> list[dict]:
+    """Retrieve all collections and data objects.
+
+    (the absolute collection path,
+    data object or collection name) to the given user-defined and system metadata.
+    By Default all accessible collections and data objects will be returned.
+    Wildcard: %
+
+    Parameters
+    ----------
+    session:
+        Session to search with.
+    path: str
+        (Partial) path or IrodsPath
+    checksum: str
+        (Partial) checksum
+    key_vals : dict
+        Attribute name mapping to values.
+
+    Raises
+    ------
+    ValueError:
+        If no search criterium is supplied.
+
+    Returns
+    -------
+    list: [dict]
+        List of dictionaries with keys:
+        COLL_NAME (absolute path of the collection),
+        DATA_NAME (name of the data object),
+        D_DATA_CHECKSUM (checksum of the data object)
+        The latter two keys are only present of the found item is a data object.
+
+    """
+    if path is None and checksum is None and key_vals is None:
+        raise ValueError(
+                "QUERY: Error while searching in the metadata: No query criteria set." \
+                        + " Please supply either a path, checksum or key_vals.")
+
+    # create the query for collections; we only want to return the collection name
+    coll_query = session.irods_session.query(icat.COLL_NAME)
+    # create the query for data objects; we need the collection name, the data name and its checksum
+    data_query = session.irods_session.query(icat.COLL_NAME, icat.DATA_NAME,
+                                             icat.DATA_CHECKSUM)
+    if path:
+        coll_query = coll_query.filter(icat.LIKE(icat.COLL_NAME, str(path)))
+        data_query = data_query.filter(icat.LIKE(icat.COLL_NAME, str(path)))
+    if key_vals:
+        for key in key_vals:
+            data_query.filter(icat.LIKE(icat.META_DATA_ATTR_NAME, key))
+            coll_query.filter(icat.LIKE(icat.META_COLL_ATTR_NAME, key))
+            if key_vals[key]:
+                data_query.filter(icat.LIKE(icat.META_DATA_ATTR_VALUE, key_vals[key]))
+                coll_query.filter(icat.LIKE(icat.META_COLL_ATTR_VALUE, key_vals[key]))
+    if checksum:
+        data_query = data_query.filter(icat.LIKE(icat.DATA_CHECKSUM, checksum))
+    # gather results
+    results = list(data_query.get_results())
+    if checksum is None:
+        coll_res = list(coll_query.get_results())
+        if len(coll_res) > 0:
+            results.extend(coll_res)
+
+    for item in results:
+        if isinstance(item, dict):
+            new_keys = [k.icat_key for k in item.keys()]
+            for n_key, o_key in zip(new_keys, item.keys()):
+                item[n_key] = item.pop(o_key)
+
+    return results
---
+++ packages/ibridges/ibridges-0.1.6.tar.gz/ibridges-0.1.6/tests/test_irodspath.py
@@ -0,0 +1,78 @@
+from pathlib import PurePosixPath
+from pytest import mark
+import os
+from pathlib import Path
+
+from ibridges import IrodsPath
+from ibridges.data_operations import _create_irods_dest
+
+class MockIrodsSession:
+    zone = "testzone"
+    user = "testuser"
+    host = "test.host.nl"
+    server_version = "test_version"
+    port = 9876
+    home = "/testzone/home/testuser"
+    irods_session = None
+    # def home(self):
+        # return "/"+self.zone+"/home/"+self.user
+
+
+# IrodsPath tests
+mock_session = MockIrodsSession()
+dirname = "blabla"
+filename = "blublub"
+irods_path = IrodsPath(mock_session, dirname, filename) 
+windows_path = "windows\\path\\directory"
+linux_path = "linux/or/mac/path"
+
+@mark.parametrize(
+    "input,abs_path,name,parent",
+    [
+        ([], "/testzone/home/testuser", "", "."),
+        (["~"], "/testzone/home/testuser", "~", "."),
+        ([""], "/testzone/home/testuser", "", "."),
+        (["."], "/testzone/home/testuser", "", "."),
+        ([PurePosixPath(".")], "/testzone/home/testuser", "", "."),
+        (["~", "xyz"], "/testzone/home/testuser/xyz", "xyz", "~"),
+        (["xyz"], "/testzone/home/testuser/xyz", "xyz", "."),
+        ([".", "xyz"], "/testzone/home/testuser/xyz", "xyz", "."),
+        ([PurePosixPath("."), "xyz"], "/testzone/home/testuser/xyz", "xyz", "."),
+        ([PurePosixPath(".", "xyz")], "/testzone/home/testuser/xyz", "xyz", "."),
+        (["/x/y/z"], "/x/y/z", "z", "/x/y"),
+        (["/x/y", "z"], "/x/y/z", "z", "/x/y"),
+        ([IrodsPath(MockIrodsSession(), "/x/y"), "z"], "/x/y/z", "z", "/x/y")
+        # ([PureWindowsPath("c:\\x\\y\\z")], "c:\\/x/y/z", "z", "/x/y")
+    ])
+def test_absolute_path(input, abs_path, name, parent):
+    session = MockIrodsSession()
+    ipath = IrodsPath(session, *input)
+    assert ipath.absolute_path() == abs_path
+    assert ipath.name == name
+    assert isinstance(ipath.parent, IrodsPath)
+    assert str(ipath.parent._path) == parent, str(ipath.parent)
+
+@mark.parametrize(
+    "path,to_join,result",
+    [
+        ("/etc", ["test"], "/etc/test"),
+        ("123", ["test", "test2"], "123/test/test2"),
+        ("~", [IrodsPath(MockIrodsSession(), "test")], "~/test")
+    ]
+)
+def test_join_path(path, to_join, result):
+    irods_path = IrodsPath(MockIrodsSession(), path)
+    assert str(irods_path.joinpath(*to_join)._path) == result
+
+# Create upload and download path tests for data_operations
+
+def test_create_irods_paths():
+    session = MockIrodsSession()
+    local_path = Path("tests/testdata").absolute()
+    irods_path = IrodsPath(MockIrodsSession(), session.home)
+    source_to_dest = _create_irods_dest(local_path, irods_path)
+    for source, dest in source_to_dest:
+        local_parts = source.parts[source.parts.index("testdata"):]
+        irods_parts = dest.parts[dest.parts.index("testdata"):]
+        assert list(local_parts) == list(irods_parts)
+        assert str(dest).split("testdata")[0].rstrip("/") == session.home
