50fbe644d33f7a894f92acf699b14b8c30bc8e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 06:05:18 +0000

    Add package comfy-cli/0.0.23/comfy_cli-0.0.23.tar.gz

---
+++ packages/comfy-cli/comfy_cli-0.0.23.tar.gz/comfy_cli-0.0.23/comfy_cli/utils.py
@@ -0,0 +1,66 @@
+"""
+Module for utility functions.
+"""
+
+import sys
+from comfy_cli import constants
+import psutil
+
+
+def singleton(cls):
+    """
+    Decorator that implements the Singleton pattern for the decorated class.
+
+    e.g.
+    @singleton
+    class MyClass:
+        pass
+
+    """
+    instances = {}
+
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return get_instance
+
+
+def get_os():
+    if sys.platform == "darwin":
+        return constants.OS.MACOS
+    elif "win" in sys.platform:
+        return constants.OS.WINDOWS
+
+    return constants.OS.LINUX
+
+
+def get_not_user_set_default_workspace():
+    return constants.DEFAULT_COMFY_WORKSPACE[get_os()]
+
+
+def kill_all(pid):
+    try:
+        parent = psutil.Process(pid)
+        children = parent.children(recursive=True)
+        for child in children:
+            child.kill()
+        return True
+    except Exception:
+        return False
+
+
+def is_running(pid):
+    try:
+        psutil.Process(pid)
+        return True
+    except psutil.NoSuchProcess:
+        return False
+
+
+def create_choice_completer(opts):
+    def f(incomplete: str) -> list[str]:
+        return [opt for opt in opts if opt.startswith(incomplete)]
+
+    return f
