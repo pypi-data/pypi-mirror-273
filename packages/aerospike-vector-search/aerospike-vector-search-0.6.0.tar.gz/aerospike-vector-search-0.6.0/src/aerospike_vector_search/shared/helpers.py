3c06deb07bbbd7e1d05aba2647f191c260816cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 05:04:00 +0000

    Add package aerospike-vector-search/0.6.0/aerospike-vector-search-0.6.0.tar.gz

---
+++ packages/aerospike-vector-search/aerospike-vector-search-0.6.0.tar.gz/aerospike-vector-search-0.6.0/src/aerospike_vector_search/shared/helpers.py
@@ -0,0 +1,33 @@
+import time
+from .. import types
+from .proto_generated import types_pb2
+from .proto_generated import index_pb2_grpc
+
+
+def _prepare_seeds(seeds) -> None:
+
+    if not seeds:
+        raise Exception("at least one seed host needed")
+
+    if isinstance(seeds, types.HostPort):
+        seeds = (seeds,)
+
+    return seeds
+
+
+def _prepare_wait_for_index_waiting(self, namespace, name, wait_interval):
+
+    unmerged_record_initialized = False
+    start_time = time.monotonic()
+    consecutive_index_validations = 0
+
+    index_stub = index_pb2_grpc.IndexServiceStub(self._channel_provider.get_channel())
+    index_wait_request = types_pb2.IndexId(namespace=namespace, name=name)
+    return (
+        index_stub,
+        wait_interval,
+        start_time,
+        unmerged_record_initialized,
+        consecutive_index_validations,
+        index_wait_request,
+    )
