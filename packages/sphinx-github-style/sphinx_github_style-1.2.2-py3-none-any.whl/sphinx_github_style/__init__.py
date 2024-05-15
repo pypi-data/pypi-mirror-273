31903615c69639014f8948d2eb841c36021d3024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:04:06 +0000

    Add package sphinx-github-style/1.2.2/sphinx_github_style-1.2.2-py3-none-any.whl

---
+++ packages/sphinx-github-style/sphinx_github_style-1.2.2-py3-none-any.whl/sphinx_github_style/__init__.py
@@ -0,0 +1,53 @@
+import sphinx
+from pathlib import Path
+from typing import Dict, Any
+from sphinx.application import Sphinx
+from .utils.sphinx import get_conf_val, set_conf_val
+from .utils.linkcode import get_linkcode_url, get_linkcode_revision, get_linkcode_resolve
+
+__version__ = "1.2.2"
+__author__ = 'Adam Korn <hello@dailykitten.net>'
+
+from .add_linkcode_class import add_linkcode_node_class
+from .github_style import GitHubStyle
+from .lexer import GitHubLexer
+
+
+def setup(app: Sphinx) -> Dict[str, Any]:
+    app.setup_extension('sphinx.ext.linkcode')
+    app.connect("builder-inited", add_static_path)
+    app.connect('doctree-resolved', add_linkcode_node_class)
+
+    app.add_config_value('linkcode_blob', 'head', True)
+    app.add_config_value('linkcode_link_text', 'View on GitHub', 'html')
+
+    linkcode_blob = get_conf_val(app, "linkcode_blob")
+    linkcode_url = get_linkcode_url(
+        blob=get_linkcode_revision(linkcode_blob),
+        url=get_conf_val(app, 'linkcode_url'),
+        context=get_conf_val(app, 'html_context'),
+    )
+    linkcode_func = get_conf_val(app, "linkcode_resolve")
+
+    if not callable(linkcode_func):
+        print(
+            "Function `linkcode_resolve` not found in ``conf.py``; "
+            "using default function from ``sphinx_github_style``"
+        )
+        linkcode_func = get_linkcode_resolve(linkcode_url)
+        set_conf_val(app, 'linkcode_resolve', linkcode_func)
+
+    if not get_conf_val(app, 'pygments_style'):
+        set_conf_val(app, 'pygments_style', 'sphinx_github_style.GitHubStyle')
+
+    app.add_lexer('python', GitHubLexer)
+    app.add_css_file('github_style.css')
+
+    return {'version': sphinx.__display_version__, 'parallel_read_safe': True}
+
+
+def add_static_path(app) -> None:
+    """Add the path for the ``_static`` folder"""
+    app.config.html_static_path.append(
+        str(Path(__file__).parent.joinpath("_static").absolute())
+    )
---
+++ packages/sphinx-github-style/sphinx_github_style-1.2.2-py3-none-any.whl/sphinx_github_style/lexer.py
@@ -0,0 +1,55 @@
+import builtins
+from typing import Set, Dict
+from pygments.token import Name, Keyword
+from pygments.lexers.python import PythonLexer
+from inspect import getmembers, isclass, isbuiltin, ismethoddescriptor
+
+
+def get_builtins() -> Dict[str, Set]:
+    """Returns a dictionary containing names of built-in functions, classes, and methods"""
+    funcs_meths = set(dict(getmembers(builtins, isbuiltin)))
+    classes = set()
+
+    for class_name, _class in getmembers(builtins, isclass):
+        methods = getmembers(_class, ismethoddescriptor)
+        funcs_meths.update(dict(methods))
+        classes.add(class_name)
+
+    return {
+        'funcs': funcs_meths,
+        'classes': classes
+    }
+
+
+BUILTINS = get_builtins()
+
+
+class GitHubLexer(PythonLexer):
+    """A Pygments Lexer that adds syntax highlighting for the methods, classes, type hints, etc. in a Python package"""
+
+    name = 'TDK'
+    url = 'https://github.com/TDKorn'
+    aliases = ['tdk']
+
+    def get_tokens_unprocessed(self, text):
+        """Override to add better syntax highlighting"""
+        tokens = list(PythonLexer.get_tokens_unprocessed(self, text))
+
+        for token_idx, (index, token, value) in enumerate(tokens):
+            # Highlight builtins as either function calls or type hints
+            if token is Name.Builtin and value in BUILTINS['classes']:
+                if tokens[token_idx+1][-1] == '(':
+                    yield index, Name.Builtin, value
+                else:
+                    yield index, Name.Builtin.Pseudo, value
+
+            elif token is Name:
+                if value[0].isupper():  # Highlight as class
+                    yield index, Name.Class, value
+                elif tokens[token_idx+1][-1] == '(':  # Highlight as function
+                    yield index, Keyword.Pseudo, value
+                else:
+                    yield index, Name, value
+
+            else:
+                yield index, token, value
