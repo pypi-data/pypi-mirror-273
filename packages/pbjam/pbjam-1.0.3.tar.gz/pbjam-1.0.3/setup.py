cd3c9bd34a5cfd1aefbe5335ea87017c5fc96799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 05:03:53 +0000

    Add package pbjam/1.0.3/pbjam-1.0.3.tar.gz

---
+++ packages/pbjam/pbjam-1.0.3.tar.gz/pbjam-1.0.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+import os
+
+version = {}
+
+with open(os.path.join(*['pbjam','version.py'])) as fp:
+	exec(fp.read(), version)
+
+setuptools.setup(
+    name="pbjam",
+    version=version['__version__'],
+    author="Martin Nielsen, Guy Davies, Oliver Hall",
+    author_email="m.b.nielsen.1@bham.ac.uk",
+    description="A package for peakbagging solar-like oscillators",
+    long_description=open("README.rst").read(),
+    url="https://pbjam.readthedocs.io/",
+    packages=['pbjam'],
+    install_requires=open('requirements.txt').read().splitlines(),
+    extras_require={'docs': ["nbsphinx"]},
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Development Status :: 3 - Alpha"
+    ],
+)
