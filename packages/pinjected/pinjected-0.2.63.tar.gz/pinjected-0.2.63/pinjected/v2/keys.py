5bc0600dff7af28dbb21ccfa327900232c3e0f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:04:13 +0000

    Add package pinjected/0.2.63/pinjected-0.2.63.tar.gz

---
+++ packages/pinjected/pinjected-0.2.63.tar.gz/pinjected-0.2.63/pinjected/v2/keys.py
@@ -0,0 +1,30 @@
+import abc
+from abc import ABC
+from dataclasses import dataclass
+
+
+class IBindKey(ABC):
+    """
+    hold a providing key and metadata
+    """
+    pass
+
+    @abc.abstractmethod
+    def ide_hint_string(self):
+        return repr(self)
+
+
+@dataclass(frozen=True)
+class StrBindKey(IBindKey):
+    name: str
+
+    def ide_hint_string(self):
+        return self.name
+
+
+@dataclass(frozen=True)
+class DestructorKey(IBindKey):
+    tgt: IBindKey
+
+    def ide_hint_string(self):
+        return f"destructor({self.tgt.ide_hint_string()})"
