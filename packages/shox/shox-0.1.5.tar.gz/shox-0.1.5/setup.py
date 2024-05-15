7d7695f78c13fd4d30eb164f9bad73a377ae7218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 06:05:16 +0000

    Add package shox/0.1.5/shox-0.1.5.tar.gz

---
+++ packages/shox/shox-0.1.5.tar.gz/shox-0.1.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shox',
+    version='0.1.5',
+    description='A powerful tool that combines the port-scanning and OSINT-gathering capabilities of shodan.io with the onhand downloadable exploits of ExploitDB.',
+    author='Vadin02',
+    url='https://gitlab.com/JIbald/shox',
+    packages=find_packages(),
+    install_requires=[
+        'flask',
+        'flasgger',
+        'click',
+        'requests',
+        'beautifulsoup4',
+        'selenium',
+    ],
+    entry_points={
+        'console_scripts': [
+            'shox-api=api:start',
+            'shox-cli=cli:call_api',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
