050defda607260884da48aab90b27614e9fb027d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 05:04:23 +0000

    Add package monkey-agentpluginapi/0.7.0/monkey_agentpluginapi-0.7.0-py3-none-any.whl

---
+++ packages/monkey-agentpluginapi/monkey_agentpluginapi-0.7.0-py3-none-any.whl/agentpluginapi/agent_binary_request.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from typing import TypeAlias
+from uuid import UUID
+
+from monkeytypes import Event, OperatingSystem
+
+ReservationID: TypeAlias = UUID
+
+
+@dataclass(frozen=True)
+class AgentBinaryDownloadReservation:
+    id: ReservationID
+    operating_system: OperatingSystem
+    agent_binary_wrapper_template: bytes | None
+    download_url: str
+    download_completed: Event
+
+
+@dataclass(frozen=True)
+class AgentBinaryDownloadTicket:
+    id: ReservationID
+    download_url: str
+    download_completed: Event
