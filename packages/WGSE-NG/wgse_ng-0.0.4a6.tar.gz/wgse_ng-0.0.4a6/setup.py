8c2f31042800458e89c8f35aaefc990cae0cc1c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 03:06:06 +0000

    Add package WGSE-NG/0.0.4a6/wgse_ng-0.0.4a6.tar.gz

---
+++ packages/WGSE-NG/wgse_ng-0.0.4a6.tar.gz/wgse_ng-0.0.4a6/setup.py
@@ -0,0 +1,51 @@
+from setuptools import find_packages, setup
+from wgse import VERSION
+
+DEPENDENCIES = [
+    "setuptools",
+    "pefile",
+    "pycurl",
+    "tqdm",
+    "google-cloud-storage",
+    "sphinx",
+    "pytest",
+    "pyqt6",
+    "pyinstaller",
+    "PySide6",
+    "jinja2",
+    "vcf_parser",
+    "WGSE-NG-3rd-party",
+]
+
+DOC = ""
+
+setup(
+    name="WGSE-NG",
+    packages=find_packages(include=["wgse", "wgse.*"]),
+    author="Multiple",
+    author_email="",
+    include_package_data=True,
+    package_data={
+        "wgse.mtDNA": ["*.*"], 
+        "wgse.metadata.microarray_templates.head": ["*.*"],
+        "wgse.metadata.microarray_templates.body": ["*.*"],
+        "wgse.metadata.bed_templates": ["*.*"],
+        "wgse.metadata.report_templates": ["*.*"],
+        },
+    description="Whole Genome Sequencing data manipulation tool",
+    long_description=open("README.md","r").read(),
+    install_requires=DEPENDENCIES,
+    url="https://github.com/chaplin89/WGSE-NG",
+    version=VERSION,
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    keywords="bioinformatics",
+)
