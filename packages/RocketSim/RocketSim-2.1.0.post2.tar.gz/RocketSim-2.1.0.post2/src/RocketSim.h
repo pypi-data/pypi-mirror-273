d1990cde533f93dabac4aa13f0c6d668876916ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:03:46 +0000

    Add package RocketSim/2.1.0.post2/RocketSim-2.1.0.post2.tar.gz

---
+++ packages/RocketSim/RocketSim-2.1.0.post2.tar.gz/RocketSim-2.1.0.post2/src/RocketSim.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Sim/Car/Car.h"
+#include "Sim/Ball/Ball.h"
+#include "Sim/Arena/Arena.h"
+
+#include "Math/Math.h"
+
+// AVAILABLE DEFS FOR ROCKETSIM:
+//	RS_MAX_SPEED: Define this to remove certain sanity checks for faster speed
+//	RS_DONT_LOG: Define this to disable all logging output
+//	RS_NO_SUSPCOLGRID: Disable the suspension-collision grid optimization
+//	RS_NO_NAMESPACE: Disable the RocketSim namespace encapsulating all RocketSim classes/structs
+
+class btBvhTriangleMeshShape;
+
+namespace RocketSim {
+	enum class RocketSimStage : byte {
+		UNINITIALIZED,
+		INITIALIZING,
+		INITIALIZED
+	};
+
+	extern std::filesystem::path _collisionMeshesFolder;
+	extern std::mutex _beginInitMutex;
+
+	void Init(std::filesystem::path collisionMeshesFolder);
+	void AssertInitialized(const char* errorMsgPrefix);
+
+	RocketSimStage GetStage();
+
+	std::vector<btBvhTriangleMeshShape*>& GetArenaCollisionShapes(GameMode gameMode);
+
+#ifndef RS_NO_SUSPCOLGRID
+	SuspensionCollisionGrid& GetDefaultSuspColGrid(GameMode gameMode, bool isLight);
+#endif
+}
