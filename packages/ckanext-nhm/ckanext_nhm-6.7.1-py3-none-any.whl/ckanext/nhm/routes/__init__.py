0dfb728074708a087ba1a7db9ac2cd0cc0c9db74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 03:05:01 +0000

    Add package ckanext-nhm/6.7.1/ckanext_nhm-6.7.1-py3-none-any.whl

---
+++ packages/ckanext-nhm/ckanext_nhm-6.7.1-py3-none-any.whl/ckanext/nhm/routes/__init__.py
@@ -0,0 +1,32 @@
+# !/usr/bin/env python
+# encoding: utf-8
+#
+# This file is part of ckanext-nhm
+# Created by the Natural History Museum in London, UK
+
+from . import (
+    about,
+    legal,
+    statistics,
+    record,
+    object,
+    help,
+    bbcm,
+    beetle_iiif,
+    misc,
+    liv,
+)
+
+blueprints = [
+    about.blueprint,
+    statistics.blueprint,
+    legal.blueprint,
+    record.blueprint,
+    object.blueprint,
+    object.specimen_blueprint,
+    help.blueprint,
+    bbcm.blueprint,
+    beetle_iiif.blueprint,
+    misc.blueprint,
+    liv.blueprint,
+]
