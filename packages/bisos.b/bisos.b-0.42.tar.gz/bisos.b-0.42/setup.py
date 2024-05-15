eb4ddd6584221d533e223c1c278b46ba2ea3adec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 14:04:22 +0000

    Add package bisos.b/0.42/bisos.b-0.42.tar.gz

---
+++ packages/bisos.b/bisos.b-0.42.tar.gz/bisos.b-0.42/setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import setuptools
+import re
+
+
+def readme():
+    with open('./README.org') as file:
+        while line := file.readline():
+            if match := re.search(r'^#\+title: (.*)',  line.rstrip()):
+                return match.group(1)
+            return "MISSING TITLE in ./README.org"
+
+def longDescription():
+    try:
+        import pypandoc
+    except ImportError:
+        result = "warning: pypandoc module not found, could not convert to RST"
+        return result
+    return pypandoc.convert_file('README.org', 'rst')
+
+####+BEGIN: b:py3:pypi/nextVersion :increment 0.01
+
+def pkgVersion(): return '0.42'
+####+END:
+
+####+BEGIN: b:py3:pypi/requires :extras ("rpyc" "bisos.transit")
+
+requires = [
+"blee",
+"blee.icmPlayer",
+"bisos",
+"bisos.examples",
+"bisos.transit",
+"rpyc",
+"bisos.transit",
+]
+####+END:
+
+####+BEGIN: b:py3:pypi/scripts :comment ""
+
+scripts = [
+'./bin/csExamples.cs',
+'./bin/csIcmExamples.cs',
+]
+####+END:
+
+
+setuptools.setup(
+    name='bisos.b',
+    version=pkgVersion(),
+    # namespace_packages=['bisos'],
+    packages=setuptools.find_packages(),
+    scripts=scripts,
+    requires=requires,
+    include_package_data=True,
+    zip_safe=False,
+    author='Mohsen Banan',
+    author_email='libre@mohsen.1.banan.byname.net',
+    maintainer='Mohsen Banan',
+    maintainer_email='libre@mohsen.1.banan.byname.net',
+    url='http://www.by-star.net/PLPC/180047',
+    license='AGPL',
+    description=readme(),
+    long_description=longDescription(),
+    download_url='http://www.by-star.net/PLPC/180047',
+    install_requires=requires,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
+    )
