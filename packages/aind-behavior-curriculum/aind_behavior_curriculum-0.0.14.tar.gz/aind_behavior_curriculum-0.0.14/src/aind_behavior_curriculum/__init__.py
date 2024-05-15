84f6134cf06a3c84343474842e5dd5d8ac61dd4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 06:04:22 +0000

    Add package aind-behavior-curriculum/0.0.14/aind_behavior_curriculum-0.0.14.tar.gz

---
+++ packages/aind-behavior-curriculum/aind_behavior_curriculum-0.0.14.tar.gz/aind_behavior_curriculum-0.0.14/src/aind_behavior_curriculum/__init__.py
@@ -0,0 +1,10 @@
+"""Init package"""
+
+__version__ = "0.0.14"
+
+from .base import *  # noqa: F401, F403
+from .task import *  # noqa: F401, F403
+
+from .curriculum import *  # noqa: F401, F403
+from .curriculum_utils import *  # noqa: F401, F403
+from .trainer import *  # noqa: F401, F403
