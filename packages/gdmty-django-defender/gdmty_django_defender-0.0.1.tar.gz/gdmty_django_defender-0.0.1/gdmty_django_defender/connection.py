be1ee075f951d676876ed78962ca5f90b55e5a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:03:51 +0000

    Add package gdmty-django-defender/0.0.1/gdmty_django_defender-0.0.1.tar.gz

---
+++ packages/gdmty-django-defender/gdmty_django_defender-0.0.1.tar.gz/gdmty_django_defender-0.0.1/gdmty_django_defender/connection.py
@@ -0,0 +1,39 @@
+import urllib.parse as urlparse
+
+import redis
+
+from django.core.cache import caches
+from django.core.cache.backends.base import InvalidCacheBackendError
+import django
+
+from . import config
+
+# Register database schemes in URLs.
+urlparse.uses_netloc.append("redis")
+
+INVALID_CACHE_ERROR_MSG = "The cache {} was not found on the django cache" " settings."
+
+
+def get_redis_connection():
+    """ Get the redis connection if not using mock """
+    if config.MOCK_REDIS:  # pragma: no cover
+        import mockredis
+
+        return mockredis.mock_strict_redis_client()  # pragma: no cover
+    elif config.DEFENDER_REDIS_NAME:  # pragma: no cover
+        try:
+            cache = caches[config.DEFENDER_REDIS_NAME]
+        except InvalidCacheBackendError:
+            raise KeyError(INVALID_CACHE_ERROR_MSG.format(config.DEFENDER_REDIS_NAME))
+        # every redis backend implement it own way to get the low level client
+        if django.VERSION[0] >= 5 and type(cache) is django.core.cache.backends.redis.RedisCache:
+            return cache
+        else:
+            try:
+                # redis_cache.RedisCache case (django-redis-cache package)
+                return cache.get_master_client()
+            except AttributeError:
+                # django_redis.cache.RedisCache case (django-redis package)
+                return cache.client.get_client(True)
+    else:  # pragma: no cover)
+        return redis.StrictRedis.from_url(config.DEFENDER_REDIS_URL)
