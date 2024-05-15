8d204f7b16efc688656ca524e3eed143c8ce1f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:03:57 +0000

    Add package kscale-onshape-library/0.0.12/kscale_onshape_library-0.0.12-py3-none-any.whl

---
+++ packages/kscale-onshape-library/kscale_onshape_library-0.0.12-py3-none-any.whl/kol/scripts/get_urdf.py
@@ -0,0 +1,71 @@
+# mypy: disable-error-code="attr-defined"
+"""Defines utility functions for converting an OnShape model to a URDF file."""
+
+import argparse
+import logging
+from typing import Sequence, get_args
+
+import numpy as np
+
+from kol.formats import urdf
+from kol.logging import configure_logging
+from kol.mesh import MeshExt
+from kol.onshape.converter import Converter
+
+
+def main(args: Sequence[str] | None = None) -> None:
+    parser = argparse.ArgumentParser(description="Import the robot model from OnShape")
+    parser.add_argument("document_url", type=str, help="The ID of the document to import")
+    parser.add_argument("-o", "--output-dir", type=str, help="The path to save the imported model")
+    parser.add_argument("--debug", action="store_true", help="Enable debug logging")
+    parser.add_argument("--max-force", type=float, default=80.0, help="The maximum force for a prismatic joint")
+    parser.add_argument("--max-velocity", type=float, default=5.0, help="The maximum velocity for a prismatic joint")
+    parser.add_argument("--max-length", type=float, default=1.0, help="The maximum length, in meters")
+    parser.add_argument("--max-torque", type=float, default=80.0, help="The maximum force, in Nm")
+    parser.add_argument("--max-ang-velocity", type=float, default=5.0, help="The maximum angular velocity, in rad/s")
+    parser.add_argument("--max-angle", type=float, default=np.pi, help="The maximum angle, in radians")
+    parser.add_argument("--suffix-to-joint-effort", type=str, nargs="+", help="The suffix to joint effort mapping")
+    parser.add_argument("--suffix-to-joint-velocity", type=str, nargs="+", help="The suffix to joint velocity mapping")
+    parser.add_argument("--disable-mimics", action="store_true", help="Disable the mimic joints")
+    parser.add_argument("--mesh-ext", type=str, default="stl", choices=get_args(MeshExt), help="The mesh file format")
+    parsed_args = parser.parse_args(args)
+
+    configure_logging(level=logging.DEBUG if parsed_args.debug else logging.INFO)
+
+    suffix_to_joint_effort: list[tuple[str, float]] = []
+    if parsed_args.suffix_to_joint_effort:
+        for mapping in parsed_args.suffix_to_joint_effort:
+            suffix, effort = mapping.split("=")
+            suffix_to_joint_effort.append((suffix, float(effort.strip())))
+
+    suffix_to_joint_velocity: list[tuple[str, float]] = []
+    if parsed_args.suffix_to_joint_velocity:
+        for mapping in parsed_args.suffix_to_joint_velocity:
+            suffix, velocity = mapping.split("=")
+            suffix_to_joint_velocity.append((suffix, float(velocity.strip())))
+
+    Converter(
+        document_url=parsed_args.document_url,
+        output_dir=parsed_args.output_dir,
+        default_prismatic_joint_limits=urdf.JointLimits(
+            parsed_args.max_force,
+            parsed_args.max_velocity,
+            -parsed_args.max_length,
+            parsed_args.max_length,
+        ),
+        default_revolute_joint_limits=urdf.JointLimits(
+            parsed_args.max_torque,
+            parsed_args.max_ang_velocity,
+            -parsed_args.max_angle,
+            parsed_args.max_angle,
+        ),
+        suffix_to_joint_effort=suffix_to_joint_effort,
+        suffix_to_joint_velocity=suffix_to_joint_velocity,
+        disable_mimics=parsed_args.disable_mimics,
+        mesh_ext=parsed_args.mesh_ext,
+    ).save_urdf()
+
+
+if __name__ == "__main__":
+    # python -m kol.scripts.get_urdf
+    main()
