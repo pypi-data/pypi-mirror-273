976263bcc83cd5c3888c5e861d0f078221286146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 16:04:25 +0000

    Add package lungo-cli/0.4.0/lungo_cli-0.4.0-py3-none-any.whl

---
+++ packages/lungo-cli/lungo_cli-0.4.0-py3-none-any.whl/lungo_cli/resources/web/src/lib/utils/api.ts
@@ -0,0 +1,88 @@
+import { type Cookies, error } from "@sveltejs/kit"
+import parser from "set-cookie-parser"
+import { concatenateUrl } from "$lib/utils"
+
+export function getCookieHeader(request: Request): string | null {
+    return request.headers.get("Cookie")
+}
+
+export function wrapFetch({
+    fetch,
+    baseUrl,
+    cookieHeader,
+    cookiePath,
+    cookies,
+    credentials,
+    headers,
+    ensureOk,
+}: {
+    fetch: typeof global.fetch
+    baseUrl?: string | URL
+    cookieHeader?: string | null
+    cookiePath?: string
+    cookies?: Cookies
+    credentials?: RequestCredentials
+    headers?: HeadersInit
+    ensureOk?: boolean
+}): typeof global.fetch {
+    return async (input, init?) => {
+        let response: Response
+
+        try {
+            init ??= {}
+
+            if (input instanceof Request) {
+                init.headers = new Headers(init.headers || input.headers)
+            } else {
+                init.headers = new Headers(init.headers)
+            }
+
+            if (baseUrl) {
+                if (typeof input === "string" || input instanceof URL) {
+                    input = concatenateUrl(input, baseUrl)
+                } else if (input instanceof Request) {
+                    input = new Request(concatenateUrl(input.url, baseUrl), input)
+                }
+            }
+
+            if (cookieHeader) {
+                init.headers.set("Cookie", cookieHeader)
+            }
+
+            if (credentials) {
+                init.credentials = credentials
+            }
+
+            if (headers) {
+                for (const [key, value] of Object.entries(headers)) {
+                    init.headers.set(key, value)
+                }
+            }
+
+            if (input instanceof Request) {
+                response = await fetch(new Request(input, init))
+            } else {
+                response = await fetch(input, init)
+            }
+
+            if (cookies) {
+                for (const cookie of parser.parse(response.headers.getSetCookie(), { decodeValues: false })) {
+                    cookies.set(cookie.name, cookie.value, {
+                        encode: (value) => value,
+                        path: cookiePath || "/",
+                        expires: cookie.expires,
+                        maxAge: cookie.maxAge,
+                    })
+                }
+            }
+        } catch (e) {
+            error(500)
+        }
+
+        if (ensureOk && response.status >= 400 && response.status < 600) {
+            error(response.status)
+        }
+
+        return response
+    }
+}
