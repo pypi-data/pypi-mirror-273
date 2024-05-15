a3f17c6a9b4314344ccc0b35fa436f49657f4f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 05:04:21 +0000

    Add package BartePaySDK/0.0.5/BartePaySDK-0.0.5.tar.gz

---
+++ packages/BartePaySDK/BartePaySDK-0.0.5.tar.gz/BartePaySDK-0.0.5/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='BartePaySDK',
+    version='0.0.5',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='Engenharia de Plataforma da Barte',
+    author_email='devops@barte.com',
+    description='SDK para interação com a API da Barte.',
+    license='MIT',
+    keywords='barte pay sdk',
+)
