71a63680e3d1fcf2c110d08c4e1601e4b7755460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 04:03:24 +0000

    Add package chainbench/0.7.0/chainbench-0.7.0-py3-none-any.whl

---
+++ packages/chainbench/chainbench-0.7.0-py3-none-any.whl/chainbench/profile/evm/debug_trace.py
@@ -0,0 +1,32 @@
+"""
+EVM profile (debug_trace methods).
+"""
+
+from locust import constant_pacing
+
+from chainbench.user import EvmUser
+from chainbench.user.tag import clear_tags
+
+
+class EvmDebugTraceProfile(EvmUser):
+    wait_time = constant_pacing(10)
+
+    rpc_calls = {
+        # EvmUser.debug_trace_transaction: 1229,
+        # EvmUser.trace_transaction: 484,
+        # EvmUser.trace_block: 143,
+        # EvmUser.debug_trace_call: 60,
+        EvmUser.trace_call: 1,
+        EvmUser.trace_call_many: 1,
+        EvmUser.trace_filter: 1,
+        # EvmUser.trace_replay_block_transactions: 21,
+        # EvmUser.trace_replay_transaction: 11,
+        # EvmUser.debug_trace_block_by_hash: 6,
+        # EvmUser.debug_trace_block_by_number: 6,
+        # EvmUser.debug_get_raw_receipts: 1,
+    }
+
+    tasks = EvmUser.expand_tasks(rpc_calls)
+
+    # Clear locust tags to ignore debug-trace method exclusion
+    clear_tags(tasks)
