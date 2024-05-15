b50679ce4ba3c6ed521944f043b1fdf60783c1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 05:04:05 +0000

    Add package arm-adi/3.21.1/arm_adi-3.21.1-cp311-cp311-manylinux1_x86_64.whl

---
+++ packages/arm-adi/arm_adi-3.21.1-cp311-cp311-manylinux1_x86_64.whl/adi_py/__init__.py
@@ -0,0 +1,25 @@
+"""-----------------------------------------------------------------------
+This module provides the new ADI Python bindings which incorporate full
+XArray compatibility.
+-----------------------------------------------------------------------"""
+
+from .constants import (
+    ADIAtts,
+    ADIDatasetType,
+    BitAssessment,
+    SplitMode,
+    SpecialXrAttributes,
+    TransformAttributes
+)
+
+from .process import Process
+
+from .utils import DatastreamIdentifier
+
+from .xarray_accessors import ADIDatasetAccessor, ADIDataArrayAccessor
+
+from .exception import SkipProcessingIntervalException, DatasetConversionException, LogLevel
+
+from .logger import ADILogger
+
+
