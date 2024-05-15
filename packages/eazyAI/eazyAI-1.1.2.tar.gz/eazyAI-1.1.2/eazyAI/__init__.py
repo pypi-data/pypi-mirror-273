8f3d3649f37df280df856cef6b799b9f347e040e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 03:05:11 +0000

    Add package eazyAI/1.1.2/eazyAI-1.1.2.tar.gz

---
+++ packages/eazyAI/eazyAI-1.1.2.tar.gz/eazyAI-1.1.2/eazyAI/__init__.py
@@ -0,0 +1,3 @@
+from .main import just_plot
+from .main import ezhelp
+from .main import gen_data
---
+++ packages/eazyAI/eazyAI-1.1.2.tar.gz/eazyAI-1.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "eazyAI",
+    version = '1.1.02',
+    packages = find_packages(),
+    author = 'Puneet Dhankar',
+    author_email = "puneetd2005@gmail.com",
+    description = 'A one function go library for Ski-Kit learn and TensorFlow AI/ML models build-in.',
+    url = 'https://github.com/self-Puneet/eazyAI',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'tensorflow',
+    ],
+)
