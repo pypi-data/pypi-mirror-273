dd4b5d1770c0f6ae65b72fd24d7f3ae6da945195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 14:04:16 +0000

    Add package ray/2.22.0/ray-2.22.0-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/ray/ray-2.22.0-cp310-cp310-manylinux2014_x86_64.whl/ray/rllib/examples/custom_logger.py
@@ -0,0 +1,6 @@
+msg = """
+This script has been moved to
+`ray.rllib.examples.ray_tune.custom_logger.py`
+"""
+
+raise NotImplementedError(msg)
---
+++ packages/ray/ray-2.22.0-cp310-cp310-manylinux2014_x86_64.whl/ray/rllib/examples/fractional_gpus.py
@@ -0,0 +1,6 @@
+msg = """
+This script has been moved to
+`ray.rllib.examples.gpus.fractional_gpus.py`
+"""
+
+raise NotImplementedError(msg)
