0e186e3fc64d6d7ec977f491ff975e9cd4350eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 04:03:34 +0000

    Add package magolib/0.0.1/magolib-0.0.1-py3-none-any.whl

---
+++ packages/magolib/magolib-0.0.1-py3-none-any.whl/magolib/__init__.py
@@ -0,0 +1,24 @@
+'''
+Magolib is a library that provides functions to convert between maghi and meters.
+It also provides functions to format and parse maghi and meters.
+
+The library provides the following functions:
+
+- `maghi_to_meters`: Convert maghi to meters.
+- `meters_to_maghi`: Convert meters to maghi.
+- `format_maghi`: Format a maghi value as a string.
+- `convert_maghi`: Convert a value between maghi and meters.
+- `parse_meters`: Parse a value in meters.
+- `parse_maghi`: Parse a value in maghi.
+'''
+
+from magolib.magolib import (
+    maghi_to_meters,
+    meters_to_maghi,
+    format_maghi,
+    convert_maghi,
+    parse_meters,
+    parse_maghi
+)
+
+__version__ = "0.0.1"
