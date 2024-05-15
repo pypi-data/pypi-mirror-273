f64189dae1a6919ccff1b0d20655098303c97027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:04:05 +0000

    Add package que-binary/0.4/que_binary-0.4.tar.gz

---
+++ packages/que-binary/que_binary-0.4.tar.gz/que_binary-0.4/README.md
@@ -0,0 +1,1 @@
+##### quick collection of python files
---
+++ packages/que-binary/que_binary-0.4.tar.gz/que_binary-0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="que_binary",
+    version="0.4",
+    packages=find_packages(),
+    install_requires=[],
+    author="vegito5ama",
+    # author_email='vegitosan@protonmail.com',
+    description="A quick collection of  Python files",
+    long_description="This library contains various Python files for different purposes.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
