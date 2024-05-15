9f573801a7d09374ada5f992dddef2727e719abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:04:11 +0000

    Add package onediffx/1.0.0/onediffx-1.0.0-py3-none-any.whl

---
+++ packages/onediffx/onediffx-1.0.0-py3-none-any.whl/onediffx/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "1.0.0"
+from onediff.infer_compiler import compile_options
+from .compilers.diffusion_pipeline_compiler import compile_pipe, save_pipe, load_pipe
+
+__all__ = ["compile_pipe", "compile_options", "save_pipe", "load_pipe"]
