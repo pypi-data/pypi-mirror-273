e82ba1e488f080b39c4158257d0d5495ec135428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 16:03:58 +0000

    Add package kaxe/0.1.5/kaxe-0.1.5-py3-none-any.whl

---
+++ packages/kaxe/kaxe-0.1.5-py3-none-any.whl/kaxe/plot/__init__.py
@@ -0,0 +1,10 @@
+
+from .themes import Themes
+from .log import LogPlot
+from .standard import Plot, XYPLOT
+from .polar import PolarPlot, POLARPLOT
+from .box import BoxPlot
+
+class identities:
+    XYPLOT = XYPLOT
+    POLAR = POLARPLOT
