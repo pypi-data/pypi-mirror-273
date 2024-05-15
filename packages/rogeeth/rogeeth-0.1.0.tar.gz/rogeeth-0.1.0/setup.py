c9da643cfa51dad985a124ee2e0ebfcc3f51d4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 05:04:20 +0000

    Add package rogeeth/0.1.0/rogeeth-0.1.0.tar.gz

---
+++ packages/rogeeth/rogeeth-0.1.0.tar.gz/rogeeth-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+name='rogeeth',
+version='0.1.0',
+author='Maari',
+author_email='iamvasanthanpb@gmail.com',
+description='w team rlt package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+keywords='rogeeth'
+)
+
