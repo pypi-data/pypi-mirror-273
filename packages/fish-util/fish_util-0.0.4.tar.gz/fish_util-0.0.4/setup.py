2ad0b13d272dca3bee57b0b88cb347d720d4d8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 03:06:15 +0000

    Add package fish-util/0.0.4/fish_util-0.0.4.tar.gz

---
+++ packages/fish-util/fish_util-0.0.4.tar.gz/fish_util-0.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from fish_util.src.yaml_util import update_version
+
+packages = find_packages()
+print(packages)
+
+# 使用
+settings = update_version("settings.yaml")
+print(f"New Settings: {settings}")
+
+setup(
+    name=settings["name"],
+    version=settings["version"],
+    packages=packages,
+    description=settings["description"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author=settings["author"],
+    author_email=settings["author_email"],
+    url=settings["url"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
