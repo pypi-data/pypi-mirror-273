741d5ee3649ef4d86135ef8b41586bcf69c414a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 14:04:14 +0000

    Add package ray/2.22.0/ray-2.22.0-cp311-cp311-manylinux2014_x86_64.whl

---
+++ packages/ray/ray-2.22.0-cp311-cp311-manylinux2014_x86_64.whl/ray/rllib/examples/complex_struct_space.py
@@ -0,0 +1,6 @@
+msg = """
+This script has been moved to
+`ray.rllib.examples._old_api_stack.complex_struct_space.py`
+"""
+
+raise NotImplementedError(msg)
---
+++ packages/ray/ray-2.22.0-cp311-cp311-manylinux2014_x86_64.whl/ray/rllib/examples/multi_agent_independent_learning.py
@@ -0,0 +1,6 @@
+msg = """
+This script has been moved to
+`ray.rllib.examples.multi_agent.independent_learning.py`
+"""
+
+raise NotImplementedError(msg)
