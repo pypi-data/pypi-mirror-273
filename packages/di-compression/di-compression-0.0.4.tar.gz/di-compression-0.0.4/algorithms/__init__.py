8c993155d7210d432ab806a0d0858eccc89de71f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:03:53 +0000

    Add package di-compression/0.0.4/di-compression-0.0.4.tar.gz

---
+++ packages/di-compression/di-compression-0.0.4.tar.gz/di-compression-0.0.4/algorithms/__init__.py
@@ -0,0 +1,5 @@
+from algorithms.lzw import LZW
+from algorithms.huffman import HuffmanCompression
+from algorithms.lz77 import LZ77
+from algorithms.deflate import Deflate
+from algorithms.lz78 import LZ78
