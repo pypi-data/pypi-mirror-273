a95ee372d693f432ecebdc4829cc8dc87b370e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:03:51 +0000

    Add package BiblioDig/1.0.1/bibliodig-1.0.1.tar.gz

---
+++ packages/BiblioDig/bibliodig-1.0.1.tar.gz/bibliodig-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BiblioDig',
+    version='1.0.1',
+    author='Luz Lizeth Vazquez Garcia',
+    author_email='luzlizet35@gmail.com',
+    description='Biblioteca que busque un libro ya sea por nombre, autor, año o alguna otra caracteristica',
+    packages=["BiblioDig_Py"],
+    package_data={'BiblioDig_Py': ['books.csv']},
+    install_requires=[
+        'pandas',
+        'twine',
+        'wheel',
+        'setuptools',
+    ],
+)
