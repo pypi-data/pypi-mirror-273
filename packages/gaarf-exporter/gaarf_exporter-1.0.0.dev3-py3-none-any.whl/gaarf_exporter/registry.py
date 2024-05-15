524f8185b4e51a2164c0988990395d98643a2d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 16:03:50 +0000

    Add package gaarf-exporter/1.0.0.dev3/gaarf_exporter-1.0.0.dev3-py3-none-any.whl

---
+++ packages/gaarf-exporter/gaarf_exporter-1.0.0.dev3-py3-none-any.whl/gaarf_exporter/registry.py
@@ -0,0 +1,205 @@
+# Copyright 2023 Google LLC
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module for defining collectors.
+
+Collectors are converted to gaarf queries that are sent to Ads API.
+"""
+from __future__ import annotations
+
+import glob
+import itertools
+import pathlib
+from collections import defaultdict
+from collections.abc import MutableSet
+
+import yaml
+
+from gaarf_exporter import collector as query_collector
+
+_SCRIPT_DIR = pathlib.Path(__file__).parent
+
+
+class Registry:
+  """Maps collector names to corresponding classes.
+
+  Registry simplifies searching for collectors as well as adding new ones.
+
+  Attributes:
+    collectors: Mapping between collector names and corresponding class.
+  """
+
+  def __init__(self, collectors: dict | None = None) -> None:
+    """Creates Registry based on module level variable _REGISTRY."""
+    self.collectors = dict(collectors) or dict()
+
+  @classmethod
+  def from_collector_definitions(
+      cls,
+      path_to_definitions: str = f'{_SCRIPT_DIR}/collector_definitions/*.yaml'
+  ) -> Registry:
+    collectors = define_collectors(path_to_definitions)
+    return cls(collectors)
+
+  @property
+  def default_collectors(self) -> CollectorSet:
+    """Helper for getting only default collectors from the registry."""
+    return CollectorSet(collectors=set(self.collectors.get('default').values()))
+
+  @property
+  def all_subregistries(self) -> CollectorSet:
+    """Helper for getting only sub-registries. """
+    collector_names = set()
+    for name, collector in self.collectors.items():
+      if isinstance(collector, dict):
+        collector_names.add(name)
+    subregistries_collector_names = ','.join(collector_names)
+    return self.find_collectors(collector_names=subregistries_collector_names)
+
+  @property
+  def all_collectors(self) -> CollectorSet:
+    """Helper for getting all collectors from the registry."""
+    all_collector_names = ','.join(self.collectors.keys())
+    return self.find_collectors(collector_names=all_collector_names)
+
+  def find_collectors(self, collector_names: str | None = None) -> CollectorSet:
+    """Extracts collectors from registry and returns their initialized collectors.
+
+    Args:
+      collector_names:
+        Names of collectors that need to be fetched from registry.
+
+    Returns:
+      Found collectors.
+    """
+    if not collector_names:
+      return CollectorSet()
+    if collector_names == 'all':
+      return self.all_collectors
+    collectors_subset = [
+        collector for name, collector in self.collectors.items()
+        if name in collector_names.strip().split(',')
+    ]
+    found_collectors = set()
+    for collector in collectors_subset:
+      if isinstance(collector, dict):
+        for collector_ in collector.values():
+          found_collectors.add(collector_)
+      else:
+        found_collectors.add(collector)
+    return CollectorSet(collectors=set(found_collectors))
+
+
+class CollectorSet(MutableSet):
+  """Represent a set of collectors returned from Registry."""
+
+  def __init__(self,
+               collectors: set[query_collector.Collector] | None = None,
+               service_collectors: bool = True) -> None:
+    """Initializes CollectorSet based on provided collectors."""
+    self._collectors = collectors or set()
+    self._service_collectors = service_collectors
+
+  @property
+  def collectors(self) -> set[query_collector.Collector]:
+    """Return customized or original collectors of the CollectorSet."""
+    self.deduplicate_collectors()
+    if self._service_collectors:
+      # FIXME: ServiceCollector might not be of mapping type
+      has_service_collector = any([
+          isinstance(collector, query_collector.ServiceCollector)
+          for collector in self._collectors
+      ])
+      if not has_service_collector:
+        valid_collector_levels = [
+            collector.level
+            for collector in self._collectors
+            if collector.level != query_collector.CollectorLevel.UNKNOWN
+        ]
+        if valid_collector_levels:
+          default_service_collector = (
+              query_collector.create_default_service_collector(
+                  min(valid_collector_levels)))
+          self._collectors.add(default_service_collector)
+
+    return self._collectors
+
+  def deduplicate_collectors(self) -> None:
+    """Dedupicates collectors in the set.
+
+    If there are similar collectors in the list return only those with
+    the lowest level.
+    """
+    combinations = itertools.combinations(self._collectors, 2)
+    for collector_1, collector_2 in combinations:
+      if collector_1.is_similar(collector_2):
+        max_collector = max(collector_1, collector_2)
+        self._collectors.remove(max_collector)
+
+  def customize(self, kwargs: dict) -> None:
+    """Changes collectors in the set based on provided arguments mapping.
+
+    Args:
+      kwargs:
+        Mapping between name and values of elements in collector to be
+        customized.
+    """
+    for collector in self.collectors:
+      collector.customize(kwargs)
+
+  def __bool__(self):
+    return bool(self.collectors)
+
+  def __eq__(self, other) -> bool:
+    return self.collectors == other.collectors
+
+  def __contains__(self, key: query_collector.Collector) -> bool:
+    return key in self.collectors
+
+  def __iter__(self):
+    return iter(self.collectors)
+
+  def __len__(self) -> int:
+    return len(self.collectors)
+
+  def add(self, collector) -> None:
+    self._collectors.add(collector)
+
+  def discard(self, collector) -> None:
+    self._collectors.discard(collector)
+
+
+def _read_files(path):
+  with open(path, 'r', encoding='utf-8') as f:
+    data = yaml.safe_load(f)
+  return data
+
+
+def define_collectors(path: str):
+  _registry: dict = defaultdict(dict)
+  files = [file for file in glob.glob(path)]
+  results = [_read_files(file) for file in files]
+  for data in results:
+    for collector_data in data:
+      if collector_data.get('type') == 'service' or collector_data.get(
+          'type', {}).get('service'):
+        coll = query_collector.ServiceCollector.from_definition(collector_data)
+      else:
+        coll = query_collector.Collector.from_definition(collector_data)
+      _registry[coll.name] = coll
+      if subregistries := collector_data.get('registries'):
+        for subregistry in subregistries:
+          _registry[subregistry].update({coll.name: coll})
+      if 'has_conversion_split' in collector_data:
+        conv_coll = coll.create_conv_collector()
+        _registry[conv_coll.name] = conv_coll
+  return _registry
