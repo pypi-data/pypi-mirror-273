4958e6c7c7cafe9d70d00c86a2450a005102ad12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 05:03:42 +0000

    Add package galaxy-datasets/0.0.18/galaxy-datasets-0.0.18.tar.gz

---
+++ packages/galaxy-datasets/galaxy-datasets-0.0.18.tar.gz/galaxy-datasets-0.0.18/galaxy_datasets/pytorch/galaxy_datamodule.py
@@ -0,0 +1,280 @@
+
+from typing import Optional, Union
+import logging
+from functools import partial
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+# import torch
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from torchvision.transforms import v2 as T
+
+from galaxy_datasets.pytorch import galaxy_dataset
+from galaxy_datasets.transforms import default_transforms
+
+
+# https://pytorch-lightning.readthedocs.io/en/stable/extensions/datamodules.html
+class GalaxyDataModule(pl.LightningDataModule):
+    # takes generic catalogs (which are already downloaded and happy),
+    # splits if needed, and creates generic datasets->dataloaders etc
+    # easy to make dataset-specific default transforms if desired
+    def __init__(
+        self,
+        label_cols,
+        # provide full catalog for automatic split, or...
+        catalog=None,
+        train_fraction=0.7,
+        val_fraction=0.1,
+        test_fraction=0.2,
+        # provide train/val/test catalogs for your own previous split
+        train_catalog: Optional[pd.DataFrame]=None,
+        val_catalog: Optional[pd.DataFrame]=None,
+        test_catalog: Optional[pd.DataFrame]=None,
+        predict_catalog: Optional[pd.DataFrame]=None,
+        # augmentation params (sensible supervised defaults)
+        greyscale=False,
+        # album=False,  # now True always
+        crop_scale_bounds=(0.7, 0.8),
+        crop_ratio_bounds=(0.9, 1.1),
+        resize_after_crop=224,
+        custom_albumentation_transform=None,  # will override the settings above. If tuple, assume (train, test) transforms
+        custom_torchvision_transform=None,  # similarly
+        # hardware params
+        batch_size=256,  # careful - will affect final performance
+        use_memory=False,  # deprecated
+        num_workers=4,
+        prefetch_factor=4,
+        seed=42
+    ):
+        super().__init__()
+
+        if catalog is not None:  # catalog provided, should not also provide explicit split catalogs
+            assert train_catalog is None
+            assert val_catalog is None
+            assert test_catalog is None
+        else:  # catalog not provided, must provide explicit split catalogs - at least one
+            assert (train_catalog is not None) or (val_catalog is not None) or (test_catalog is not None) or (predict_catalog is not None)
+            # see setup() for how having only some explicit catalogs is handled
+            
+
+        self.label_cols = label_cols
+
+        self.catalog = catalog
+        self.train_catalog = train_catalog
+        self.val_catalog = val_catalog
+        self.test_catalog = test_catalog
+        self.predict_catalog = predict_catalog
+
+        self.batch_size = batch_size
+
+        self.use_memory = use_memory
+        if self.use_memory:
+            raise NotImplementedError
+
+        self.num_workers = num_workers
+        self.seed = seed
+
+        assert np.isclose(train_fraction + val_fraction + test_fraction, 1.)
+        self.train_fraction = train_fraction
+        self.val_fraction = val_fraction
+        self.test_fraction = test_fraction
+
+        if self.num_workers == 0:
+            logging.warning('num_workers=0, setting prefetch=None and timeout=0 as no multiprocessing')
+            self.prefetch_factor = None
+            self.dataloader_timeout = 0
+        else:
+            self.prefetch_factor = prefetch_factor
+            self.dataloader_timeout = 600  # seconds aka 10 mins
+
+        logging.info('Num workers: {}'.format(self.num_workers))
+        logging.info('Prefetch factor: {}'.format(self.prefetch_factor))
+
+
+        self.custom_albumentation_transform = custom_albumentation_transform
+        self.custom_torchvision_transform = custom_torchvision_transform
+        self.resize_after_crop = resize_after_crop
+        self.crop_scale_bounds = crop_scale_bounds
+        self.crop_ratio_bounds = crop_ratio_bounds
+        self.greyscale = greyscale
+
+        if custom_torchvision_transform is not None:
+            logging.info('Using custom torchvision transform for augmentations')
+            self.transform_with_torchvision()
+        else:
+            logging.info('Using albumentations transform for augmentations')
+            self.transform_with_albumentations()
+
+    def transform_with_torchvision(self):
+        # TODO
+        # galaxy_datasets loads PIL images, likely need PILToTensor() in any custom torchvision transform. review.
+        if isinstance(self.custom_torchvision_transform, tuple):
+            logging.info('Using different torchvision transforms for train and test')
+            assert len(self.custom_torchvision_transform) == 2
+            self.train_transform = self.custom_torchvision_transform[0]
+            self.test_transform = self.custom_torchvision_transform[1]
+        else:
+            self.train_transform = self.custom_torchvision_transform
+            self.test_transform = self.custom_torchvision_transform
+
+    def transform_with_albumentations(self):
+        import albumentations as A
+        if self.custom_albumentation_transform is not None:
+            if isinstance(self.custom_albumentation_transform, tuple):
+                logging.info('Using different albumentations transforms for train and test')
+                assert len(self.custom_albumentation_transform) == 2
+                assert isinstance(self.custom_albumentation_transform[0], A.Compose)
+                self.train_transform = partial(do_transform, transforms_to_apply=self.custom_albumentation_transform[0])
+                self.test_transform = partial(do_transform, transforms_to_apply=self.custom_albumentation_transform[1])
+            else:
+                logging.info('Using the same custom albumentations transforms for train and test')
+                self.train_transform = partial(do_transform, transforms_to_apply=self.custom_albumentation_transform)
+                self.test_transform = partial(do_transform, transforms_to_apply=self.custom_albumentation_transform)
+        else:
+            logging.info('Using basic albumentations transforms for augmentations')
+            # gives a transforms = Compose() object
+            transforms_to_apply = default_transforms(
+                crop_scale_bounds=self.crop_scale_bounds,
+                crop_ratio_bounds=self.crop_ratio_bounds,
+                resize_after_crop=self.resize_after_crop,
+                pytorch_greyscale=self.greyscale
+            )
+            self.train_transform = partial(do_transform, transforms_to_apply=transforms_to_apply)
+            self.test_transform = partial(do_transform, transforms_to_apply=transforms_to_apply)
+
+        
+
+    # only called on main process
+    def prepare_data(self):
+        pass   # could include some basic checks
+
+    # called on every gpu
+
+    def setup(self, stage: Optional[str] = None):
+
+        self.specify_catalogs(stage)
+
+        # Assign train/val datasets for use in dataloaders
+        # assumes dataset_class has these standard args
+        if stage == "fit" or stage is None:
+            self.train_dataset = galaxy_dataset.GalaxyDataset(
+                catalog=self.train_catalog, label_cols=self.label_cols, transform=self.train_transform
+            )
+            self.val_dataset = galaxy_dataset.GalaxyDataset(
+                catalog=self.val_catalog, label_cols=self.label_cols, transform=self.test_transform
+            )
+
+        # Assign test dataset for use in dataloader(s)
+        if stage == "test" or stage is None:
+            self.test_dataset = galaxy_dataset.GalaxyDataset(
+                catalog=self.test_catalog, label_cols=self.label_cols, transform=self.test_transform
+            )
+
+        if stage == 'predict':  # not set up by default with stage=None, only if explicitly requested
+            if self.predict_catalog is None:
+                raise ValueError('Attempting to predict, but GalaxyDataModule was init without a predict_catalog arg. init with GalaxyDataModule(predict_catalog=some_catalog, ...)')
+            self.predict_dataset = galaxy_dataset.GalaxyDataset(
+                catalog=self.predict_catalog, label_cols=self.label_cols, transform=self.test_transform
+            )
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers, pin_memory=True, persistent_workers=self.num_workers > 0, prefetch_factor=self.prefetch_factor, timeout=self.dataloader_timeout, drop_last=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, pin_memory=True, persistent_workers=self.num_workers > 0, prefetch_factor=self.prefetch_factor, timeout=self.dataloader_timeout)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, pin_memory=True, persistent_workers=self.num_workers > 0, prefetch_factor=self.prefetch_factor, timeout=self.dataloader_timeout)
+
+    def predict_dataloader(self):
+        return DataLoader(self.predict_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, pin_memory=True, persistent_workers=self.num_workers > 0, prefetch_factor=self.prefetch_factor, timeout=self.dataloader_timeout)
+
+    def specify_catalogs(self, stage):
+        if self.catalog is not None:
+            # will split the catalog into train, val, test here
+            self.train_catalog, hidden_catalog = train_test_split(
+                self.catalog, train_size=self.train_fraction, random_state=self.seed
+            )
+            self.val_catalog, self.test_catalog = train_test_split(
+                hidden_catalog, train_size=self.val_fraction/(self.val_fraction + self.test_fraction), random_state=self.seed
+            )
+            del hidden_catalog
+        else:
+            # assume you have passed pre-split catalogs
+            # (maybe not all, e.g. only a test catalog, or only train/val catalogs)
+            if stage == 'predict':
+                assert self.predict_catalog is not None
+            elif stage == 'test':
+                # only need test
+                assert self.test_catalog is not None
+            elif stage == 'fit':
+                # only need train and val
+                assert self.train_catalog is not None
+                assert self.val_catalog is not None
+            else:
+                # need all three (predict is still optional)
+                assert self.train_catalog is not None
+                assert self.val_catalog is not None
+                assert self.test_catalog is not None
+            # (could write this shorter but this is clearest)
+            
+
+def do_transform(img, transforms_to_apply):
+    # albumentations expects np array, and returns dict keyed by "image"
+    # transpose changes from BHWC (numpy/TF style) to BCHW (torch style) 
+    # cannot use a lambda or define here because must be pickleable for multi-gpu
+    return np.transpose(transforms_to_apply(image=np.array(img))["image"], axes=[2, 0, 1]).astype(np.float32)
+
+
+
+# deprecated for albumentations
+# def default_torchvision_transforms(greyscale, resize_size, crop_scale_bounds, crop_ratio_bounds):
+#     # refactored out for use elsewhere, if need exactly these transforms
+#     # assume input is 0-255 uint8 tensor
+
+#     # automatically normalises from 0-255 int to 0-1 float
+#     transforms_to_apply = [transforms.ToTensor()]  # dataset gives PIL image currently
+
+#     if greyscale:
+#         # transforms.Grayscale() adds perceptual weighting to rgb channels
+#         transforms_to_apply += [GrayscaleUnweighted()]
+
+#     transforms_to_apply += [
+#         transforms.RandomResizedCrop(
+#             size=resize_size,  # assumed square
+#             scale=crop_scale_bounds,  # crop factor
+#             ratio=crop_ratio_bounds,  # crop aspect ratio
+#             interpolation=transforms.InterpolationMode.BILINEAR),  # new aspect ratio
+#         transforms.RandomHorizontalFlip(),
+#         transforms.RandomRotation(
+#             degrees=180., interpolation=transforms.InterpolationMode.BILINEAR)
+#     ]
+    
+#     return transforms_to_apply
+
+# torchvision
+# class GrayscaleUnweighted(torch.nn.Module):
+
+#     def __init__(self, num_output_channels=1):
+#         super().__init__()
+#         self.num_output_channels = num_output_channels
+
+#     def forward(self, img):
+#         """
+#         PyTorch (and tensorflow) does greyscale conversion as a *weighted* mean by default (as colours have different perceptual brightnesses).
+#         Here, do a simple mean.
+#         Args:
+#             img (Tensor): Image to be converted to grayscale.
+
+#         Returns:
+#             Tensor: Grayscaled image.
+#         """
+#         # https://pytorch.org/docs/stable/generated/torch.mean.html
+#         return img.mean(dim=-3, keepdim=True)  # (..., C, H, W) convention
+
+#     def __repr__(self):
+#         return self.__class__.__name__ + '(num_output_channels={0})'.format(self.num_output_channels)
+
+
---
+++ packages/galaxy-datasets/galaxy-datasets-0.0.18.tar.gz/galaxy-datasets-0.0.18/galaxy_datasets/pytorch/galaxy_dataset.py
@@ -0,0 +1,150 @@
+from typing import List
+import logging
+
+import numpy as np
+import pandas as pd
+from torch.utils.data import Dataset
+from PIL import Image
+
+# not a strict requirement unless loading fits
+try:
+    from astropy.io import fits
+except ImportError:
+    pass
+
+
+
+# https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
+class GalaxyDataset(Dataset):
+    def __init__(self, catalog: pd.DataFrame, label_cols=None, transform=None, target_transform=None):
+        """
+        Create custom PyTorch Dataset using catalog of galaxy images
+
+        Catalog and images should already be downloaded.
+        Use methods under galaxy_datasets.prepared_datasets e.g. prepared_datasets.gz2()
+        
+        Note that if you want the canonical datasets (i.e. using the standard train/test split)
+        you can use the galaxy_datasets.pytorch.datasets instead of this class e.g. 
+
+        ```
+        datasets.GZ2Dataset(
+            root=os.path.join(some_dir, 'gz2'),
+            download=True
+        )
+        ```
+        
+        Reads a catalog of jpeg galaxy images
+        if transform is from albumentations, datamodule should know about the transform including 
+        catalog should be split already
+        should have correct image locations under file_loc
+
+        Args:
+            catalog (pd.DataFrame): with images under 'file_loc' and labels under label_cols
+            label_cols (List): columns in catalog with labels
+            transform (callable, optional): See Pytorch Datasets. Defaults to None.
+            target_transform (callable, optional): See Pytorch Datasets. Defaults to None.
+        """
+        # internal catalog has id_str as index for query speed
+        # (dataset.catalog still returns the int-indexed catalog via the catalog method below)
+        # self._catalog = catalog.copy().set_index('id_str', verify_integrity=True)
+        self.catalog = catalog
+        
+        self.label_cols = label_cols
+        self.transform = transform
+        self.target_transform = target_transform
+
+
+    def __len__(self) -> int:
+        return len(self.catalog)
+
+
+    def __getitem__(self, idx: int):
+        #the index is id_str so can use that for quick search on 1M+ catalo
+        # galaxy = self._catalog.loc[idx]
+        galaxy = self.catalog.iloc[idx]
+
+        # load the image into memory
+        image_loc = galaxy['file_loc']
+        try:
+            image = load_img_file(image_loc)
+            # HWC PIL image
+            # logging.info((image.shape, torch.max(image), image.dtype, label))  # always 0-255 uint8
+        except Exception as e:
+            logging.critical('Cannot load {}'.format(image_loc))
+            raise e
+    
+        if self.transform:
+            image = self.transform(image)
+
+        if self.label_cols is None:
+            return image
+        else:
+            # load the labels. If no self.label_cols, will 
+            label = get_galaxy_label(galaxy, self.label_cols)
+
+            if self.target_transform:
+                label = self.target_transform(label)
+
+            return image, label
+
+def load_img_file(loc):
+    # wrapper around specific loaders below
+    # could be more performant with a dict of {format:loader} but doubt significant
+    if loc.endswith('png'):
+        return load_png_file(loc)
+    elif loc.endswith('jpg'):
+        return load_jpg_file(loc)
+    elif loc.endswith('jpeg'):
+        return load_jpg_file(loc)
+    elif loc.endswith('fits'):
+        return load_fits_file(loc)  # careful, these often need a transform to have reasonable dynamic range
+    else:
+        raise ValueError('File format of {} not recognised - should be jpeg|jpg (preferred) or png'.format(loc))
+
+
+def load_jpg_file(loc):
+    im = Image.open(loc, mode='r') # HWC
+    im.load()  # avoid lazy open
+    return im
+    # below works, but deprecated due to simplejpeg install issue on my M1 mac
+    # let's just keep dependencies simple...
+    # with open(loc, 'rb') as f:
+        # return Image.fromarray(decode_jpeg(f.read()))  # HWC PIL image via simplejpeg
+# def decode_jpeg(encoded_bytes):
+#     return simplejpeg.decode_jpeg(encoded_bytes, fastdct=True, fastupsample=True)
+
+
+def load_png_file(loc):
+    # TODO now duplicate with the above
+    im = Image.open(loc, mode='r') # HWC
+    im.load()  # avoid lazy open
+    return im
+
+def load_fits_file(loc):  
+    x = fits.open(loc)[0].data.astype(np.float32)
+    # assumes single channel - add channel dimension
+    return np.expand_dims(x, axis=2)  # HWC
+
+
+def get_galaxy_label(galaxy: pd.Series, label_cols: List) -> np.ndarray:
+    # pytorch 1.12 is happy with float32 for both dirichlet and cross-entropy
+    return galaxy[label_cols].astype(np.float32).values.squeeze()  # squeeze for if there's one label_col
+
+
+if __name__ == '__main__':
+
+    # lazy test/example
+    import glob
+    mixed_file_paths = glob.glob('tests/test_data/png_jpg_mix/*')
+    assert len(mixed_file_paths) > 0
+    data = {
+        'file_loc': mixed_file_paths, 
+        'id_str': [str(x) for x in np.arange(len(mixed_file_paths))]
+    }
+    df = pd.DataFrame(data)
+
+    dataset = GalaxyDataset(catalog=df)
+    for im in dataset:
+        im = np.array(im)  # returns PIL.Image, if not given label_cols or transform
+        print(im.shape)
+        print(im.mean(), im.min(), im.max())
