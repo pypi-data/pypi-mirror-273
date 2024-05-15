f4d27b7f8f8ec0ed93b12d1a414c917ab428d5fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 05:04:25 +0000

    Add package sleipnirgroup-jormungandr/0.0.1.dev142/sleipnirgroup_jormungandr-0.0.1.dev142-cp312-cp312-win_amd64.whl

---
+++ packages/sleipnirgroup-jormungandr/sleipnirgroup_jormungandr-0.0.1.dev142-cp312-cp312-win_amd64.whl/jormungandr/cpp/optimization/BindSolverStatus.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) Sleipnir contributors
+
+#include <pybind11/pybind11.h>
+#include <sleipnir/optimization/SolverStatus.hpp>
+
+#include "Docstrings.hpp"
+
+namespace py = pybind11;
+
+namespace sleipnir {
+
+void BindSolverStatus(py::module_& optimization) {
+  using namespace pybind11::literals;
+
+  py::class_<SolverStatus> cls{optimization, "SolverStatus",
+                               DOC(sleipnir, SolverStatus)};
+  cls.def(py::init<>());
+  cls.def(py::init<ExpressionType, ExpressionType, ExpressionType,
+                   SolverExitCondition, double>(),
+          "cost_function_type"_a = ExpressionType::kNone,
+          "equality_constraint_type"_a = ExpressionType::kNone,
+          "inequality_constraint_type"_a = ExpressionType::kNone,
+          "exit_condition"_a = SolverExitCondition::kSuccess, "cost"_a = 0.0);
+  cls.def_readwrite("cost_function_type", &SolverStatus::costFunctionType,
+                    DOC(sleipnir, SolverStatus, costFunctionType));
+  cls.def_readwrite("equality_constraint_type",
+                    &SolverStatus::equalityConstraintType,
+                    DOC(sleipnir, SolverStatus, equalityConstraintType));
+  cls.def_readwrite("inequality_constraint_type",
+                    &SolverStatus::inequalityConstraintType,
+                    DOC(sleipnir, SolverStatus, inequalityConstraintType));
+  cls.def_readwrite("exit_condition", &SolverStatus::exitCondition,
+                    DOC(sleipnir, SolverStatus, exitCondition));
+  cls.def_readwrite("cost", &SolverStatus::cost,
+                    DOC(sleipnir, SolverStatus, cost));
+}
+
+}  // namespace sleipnir
