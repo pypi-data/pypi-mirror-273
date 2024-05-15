d66e07b298e7db716a3ce9058d08852496ee418a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 05:04:06 +0000

    Add package pythfinder/0.0.3.6/pythfinder-0.0.3.6.tar.gz

---
+++ packages/pythfinder/pythfinder-0.0.3.6.tar.gz/pythfinder-0.0.3.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+def find_image_files(directory):
+    image_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.png') or file.endswith('.jpg'):
+                image_files.append(os.path.relpath(os.path.join(root, file), directory))
+    return image_files
+
+
+setup(
+    name='pythfinder',
+    version='0.0.3.6',
+    license='MIT',
+    author='Contraș Adrian',
+    author_email='omegacoresincai@gmail.com',
+    description='Motion Planning library designed for FLL teams',
+    packages=find_packages(),
+    keywords=[
+        'motion-planning',
+        'mobile-robots',
+        'robotics',
+        'first-lego-league',
+        'first-robotics',
+        'fll'
+    ],
+    install_requires=[''],
+    include_package_data=True,
+    package_data={'pythfinder': find_image_files('pythfinder/Images')}
+    
+)
