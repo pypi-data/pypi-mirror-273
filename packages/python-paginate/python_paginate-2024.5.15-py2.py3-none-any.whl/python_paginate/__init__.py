0146729de531250671dd7c9cd9a3da3f0dc631be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 16:04:02 +0000

    Add package python-paginate/2024.5.15/python_paginate-2024.5.15-py2.py3-none-any.whl

---
+++ packages/python-paginate/python_paginate-2024.5.15-py2.py3-none-any.whl/python_paginate/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+pagation for python web frameworks
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Adds pagination support to python web frameworks.
+
+:copyright: (c) 2016 by Lix Xu.
+:license: BSD, see LICENSE for more details
+"""
+
+from __future__ import unicode_literals
+
+__version__ = "2024.05.15"
