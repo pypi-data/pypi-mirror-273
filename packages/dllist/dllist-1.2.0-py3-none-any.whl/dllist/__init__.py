1c71b83fa13fbecf894a3eeec1c851588ba30ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 06:04:25 +0000

    Add package dllist/1.2.0/dllist-1.2.0-py3-none-any.whl

---
+++ packages/dllist/dllist-1.2.0-py3-none-any.whl/dllist/__init__.py
@@ -0,0 +1,49 @@
+"""A reimplementation of Julia's dllist"""
+
+import platform
+import warnings
+from typing import List
+
+__version__ = "1.2.0"
+
+_system = platform.system().lower()
+if (
+    _system.startswith("linux")
+    or _system.startswith("freebsd")
+    or _system.startswith("openbsd")
+    or _system.startswith("sunos")
+    or _system.startswith("solaris")
+):
+    from .unix_like import _platform_specific_dllist
+elif _system.startswith("darwin"):
+    from .macos import _platform_specific_dllist
+elif _system.startswith("windows") or _system.startswith("microsoft"):
+    from .windows import _platform_specific_dllist
+else:
+
+    def _platform_specific_dllist() -> List[str]:
+        warnings.warn(
+            f"Unable to list loaded libraries for unsupported platform {_system}"
+        )
+        return []
+
+
+def dllist() -> List[str]:
+    """
+    List the dynamic libraries loaded by the current process.
+
+    This is a wrapper for platform-specific APIs on Windows, Linux, and macOS.
+    On other platforms, this function will return an empty list.
+
+    Returns
+    -------
+    List[str]
+        The names of the dynamic libraries loaded by the current process.
+    """
+    try:
+        return _platform_specific_dllist()
+    except Exception as e:
+        warnings.warn(
+            f"Unable to list loaded libraries: {e}",
+        )
+        return []
---
+++ packages/dllist/dllist-1.2.0-py3-none-any.whl/dllist/unix_like.py
@@ -0,0 +1,48 @@
+import ctypes
+import warnings
+from ctypes.util import find_library
+from typing import List
+
+# this uses functions common to Linux and a few other Unix-like systems
+# https://man7.org/linux/man-pages/man3/dl_iterate_phdr.3.html
+# https://man.freebsd.org/cgi/man.cgi?query=dl_iterate_phdr
+# https://man.openbsd.org/dl_iterate_phdr
+# https://docs.oracle.com/cd/E88353_01/html/E37843/dl-iterate-phdr-3c.html
+
+
+class dl_phdr_info(ctypes.Structure):
+    _fields_ = [
+        ("dlpi_addr", ctypes.c_void_p),
+        ("dlpi_name", ctypes.c_char_p),
+        ("dlpi_phdr", ctypes.c_void_p),
+        ("dlpi_phnum", ctypes.c_ushort),
+    ]
+
+
+@ctypes.CFUNCTYPE(
+    ctypes.c_int,
+    ctypes.POINTER(dl_phdr_info),
+    ctypes.c_size_t,
+    ctypes.POINTER(ctypes.py_object),
+)
+def info_callback(info, _size, data):
+    libraries = data.contents.value
+    try:
+        name = info.contents.dlpi_name.decode("utf-8")
+        libraries.append(name)
+    except:
+        warnings.warn(f"Could not decode library name {info.contents.dlpi_name}")
+
+    return 0
+
+
+def _platform_specific_dllist() -> List[str]:
+    libraries: List[str] = []
+    libc = ctypes.CDLL(find_library("c"))
+    libc.dl_iterate_phdr(info_callback, ctypes.byref(ctypes.py_object(libraries)))
+
+    if libraries:
+        # remove the first entry, which is the executable itself
+        libraries.pop(0)
+
+    return libraries
