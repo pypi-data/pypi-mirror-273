030e5f6853e647c8f3dc8a1967bc6e2aefa341cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 04:03:24 +0000

    Add package cloud-pak-operations-cli/0.7.3/cloud_pak_operations_cli-0.7.3-py3-none-any.whl

---
+++ packages/cloud-pak-operations-cli/cloud_pak_operations_cli-0.7.3-py3-none-any.whl/cpo/commands/adm/store_credentials.py
@@ -0,0 +1,41 @@
+#  Copyright 2021, 2024 IBM Corporation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import click
+
+import cpo.config
+
+from cpo.utils.logging import loglevel_command
+
+
+@loglevel_command()
+@click.option("--artifactory-password", help="Artifactory password")
+@click.option("--artifactory-username", help="Artifactory username")
+@click.option(
+    "--ibm-cloud-pak-for-data-entitlement-key",
+    help="IBM Cloud Pak for Data entitlement key (see https://myibm.ibm.com/products-services/containerlibrary)",
+)
+def store_credentials(
+    artifactory_password: str | None,
+    artifactory_username: str | None,
+    ibm_cloud_pak_for_data_entitlement_key: str | None,
+):
+    """Store credentials
+
+    Credentials are stored in ~/.cpo/credentials.json.
+    """
+
+    credentials_to_be_stored = locals().copy()
+
+    cpo.config.configuration_manager.store_credentials(credentials_to_be_stored)
