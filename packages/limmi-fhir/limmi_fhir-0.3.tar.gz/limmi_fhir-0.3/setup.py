d8a62ce9bef6f3288a870b3c0daf421d37d7af4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:03:35 +0000

    Add package limmi-fhir/0.3/limmi_fhir-0.3.tar.gz

---
+++ packages/limmi-fhir/limmi_fhir-0.3.tar.gz/limmi_fhir-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='limmi_fhir',
+    version='0.3',
+    packages=find_packages(),
+    description='Limmi FHIR Resource Library',
+    author='Limmi, Inc.',
+    author_email='todd@mentorhead.io',
+    url='https://github.com/yourusername/mypackage',
+    install_requires=[],
+)
