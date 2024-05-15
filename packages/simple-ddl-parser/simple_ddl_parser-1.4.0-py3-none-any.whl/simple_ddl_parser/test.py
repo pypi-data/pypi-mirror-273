dd1bced2f824771926e2b8e6fef166aa637bdecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 06:05:09 +0000

    Add package simple-ddl-parser/1.4.0/simple_ddl_parser-1.4.0-py3-none-any.whl

---
+++ packages/simple-ddl-parser/simple_ddl_parser-1.4.0-py3-none-any.whl/simple_ddl_parser/test.py
@@ -0,0 +1,28 @@
+from simple_ddl_parser import DDLParser
+import pprint
+
+ddl = """
+
+CREATE TABLE COMTCCMMNCODE
+(
+	CODE_ID               VARCHAR2(6)  NOT NULL ,
+	CODE_ID_NM            VARCHAR2(60)  NULL ,
+	CODE_ID_DC            VARCHAR2(200)  NULL ,
+	USE_AT                CHAR(1)  NULL ,
+	CL_CODE               CHAR(3)  NULL ,
+	FRST_REGIST_PNTTM     DATE  NULL ,
+	FRST_REGISTER_ID      VARCHAR2(20)  NULL ,
+	LAST_UPDT_PNTTM       DATE  NULL ,
+	LAST_UPDUSR_ID        VARCHAR2(20)  NULL ,
+CONSTRAINT  COMTCCMMNCODE_PK PRIMARY KEY (CODE_ID),
+CONSTRAINT  COMTCCMMNCODE_FK1 FOREIGN KEY (CL_CODE) REFERENCES COMTCCMMNCLCODE(CL_CODE) ON DELETE SET NULL
+);
+
+
+
+"""
+
+result = DDLParser(ddl).run(output_mode="oracle")
+pprint.pprint(result)
+
+
