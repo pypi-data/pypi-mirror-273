2763dd1791952bb97d788c5a5098d7e585dd0183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:03:38 +0000

    Add package fazah/3.31/fazah-3.31.tar.gz

---
+++ packages/fazah/fazah-3.31.tar.gz/fazah-3.31/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fazah',
+    version='3.31',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Aiden Lang, Will Foster',
+    author_email='ajlang5@wisc.edu, wjfoster2@wisc.edu',
+    packages=find_packages(),
+    install_requires=[
+        'deep_translator',
+        'langdetect'
+    ],
+)
