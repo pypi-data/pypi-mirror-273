1914c059c48bb44d3c91e0ce3ec088873d01a025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 04:04:14 +0000

    Add package avis-client/0.8.0/avis_client-0.8.0.tar.gz

---
+++ packages/avis-client/avis_client-0.8.0.tar.gz/avis_client-0.8.0/avis_client/api/__init__.py
@@ -0,0 +1,21 @@
+# flake8: noqa
+
+# import apis into api package
+from avis_client.api.api_keys_api import ApiKeysApi
+from avis_client.api.configuration_api import ConfigurationApi
+from avis_client.api.image_api import ImageApi
+from avis_client.api.image_attribute_api import ImageAttributeApi
+from avis_client.api.image_attribute_category_api import ImageAttributeCategoryApi
+from avis_client.api.inspection_api import InspectionApi
+from avis_client.api.membership_api import MembershipApi
+from avis_client.api.metadata_api import MetadataApi
+from avis_client.api.metadata_schema_api import MetadataSchemaApi
+from avis_client.api.ml_api import MlApi
+from avis_client.api.product_api import ProductApi
+from avis_client.api.product_category_api import ProductCategoryApi
+from avis_client.api.quality_criteria_api import QualityCriteriaApi
+from avis_client.api.result_api import ResultApi
+from avis_client.api.statistics_api import StatisticsApi
+from avis_client.api.teams_api import TeamsApi
+from avis_client.api.user_api import UserApi
+
