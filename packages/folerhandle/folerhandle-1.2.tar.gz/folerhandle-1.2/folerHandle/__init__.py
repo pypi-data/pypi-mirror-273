429f6fa0ab4aeaac81fd9c13adc7d70353f53a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 16:04:16 +0000

    Add package folerhandle/1.2/folerhandle-1.2.tar.gz

---
+++ packages/folerhandle/folerhandle-1.2.tar.gz/folerhandle-1.2/folerHandle/__init__.py
@@ -0,0 +1,6 @@
+"""
+文件夹分析库
+"""
+__version__ = "1.2"
+from .folderAnalysis import GetDirSize
+from .folderAnalysis import DirAnalysis
---
+++ packages/folerhandle/folerhandle-1.2.tar.gz/folerhandle-1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+"""
+@Author: 风吹落叶
+@Contact: waitKey1@outlook.com
+@Version: 1.0
+@Date: 2024/5/15 16:15
+@Describe: 
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='folerhandle',
+    version='1.2',
+    packages=find_packages(),
+    description='文件夹结构处理模块',
+  #  long_description=open('README.md').read(),
+    # python3，readme文件中文报错
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='http://github.com/yourusername/my_package',
+    author='WhiteCome',
+    author_email='waitKey1@outlook.com',
+    license='MIT',
+    install_requires=[
+        # 依赖列表
+    ],
+    classifiers=[
+        # 分类信息
+    ]
+)
