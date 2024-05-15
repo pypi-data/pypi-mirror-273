bd6dec1e99fcb76fdcfbdde6786bf7b088f28a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 06:05:27 +0000

    Add package aind-behavior-curriculum/0.0.16/aind_behavior_curriculum-0.0.16-py3-none-any.whl

---
+++ packages/aind-behavior-curriculum/aind_behavior_curriculum-0.0.16-py3-none-any.whl/aind_behavior_curriculum/__init__.py
@@ -0,0 +1,10 @@
+"""Init package"""
+
+__version__ = "0.0.16"
+
+from .base import *  # noqa: F401, F403
+from .task import *  # noqa: F401, F403
+
+from .curriculum import *  # noqa: F401, F403
+from .curriculum_utils import *  # noqa: F401, F403
+from .trainer import *  # noqa: F401, F403
