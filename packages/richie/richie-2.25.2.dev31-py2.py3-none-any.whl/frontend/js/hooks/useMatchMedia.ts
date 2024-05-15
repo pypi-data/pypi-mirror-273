0a6a75ee3cd9409612acd3abc0bdf60186025395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 04:03:44 +0000

    Add package richie/2.25.2.dev31/richie-2.25.2.dev31-py2.py3-none-any.whl

---
+++ packages/richie/richie-2.25.2.dev31-py2.py3-none-any.whl/frontend/js/hooks/useMatchMedia.ts
@@ -0,0 +1,37 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { matchMedia } from 'utils/indirection/window';
+
+/**
+ * Hook which listens if a mediaQuery matched.
+ * It is useful to show/hide component through a mediaQuery as we do in CSS.
+ * e.g: Show a burger menu when we reached mobile width.
+ * @param query The media query to monitor
+ */
+const useMatchMedia = (query: string): boolean => {
+  const mediaQuery = useMemo(() => matchMedia(query), [query]);
+  const [matches, setMatches] = useState(mediaQuery.matches);
+  const handleChange = useCallback(
+    (mQuery: MediaQueryListEvent) => setMatches(mQuery.matches),
+    [setMatches],
+  );
+
+  useEffect(() => {
+    if (mediaQuery.addEventListener) {
+      mediaQuery.addEventListener('change', handleChange);
+    } else {
+      mediaQuery.addListener(handleChange);
+    }
+
+    return () => {
+      if (mediaQuery.removeEventListener) {
+        mediaQuery.removeEventListener('change', handleChange);
+      } else {
+        mediaQuery.removeListener(handleChange);
+      }
+    };
+  }, [mediaQuery]);
+
+  return matches;
+};
+
+export default useMatchMedia;
