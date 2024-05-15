bc00adbb58d4794d6f1c56adb5e0df00116f8003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 14:04:33 +0000

    Add package amisc/0.3.0/amisc-0.3.0.tar.gz

---
+++ packages/amisc/amisc-0.3.0.tar.gz/amisc-0.3.0/src/amisc/__init__.py
@@ -0,0 +1,17 @@
+"""Adaptive multi-index stochastic collocation for metamodeling/surrogates of multidisciplinary systems.
+
+- Author - Joshua Eckels (eckelsjd@umich.edu)
+- License - GNU GPLv3
+"""
+import numpy as np
+
+from amisc.interpolator import BaseInterpolator
+from amisc.rv import BaseRV
+
+__version__ = "0.3.0"
+
+# Custom types that are used frequently
+IndexSet = list[tuple[tuple, tuple]]
+MiscTree = dict[str: dict[str: float | BaseInterpolator]]
+InterpResults = BaseInterpolator | tuple[list[int | tuple | str], np.ndarray, BaseInterpolator]
+IndicesRV = list[int | str | BaseRV] | int | str | BaseRV
