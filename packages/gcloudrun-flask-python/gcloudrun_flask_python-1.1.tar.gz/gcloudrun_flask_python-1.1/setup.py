35d62dc5e06b176a06ca4ebfa7422db60739f14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:03:40 +0000

    Add package gcloudrun-flask-python/1.1/gcloudrun_flask_python-1.1.tar.gz

---
+++ packages/gcloudrun-flask-python/gcloudrun_flask_python-1.1.tar.gz/gcloudrun_flask_python-1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gcloudrun_flask_python',
+    version='1.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Flask==3.0.3',
+        'gunicorn==22.0.0',
+        'Werkzeug==3.0.3'
+    ],
+    entry_points={
+        'console_scripts': [
+            'gcloudrun_flask_python=gcloudrun_flask_python.main:main',
+        ],
+    },
+)
