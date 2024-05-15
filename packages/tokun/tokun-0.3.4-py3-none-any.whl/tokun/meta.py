6c68366d58f5bc20e43451d32d180450fc955b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 03:05:37 +0000

    Add package tokun/0.3.4/tokun-0.3.4-py3-none-any.whl

---
+++ packages/tokun/tokun-0.3.4-py3-none-any.whl/tokun/meta.py
@@ -0,0 +1,7 @@
+"""Setup the hyper parameters for tokun."""
+
+def rates(normalization: bool=True) -> tuple:
+    return (0.00001, 0.0001, 0.8) if normalization else (0.0001, 0.001, 0.8)
+
+def version(groups: list, attention: bool=True, normalization: bool=True) -> list:
+    return ['x'.join(str(__g) for __g in groups), str(attention), str(normalization)]
