0ef5ac0bf39629bc4c8d00baf2c50ced631b8ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 14:04:04 +0000

    Add package cosolvkit/0.4.6/cosolvkit-0.4.6.tar.gz

---
+++ packages/cosolvkit/cosolvkit-0.4.6.tar.gz/cosolvkit-0.4.6/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import glob
+import fnmatch
+from setuptools import setup, find_packages
+
+
+def find_files(directory):
+    matches = []
+
+    for root, dirnames, filenames in os.walk(directory):
+        for filename in fnmatch.filter(filenames, '*'):
+            matches.append(os.path.join(root, filename))
+
+    return matches
+
+
+setup(name="cosolvkit",
+      version='0.4.6',
+      description="CosolvKit",
+      author="Niccolo Bruciaferri, Jerome Eberhardt",
+      author_email="forli@scripps.edu",
+      url="https://github.com/forlilab/cosolvkit",
+      packages=find_packages(exclude=['docs']),
+      package_data={"cosolvkit" : ["data/*"]},
+      data_files=[("", ["README.md", "LICENSE"])],
+      include_package_data=True,
+      zip_safe=False,
+      license="LGPL-2.1",
+      keywords=["molecular modeling", "drug design",
+                "cosolvent", "MD simulations"],
+      classifiers=["Programming Language :: Python",
+                   "Operating System :: Unix",
+                   "Operating System :: MacOS",
+                   "Topic :: Scientific/Engineering"],
+      entry_points={
+          'console_scripts': [
+              'create_cosolvent_system=scripts.create_cosolvent_system:main',
+              'post_simulation_processing=scripts.post_simulation_processing:main'
+          ]
+      }
+      
+)
