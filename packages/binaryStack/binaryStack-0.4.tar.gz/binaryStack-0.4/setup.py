c759a44d731e1201ef5c81e36927557c26e613fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:04:06 +0000

    Add package binaryStack/0.4/binaryStack-0.4.tar.gz

---
+++ packages/binaryStack/binaryStack-0.4.tar.gz/binaryStack-0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="binaryStack",
+    version="0.4",
+    packages=find_packages(),
+    install_requires=[],
+    author="vegito5ama",
+    description="A quick collection of  Python files",
+    long_description="This library contains various Python files for different purposes.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
