9a7910d61a54d30d10890b1aa94d6040773814c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 06:04:47 +0000

    Add package habits.txt/0.2.0/habits_txt-0.2.0.tar.gz

---
+++ packages/habits.txt/habits_txt-0.2.0.tar.gz/habits_txt-0.2.0/habits_txt/cli.py
@@ -0,0 +1,143 @@
+import datetime as dt
+
+import click
+
+import habits_txt.config as config_
+import habits_txt.defaults as defaults
+import habits_txt.journal as journal_
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("file", type=click.File("r+"))
+@click.option(
+    "-d",
+    "--date",
+    type=click.DateTime(formats=["%Y-%m-%d"]),
+    default=dt.date.today().strftime(defaults.DATE_FMT),
+    callback=lambda ctx, param, value: value.date(),
+    help="Date to use (defaults to today)",
+)
+@click.option("-i", "--interactive", is_flag=True, help="Interactive mode")
+@click.option(
+    "-t",
+    "--write-top",
+    is_flag=True,
+    help="Write output to the top of the journal file",
+)
+@click.option(
+    "-b",
+    "--write-bottom",
+    is_flag=True,
+    help="Write output to the bottom of the journal file",
+)
+def fill(file, date, interactive, write_top, write_bottom):
+    """
+    Fill habits on a given date using FILE.
+    """
+    records = journal_.fill_day(
+        file.name,
+        date,
+        interactive,
+    )
+    if records:
+        records_str = "\n".join([str(record) for record in records]) if records else ""
+        if write_top:
+            content = file.read()
+            file.seek(0)
+            file.write(records_str + "\n\n" + content)
+        elif write_bottom:
+            file.write("\n" + records_str + "\n")
+        else:
+            click.echo(records_str)
+    else:
+        click.echo(f"{defaults.COMMENT_CHAR} Nothing to fill for {date}")
+
+
+@cli.command()
+@click.argument("file", type=click.File("r"))
+@click.option(
+    "-s",
+    "--start",
+    type=click.DateTime(formats=["%Y-%m-%d"]),
+    callback=lambda ctx, param, value: value.date() if value else None,
+    help="Start date",
+)
+@click.option(
+    "-e",
+    "--end",
+    type=click.DateTime(formats=["%Y-%m-%d"]),
+    default=dt.date.today().strftime(defaults.DATE_FMT),
+    callback=lambda ctx, param, value: value.date(),
+    help="End date",
+)
+@click.option("-n", "--name", help="Filter by habit name")
+def filter(file, start, end, name):
+    """
+    Filter habit records using FILE.
+    """
+    records = journal_.filter(file.name, start, end, name)
+    if records:
+        records_str = "\n".join([str(record) for record in records])
+        click.echo(records_str)
+    else:
+        click.echo(f"{defaults.COMMENT_CHAR} No records found")
+
+
+@cli.command()
+@click.argument("file", type=click.File("r"))
+def edit(file):
+    """
+    Edit FILE.
+    """
+    click.edit(filename=file.name)
+
+
+@cli.group()
+def config():
+    config_.setup()
+
+
+@config.group()
+def set():
+    try:
+        config_.validate()
+    except ValueError as e:
+        click.echo(e)
+        raise click.Abort()
+
+
+@set.command()
+@click.argument("path", type=click.Path())
+def journal(path):
+    """
+    Set the journal file path.
+    """
+    config_.set("journal", path, "CLI")
+    click.echo(f"Journal file path set to {path}")
+
+
+@config.command()
+def get():
+    """
+    Get all configuration settings.
+    """
+    try:
+        config_.validate()
+    except ValueError as e:
+        click.echo(e)
+        raise click.Abort()
+    with open(defaults.APPDATA_PATH + "/config.ini") as f:
+        click.echo(f.read())
+
+
+@config.command(name="edit")
+def config_edit():
+    """
+    Edit configuration settings.
+    """
+    click.edit(filename=defaults.APPDATA_PATH + "/config.ini")
---
+++ packages/habits.txt/habits_txt-0.2.0.tar.gz/habits_txt-0.2.0/habits_txt/models.py
@@ -0,0 +1,87 @@
+import datetime as dt
+from dataclasses import dataclass
+
+import croniter
+
+import habits_txt.defaults as defaults
+
+
+@dataclass
+class Frequency:
+    """
+    Frequency of a habit, following a simplified cron-like syntax.
+
+    Example - every weekday:
+      day: *
+      month: *
+      day_of_week: 1-5
+    """
+
+    day: str
+    month: str
+    day_of_week: str
+
+    def get_next_date(self, date: dt.date) -> dt.date:
+        """
+        Get the next date based on the frequency.
+
+        :param date: Current date.
+        :return: Next date.
+        """
+        cron_str = f"0 0 {self.day} {self.month} {self.day_of_week}"
+        cron = croniter.croniter(cron_str, dt.datetime.combine(date, dt.time()))
+        return cron.get_next(dt.datetime).date()
+
+
+@dataclass
+class Habit:
+    """
+    Habit tracked by the user.
+
+    Example:
+      name: "Sample habit"
+      frequency: Frequency("*", "*", "*")
+      is_measurable: False
+    """
+
+    name: str
+    frequency: Frequency
+    is_measurable: bool = False
+
+    def __hash__(self):
+        return hash(self.name)
+
+
+@dataclass
+class HabitRecord:
+    """
+    Record of a habit on a specific date.
+
+    Example:
+        date: dt.date(2024, 1, 1)
+        habit_name: "Sample habit"
+        value: 2.0
+    """
+
+    date: dt.date
+    habit_name: str
+    value: bool | float | None
+
+    @property
+    def is_complete(self) -> bool:
+        return bool(self.value)
+
+    def __str__(self) -> str:
+        return (
+            f"{dt.datetime.strftime(self.date, defaults.DATE_FMT)} "
+            f'"{self.habit_name}" {self._str_value()}'
+        )
+
+    def _str_value(self) -> str:
+        if self.value is True:
+            return defaults.BOOLEAN_TRUE
+        elif self.value is False:
+            return defaults.BOOLEAN_FALSE
+        elif self.value is None:
+            return ""
+        return str(self.value)
