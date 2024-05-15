743e971f6bf13d5500b6f4eefbc3ee478b450d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 03:05:45 +0000

    Add package ibrahiskiisidususm/0.1/ibrahiskiisidususm-0.1.tar.gz

---
+++ packages/ibrahiskiisidususm/ibrahiskiisidususm-0.1.tar.gz/ibrahiskiisidususm-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='ibrahiskiisidususm',
+    version='0.1',
+    long_description_content_type='text/x-rst',
+    long_description="Python is a high-level, interpreted programming language known for its simplicity and readability. Created by Guido van Rossum and first released in 1991, Python emphasizes code readability with its clear and concise syntax, making it an ideal language for beginners and experienced programmers alike.Python supports multiple programming paradigms, including procedural, object-oriented, and functional programming. It offers dynamic typing and automatic memory management, which simplifies development by reducing the amount of code needed to perform tasks compared to lower-level languages.Python has a vast ecosystem of libraries and frameworks, making it suitable for a wide range of applications, from web development and data analysis to artificial intelligence and scientific computing. Popular frameworks such as Django and Flask are widely used for building web applications, while libraries like NumPy and Pandas are essential for data manipulation and analysis.One of Python's key strengths is its strong community support and active development, with a large number of contributors continuously improving the language and its ecosystem. This vibrant community contributes to Python's versatility and ensures its relevance in various industries and domains.",
+    author='ibrahim mohammed',
+    author_email='ibrahom0780@gmail.com',
+    url='https://github.com/ibrahimmmm/ibrahim',
+    
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
