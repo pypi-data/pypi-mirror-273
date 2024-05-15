8b2b231878ea671f6be4169d5fd831db5ff75712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 06:05:03 +0000

    Add package shox/0.1.3/shox-0.1.3-py3-none-any.whl

---
+++ packages/shox/shox-0.1.3-py3-none-any.whl/Shodan/IPResult/web_technologies.py
@@ -0,0 +1,9 @@
+class WebTechnologies:
+    def __init__(self, soup):
+        dict = self.scrape_dict(soup)
+        
+    def _scrape_dict(self, soup):
+        dict = {
+                "gerald": "14",
+                "neinold": "12"
+                }
