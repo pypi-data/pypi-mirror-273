381ba0c73069c7cf0947c210b93fcdace90d8801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 05:04:20 +0000

    Add package replicate/0.26.0/replicate-0.26.0.tar.gz

---
+++ packages/replicate/replicate-0.26.0.tar.gz/replicate-0.26.0/tests/test_training.py
@@ -0,0 +1,190 @@
+import pytest
+
+import replicate
+from replicate.exceptions import ReplicateException
+
+input_images_url = "https://replicate.delivery/pbxt/JMV5OrEWpBAC5gO8rre0tPOyJIOkaXvG0TWfVJ9b4zhLeEUY/data.zip"
+
+
+@pytest.mark.vcr("trainings-create.yaml")
+@pytest.mark.asyncio
+@pytest.mark.parametrize("async_flag", [True, False])
+async def test_trainings_create(async_flag, mock_replicate_api_token):
+    if async_flag:
+        training = await replicate.trainings.async_create(
+            model="stability-ai/sdxl",
+            version="39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+            input={
+                "input_images": input_images_url,
+                "use_face_detection_instead": True,
+            },
+            destination="replicate/dreambooth-sdxl",
+        )
+    else:
+        training = replicate.trainings.create(
+            model="stability-ai/sdxl",
+            version="39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+            input={
+                "input_images": input_images_url,
+                "use_face_detection_instead": True,
+            },
+            destination="replicate/dreambooth-sdxl",
+        )
+
+    assert training.id is not None
+    assert training.status == "starting"
+
+
+@pytest.mark.vcr("trainings-create.yaml")
+@pytest.mark.asyncio
+@pytest.mark.parametrize("async_flag", [True, False])
+async def test_trainings_create_with_named_version_argument(
+    async_flag, mock_replicate_api_token
+):
+    if async_flag:
+        # The overload with a model version identifier is soft-deprecated
+        # and not supported in the async version.
+        return
+    else:
+        training = replicate.trainings.create(
+            version="stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+            input={
+                "input_images": input_images_url,
+                "use_face_detection_instead": True,
+            },
+            destination="replicate/dreambooth-sdxl",
+        )
+
+    assert training.id is not None
+    assert training.status == "starting"
+
+
+@pytest.mark.vcr("trainings-create.yaml")
+@pytest.mark.asyncio
+@pytest.mark.parametrize("async_flag", [True, False])
+async def test_trainings_create_with_positional_argument(
+    async_flag, mock_replicate_api_token
+):
+    if async_flag:
+        # The overload with positional arguments is soft-deprecated
+        # and not supported in the async version.
+        return
+    else:
+        training = replicate.trainings.create(
+            "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+            {
+                "input_images": input_images_url,
+                "use_face_detection_instead": True,
+            },
+            "replicate/dreambooth-sdxl",
+        )
+
+        assert training.id is not None
+        assert training.status == "starting"
+
+
+@pytest.mark.vcr("trainings-create__invalid-destination.yaml")
+@pytest.mark.asyncio
+@pytest.mark.parametrize("async_flag", [True, False])
+async def test_trainings_create_with_invalid_destination(
+    async_flag, mock_replicate_api_token
+):
+    with pytest.raises(ReplicateException):
+        if async_flag:
+            await replicate.trainings.async_create(
+                model="stability-ai/sdxl",
+                version="39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+                input={
+                    "input_images": input_images_url,
+                    "use_face_detection_instead": True,
+                },
+                destination="<invalid>",
+            )
+        else:
+            replicate.trainings.create(
+                model="stability-ai/sdxl",
+                version="39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+                input={
+                    "input_images": input_images_url,
+                },
+                destination="<invalid>",
+            )
+
+
+@pytest.mark.vcr("trainings-get.yaml")
+@pytest.mark.asyncio
+@pytest.mark.parametrize("async_flag", [True, False])
+async def test_trainings_get(async_flag, mock_replicate_api_token):
+    id = "medrnz3bm5dd6ultvad2tejrte"
+
+    if async_flag:
+        training = await replicate.trainings.async_get(id)
+    else:
+        training = replicate.trainings.get(id)
+
+    assert training.id == id
+    assert training.status == "processing"
+
+
+@pytest.mark.vcr("trainings-cancel.yaml")
+@pytest.mark.asyncio
+@pytest.mark.parametrize("async_flag", [True, False])
+async def test_trainings_cancel(async_flag, mock_replicate_api_token):
+    input = {
+        "input_images": input_images_url,
+        "use_face_detection_instead": True,
+    }
+
+    destination = "replicate/dreambooth-sdxl"
+
+    if async_flag:
+        training = await replicate.trainings.async_create(
+            model="stability-ai/sdxl",
+            version="39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+            input=input,
+            destination=destination,
+        )
+
+        assert training.status == "starting"
+
+        training = replicate.trainings.cancel(training.id)
+        assert training.status == "canceled"
+    else:
+        training = replicate.trainings.create(
+            version="stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+            destination=destination,
+            input=input,
+        )
+
+        assert training.status == "starting"
+
+        training = replicate.trainings.cancel(training.id)
+        assert training.status == "canceled"
+
+
+@pytest.mark.vcr("trainings-cancel.yaml")
+@pytest.mark.asyncio
+@pytest.mark.parametrize("async_flag", [True, False])
+async def test_trainings_cancel_instance_method(async_flag, mock_replicate_api_token):
+    input = {
+        "input_images": input_images_url,
+        "use_face_detection_instead": True,
+    }
+
+    destination = "replicate/dreambooth-sdxl"
+
+    if async_flag:
+        # The cancel instance method is soft-deprecated,
+        # and not supported in the async version.
+        return
+    else:
+        training = replicate.trainings.create(
+            version="stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+            destination=destination,
+            input=input,
+        )
+
+        assert training.status == "starting"
+
+        training.cancel()
+        assert training.status == "canceled"
