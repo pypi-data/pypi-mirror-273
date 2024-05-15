67c2420f0093cc29511453637438bb5be23655de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 16:04:04 +0000

    Add package lycoris-lora/2.3.0.dev11/lycoris_lora-2.3.0.dev11.tar.gz

---
+++ packages/lycoris-lora/lycoris_lora-2.3.0.dev11.tar.gz/lycoris_lora-2.3.0.dev11/lycoris/modules/full.py
@@ -0,0 +1,134 @@
+import torch
+import torch.nn as nn
+
+from .base import LycorisBaseModule
+
+
+class FullModule(LycorisBaseModule):
+    support_module = {
+        "linear",
+        "conv1d",
+        "conv2d",
+        "conv3d",
+    }
+
+    def __init__(
+        self,
+        lora_name,
+        org_module: nn.Module,
+        multiplier=1.0,
+        lora_dim=4,
+        alpha=1,
+        dropout=0.0,
+        rank_dropout=0.0,
+        module_dropout=0.0,
+        use_tucker=False,
+        use_scalar=False,
+        rank_dropout_scale=False,
+        bypass_mode=False,
+        **kwargs,
+    ):
+        super().__init__(
+            lora_name,
+            org_module,
+            multiplier,
+            dropout,
+            rank_dropout,
+            module_dropout,
+            rank_dropout_scale,
+            bypass_mode,
+        )
+        if self.module_type not in self.support_module:
+            raise ValueError(f"{self.module_type} is not supported in Full algo.")
+
+        if self.bypass_mode:
+            raise ValueError("bypass mode is not supported in Full algo.")
+
+        self.weight = nn.Parameter(torch.zeros_like(org_module.weight))
+        if org_module.bias is not None:
+            self.bias = nn.Parameter(torch.zeros_like(org_module.bias))
+        else:
+            self.bias = None
+
+    def apply_to(self, **kwargs):
+        self.org_forward = self.org_module[0].forward
+        self.org_module[0].forward = self.forward
+        self.weight.data.add_(self.org_module[0].weight.data)
+        self._org_weight = [self.org_module[0].weight.data.cpu().clone()]
+        delattr(self.org_module[0], "weight")
+        if self.org_module[0].bias is not None:
+            self.bias.data.add_(self.org_module[0].bias.data)
+            self.org_bias = [self.org_module[0].bias.data.cpu().clone()]
+            delattr(self.org_module[0], "bias")
+        else:
+            self.org_bias = None
+
+    def custom_state_dict(self):
+        sd = {"diff": self.weight.data.cpu() - self._org_weight[0]}
+        if self.bias is not None:
+            sd["diff_b"] = self.bias.data.cpu() - self.org_bias[0]
+        return sd
+
+    def load_weight_prehook(
+        self,
+        state_dict,
+        prefix,
+        local_metadata,
+        strict,
+        missing_keys,
+        unexpected_keys,
+        error_msgs,
+    ):
+        diff_weight = state_dict["diff"]
+        self.weight.data.add_(diff_weight)
+        if "diff_b" in state_dict:
+            diff_bias = state_dict["diff_b"]
+            self.bias.data.add_(diff_bias)
+
+    def make_weight(self, scale=1, device=None, original=False):
+        if original:
+            weight = self._org_weight[0].to(device, dtype=self.weight.dtype)
+            if self.org_bias is not None:
+                bias = self.org_bias[0].to(device, dtype=self.bias.dtype)
+            else:
+                bias = None
+            return weight, bias
+        drop = (
+            torch.rand(self.dim, device=device) > self.rank_dropout
+            if self.rank_dropout and self.training
+            else 1
+        )
+        if drop != 1 or scale != 1:
+            org_weight = self.org_module[0].weight.to(device, dtype=self.weight.dtype)
+            diff = self.weight.to(device) - org_weight
+            weight = diff * drop * scale + org_weight
+            if self.bias is not None:
+                org_bias = self.org_module[0].bias.to(device, dtype=self.bias.dtype)
+                diff_b = self.bias.to(device) - org_bias
+                bias = diff_b * drop * scale + org_bias
+        else:
+            weight = self.weight
+            bias = self.bias
+        return weight, bias
+
+    def get_merged_weight(self, multiplier=1, shape=None, device=None):
+        weight, bias = self.make_weight(multiplier, device)
+        if shape is not None:
+            weight = weight.view(shape)
+            if bias is not None:
+                bias = bias.view(shape[0], 1)
+        return weight, bias
+
+    def forward(self, x: torch.Tensor, *args, **kwargs):
+        if (
+            self.module_dropout
+            and self.training
+            and torch.rand(1) < self.module_dropout
+        ):
+            original = True
+        else:
+            original = False
+        scale = self.multiplier
+        weight, bias = self.make_weight(scale, x.device, original=original)
+        kw_dict = self.kw_dict | {"weight": weight, "bias": bias}
+        return self.op(x, **kw_dict)
---
+++ packages/lycoris-lora/lycoris_lora-2.3.0.dev11.tar.gz/lycoris_lora-2.3.0.dev11/lycoris/modules/norms.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+
+from .base import LycorisBaseModule
+
+
+class NormModule(LycorisBaseModule):
+    support_module = {
+        "layernorm",
+        "groupnorm",
+    }
+
+    def __init__(
+        self,
+        lora_name,
+        org_module: nn.Module,
+        multiplier=1.0,
+        rank_dropout=0.0,
+        module_dropout=0.0,
+        rank_dropout_scale=False,
+        **kwargs,
+    ):
+        """if alpha == 0 or None, alpha is rank (no scaling)."""
+        super().__init__(
+            lora_name=lora_name,
+            org_module=org_module,
+            multiplier=multiplier,
+            rank_dropout=rank_dropout,
+            module_dropout=module_dropout,
+            rank_dropout_scale=rank_dropout_scale,
+            **kwargs,
+        )
+        if self.module_type not in self.support_module:
+            raise ValueError(f"{self.module_type} is not supported in Norm algo.")
+
+        self.w_norm = nn.Parameter(torch.zeros(self.dim))
+        self.b_norm = nn.Parameter(torch.zeros(self.dim))
+
+    def make_weight(self, scale=1, device=None):
+        org_weight = self.org_module[0].weight.to(device, dtype=self.w_norm.dtype)
+        org_bias = self.org_module[0].bias.to(device, dtype=self.b_norm.dtype)
+        if self.rank_dropout and self.training:
+            drop = (torch.rand(self.dim, device=device) < self.rank_dropout).to(
+                self.w_norm.device
+            )
+            if self.rank_dropout_scale:
+                drop /= drop.mean()
+        else:
+            drop = 1
+        drop = (
+            torch.rand(self.dim, device=device) < self.rank_dropout
+            if self.rank_dropout and self.training
+            else 1
+        )
+        weight = self.w_norm.to(device) * drop * scale
+        bias = self.b_norm.to(device) * drop * scale
+        return org_weight + weight, org_bias + bias
+
+    def get_merged_weight(self, multiplier=1, shape=None, device=None):
+        return self.make_weight(multiplier, device)
+
+    def forward(self, x):
+        if self.module_dropout and self.training:
+            if torch.rand(1) < self.module_dropout:
+                return self.org_forward(x)
+        scale = self.multiplier
+
+        w, b = self.make_weight(scale, x.device)
+        kw_dict = self.kw_dict | {"weight": w, "bias": b}
+        return self.op(x, **kw_dict)
