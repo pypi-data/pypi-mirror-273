8033cca5f106bd3265d5ca0d5159bac8cac47a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 14:04:13 +0000

    Add package matematikk/0.0.159/matematikk-0.0.159.tar.gz

---
+++ packages/matematikk/matematikk-0.0.159.tar.gz/matematikk-0.0.159/src/matematikk/_exports/def_deriver_mas.py
@@ -0,0 +1,9 @@
+from matematikk import diff
+
+# Alias > 1
+deriver                         = diff
+derivert                        = diff
+momentan_vekst                  = diff
+momentan_vekstfart              = diff
+ 
+# Alias > 2 > ...
