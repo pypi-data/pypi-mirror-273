02633c9793ead035c31567e4dc4364a738c002cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 16:04:03 +0000

    Add package crlfi/0.0.6/crlfi-0.0.6.tar.gz

---
+++ packages/crlfi/crlfi-0.0.6.tar.gz/crlfi-0.0.6/crlfi/utils/const.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+ * crlfi
+ * crlfi Bug scanner for WebPentesters and Bugbounty Hunters
+ *
+ * @Developed By Cappricio Securities <https://cappriciosec.com>
+ */
+
+
+"""
+
+
+class Data:
+    blog = 'https://blogs.cappriciosec.com/application/138/Cappricio%20Securities%20Discovers%20CRLF%20Injection%20Vulnerability%20in%20Popular%20Website,%20Responsible%20Disclosure%20Earns%20Bounty'
+    api = 'https://api.cappriciosec.com/Telegram/cappriciosecbot.php'
+    config_path = '~/.config/cappriciosec-tools/cappriciosec.yaml'
+    payloadurl = 'https://raw.githubusercontent.com/Cappricio-Securities/PayloadAllTheThings/main/crlfi.txt'
+    bugname = 'CRLF Injection'
+
+
+class Colors:
+    RED = '\x1b[31;1m'
+    BLUE = '\x1b[34;1m'
+    GREEN = '\x1b[32;1m'
+    RESET = '\x1b[0m'
+    MAGENTA = '\x1b[35;1m'
