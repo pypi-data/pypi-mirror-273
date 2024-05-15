c315c7a33892de18f4dbd90f17821fea7c598618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 05:04:30 +0000

    Add package almgroad0aa/2.0/almgroad0aa-2.0.tar.gz

---
+++ packages/almgroad0aa/almgroad0aa-2.0.tar.gz/almgroad0aa-2.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+setuptools.setup(
+    name="almgroad0aa",
+    version="2.0",
+    author="BKlooppoBB",
+    description="Is a dhsh FREE asynchronous library from reverse engineered Shazam API written in Python 3.6+jdjs siisd 76x  with asyncio and aiohttp. Includes all the methods that Shazam has, including searching for a song by file.",
+    long_description="Python is a high-level, interpreted programming language known for its simplicity and readability. Created by Guido van Rossum and first released in 1991, Python emphasizes code readability with its clear and concise syntax, making it an ideal language for beginners and experienced programmers alike.Python supports multiple programming paradigms, including procedural, object-ask are widely used for building web applications, while libraries like NumPy and Pandas are essential for data manipulation and analysis.One of Pythons key strengths is its strong community support and active development, with a large number of contributors continuously improving the language and its ecosystem.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/dotX12/ShazamIO",
+    packages=setuptools.find_packages(),
+    
+    install_requires=[
+        "requests",
+    ],
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
