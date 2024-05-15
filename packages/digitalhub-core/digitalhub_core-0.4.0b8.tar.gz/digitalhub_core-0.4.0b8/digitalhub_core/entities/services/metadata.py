b408f290b308a1e67fe37a249fe031c65d45952b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 03:05:39 +0000

    Add package digitalhub-core/0.4.0b8/digitalhub_core-0.4.0b8.tar.gz

---
+++ packages/digitalhub-core/digitalhub_core-0.4.0b8.tar.gz/digitalhub_core-0.4.0b8/digitalhub_core/entities/services/metadata.py
@@ -0,0 +1,12 @@
+"""
+Service metadata module.
+"""
+from __future__ import annotations
+
+from digitalhub_core.entities._base.metadata import Metadata
+
+
+class ServiceMetadata(Metadata):
+    """
+    A class representing Service metadata.
+    """
