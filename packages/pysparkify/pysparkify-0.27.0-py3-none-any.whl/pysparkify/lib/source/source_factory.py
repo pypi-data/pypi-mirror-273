6624f551b5ca1f7c9322ff639d0cc2544d141077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 04:03:39 +0000

    Add package pysparkify/0.27.0/pysparkify-0.27.0-py3-none-any.whl

---
+++ packages/pysparkify/pysparkify-0.27.0-py3-none-any.whl/pysparkify/lib/source/source_factory.py
@@ -0,0 +1,22 @@
+from abc import abstractstaticmethod
+from .source import Source
+from .csv_source import CsvSource
+from .s3_source import S3Source
+from .redshift_source import RedshiftSource
+from .postgres_source import PostgresSource
+
+class SourceFactory():
+    
+    @abstractstaticmethod
+    def build_source(source_type: str, config) -> Source:
+        if source_type == "CsvSource": 
+            return CsvSource(config)
+        elif source_type == "S3Source":
+            return S3Source(config)
+        elif source_type == "RedshiftSource":
+            return RedshiftSource(config)
+        elif source_type == "PostgresSource":
+            return PostgresSource(config)
+        #TODO: Add more sinks to factory
+        print(f"Unsupported source {source_type}")
+        return -1
