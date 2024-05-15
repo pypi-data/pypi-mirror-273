b96692572be0abfff0a7f5d6b97ea791df43c6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 15:04:08 +0000

    Add package PlacaFipy/1.0.1/placafipy-1.0.1.tar.gz

---
+++ packages/PlacaFipy/placafipy-1.0.1.tar.gz/placafipy-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='PlacaFipy',
+    version='1.0.1',
+    packages=['placafipy'],
+    install_requires=[
+        'beautifulsoup4==4.12.3',
+        'Unidecode==1.3.8',
+        'requests==2.31.0'
+    ],
+    author='Antônio Roberto Júnior',
+    author_email='pix@jrkrz.com',
+    description='Consultas FIPE pela placa do veículo.',
+    url='https://github.com/juniorkrz/placafipy',
+    license='MIT',
+    keywords='placa fipe veiculo'
+)
