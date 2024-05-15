64942802558fb826985759e10c9f9d82d6214b3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 14:04:33 +0000

    Add package swarmauri/0.1.124.dev5/swarmauri-0.1.124.dev5.tar.gz

---
+++ packages/swarmauri/swarmauri-0.1.124.dev5.tar.gz/swarmauri-0.1.124.dev5/swarmauri/standard/prompts/concrete/PromptMatrix.py
@@ -0,0 +1,7 @@
+# swarmauri/standard/prompts/concrete/PromptMatrix.py
+from swarmauri.standard.prompts.base.BasePromptMatrix import BasePromptMatrix
+
+class PromptMatrix(BasePromptMatrix):
+    
+    def __init__(self):
+        BasePromptMatrix.__init__(self)
