34a4990b7e9441e14fd395bca47f57a579a9f227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 03:05:33 +0000

    Add package predictionguard/1.9.4/predictionguard-1.9.4.tar.gz

---
+++ packages/predictionguard/predictionguard-1.9.4.tar.gz/predictionguard-1.9.4/predictionguard/__init__.py
@@ -0,0 +1,4 @@
+"""Create controlled and compliant AI systems with PredictionGuard."""
+from .client import Client, Completion, Chat, Translate, Factuality, Toxicity, PII, Injection
+
+__version__ = "1.9.4"
