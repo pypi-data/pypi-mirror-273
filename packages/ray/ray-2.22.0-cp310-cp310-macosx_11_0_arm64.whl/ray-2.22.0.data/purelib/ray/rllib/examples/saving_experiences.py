5e190da48c2b92c71e3029eed7a4525bf73a9f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 14:04:40 +0000

    Add package ray/2.22.0/ray-2.22.0-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/ray/ray-2.22.0-cp310-cp310-macosx_11_0_arm64.whl/ray-2.22.0.data/purelib/ray/rllib/examples/saving_experiences.py
@@ -0,0 +1,6 @@
+msg = """
+This script has been moved to
+`ray.rllib.examples.offline_rl.saving_experiences.py`
+"""
+
+raise NotImplementedError(msg)
---
+++ packages/ray/ray-2.22.0-cp310-cp310-macosx_11_0_arm64.whl/ray-2.22.0.data/purelib/ray/rllib/examples/sb2rllib_rllib_example.py
@@ -0,0 +1,6 @@
+msg = """
+This script has been moved to
+`ray.rllib.examples._old_api_stack.sb2rllib_rllib_example.py`
+"""
+
+raise NotImplementedError(msg)
