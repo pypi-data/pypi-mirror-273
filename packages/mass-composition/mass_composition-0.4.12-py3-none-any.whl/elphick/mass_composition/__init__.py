6ffb592e03f97a5d87149ef51d28739296427dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-15 14:04:46 +0000

    Add package mass-composition/0.4.12/mass_composition-0.4.12-py3-none-any.whl

---
+++ packages/mass-composition/mass_composition-0.4.12-py3-none-any.whl/elphick/mass_composition/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from .mass_composition import MassComposition
+from .stream import Stream
+from .mc_xarray import MassCompositionAccessor
+
+from importlib import metadata
+
+try:
+    __version__ = metadata.version('mass-composition')
+except metadata.PackageNotFoundError:
+    # Package is not installed
+    pass
