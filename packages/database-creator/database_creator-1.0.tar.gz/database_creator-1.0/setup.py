03c3834969b91245d03d3df66be7b9fc684dd7cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 04:05:00 +0000

    Add package database-creator/1.0/database_creator-1.0.tar.gz

---
+++ packages/database-creator/database_creator-1.0.tar.gz/database_creator-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='database_creator',
+      version='1.0',
+      description="This repository contains a powerful and easy-to-use library for working with SQLite3 databases in your projects. With this library, you can perform various database operations like creating tables, inserting data, querying data, and updating data with just a few lines of code.",
+      packages=['database_creator'],
+      author_email='hirikayato@mail.ru',
+      zip_safe=False)
