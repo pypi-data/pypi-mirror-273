ad70d86ea0dfe72e2b8c66cd67994ca132553110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 05:04:50 +0000

    Add package telegram-wallet-pay/0.6.0/telegram_wallet_pay-0.6.0-py3-none-any.whl

---
+++ packages/telegram-wallet-pay/telegram_wallet_pay-0.6.0-py3-none-any.whl/telegram_wallet_pay/schemas/webhook_message.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from typing import Iterator, List, Literal
+
+from pydantic import Field
+
+from telegram_wallet_pay.enums import WebhookMessageType
+
+from ._default import DefaultModel, DefaultRootModel
+from .webhook_payload import WebhookPayload
+
+
+class WebhookMessage(DefaultModel):
+    event_datetime: datetime = Field(alias="eventDateTime")
+    event_id: int
+    type: Literal[
+        WebhookMessageType.ORDER_PAID,
+        WebhookMessageType.ORDER_FAILED,
+    ]
+    payload: WebhookPayload
+
+
+class WebhookMessages(DefaultRootModel):
+    root: List[WebhookMessage]
+
+    def __iter__(self) -> Iterator[WebhookMessage]:  # type: ignore[override]
+        """Iterate over root model."""
+        return iter(self.root)
