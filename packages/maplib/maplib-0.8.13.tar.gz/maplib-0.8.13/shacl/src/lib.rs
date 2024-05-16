abb69a1319507bd57f57a4aff9b189f4ec75bb5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 03:04:24 +0000

    Add package maplib/0.8.13/maplib-0.8.13.tar.gz

---
+++ packages/maplib/maplib-0.8.13.tar.gz/maplib-0.8.13/shacl/src/lib.rs
@@ -0,0 +1,22 @@
+pub mod errors;
+use errors::ShaclError;
+use polars::prelude::DataFrame;
+use representation::RDFNodeType;
+use std::collections::HashMap;
+use representation::solution_mapping::EagerSolutionMappings;
+use triplestore::Triplestore;
+
+pub struct ValidationReport {
+    pub conforms: bool,
+    pub df: Option<DataFrame>,
+    pub rdf_node_types: Option<HashMap<String, RDFNodeType>>,
+    pub details: Option<EagerSolutionMappings>,
+}
+
+pub fn validate(_triplestore: &mut Triplestore) -> Result<ValidationReport, ShaclError> {
+    unimplemented!("Contact Data Treehouse to try")
+}
+
+pub fn validate_shacl(_triplestore: &mut Triplestore) -> Result<ValidationReport, ShaclError> {
+    unimplemented!("Contact Data Treehouse to try")
+}
