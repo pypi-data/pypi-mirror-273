39c17378d9ea2e3851ba7e987c9e2521897df6ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 06:04:52 +0000

    Add package pymonit/1.3.13/pymonit-1.3.13.tar.gz

---
+++ packages/pymonit/pymonit-1.3.13.tar.gz/pymonit-1.3.13/pymonit/error.py
@@ -0,0 +1,9 @@
+SetupError="SetupError"
+DatabaseError="DatabaseError"
+HTTPError="HTTPError"
+FileError="FileError"
+FolderError="FolderError"
+TooManyRequests="TooManyRequests"
+DataCreateError="DataCreateError"
+DataUpdateError="DataUpdateError"
+
