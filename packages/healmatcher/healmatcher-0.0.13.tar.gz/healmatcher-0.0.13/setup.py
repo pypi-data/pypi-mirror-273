8696526f6b8bbe87aaf7a232ebfafac0973947d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 06:04:33 +0000

    Add package healmatcher/0.0.13/healmatcher-0.0.13.tar.gz

---
+++ packages/healmatcher/healmatcher-0.0.13.tar.gz/healmatcher-0.0.13/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.13' 
+DESCRIPTION = 'Fast and simple probabilistic data matching package'
+
+# Setting up
+setup(
+        name="healmatcher", 
+        version=VERSION,
+        author="Joseph Shim github.com/JosephKBS",
+        author_email="<joseph.shim.rok@gmail.com>",
+        description=DESCRIPTION,
+   	long_description=open("README.md", 'r').read(),
+    	long_description_content_type='text/markdown',
+        packages=find_packages(),
+        install_requires=['splink','pandas','numpy'], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['probabilistic match', 'probabilistic data match', 'splink'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
