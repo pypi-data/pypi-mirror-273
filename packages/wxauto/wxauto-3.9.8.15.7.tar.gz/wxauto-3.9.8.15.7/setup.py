cdda1dfc3d8fc2aa51715846ac26780ac2f68743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 04:04:18 +0000

    Add package wxauto/3.9.8.15.7/wxauto-3.9.8.15.7.tar.gz

---
+++ packages/wxauto/wxauto-3.9.8.15.7.tar.gz/wxauto-3.9.8.15.7/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wxauto',
+    version='3.9.8.15.7',
+    author='Cluic',
+    author_email='tikic@qq.com',
+    description='A simple wechat automation tool',
+    long_description=open('README.md', encoding='utf8').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'pywin32',
+        'pyperclip',
+        'Pillow',
+        'psutil',
+        'typing_extensions',
+        'comtypes',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+    ],
+    python_requires='>=3.6',
+)
