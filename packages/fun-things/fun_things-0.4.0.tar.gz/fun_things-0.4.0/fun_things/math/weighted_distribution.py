a804a9039d53485313f436bebbf4d99fd54fd678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 14:04:44 +0000

    Add package fun-things/0.4.0/fun_things-0.4.0.tar.gz

---
+++ packages/fun-things/fun_things-0.4.0.tar.gz/fun_things-0.4.0/fun_things/math/weighted_distribution.py
@@ -0,0 +1,39 @@
+from typing import Callable, Any, Iterable, TypeVar, Generator
+from functools import reduce
+from random import random
+
+T = TypeVar("T")
+
+
+def weighted_distribution(
+    items: Iterable[T],
+    weight_selector: Callable[[T], float],
+) -> Generator[T, Any, None]:
+    """
+    Choose a random item based on their given weight.
+
+    A higher weight means they have
+    a higher chance of being chosen.
+    """
+    weighted_items = [
+        {
+            "item": item,
+            "weight": weight_selector(item),
+        }
+        for item in items
+    ]
+    weighted_items.sort(key=lambda item: item["weight"])
+
+    max_weight = reduce(
+        lambda value, item: value + item["weight"],
+        weighted_items,
+        0,
+    )
+
+    while len(weighted_items) > 0:
+        value = random() * max_weight
+
+        for item in weighted_items:
+            if value < item["weight"]:
+                yield item["item"]
+                break
