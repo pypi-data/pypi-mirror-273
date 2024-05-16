c77c14747d69b3f45baae29a4fb506f8aa674523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 04:04:44 +0000

    Add package ddeutil-model/0.0.2/ddeutil_model-0.0.2-py3-none-any.whl

---
+++ packages/ddeutil-model/ddeutil_model-0.0.2-py3-none-any.whl/ddeutil/model/const.py
@@ -0,0 +1,96 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) 2022 Korawich Anuttra. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for
+# license information.
+# ------------------------------------------------------------------------------
+from __future__ import annotations
+
+from typing import (
+    Annotated,
+    Optional,
+)
+
+from pydantic import BaseModel, Field
+
+
+class Const(BaseModel):
+    """Constraint Model"""
+
+    of: Annotated[
+        Optional[str],
+        Field(description="Owner of Constraint"),
+    ] = None
+
+    @property
+    def name(self) -> str:
+        if not self.of:
+            raise ValueError(
+                "This constraint does not pass `of` value for take ownership."
+            )
+        return f"{self.of}_const"
+
+
+class Pk(Const):
+    """Primary Key Model.
+
+    Examples:
+        >>> Pk(of="foo", cols=["bar", "baz"]).name
+        'foo_bar_baz_pk'
+    """
+
+    cols: Annotated[
+        list[str],
+        Field(default_factory=list, description="List of primary key columns"),
+    ]
+
+    @property
+    def name(self) -> str:
+        if not self.of:
+            raise ValueError(
+                "This constraint does not pass `of` value for take ownership."
+            )
+        if self.cols:
+            return f'{self.of}_{"_".join(self.cols)}_pk'
+        raise ValueError("This primary key does not have any columns.")
+
+
+class Ref(BaseModel):
+    """Reference Model
+
+    Examples:
+        *   {
+                "tbl": "foo",
+                "col": "bar",
+            }
+    """
+
+    tbl: str
+    col: str
+
+
+class Fk(Const):
+    """Foreign Key Model.
+
+    Examples:
+        >>> data={
+        ...     "of": "foo",
+        ...     "to": "bar",
+        ...     "ref": {
+        ...         "tbl": "ref_table",
+        ...         "col": "ref_column"
+        ...     }
+        ... }
+        >>> Fk.model_validate(data).name
+        'foo_bar_ref_table_ref_column_fk'
+    """
+
+    to: str
+    ref: Ref
+
+    @property
+    def name(self) -> str:
+        if not self.of:
+            raise ValueError(
+                "This constraint does not pass `of` value for take ownership."
+            )
+        return f"{self.of}_{self.to}_{self.ref.tbl}_{self.ref.col}_fk"
---
+++ packages/ddeutil-model/ddeutil_model-0.0.2-py3-none-any.whl/ddeutil/model/datasets/file.py
@@ -0,0 +1,40 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) 2022 Korawich Anuttra. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for
+# license information.
+# ------------------------------------------------------------------------------
+from typing import Literal
+
+from ..__base import BaseUpdatableModel
+from .col import Col
+
+
+class BaseFl(BaseUpdatableModel):
+    name: str
+
+
+class CsvFl(BaseFl):
+    """Csv File Model"""
+
+    type: Literal["csv"] = "csv"
+    header: bool = True
+    feature: list[Col]
+    sep: str = ","
+    comment: str = "#"
+    skip_rows: int = 0
+    skip_footer: int = 0
+    quote_char: str = '"'
+    encoding: str = "utf-8"
+
+
+class JsonFl(BaseFl):
+    """Json File Model"""
+
+    type: Literal["json"] = "json"
+
+
+class ParqFl(BaseFl):
+    """Parquet File Model"""
+
+    type: Literal["parquet"] = "parquet"
+    compress: str = "gzip"
