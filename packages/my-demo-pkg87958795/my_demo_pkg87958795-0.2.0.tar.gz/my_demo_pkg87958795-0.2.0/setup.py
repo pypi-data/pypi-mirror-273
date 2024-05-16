4c9049d0847491934518288e2da42b685337150b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 05:04:57 +0000

    Add package my-demo-pkg87958795/0.2.0/my_demo_pkg87958795-0.2.0.tar.gz

---
+++ packages/my-demo-pkg87958795/my_demo_pkg87958795-0.2.0.tar.gz/my_demo_pkg87958795-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+name='my_demo_pkg87958795',
+version='0.2.0',
+author='Dhileep Kumar Munige',
+author_email='dmunige@gmail.com',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
+
