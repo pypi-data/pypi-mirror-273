f7c5419a1a4871bc6d045a87c48aa06df46aaed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 03:04:26 +0000

    Add package fcaptcha/0.0.1/fcaptcha-0.0.1.tar.gz

---
+++ packages/fcaptcha/fcaptcha-0.0.1.tar.gz/fcaptcha-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fcaptcha',
+    version='0.0.1',
+    author='Body Alhoha',
+    author_email='bodyalhoha@gmail.com',
+    description='Official fCaptcha Wrapper',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
