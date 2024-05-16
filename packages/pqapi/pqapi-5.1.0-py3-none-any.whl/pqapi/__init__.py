09d77e766a7fd9dfc0b6f3c458f3f5e63a6697cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 06:05:05 +0000

    Add package pqapi/5.1.0/pqapi-5.1.0-py3-none-any.whl

---
+++ packages/pqapi/pqapi-5.1.0-py3-none-any.whl/pqapi/__init__.py
@@ -0,0 +1,45 @@
+from .api import (
+    agent_query,
+    async_agent_query,
+    async_delete_bibliography,
+    async_get_bibliography,
+    async_get_feedback,
+    async_query,
+    async_scrape,
+    async_send_feedback,
+    check_dois,
+    delete_bibliography,
+    get_bibliography,
+    upload_file,
+    upload_paper,
+)
+from .models import (
+    AnswerResponse,
+    QueryRequest,
+    ScrapeRequest,
+    UploadMetadata,
+    get_prompts,
+)
+from .version import __version__
+
+__all__ = [
+    "AnswerResponse",
+    "QueryRequest",
+    "ScrapeRequest",
+    "UploadMetadata",
+    "__version__",
+    "agent_query",
+    "async_agent_query",
+    "async_delete_bibliography",
+    "async_get_bibliography",
+    "async_get_feedback",
+    "async_query",
+    "async_scrape",
+    "async_send_feedback",
+    "check_dois",
+    "delete_bibliography",
+    "get_bibliography",
+    "get_prompts",
+    "upload_file",
+    "upload_paper",
+]
