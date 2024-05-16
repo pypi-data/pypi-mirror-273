8493b9c13dd7d160568fe8cdbc40e1c715c1bf6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 06:04:33 +0000

    Add package formant/1.171.21/formant-1.171.21.tar.gz

---
+++ packages/formant/formant-1.171.21.tar.gz/formant-1.171.21/formant/sdk/cloud/v2/src/resources/annotations/__init__.py
@@ -0,0 +1,5 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import AnnotationMetadata, ScopeFilter, AnnotationFieldType, AnnotationFieldValue, AnnotationTemplate, AnnotationTemplateListResponse, UserParametersRolesItem, AnnotationTags, AnnotationFieldValueTags, AnnotationTemplateTags, AnnotationStreamType, AnnotationFieldValuesRequestTags, AnnotationFieldValuesResponse, ScopeFilterTypesItem, AnnotationFieldValuesRequest, SheetParameters, UserParameters, AnnotationField, TagParameters, AnnotationType, Annotation
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation import annotation_controller_post
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation_template import annotation_template_controller_list, annotation_template_controller_get_one
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
