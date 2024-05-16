38aace44da3323c27dde0bf77eb584f5f977b3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 03:04:30 +0000

    Add package pythfinder/0.0.3.12/pythfinder-0.0.3.12.tar.gz

---
+++ packages/pythfinder/pythfinder-0.0.3.12.tar.gz/pythfinder-0.0.3.12/README.md
@@ -0,0 +1,12 @@
+![pyth_finder_logo](https://github.com/omegacoreFLL/PythFinder/assets/159171107/1dc439b2-0ac0-40f8-95fd-4883b0507603)
+
+![version_badge](https://img.shields.io/badge/alpha-0.0.3-006400)
+![license](https://img.shields.io/badge/license-MIT-62e39e)
+
+
+## Advanced Usage
+
+Check out the documentation. --- **TO BE ADDED**
+
+
+*v. alpha-0.0.3*
---
+++ packages/pythfinder/pythfinder-0.0.3.12.tar.gz/pythfinder-0.0.3.12/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pythfinder',
+    version='0.0.3.12',
+    license='MIT',
+    author='Contraș Adrian',
+    author_email='omegacoresincai@gmail.com',
+    description='Motion Planning library designed for FLL teams',
+    packages=find_packages(),
+    keywords=[
+        'motion-planning',
+        'mobile-robots',
+        'robotics',
+        'first-lego-league',
+        'first-robotics',
+        'fll'
+    ],
+    install_requires=[''],
+    include_package_data=True,
+    
+)
