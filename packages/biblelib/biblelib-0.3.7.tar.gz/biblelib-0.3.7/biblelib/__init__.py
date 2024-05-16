e42323f741b6c5a5665a0b9f92fb6def8f1654bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 06:04:43 +0000

    Add package biblelib/0.3.7/biblelib-0.3.7.tar.gz

---
+++ packages/biblelib/biblelib-0.3.7.tar.gz/biblelib-0.3.7/biblelib/__init__.py
@@ -0,0 +1,6 @@
+"""Biblelib provides utilities for working with Bible books, references, pericopes, and other units.
+
+This namespace is intentionally bare: see individual packages
+(`books`, `words`, etc.) for imports.
+
+"""
