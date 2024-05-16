c4fa229a7a7467a62508db1c32f32d71250ddda3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 14:04:54 +0000

    Add package bucketed-scene-flow-eval/2.0.10/bucketed_scene_flow_eval-2.0.10-py3-none-any.whl

---
+++ packages/bucketed-scene-flow-eval/bucketed_scene_flow_eval-2.0.10-py3-none-any.whl/bucketed_scene_flow_eval/datasets/argoverse2/dataset.py
@@ -0,0 +1,151 @@
+import copy
+from pathlib import Path
+from typing import Optional, Union
+
+from bucketed_scene_flow_eval.datastructures import *
+from bucketed_scene_flow_eval.eval import (
+    BucketedEPEEvaluator,
+    Evaluator,
+    ThreeWayEPEEvaluator,
+)
+from bucketed_scene_flow_eval.interfaces import (
+    CausalSeqLoaderDataset,
+    EvalType,
+    NonCausalSeqLoaderDataset,
+)
+
+from .argoverse_raw_data import DEFAULT_POINT_CLOUD_RANGE, PointCloudRange
+from .argoverse_scene_flow import (
+    CATEGORY_MAP,
+    ArgoverseNoFlowSequenceLoader,
+    ArgoverseSceneFlowSequenceLoader,
+)
+from .av2_metacategories import (
+    BUCKETED_METACATAGORIES,
+    BUCKETED_VOLUME_METACATAGORIES,
+    THREEWAY_EPE_METACATAGORIES,
+)
+
+
+def _make_av2_evaluator(eval_type: EvalType, eval_args: dict) -> Evaluator:
+    eval_args_copy = copy.deepcopy(eval_args)
+    # Builds the evaluator object for this dataset.
+    if eval_type == EvalType.BUCKETED_EPE:
+        if "meta_class_lookup" not in eval_args_copy:
+            eval_args_copy["meta_class_lookup"] = BUCKETED_METACATAGORIES
+        if "class_id_to_name" not in eval_args_copy:
+            eval_args_copy["class_id_to_name"] = CATEGORY_MAP
+        return BucketedEPEEvaluator(**eval_args_copy)
+    elif eval_type == EvalType.THREEWAY_EPE:
+        if "meta_class_lookup" not in eval_args_copy:
+            eval_args_copy["meta_class_lookup"] = THREEWAY_EPE_METACATAGORIES
+        if "class_id_to_name" not in eval_args_copy:
+            eval_args_copy["class_id_to_name"] = CATEGORY_MAP
+        return ThreeWayEPEEvaluator(**eval_args_copy)
+    elif eval_type == EvalType.BUCKETED_VOLUME_EPE:
+        if "meta_class_lookup" not in eval_args_copy:
+            eval_args_copy["meta_class_lookup"] = BUCKETED_VOLUME_METACATAGORIES
+        if "class_id_to_name" not in eval_args_copy:
+            eval_args_copy["class_id_to_name"] = {
+                -1: "BACKGROUND",
+                0: "SMALL",
+                1: "MEDIUM",
+                2: "LARGE",
+            }
+        return BucketedEPEEvaluator(**eval_args_copy)
+    else:
+        raise ValueError(f"Unknown eval type {eval_type}")
+
+
+class Argoverse2CausalSceneFlow(CausalSeqLoaderDataset):
+    def __init__(
+        self,
+        root_dir: Union[Path, list[Path]],
+        subsequence_length: int = 2,
+        with_ground: bool = True,
+        with_rgb: bool = False,
+        cache_root: Path = Path("/tmp/"),
+        use_gt_flow: bool = True,
+        flow_data_path: Optional[Union[Path, list[Path]]] = None,
+        eval_type: str = "bucketed_epe",
+        eval_args=dict(),
+        expected_camera_shape: tuple[int, int, int] = (1550, 2048, 3),
+        point_cloud_range: PointCloudRange | None = DEFAULT_POINT_CLOUD_RANGE,
+        use_cache=True,
+        load_flow: bool = True,
+    ) -> None:
+        if load_flow:
+            self.sequence_loader = ArgoverseSceneFlowSequenceLoader(
+                root_dir,
+                with_rgb=with_rgb,
+                use_gt_flow=use_gt_flow,
+                flow_data_path=flow_data_path,
+                expected_camera_shape=expected_camera_shape,
+                point_cloud_range=point_cloud_range,
+            )
+        else:
+            self.sequence_loader = ArgoverseNoFlowSequenceLoader(
+                root_dir,
+                with_rgb=with_rgb,
+                expected_camera_shape=expected_camera_shape,
+                point_cloud_range=point_cloud_range,
+            )
+        super().__init__(
+            sequence_loader=self.sequence_loader,
+            subsequence_length=subsequence_length,
+            with_ground=with_ground,
+            idx_lookup_cache_root=cache_root,
+            eval_type=eval_type,
+            eval_args=eval_args,
+            use_cache=use_cache,
+        )
+
+    def evaluator(self) -> Evaluator:
+        return _make_av2_evaluator(self.eval_type, self.eval_args)
+
+
+class Argoverse2NonCausalSceneFlow(NonCausalSeqLoaderDataset):
+    def __init__(
+        self,
+        root_dir: Union[Path, list[Path]],
+        subsequence_length: int = 2,
+        with_ground: bool = True,
+        with_rgb: bool = False,
+        cache_root: Path = Path("/tmp/"),
+        use_gt_flow: bool = True,
+        flow_data_path: Optional[Union[Path, list[Path]]] = None,
+        eval_type: str = "bucketed_epe",
+        eval_args=dict(),
+        expected_camera_shape: tuple[int, int, int] = (1550, 2048, 3),
+        point_cloud_range: PointCloudRange | None = DEFAULT_POINT_CLOUD_RANGE,
+        use_cache=True,
+        load_flow: bool = True,
+    ) -> None:
+        if load_flow:
+            self.sequence_loader = ArgoverseSceneFlowSequenceLoader(
+                root_dir,
+                with_rgb=with_rgb,
+                use_gt_flow=use_gt_flow,
+                flow_data_path=flow_data_path,
+                expected_camera_shape=expected_camera_shape,
+                point_cloud_range=point_cloud_range,
+            )
+        else:
+            self.sequence_loader = ArgoverseNoFlowSequenceLoader(
+                root_dir,
+                with_rgb=with_rgb,
+                expected_camera_shape=expected_camera_shape,
+                point_cloud_range=point_cloud_range,
+            )
+        super().__init__(
+            sequence_loader=self.sequence_loader,
+            subsequence_length=subsequence_length,
+            with_ground=with_ground,
+            idx_lookup_cache_root=cache_root,
+            eval_type=eval_type,
+            eval_args=eval_args,
+            use_cache=use_cache,
+        )
+
+    def evaluator(self) -> Evaluator:
+        return _make_av2_evaluator(self.eval_type, self.eval_args)
