94259af1087ed4322d37448d22adfe836d9a0825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 04:05:00 +0000

    Add package fxgui/2.1.0/fxgui-2.1.0.tar.gz

---
+++ packages/fxgui/fxgui-2.1.0.tar.gz/fxgui-2.1.0/setup.py
@@ -0,0 +1,33 @@
+"""PyPI setup script."""
+
+# Built-in
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Metadata
+__author__ = "Valentin Beaumont"
+__email__ = "valentin.onze@gmail.com"
+
+
+###### CODE ####################################################################
+
+
+# Add `README.md` as project long description
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="fxgui",
+    version="2.1.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description="Custom Python classes and utilities tailored for Qt built UI, in VFX-oriented DCC applications.",
+    author="Valentin Beaumont",
+    author_email="valentin.onze@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "qtpy",
+        "PySide2",
+    ],
+    include_package_data=True,
+)
