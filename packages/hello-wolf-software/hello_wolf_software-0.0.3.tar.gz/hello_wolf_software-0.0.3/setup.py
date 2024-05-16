18d8a0913720574b2d0185debf5368c98c306b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 04:04:56 +0000

    Add package hello-wolf-software/0.0.3/hello_wolf_software-0.0.3.tar.gz

---
+++ packages/hello-wolf-software/hello_wolf_software-0.0.3.tar.gz/hello_wolf_software-0.0.3/setup.py
@@ -0,0 +1,31 @@
+# setup.py
+
+"""Setup script."""
+
+from setuptools import setup, find_packages
+
+with open("README.md", 'r', encoding='UTF-8') as f:
+    long_description: str = f.read()
+
+setup(
+    name='hello-wolf-software',
+    version='0.0.3',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'hello=hello_wolf_software.hello:main',
+        ],
+    },
+    author='Wolf Software',
+    author_email='pypi@wolfsoftware.com',
+    description='A simple hello world test',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/DevelopersToolbox/hello-wolf-software',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
