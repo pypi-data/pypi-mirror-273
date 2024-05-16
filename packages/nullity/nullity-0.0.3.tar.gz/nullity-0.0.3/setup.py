fd25122e13fc6dd64468f58fd9812c060ed99f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 16:08:03 +0000

    Add package nullity/0.0.3/nullity-0.0.3.tar.gz

---
+++ packages/nullity/nullity-0.0.3.tar.gz/nullity-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'A package for all things linear algebra'
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name="nullity",
+    version=VERSION,
+    author="Mihir Aggarwal",
+    author_email="mail@mihiraggarwal.me",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['numpy'],
+    keywords=['linear', 'algebra', 'linear algebra', 'linalg', 'lin-alg', 'linearalgebra', 'eigen', 'eigenvalue', 'eigenvector', 'nullity', 'null', 'null space', 'rank nullity'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
