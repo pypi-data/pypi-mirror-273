814d250a77a6e726ad2a0c6d4eff6afeead7d9b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 04:04:18 +0000

    Add package prompeteer/0.3.1/prompeteer-0.3.1-py3-none-any.whl

---
+++ packages/prompeteer/prompeteer-0.3.1-py3-none-any.whl/prompeteer/providers/aws_bedrock/aws_llm_request.py
@@ -0,0 +1,42 @@
+import json
+from typing import List, Optional, Dict
+
+from prompeteer.prompt.prompt import Message, Variable, DeclaredVariable
+from prompeteer.providers.llm_request import ILLMRequest
+from prompeteer.utils.utils import inject_variables
+
+
+class AWSLLMRequest(ILLMRequest):
+
+    def __init__(self,
+                 variables_to_inject: List[Variable],
+                 declared_variables: List[DeclaredVariable],
+                 max_tokens: int,
+                 model: str,
+                 messages: List[Dict],
+                 system: Optional[str] = None,
+                 temperature: Optional[float] = None,
+                 top_p: Optional[float] = None,
+                 top_k: Optional[float] = None,
+                 stop_sequence: Optional[List[str]] = None):
+        self.max_tokens = max_tokens
+        self.model = model
+        self.messages: List[Message] = inject_variables(messages, declared_variables, variables_to_inject)
+        self.system = system
+        self.temperature = temperature
+        self.top_p = top_p
+        self.top_k = top_k
+        self.stop_sequence = stop_sequence
+
+    def get_prompt_text(self) -> str:
+        messages: List[Dict] = []
+        for message in self.messages:
+            messages.append({
+                'content': message.content,
+                'role': message.role
+            })
+        result = dict()
+        result['messages'] = messages
+        if self.system is not None:
+            result['system'] = self.system
+        return json.dumps(result)
