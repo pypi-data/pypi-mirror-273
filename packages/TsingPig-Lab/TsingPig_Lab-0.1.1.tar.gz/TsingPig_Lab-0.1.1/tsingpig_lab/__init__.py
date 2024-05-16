7c1a824e3e83d23942af1ebc31610df291ea640a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 16:08:02 +0000

    Add package TsingPig-Lab/0.1.1/TsingPig_Lab-0.1.1.tar.gz

---
+++ packages/TsingPig-Lab/TsingPig_Lab-0.1.1.tar.gz/TsingPig_Lab-0.1.1/tsingpig_lab/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+__package_name__ = "TsingPig_Lab"
+__version__ = '0.1.1'
+__author__ = 'TsingPig'
---
+++ packages/TsingPig-Lab/TsingPig_Lab-0.1.1.tar.gz/TsingPig_Lab-0.1.1/tsingpig_lab/tsingpig_lab.py
@@ -0,0 +1,7 @@
+
+from ..tsingpig_lab import __version__, __package_name__, __author__
+
+def info():
+    print(f'package_name: {__package_name__}')
+    print(f'version: {__version__}')
+    print(f'author: {__author__}')
