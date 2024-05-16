908cc184ad67f63399ff4a3f4daf675314ad9a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 04:05:16 +0000

    Add package argparse-decorations/0.0.2/argparse_decorations-0.0.2.tar.gz

---
+++ packages/argparse-decorations/argparse_decorations-0.0.2.tar.gz/argparse_decorations-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open('__version__') as f:
+    _version = f.readline().strip()
+
+setup(name='argparse_decorations',
+      version=_version,
+      description='argparse wrapper around decorations',
+      url='https://bitbucket.org/rmonico/argparse_decorations',
+      author='Rafael Monico',
+      author_email='rmonico1@gmail.com',
+      license='GPL3',
+      include_package_data=True,
+      packages=['argparse_decorations'],
+      zip_safe=False)
