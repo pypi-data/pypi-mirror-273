61240c807ef8e3d7a6670de93cb291d1d4cd89eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 05:04:58 +0000

    Add package ex4nicegui/0.6.6/ex4nicegui-0.6.6.tar.gz

---
+++ packages/ex4nicegui/ex4nicegui-0.6.6.tar.gz/ex4nicegui-0.6.6/ex4nicegui/reactive/officials/number.py
@@ -0,0 +1,80 @@
+from typing import (
+    Any,
+    Callable,
+    Optional,
+    TypeVar,
+    Dict,
+    Union,
+)
+from ex4nicegui.reactive.utils import ParameterClassifier
+from ex4nicegui.utils.apiEffect import ui_effect
+
+from ex4nicegui.utils.signals import (
+    ReadonlyRef,
+    _TMaybeRef as TMaybeRef,
+    to_value,
+)
+from nicegui import ui
+from .base import BindableUi
+
+T = TypeVar("T")
+
+
+class NumberBindableUi(BindableUi[ui.number]):
+    def __init__(
+        self,
+        label: Optional[TMaybeRef[str]] = None,
+        *,
+        placeholder: Optional[TMaybeRef[str]] = None,
+        value: Optional[Union[TMaybeRef[float], TMaybeRef[int]]] = None,
+        min: Optional[TMaybeRef[float]] = None,
+        max: Optional[TMaybeRef[float]] = None,
+        step: Optional[TMaybeRef[float]] = None,
+        prefix: Optional[TMaybeRef[str]] = None,
+        suffix: Optional[TMaybeRef[str]] = None,
+        format: Optional[TMaybeRef[str]] = None,
+        on_change: Optional[Callable[..., Any]] = None,
+        validation: Dict[str, Callable[..., bool]] = {},
+    ) -> None:
+        pc = ParameterClassifier(
+            locals(),
+            maybeRefs=[
+                "label",
+                "placeholder",
+                "value",
+                "min",
+                "max",
+                "step",
+                "prefix",
+                "suffix",
+                "format",
+                "validation",
+            ],
+            v_model=("value", "on_change"),
+            events=["on_change"],
+        )
+
+        value_kws = pc.get_values_kws()
+
+        element = ui.number(**value_kws)
+        super().__init__(element)  # type: ignore
+
+        for key, value in pc.get_bindings().items():
+            self.bind_prop(key, value)  # type: ignore
+
+    @property
+    def value(self):
+        return self.element.value
+
+    def bind_prop(self, prop: str, ref_ui: ReadonlyRef):
+        if prop == "value":
+            return self.bind_value(ref_ui)
+
+        return super().bind_prop(prop, ref_ui)
+
+    def bind_value(self, ref_ui: ReadonlyRef[float]):
+        @ui_effect
+        def _():
+            self.element.set_value(to_value(ref_ui))
+
+        return self
