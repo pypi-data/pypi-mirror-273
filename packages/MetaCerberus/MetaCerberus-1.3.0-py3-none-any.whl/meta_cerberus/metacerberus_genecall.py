9a9cdc6ff85aa97406bf8842181e3927311c651b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 05:04:52 +0000

    Add package MetaCerberus/1.3.0/MetaCerberus-1.3.0-py3-none-any.whl

---
+++ packages/MetaCerberus/MetaCerberus-1.3.0-py3-none-any.whl/meta_cerberus/metacerberus_genecall.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""metacerberus_genecall.py: Module for finding Open Reading Frames (ORF) in FASTA nucleotide files
+Uses prodigal
+Uses FGS+
+"""
+
+from pathlib import Path
+import subprocess
+
+
+# Eukaryotic option
+def findORF_fgs(contig, config, subdir):
+    path = Path(config['DIR_OUT'], subdir)
+    done = path / "complete"
+
+    baseOut = path / "proteins"
+    faaOut  = path / "proteins.faa"
+
+    if not config['REPLACE'] and done.exists() and faaOut.exists():
+            return faaOut
+    done.unlink(missing_ok=True)
+    path.mkdir(exist_ok=True, parents=True)
+
+    train = "complete"
+    if config['ILLUMINA']:
+         train = "illumina_10"
+    elif config['NANOPORE'] or config['PACBIO']:
+         train = "454_30"
+
+    command = f"{config['EXE_FGS']} -p {config['CPUS']} -s {contig} -o {baseOut} -w 1 -t {train}"
+    try:
+        with Path(path,"stdout.txt").open('w') as fout, Path(path,"stderr.txt").open('w') as ferr:
+            subprocess.run(command, shell=True, check=True, stdout=fout, stderr=ferr)
+    except Exception as e:
+        print(e)
+        return None
+
+    done.touch()
+    return faaOut
+
+
+# Microbial option
+def findORF_prod(contig, config, subdir):
+    path = Path(config['DIR_OUT'], subdir)
+    done = path / "complete"
+
+    faaOut = path / "proteins.faa"
+    fnaOut = faaOut.with_suffix(".ffn")
+
+    if not config['REPLACE'] and done.exists() and faaOut.exists():
+        return faaOut
+    done.unlink(missing_ok=True)
+    path.mkdir(exist_ok=True, parents=True)
+
+    command = f"{config['EXE_PRODIGAL']} -i {contig} -o {path}/genes.gff -a {faaOut} -d {fnaOut} -f gff"
+    try:
+        with Path(path, 'stdout.txt').open('w') as fout, Path(path, 'stderr.txt').open('w') as ferr:
+            subprocess.run(command, shell=True, check=True, stdout=fout, stderr=ferr)
+    except Exception as e:
+        print(e)
+
+    done.touch()    
+    return faaOut
+
+
+# Metagenome option
+def findORF_meta(contig, config, subdir):
+    path = Path(config['DIR_OUT'], subdir)
+    done = path / "complete"
+
+    faaOut = path / "proteins.faa"
+    fnaOut = faaOut.with_suffix(".ffn")
+
+    if not config['REPLACE'] and done.exists() and faaOut.exists():
+        return faaOut
+    done.unlink(missing_ok=True)
+    path.mkdir(exist_ok=True, parents=True)
+
+    command = f"{config['EXE_PRODIGAL']} -i {contig} -o {path}/genes.gff -a {faaOut} -d {fnaOut} -f gff -p meta"
+    try:
+        with Path(path, "stdout.txt").open('w') as fout, Path(path, "stderr.txt").open('w') as ferr:
+            subprocess.run(command, shell=True, check=True, stdout=fout, stderr=ferr)
+    except Exception as e:
+        print(e)
+
+    done.touch()
+    return faaOut
+
+# Giant Virus
+def findORF_prodgv(contig, config, subdir, meta=False):
+    path = Path(config['DIR_OUT'], subdir)
+    done = path / "complete"
+
+    faaOut  = path / "proteins.faa"
+    fnaOut = faaOut.with_suffix(".ffn")
+
+    if not config['REPLACE'] and done.exists() and faaOut.exists():
+            return faaOut
+    done.unlink(missing_ok=True)
+    path.mkdir(exist_ok=True, parents=True)
+
+    command = f"{config['EXE_PRODIGAL-GV']} -i {contig} -a {faaOut} -d {fnaOut} -o {path / 'proteins.gbk'} {'-p meta' if meta else ''}"
+    print(command)
+    try:
+        with Path(path,"stdout.txt").open('w') as fout, Path(path,"stderr.txt").open('w') as ferr:
+            subprocess.run(command, shell=True, check=True, stdout=fout, stderr=ferr)
+    except Exception as e:
+        print(e)
+        return None
+
+    done.touch()
+    return faaOut
+
+# Phage
+def findORF_phanotate(contig, config, subdir, meta=False):
+    path = Path(config['DIR_OUT'], subdir)
+    done = path / "complete"
+
+    faaOut  = path / "proteins.faa"
+
+    if not config['REPLACE'] and done.exists() and faaOut.exists():
+            return faaOut
+    done.unlink(missing_ok=True)
+    path.mkdir(exist_ok=True, parents=True)
+
+    command = [config['EXE_PHANOTATE'], '-f', 'faa', contig] #, '-o', faaOut, contig]
+        #awk '{ if (substr($1,1,1) != ">") gsub(/#|\+|\*/, ""); print $1 }'
+    try:
+        with faaOut.open('r') as fout, Path(path,"stderr.txt").open('w') as ferr:
+            pout = subprocess.run(command, stdout=faaOut, stderr=ferr)
+    except Exception as e:
+         print(e)
+    #    for line in pout:
+    #        line=line.strip()
+    #        if line.startswith('>'):
+    #            print(line, file=fout)
+    #        else:
+    #            re.sub(r'[^A-Za-z]', '', line, file=fout)
+
+    #try:
+    #    subprocess.run(['sed', '-i', 's/#//g', faaOut])
+    #except Exception as e:
+    #    with Path(path,"stderr.txt").open('a') as ferr:
+    #        print(e, file=ferr)
+
+    done.touch()
+    return faaOut
---
+++ packages/MetaCerberus/MetaCerberus-1.3.0-py3-none-any.whl/meta_cerberus/metacerberus_hmm.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+
+"""metacerberus_hmm.py Module to find FOAM annotations using Hidden Markov Models
+Uses HMMER hmmsearch
+"""
+
+import os
+import re
+from pathlib import Path
+import subprocess
+import time
+
+
+## HMMER Search
+def searchHMM(aminoAcids:dict, config:dict, subdir:str, hmmDB:tuple, CPUs:int=4):
+    minscore = config['MINSCORE']
+    evalue = config['EVALUE']
+
+    hmmKey,hmmDB = hmmDB
+
+    hmmOut = dict()
+    for key,amino in aminoAcids.items():
+        path = Path(config['DIR_OUT'], subdir, key)
+        os.makedirs(path, exist_ok=True)
+
+        name_dom = f"{key}_tmp.hmm"
+        hmmOut[os.path.join(path, name_dom)] = amino
+
+    jobs = dict()
+    outlist = list()
+    for domtbl_out,amino in hmmOut.items():
+        pathname = os.path.dirname(domtbl_out)
+        basename = os.path.basename(domtbl_out)
+        outname = os.path.splitext(basename)[0] + ".tsv"
+        outfile = os.path.join(pathname, f"{hmmKey}-{outname}")
+
+        # HMMER
+        errfile=Path(outfile).with_suffix('.err').open('w')
+        try:
+            reduce_awk = f"""grep -Ev '^#' | awk '$7 < {evalue} && $14 > {minscore} {{ print $1 "\t" $4 "\t" $7 "\t" $14 "\t" $3 "\t" $18 "\t" $19 }}' >> {outfile}"""
+            command = f"{config['EXE_HMMSEARCH']} -o /dev/null --cpu {CPUs} --domtblout /dev/stdout {hmmDB} {amino} | {reduce_awk}"
+            if not Path(outfile).exists():
+                jobs[domtbl_out] = subprocess.Popen(command, shell=True, stderr=errfile)
+            outlist.append(outfile)
+        except Exception as e:
+            print(e, file=errfile)
+            print("Error: failed to run: " + command, file=errfile)
+        errfile.close
+    
+    # Wait for jobs
+    done = False
+    while not done:
+        done = True
+        keys = list(jobs.keys())
+        for domtbl_out in keys:
+            if jobs[domtbl_out].poll() is None: # no return code yet, still running
+                done = False # At least one job still running
+        time.sleep(1)
+
+    return outlist
+
+
+# Filter HMM results
+def filterHMM(hmm_tsv:Path, outfile:Path, dbpath:Path):
+    outfile.parent.mkdir(parents=True, exist_ok=True)
+
+    for i in range(1, len(dbpath.suffixes)):
+        dbpath = Path(dbpath.with_suffix(''))
+    dbLookup = dbpath.with_suffix('.tsv')
+    match = re.search(r"^KOFam_[a-z]+_([A-Z]+)", hmm_tsv.name)
+    if match:
+        dbLookup = dbpath.with_name(f'{match.group(1)}.tsv')
+    dbLookup = dbLookup.read_text()
+
+    BH_target = dict()
+    logfile = outfile.with_suffix('.log')
+    with hmm_tsv.open() as reader, logfile.open('w') as logger:
+        for i,line in enumerate(reader, 1):
+            line = line.split('\t')
+            try:
+                target = line[0]
+                query = line[1]
+                e_value = float(line[2])
+                score = float(line[3])
+                length = int(line[4])
+                start = int(line[5])
+                end = int(line[6])
+            except:
+                print("Failed to read line:", i, hmm_tsv, file=logger)
+                continue
+            # Check if Query is in the Database
+            if not re.search(query, dbLookup, re.MULTILINE):
+                continue
+            
+            # Count Proper Hits
+            # 1) Overlapping: Count best score
+            # 2) Unique: Count both
+            if target not in BH_target:
+                BH_target[target] = [(query, e_value, score, length, start, end)]
+            else: # More than one match/target
+                #keys = list(BH_target[target].keys())
+                item = (query, e_value, score, length, start, end)
+                add = False
+                overlap = False
+                for c,match in enumerate(BH_target[target]):
+                    # Check for overlap
+                    if start <= match[5] and end >= match[4]:
+                        overlap_len = min(end, match[5]) - max(start, match[4])
+                        if overlap_len > 10:
+                            overlap = True
+                            # Equal Score
+                            if e_value == match[1] and score == match[2]:
+                                add = True
+                            # Winner takes all
+                            elif e_value < match[1]:
+                                BH_target[target][c] = item
+                            elif e_value == match[1]:
+                                if score > match[2]:
+                                    BH_target[target][c] = item
+                if add or not overlap:
+                    # Equal score OR Dual domain
+                    BH_target[target] += [item]
+            # next line
+            continue
+    # Write filtered overlaps to file
+    with outfile.open('w') as writer:
+        print("target", "query", "e-value", "score", "length", "start", "end", sep='\t', file=writer)
+        for target in sorted(BH_target):
+            for match in set(BH_target[target]):
+                query, e_value, score, length, start, end = match
+                print(target, query, e_value, score, length, start, end, sep='\t', file=writer)
+
+    return outfile
