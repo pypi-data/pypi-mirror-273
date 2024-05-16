98d4ef19ffa0db6af571d8d0679f1ddca043f476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 06:04:34 +0000

    Add package launch-cli/0.7.0/launch_cli-0.7.0-py3-none-any.whl

---
+++ packages/launch-cli/launch_cli-0.7.0-py3-none-any.whl/launch/cli/github/__init__.py
@@ -0,0 +1,17 @@
+import click
+
+from .access import access_group
+from .hooks import hooks_group
+from .repo import repo_group
+from .version import version_group
+
+
+@click.group(name="github")
+def github_group():
+    """Command family for GitHub-related tasks."""
+
+
+github_group.add_command(access_group)
+github_group.add_command(hooks_group)
+github_group.add_command(version_group)
+github_group.add_command(repo_group)
