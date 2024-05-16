8297399e556eb927778761ee2596aac850476d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 16:08:41 +0000

    Add package dfapp-base/0.0.1a1/dfapp_base-0.0.1a1-py3-none-any.whl

---
+++ packages/dfapp-base/dfapp_base-0.0.1a1-py3-none-any.whl/dfapp/components/generators/__init__.py
@@ -0,0 +1,4 @@
+from .SimpleGenerator import SimpleGeneratorComponent
+from .EvolInstructGenerator import EvolInstructGeneratorComponent
+
+__all__ = ["SimpleGeneratorComponent", "EvolInstructGeneratorComponent"]
