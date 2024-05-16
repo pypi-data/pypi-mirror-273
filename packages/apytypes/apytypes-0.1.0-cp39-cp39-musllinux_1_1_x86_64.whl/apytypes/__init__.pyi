57a3d9750d7a3c46d804d1dbd2e5ba0d6c6eb66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 03:04:39 +0000

    Add package apytypes/0.1.0/apytypes-0.1.0-cp39-cp39-musllinux_1_1_x86_64.whl

---
+++ packages/apytypes/apytypes-0.1.0-cp39-cp39-musllinux_1_1_x86_64.whl/apytypes/__init__.pyi
@@ -0,0 +1,34 @@
+from __future__ import annotations
+from apytypes._apytypes import APyFixed
+from apytypes._apytypes import APyFixedArray
+from apytypes._apytypes import APyFixedAccumulatorContext
+from apytypes._apytypes import APyFixedCastContext
+from apytypes._apytypes import APyFloat
+from apytypes._apytypes import APyFloatArray
+from apytypes._apytypes import APyFloatAccumulatorContext
+from apytypes._apytypes import OverflowMode
+from apytypes._apytypes import APyFloatQuantizationContext
+from apytypes._apytypes import QuantizationMode
+from apytypes._apytypes import get_float_quantization_mode
+from apytypes._apytypes import get_float_quantization_seed
+from apytypes._apytypes import set_float_quantization_mode
+from apytypes._apytypes import set_float_quantization_seed
+from apytypes._version import version as __version__
+
+__all__: list = [
+    "APyFixed",
+    "APyFixedArray",
+    "APyFixedAccumulatorContext",
+    "APyFixedCastContext",
+    "APyFloat",
+    "APyFloatArray",
+    "APyFloatAccumulatorContext",
+    "OverflowMode",
+    "APyFloatQuantizationContext",
+    "QuantizationMode",
+    "__version__",
+    "get_float_quantization_mode",
+    "set_float_quantization_mode",
+    "get_float_quantization_seed",
+    "set_float_quantization_seed",
+]
