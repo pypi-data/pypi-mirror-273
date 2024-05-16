cd574f08cfc73e30e32564c8c1539c4bf4c2b8ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 05:05:00 +0000

    Add package dailalib/2.4.0/dailalib-2.4.0-py3-none-any.whl

---
+++ packages/dailalib/dailalib-2.4.0-py3-none-any.whl/dailalib/__init__.py
@@ -0,0 +1,55 @@
+__version__ = "2.4.0"
+
+from .api import AIAPI, OpenAIAPI
+from libbs.api import DecompilerInterface
+
+
+def create_plugin(*args, **kwargs):
+
+    #
+    # OpenAI API (ChatGPT)
+    #
+
+    openai_api = OpenAIAPI(delay_init=True)
+    # create context menus for prompts
+    gui_ctx_menu_actions = {
+        f"DAILA/OpenAI/{prompt_name}": (prompt.desc, getattr(openai_api, prompt_name))
+        for prompt_name, prompt in openai_api.prompts_by_name.items()
+    }
+    # create context menus for others
+    gui_ctx_menu_actions["DAILA/OpenAI/update_api_key"] = ("Update API Key", openai_api.ask_api_key)
+
+    #
+    # VarModel API (local variable renaming)
+    #
+
+    from varbert.api import VariableRenamingAPI
+    var_api = VariableRenamingAPI(delay_init=True)
+
+    # add single interface, which is to rename variables
+    def make_callback(predict_for_all_variables):
+        return lambda *args, **kwargs: var_api.query_model(**kwargs, remove_bad_names=not predict_for_all_variables)
+
+    gui_ctx_menu_actions["DAILA/VarBERT/varbert_rename_vars"] = ("Suggest new variable names (source-like only)", make_callback(predict_for_all_variables=False))
+    gui_ctx_menu_actions["DAILA/VarBERT/varbert_rename_vars_all"] = ("Suggest new variable names (for all variables)", make_callback(predict_for_all_variables=True))
+
+    #
+    # Decompiler Plugin Registration
+    #
+
+    force_decompiler = kwargs.pop("force_decompiler", None)
+    deci = DecompilerInterface.discover(
+        force_decompiler=force_decompiler,
+        # decompiler-creation args
+        plugin_name="DAILA",
+        init_plugin=True,
+        gui_ctx_menu_actions=gui_ctx_menu_actions,
+        gui_init_args=args,
+        gui_init_kwargs=kwargs
+    )
+
+    openai_api.init_decompiler_interface(decompiler_interface=deci)
+    if var_api is not None:
+        var_api.init_decompiler_interface(decompiler_interface=deci)
+
+    return deci.gui_plugin
