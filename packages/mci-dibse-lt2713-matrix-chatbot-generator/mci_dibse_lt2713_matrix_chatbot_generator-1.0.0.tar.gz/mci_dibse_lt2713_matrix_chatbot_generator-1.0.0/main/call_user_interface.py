32ffbd6d5813af6dfb93d025ef6553d496a36fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 05:04:36 +0000

    Add package mci-dibse-lt2713-matrix-chatbot-generator/1.0.0/mci_dibse_lt2713_matrix_chatbot_generator-1.0.0.tar.gz

---
+++ packages/mci-dibse-lt2713-matrix-chatbot-generator/mci_dibse_lt2713_matrix_chatbot_generator-1.0.0.tar.gz/mci_dibse_lt2713_matrix_chatbot_generator-1.0.0/main/call_user_interface.py
@@ -0,0 +1,12 @@
+from classes.UserInterface import UserInterface
+from structures.questions import Questions
+from classes.QTIParser import QTIParser
+
+qti_parser = QTIParser()
+questions = qti_parser.get_questions()
+# questions.print_short()
+
+ui = UserInterface(questions)
+ui.loop()
+
+
