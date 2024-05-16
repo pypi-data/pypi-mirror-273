7ae330f83babcbdda3171c9c72e97ccee71f5815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 03:04:26 +0000

    Add package resc-backend/3.2.0/resc_backend-3.2.0.tar.gz

---
+++ packages/resc-backend/resc_backend-3.2.0.tar.gz/resc_backend-3.2.0/src/resc_backend/resc_web_service/schema/finding_status.py
@@ -0,0 +1,10 @@
+# Standard Library
+from enum import Enum
+
+
+class FindingStatus(str, Enum):
+    NOT_ANALYZED = "NOT_ANALYZED"
+    NOT_ACCESSIBLE = "NOT_ACCESSIBLE"
+    CLARIFICATION_REQUIRED = "CLARIFICATION_REQUIRED"
+    FALSE_POSITIVE = "FALSE_POSITIVE"
+    TRUE_POSITIVE = "TRUE_POSITIVE"
