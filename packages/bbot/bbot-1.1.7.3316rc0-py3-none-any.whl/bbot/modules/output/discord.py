a29c2d7f828624647694860790fa1905cb9aee88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 05:05:00 +0000

    Add package bbot/1.1.7.3316rc0/bbot-1.1.7.3316rc0-py3-none-any.whl

---
+++ packages/bbot/bbot-1.1.7.3316rc0-py3-none-any.whl/bbot/modules/output/discord.py
@@ -0,0 +1,12 @@
+from bbot.modules.templates.webhook import WebhookOutputModule
+
+
+class Discord(WebhookOutputModule):
+    watched_events = ["*"]
+    meta = {"description": "Message a Discord channel when certain events are encountered"}
+    options = {"webhook_url": "", "event_types": ["VULNERABILITY", "FINDING"], "min_severity": "LOW"}
+    options_desc = {
+        "webhook_url": "Discord webhook URL",
+        "event_types": "Types of events to send",
+        "min_severity": "Only allow VULNERABILITY events of this severity or higher",
+    }
---
+++ packages/bbot/bbot-1.1.7.3316rc0-py3-none-any.whl/bbot/modules/output/teams.py
@@ -0,0 +1,19 @@
+from bbot.modules.templates.webhook import WebhookOutputModule
+
+
+class Teams(WebhookOutputModule):
+    watched_events = ["*"]
+    meta = {"description": "Message a Teams channel when certain events are encountered"}
+    options = {"webhook_url": "", "event_types": ["VULNERABILITY", "FINDING"], "min_severity": "LOW"}
+    options_desc = {
+        "webhook_url": "Discord webhook URL",
+        "event_types": "Types of events to send",
+        "min_severity": "Only allow VULNERABILITY events of this severity or higher",
+    }
+    _max_event_handlers = 5
+    good_status_code = 200
+    content_key = "text"
+
+    def evaluate_response(self, response):
+        text = getattr(response, "text", "")
+        return text == "1"
