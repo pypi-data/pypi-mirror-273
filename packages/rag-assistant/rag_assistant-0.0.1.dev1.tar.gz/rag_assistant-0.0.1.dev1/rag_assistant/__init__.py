116e5dee1345b1e3cdab2e28804fb38ac79866af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 06:04:53 +0000

    Add package rag-assistant/0.0.1.dev1/rag_assistant-0.0.1.dev1.tar.gz

---
+++ packages/rag-assistant/rag_assistant-0.0.1.dev1.tar.gz/rag_assistant-0.0.1.dev1/rag_assistant/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.0.1.dev1"
+__short_desc__ = """The Swarmauri Rag Assistant is part of the swarmaURI framework."""
+__long_desc__ = """# Swarmauri Rag Assistant
+
+## Overview
+The Swarmauri Rag Assistant
+"""
