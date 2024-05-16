de0bc9037b5cc48b43a32a3018e9bb49e1a9c8ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 03:04:33 +0000

    Add package flake8-fill-one-line/0.4.2/flake8_fill_one_line-0.4.2.tar.gz

---
+++ packages/flake8-fill-one-line/flake8_fill_one_line-0.4.2.tar.gz/flake8_fill_one_line-0.4.2/flake8_fill_one_line/analyzers/call_analyzer.py
@@ -0,0 +1,38 @@
+import ast
+from typing import Optional
+
+from flake8_fill_one_line.utils import get_node_length, is_one_line
+
+
+class CallAnalyzer:
+    def __init__(self, skip_std_names: bool, skip_multiline_arguments: bool) -> None:
+        self.skip_std_names = skip_std_names
+        self.skip_multiline_arguments = skip_multiline_arguments
+
+    def get_length(self, node: ast.Call) -> Optional[int]:
+        if is_one_line(node):
+            return None  # skip one line call
+
+        if self.skip_std_names and isinstance(node.func, ast.Name) and node.func.id in {"list", "dict", "set", "tuple"}:
+            return None
+
+        args_lengths = []
+
+        for arg in node.args:
+            if self.skip_multiline_arguments and not is_one_line(arg):
+                return None
+
+            args_lengths.append(get_node_length(arg))
+
+        for keyword in node.keywords:
+            if not hasattr(keyword, "lineno"):
+                return None
+
+            if self.skip_multiline_arguments and not is_one_line(keyword):
+                return None
+
+            args_lengths.append(get_node_length(keyword))
+
+        length = node.func.col_offset + get_node_length(node.func) + 2
+        length += sum(args_lengths) + (len(args_lengths) - 1) * 2
+        return length
---
+++ packages/flake8-fill-one-line/flake8_fill_one_line-0.4.2.tar.gz/flake8_fill_one_line-0.4.2/flake8_fill_one_line/analyzers/def_analyzer.py
@@ -0,0 +1,52 @@
+import ast
+from typing import Optional
+
+from flake8_fill_one_line.utils import get_node_length
+
+
+class DefAnalyzer:
+    def get_length(self, node: ast.FunctionDef) -> Optional[int]:
+        length = node.col_offset + len(f"def {node.name}():")
+        args_lengths = []
+        end_line = node.lineno
+
+        for arg in node.args.args:
+            args_lengths.append(get_node_length(arg))
+            end_line = max(end_line, arg.end_lineno)
+
+        if node.args.kwonlyargs:
+            length += 3  # ", *"
+
+        for arg in node.args.kwonlyargs:
+            args_lengths.append(get_node_length(arg))
+            end_line = max(end_line, arg.end_lineno)
+
+        if node.args.vararg is not None:
+            args_lengths.append(get_node_length(node.args.vararg) + 1)  # *vararg
+            end_line = max(end_line, node.args.vararg.end_lineno)
+
+        if node.args.kwarg is not None:
+            args_lengths.append(get_node_length(node.args.kwarg) + 2)  # **kvarg
+            end_line = max(end_line, node.args.kwarg.end_lineno)
+
+        length += sum(args_lengths) + (len(args_lengths) - 1) * 2
+
+        for i, default in enumerate(node.args.defaults):
+            offset = 1 if node.args.args[-(i + 1)].annotation is None else 3
+            length += default.end_col_offset - default.col_offset + offset
+
+        for i, default in enumerate(node.args.kw_defaults):
+            if default is None:
+                continue
+
+            offset = 1 if node.args.kwonlyargs[i].annotation is None else 3
+            length += default.end_col_offset - default.col_offset + offset
+
+        if node.returns is not None:
+            length += get_node_length(node.returns) + 4  # " -> "
+            end_line = max(end_line, node.returns.end_lineno)
+
+        if end_line == node.lineno:
+            return None
+
+        return length
