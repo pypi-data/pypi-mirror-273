8e248a6006e9ffdf89c129dd43d8a3c8b377ce82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 06:04:32 +0000

    Add package shared-storage-client-4/0.1.0/shared_storage_client_4-0.1.0.tar.gz

---
+++ packages/shared-storage-client-4/shared_storage_client_4-0.1.0.tar.gz/shared_storage_client_4-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='shared_storage_client_4',
+    version='0.1.0',
+    author='Chris Maresca',
+    author_email='chris@workmait.ai',
+    description='Storage Client For Microservices',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(where='.'),  # Finds packages in the current directory
+    package_dir={'': '.'},  # Root directory
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'boto3',
+        's3fs',
+        'python-dotenv',
+    ],
+)
