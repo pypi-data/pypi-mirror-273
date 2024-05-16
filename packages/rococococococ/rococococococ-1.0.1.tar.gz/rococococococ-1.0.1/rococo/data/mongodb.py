fb943c6a12f902950c232ce751e7277683eea5d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 15:05:35 +0000

    Add package rococococococ/1.0.1/rococococococ-1.0.1.tar.gz

---
+++ packages/rococococococ/rococococococ-1.0.1.tar.gz/rococococococ-1.0.1/rococo/data/mongodb.py
@@ -0,0 +1,31 @@
+from typing import Any, List
+from rococo.data.base import DbAdapter
+from pymongo import MongoClient
+
+
+class MongoDBAdapter(DbAdapter):
+    """MongoDB adapter for interacting with MongoDB."""
+
+    def __init__(self, mongo_uri: str, mongo_database: str):
+        self.client = MongoClient(mongo_uri)
+        self.db_name = mongo_database
+        self.db = self.client.get_database(mongo_database)
+
+    def move_entity_to_audit_table(self, table, entity_id):
+        """Executes a query to move entity to audit table."""
+        entity = self.get_one(table, "entity_id", {"entity_id": entity_id})
+        self.db[f"{table}_audit"].insert_one(entity)
+        return entity
+
+    def _call_db(self, function_name, *args, **kwargs):
+        """Calls a function specified by function_name argument in MongoDB connection passing forward args and kwargs."""
+        if not self._db:
+            raise Exception("No connection to MongoDB.")
+        return self._event_loop.run_until_complete(getattr(self._db, function_name)(*args, **kwargs))
+
+    def run_transaction(self, operations_list: List[Any]):
+        """Executes a list of operations against the database as a transaction."""
+        with self.client.start_session() as session:
+            with session.start_transaction():
+                for operation in operations_list:
+                    operation()
