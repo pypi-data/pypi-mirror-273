f035d390e30fe369e8a780e9957995b291fcc037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 03:04:10 +0000

    Add package mns-scheduler/1.0.4.8/mns_scheduler-1.0.4.8-py3-none-any.whl

---
+++ packages/mns-scheduler/mns_scheduler-1.0.4.8-py3-none-any.whl/mns_scheduler/finance/financial_high_risk_stock_clean_service_api.py
@@ -0,0 +1,168 @@
+import sys
+import os
+
+file_path = os.path.abspath(__file__)
+end = file_path.index('mns') + 16
+project_path = file_path[0:end]
+sys.path.append(project_path)
+from datetime import datetime
+import mns_common.component.self_choose.black_list_service_api as black_list_service_api
+import mns_scheduler.finance.finance_common_api as finance_common_api
+from loguru import logger
+import mns_common.constant.db_name_constant as db_name_constant
+from mns_common.db.MongodbUtil import MongodbUtil
+import mns_common.component.trade_date.trade_date_common_service_api as trade_date_common_service_api
+import mns_common.component.common_service_fun_api as common_service_fun_api
+import pandas as pd
+
+mongodb_util = MongodbUtil('27017')
+# 审核标准意见
+OPINION_TYPE = "标准无保留意见"
+
+# 利润为负的时候最小营业收入 主板
+MIN_INCOME_MAIN = 350000000
+# 利润为负的时候最小营业收入 科创 创业
+MIN_INCOME_SUB = 150000000
+# 最大负债比
+MAX_LIABILITY_RATIO = 90
+# 最迟出报告的天数
+LATE_REPORT_DAYS = 3
+
+
+#### 退市新规 ####
+# 1 股价类:连续20个交易日估价低于1元
+# 2 市值类: 主板小于5亿、创业板3亿
+# 3 财务类: (1) 利润总额 净利润 扣非净利润三者最小值为负 且营业收入小于3亿 创业板营业收入小于1元
+#          (2) 资不抵债
+
+# 财报审核
+def financial_report_check(new_report_df, period_time, period, report_type):
+    if period == 4:
+        # 年报异常审核
+        year_report_exception_check(new_report_df, period_time, report_type)
+    # 负债过高
+    liability_ratio_check(report_type, new_report_df, period_time)
+
+
+# 年报审核异常
+def year_report_exception_check(new_report_df, period_time, report_type):
+    new_report_one_df = new_report_df.loc[new_report_df['REPORT_DATE'] == period_time]
+    # 审核意见
+    opinion_type = list(new_report_one_df['OPINION_TYPE'])[0]
+    symbol = list(new_report_one_df['SECURITY_CODE'])[0]
+    name = list(new_report_one_df['SECURITY_NAME_ABBR'])[0]
+    now_date = datetime.now()
+    str_day = now_date.strftime('%Y-%m-%d')
+    str_now_date = now_date.strftime('%Y-%m-%d %H:%M:%S')
+    id_key = symbol + "_" + period_time
+    # 年报有问题
+    if opinion_type != OPINION_TYPE:
+        black_list_service_api.save_black_stock(id_key,
+                                                symbol,
+                                                name,
+                                                str_day,
+                                                str_now_date,
+                                                '年报审计有问题',
+                                                '年报审计有问题',
+                                                '',
+                                                black_list_service_api.FINANCIAL_PROBLEM)
+
+    if report_type == db_name_constant.EM_STOCK_PROFIT:
+        # 利润总额  净利润 扣除非经常性损益后的净利润  三者最小为负
+        # 利润总额
+        total_profit = list(new_report_one_df['TOTAL_PROFIT'])[0]
+        #  净利润
+        net_profit = list(new_report_one_df['NETPROFIT'])[0]
+        # 营业利润
+        operate_profit = list(new_report_one_df['OPERATE_PROFIT'])[0]
+        # 持续经营净利润
+        continued_profit = list(new_report_one_df['CONTINUED_NETPROFIT'])[0]
+        # 归属于母公司股东的净利润
+        parent_profit = list(new_report_one_df['PARENT_NETPROFIT'])[0]
+        # 扣除非经常性损益后的净利润
+        deduct_parent_profit = list(new_report_one_df['DEDUCT_PARENT_NETPROFIT'])[0]
+        # 营业总收入
+        total_operate_income = list(new_report_one_df['TOTAL_OPERATE_INCOME'])[0]
+
+        # 最小利润收入
+        min_profit = min(total_profit, net_profit, operate_profit,
+                         continued_profit, parent_profit, deduct_parent_profit)
+        if min_profit < 0:
+
+            classification = common_service_fun_api.classify_symbol_one(symbol)
+            if ((classification in ['S', 'H'] and total_operate_income < MIN_INCOME_MAIN)
+                    | (classification in ['K', 'C'] and total_operate_income < MIN_INCOME_SUB)):
+                id_key = symbol + "_" + period_time
+                black_list_service_api.save_black_stock(id_key,
+                                                        symbol,
+                                                        name,
+                                                        str_day,
+                                                        str_now_date,
+                                                        '年报&利润收入触发退市',
+                                                        '年报&利润收入触发退市',
+                                                        '',
+                                                        black_list_service_api.FINANCIAL_PROBLEM)
+
+
+# 负债比校验
+def liability_ratio_check(report_type, new_report_df, period_time):
+    if report_type == db_name_constant.EM_STOCK_ASSET_LIABILITY:
+        new_report_df = new_report_df.sort_values(by=['REPORT_DATE'], ascending=False)
+        new_report_one_df = new_report_df.iloc[0:1]
+        # 负债比
+        liability_ratio = list(new_report_one_df['liability_ratio'])[0]
+
+        symbol = list(new_report_one_df['SECURITY_CODE'])[0]
+        name = list(new_report_one_df['SECURITY_NAME_ABBR'])[0]
+        now_date = datetime.now()
+        str_day = now_date.strftime('%Y-%m-%d')
+        str_now_date = now_date.strftime('%Y-%m-%d %H:%M:%S')
+        id_key = symbol + "_" + period_time
+
+        if liability_ratio >= MAX_LIABILITY_RATIO:
+            black_list_service_api.save_black_stock(id_key,
+                                                    symbol,
+                                                    name,
+                                                    str_day,
+                                                    str_now_date,
+                                                    '负债过高',
+                                                    '负债过高',
+                                                    '',
+                                                    black_list_service_api.FINANCIAL_PROBLEM)
+
+
+# 未出财报
+def un_report_check(sync_time, now_year, period, period_time):
+    un_report_asset_df = finance_common_api.find_un_report_symbol(period_time,
+                                                                  db_name_constant.EM_STOCK_ASSET_LIABILITY)
+    un_report_profit_df = finance_common_api.find_un_report_symbol(period_time,
+                                                                   db_name_constant.EM_STOCK_PROFIT)
+    un_report_df = pd.concat([un_report_asset_df, un_report_profit_df])
+    if period == 4 or period == 1:
+        last_report_day = str(now_year) + "-05-01"
+    elif period == 2:
+        last_report_day = str(now_year) + "-07-01"
+    elif period == 3:
+        last_report_day = str(now_year) + "-10-01"
+    max_report_day = trade_date_common_service_api.get_before_trade_date(last_report_day, LATE_REPORT_DAYS)
+    if max_report_day > sync_time:
+
+        for un_asset_one in un_report_df.itertuples():
+            symbol = un_asset_one.symbol
+            id_key = symbol + "_" + period_time
+            name = un_asset_one.name
+            now_date = datetime.now()
+            str_day = now_date.strftime('%Y-%m-%d')
+            try:
+
+                black_list_service_api.save_black_stock(id_key,
+                                                        symbol,
+                                                        name,
+                                                        str_day,
+                                                        sync_time,
+                                                        '未出财报',
+                                                        '未出财报',
+                                                        '',
+                                                        black_list_service_api.FINANCIAL_PROBLEM)
+            except Exception as e:
+                logger.error("同步利润表异常:{},{},{}", symbol, period_time, e)
---
+++ packages/mns-scheduler/mns_scheduler-1.0.4.8-py3-none-any.whl/mns_scheduler/finance/sync_financial_report_service_api.py
@@ -0,0 +1,127 @@
+import sys
+import os
+
+file_path = os.path.abspath(__file__)
+end = file_path.index('mns') + 16
+project_path = file_path[0:end]
+sys.path.append(project_path)
+from datetime import datetime
+import mns_common.api.em.east_money_stock_api as east_money_stock_api
+import mns_scheduler.finance.finance_common_api as finance_common_api
+import mns_common.constant.db_name_constant as db_name_constant
+import mns_scheduler.finance.em_financial_profit_sync_service_api as em_financial_profit_sync_service_api
+from mns_common.db.MongodbUtil import MongodbUtil
+from loguru import logger
+import \
+    mns_scheduler.finance.em_financial_asset_liability_sync_service_api as em_financial_asset_liability_sync_service_api
+import mns_scheduler.finance.financial_high_risk_stock_clean_service_api as financial_high_risk_stock_clean_service_api
+import mns_common.utils.data_frame_util as data_frame_util
+
+mongodb_util = MongodbUtil('27017')
+
+
+# 上市公司年报披露时间:每年1月1日一- 4月30日。
+# 2、上市公司中年报披露时间:每年7月1日--8月30日。
+# 3、上市公司季报披露时间:
+#    1季报:每年4月1日-- -4月30日。
+#    2季报(中报) :每年7月1日--8月30日。
+#    3季报:每年10月1日--10月31日4季报(年报) :每年1月1日--4月30日
+
+def sync_financial_report():
+    now_date = datetime.now()
+    now_year = now_date.year
+    now_month = now_date.month
+    sync_time = now_date.strftime('%Y-%m-%d %H:%M:%S')
+    # 年报
+    if 1 <= now_month <= 5:
+        period = 4
+        period_time = str(now_year - 1) + "-12-31 00:00:00"
+        sync_profit_report(period_time, sync_time, period, now_year)
+        sync_asset_liability_report(period_time, sync_time, period, now_year)
+
+    # 一季报
+    elif now_month == 5:
+        period = 1
+        period_time = str(now_year) + "-03-31 00:00:00"
+        sync_profit_report(period_time, sync_time, period, now_year)
+        sync_asset_liability_report(period_time, sync_time, period, now_year)
+
+    # 二季报
+    elif 7 <= now_month <= 8:
+        period = 2
+        period_time = str(now_year) + "-06-30 00:00:00"
+        sync_profit_report(period_time, sync_time, period, now_year)
+        sync_asset_liability_report(period_time, sync_time, period, now_year)
+    # 三季报
+    elif now_month == 10:
+        period = 3
+        period_time = str(now_year) + "-09-30 00:00:00"
+        sync_profit_report(period_time, sync_time, period, now_year)
+        sync_asset_liability_report(period_time, sync_time, period, now_year)
+    # 未出报告check
+    financial_high_risk_stock_clean_service_api.un_report_check(sync_time, now_year, period, period_time)
+
+
+# 同步资产表
+def sync_asset_liability_report(period_time, sync_time, period, now_year):
+    un_report_asset_df = find_un_report_symbol(period_time, db_name_constant.EM_STOCK_ASSET_LIABILITY)
+    for un_report_asset_one in un_report_asset_df.itertuples():
+        try:
+            symbol = un_report_asset_one.symbol
+            new_asset_df = em_financial_asset_liability_sync_service_api.get_em_asset_liability_api(symbol)
+            # 负债比
+            new_asset_df['liability_ratio'] = round(
+                new_asset_df['TOTAL_LIABILITIES'] * 100 / new_asset_df['TOTAL_ASSETS'],
+                2)
+            new_asset_df['sync_time'] = sync_time
+            if data_frame_util.is_empty(new_asset_df):
+                continue
+            new_asset_df['symbol'] = symbol
+            mongodb_util.insert_mongo(new_asset_df, db_name_constant.EM_STOCK_ASSET_LIABILITY)
+
+            # 年报审核
+            financial_high_risk_stock_clean_service_api.financial_report_check(new_asset_df, period_time, period,
+                                                                               db_name_constant.EM_STOCK_ASSET_LIABILITY)
+
+        except Exception as e:
+            logger.error("同步利润表异常:{},{},{}", symbol, period_time, e)
+
+
+# 同步利润表
+def sync_profit_report(period_time, sync_time, period, now_year):
+    un_report_profit_df = find_un_report_symbol(period_time, db_name_constant.EM_STOCK_PROFIT)
+    for un_report_profit_one in un_report_profit_df.itertuples():
+        try:
+            symbol = un_report_profit_one.symbol
+            new_profit_df = em_financial_profit_sync_service_api.get_em_profit_api(symbol)
+            new_profit_df['sync_time'] = sync_time
+            if data_frame_util.is_empty(new_profit_df):
+                continue
+            new_profit_df['symbol'] = symbol
+            mongodb_util.insert_mongo(new_profit_df, db_name_constant.EM_STOCK_PROFIT)
+
+            # 年报审核
+            financial_high_risk_stock_clean_service_api.financial_report_check(new_profit_df, period_time,
+                                                                               period, db_name_constant.EM_STOCK_PROFIT)
+        except Exception as e:
+            logger.error("同步利润表异常:{},{},{}", symbol, period_time, e)
+
+
+# 查出未报告的股票
+def find_un_report_symbol(period_time, report_name):
+    real_time_quotes_df = east_money_stock_api.get_real_time_quotes_all_stocks()
+    if report_name == db_name_constant.EM_STOCK_ASSET_LIABILITY:
+        had_asset_df = finance_common_api.find_asset_liability_report(period_time)
+        if data_frame_util.is_not_empty(had_asset_df):
+            real_time_quotes_df = real_time_quotes_df.loc[
+                ~(real_time_quotes_df['symbol'].isin(list(had_asset_df['SECURITY_CODE'])))]
+    if report_name == db_name_constant.EM_STOCK_PROFIT:
+        had_profit_df = finance_common_api.find_profit_report(period_time)
+        if data_frame_util.is_not_empty(had_profit_df):
+            real_time_quotes_df = real_time_quotes_df.loc[
+                ~(real_time_quotes_df['symbol'].isin(list(had_profit_df['SECURITY_CODE'])))]
+    return real_time_quotes_df
+
+
+if __name__ == '__main__':
+    sync_financial_report()
