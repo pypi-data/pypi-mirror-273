79357fb0c70e2cec17263507099ccb1ddaa3442c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 05:05:10 +0000

    Add package kelvin-python-sdk/0.0.25/kelvin_python_sdk-0.0.25-py3-none-any.whl

---
+++ packages/kelvin-python-sdk/kelvin_python_sdk-0.0.25-py3-none-any.whl/kelvin/krn/__init__.py
@@ -0,0 +1,25 @@
+"""Kelvin Messages."""
+
+from .krn import (
+    KRN,
+    KRNAppVersion,
+    KRNAsset,
+    KRNAssetDataStream,
+    KRNAssetParameter,
+    KRNParameter,
+    KRNRecommendation,
+    KRNWorkload,
+    KRNWorkloadAppVersion,
+)
+
+__all__ = [
+    "KRN",
+    "KRNAsset",
+    "KRNAssetDataStream",
+    "KRNAssetParameter",
+    "KRNParameter",
+    "KRNWorkload",
+    "KRNWorkloadAppVersion",
+    "KRNAppVersion",
+    "KRNRecommendation",
+]
