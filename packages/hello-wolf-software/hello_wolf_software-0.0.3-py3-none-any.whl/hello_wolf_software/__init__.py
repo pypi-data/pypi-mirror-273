8b1ec71d0dacbb736c8663d0c54c509dbee4a58a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 04:04:58 +0000

    Add package hello-wolf-software/0.0.3/hello_wolf_software-0.0.3-py3-none-any.whl

---
+++ packages/hello-wolf-software/hello_wolf_software-0.0.3-py3-none-any.whl/hello_wolf_software/__init__.py
@@ -0,0 +1,12 @@
+# hello_world_software/__init__.py
+
+"""
+hello_world_software.
+
+A simple Python package that prints "Hello, World!" when run.
+
+Usage:
+    import hello_world_software
+"""
+
+# The __init__.py file can be empty or contain package-level variables and imports
---
+++ packages/hello-wolf-software/hello_wolf_software-0.0.3-py3-none-any.whl/hello_wolf_software/hello.py
@@ -0,0 +1,12 @@
+# hello.py
+
+"""Test Script."""
+
+
+def main() -> None:
+    """Test things work as planned."""
+    print("Hello, Wolf Software!")
+
+
+if __name__ == "__main__":
+    main()
