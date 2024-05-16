f455b192937d79d76542e1fed46b7d5ead1e1f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 06:05:14 +0000

    Add package aind-behavior-curriculum/0.0.21/aind_behavior_curriculum-0.0.21-py3-none-any.whl

---
+++ packages/aind-behavior-curriculum/aind_behavior_curriculum-0.0.21-py3-none-any.whl/aind_behavior_curriculum/__init__.py
@@ -0,0 +1,24 @@
+"""
+Init package
+
+Modules in this file are public facing.
+"""
+
+__version__ = "0.0.21"
+
+from .task import Task, TaskParameters
+
+from .curriculum import (
+    Metrics,
+    Rule,
+    Policy,
+    PolicyTransition,
+    Stage,
+    StageGraph,
+    StageTransition,
+    Curriculum,
+)
+
+from .curriculum_utils import *
+
+from .trainer import SubjectHistory, Trainer
