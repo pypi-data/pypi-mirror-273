22779e8b7ab0b89324c2723dd0de4a7cb7043cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 05:05:00 +0000

    Add package offat/0.18.0/offat-0.18.0.tar.gz

---
+++ packages/offat/offat-0.18.0.tar.gz/offat-0.18.0/offat/api/jobs.py
@@ -0,0 +1,23 @@
+from sys import exc_info
+from offat.api.models import CreateScanModel
+from offat.tester.handler import generate_and_run_tests
+from offat.parsers import create_parser
+from offat.logger import logger
+
+
+def scan_api(body_data: CreateScanModel):
+    try:
+        api_parser = create_parser(fpath_or_url=None, spec=body_data.openAPI)
+
+        results = generate_and_run_tests(
+            api_parser=api_parser,
+            regex_pattern=body_data.regex_pattern,
+            req_headers=body_data.req_headers,
+            rate_limit=body_data.rate_limit,
+            test_data_config=body_data.test_data_config,
+        )
+        return results
+    except Exception as e:
+        logger.error('Error occurred while creating a job: %s', repr(e))
+        logger.debug('Debug Data:', exc_info=exc_info())
+        return [{'error': str(e)}]
