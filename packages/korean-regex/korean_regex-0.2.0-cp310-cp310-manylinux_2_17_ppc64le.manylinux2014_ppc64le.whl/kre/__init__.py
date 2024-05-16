967dee7a5553f78a43923a16bd5fb2fcfaeb7487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 03:04:18 +0000

    Add package korean-regex/0.2.0/korean_regex-0.2.0-cp310-cp310-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/korean-regex/korean_regex-0.2.0-cp310-cp310-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/kre/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from ._core import compilestr
+from ._regex import *
+
+# fmt: off
+__all__ = [
+    "compilestr", "CHOSUNGS", "JUNGSUNGS", "JONGSUNGS",
+    "CHOSUNGS_REGFIRST", "JUNGSUNGS_REGFIRST", "JONSGSUNGS_REGFIRST",
+    "match", "fullmatch", "search", "sub", "subn", "split",
+    "findall", "finditer", "compile", "escape",
+    "Pattern", "Match", "A", "I", "L", "M", "S", "X", "U",
+    "ASCII", "IGNORECASE", "LOCALE", "MULTILINE", "DOTALL", "VERBOSE",
+    "UNICODE", "NOFLAG", "RegexFlag",
+]
+# fmt: on
+
+CHOSUNGS = "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ"
+JUNGSUNGS = "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ"
+JONGSUNGS = "0ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ"
+CHOSUNGS_REGFIRST = "ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎㄲㄸㅃㅆㅉ"
+JUNGSUNGS_REGFIRST = "ㅏㅑㅓㅕㅗㅛㅜㅠㅡㅣㅐㅒㅔㅖㅘㅙㅚㅝㅞㅟㅢ"
+JONSGSUNGS_REGFIRST = "0ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎㄲㄳㄵㄶㄺㄻㄼㄽㄾㄿㅀㅄㅆ"
