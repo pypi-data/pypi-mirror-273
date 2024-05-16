e80201ae6dadb694b6b2c8924fa0f68e9f84e877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 15:05:12 +0000

    Add package NJFU/0.0.9/NJFU-0.0.9.tar.gz

---
+++ packages/NJFU/NJFU-0.0.9.tar.gz/NJFU-0.0.9/NJFU/pcd.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def STD_TXT(input_txt_dir, del_col_list):
+    data = np.loadtxt(input_txt_dir)
+    data = np.delete(data, del_col_list, axis=1)
+    output_txt_dir = input_txt_dir.replace(input_txt_dir.split('\\')[-1], "new_" + input_txt_dir.split('\\')[-1])
+    np.savetxt(output_txt_dir, data, fmt='%.4f')
+
