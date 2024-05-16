c81ed88b6230ed65d30e3f9cb42e547b960baccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 03:04:11 +0000

    Add package pysdbots/3.0/pysdbots-3.0-py3-none-any.whl

---
+++ packages/pysdbots/pysdbots-3.0-py3-none-any.whl/pysdbots/__init__.py
@@ -0,0 +1,18 @@
+from .pysdbots import *
+
+__docs__ = req["DOCS"]
+__all__ = [
+    "anime_logo",
+    "tiktok",
+    "apod",
+    "detect_lang",
+    "write",
+    "chk",
+    "sk_checker",
+    "lyrics",
+    "ipinfo",
+    "hirunews",
+    "logohq",
+    "fakeinfo",
+]
+
