7119483532533c1e3e42fb0b78b6b2a5ae3f85c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 05:05:02 +0000

    Add package shared-storage-client/0.1.0/shared_storage_client-0.1.0.tar.gz

---
+++ packages/shared-storage-client/shared_storage_client-0.1.0.tar.gz/shared_storage_client-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shared_storage_client',
+    version='0.1.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A brief description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        # List your package dependencies here
+        'requests',  # example dependency
+    ],
+)
