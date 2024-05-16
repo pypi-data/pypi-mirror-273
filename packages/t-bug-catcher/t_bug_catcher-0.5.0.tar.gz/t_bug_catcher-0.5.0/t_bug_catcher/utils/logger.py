3f5cfa2d1af281fa87190e765cf0a336661d16f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 16:08:23 +0000

    Add package t-bug-catcher/0.5.0/t_bug_catcher-0.5.0.tar.gz

---
+++ packages/t-bug-catcher/t_bug_catcher-0.5.0.tar.gz/t_bug_catcher-0.5.0/t_bug_catcher/utils/logger.py
@@ -0,0 +1,21 @@
+"""Logger."""
+try:
+    from libraries import logger
+except ImportError:
+    import logging
+
+    log_format = logging.Formatter(
+        r"%(asctime)s - %(levelname)-7s %(threadName)-12s [%(filename)s:%(lineno)s - %(funcName)s()] - %(message)s"
+    )
+    log_level = logging.DEBUG
+
+    logger = logging.getLogger("t_bug_catcher")
+    if logger.hasHandlers():
+        logger.handlers = []
+    logger.setLevel(log_level)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    handler.setLevel(log_level)
+    handler.setFormatter(log_format)
+    logger.addHandler(handler)
