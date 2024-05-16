49a2d596b98ea3c19b8a8baa2ab9a546d57c8e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 03:04:06 +0000

    Add package TestGeneratorPluginLib/1.0.5/TestGeneratorPluginLib-1.0.5.tar.gz

---
+++ packages/TestGeneratorPluginLib/TestGeneratorPluginLib-1.0.5.tar.gz/TestGeneratorPluginLib-1.0.5/TestGeneratorPluginLib/__init__.py
@@ -0,0 +1,5 @@
+from TestGeneratorPluginLib._widgets import MainTab, SideTab, SideTabButton
+from TestGeneratorPluginLib._managers import BackendManager, Manager
+from TestGeneratorPluginLib._language import Language, FastRunFunction, FastRunCommand
+
+from TestGeneratorPluginLib._plugin import Plugin
