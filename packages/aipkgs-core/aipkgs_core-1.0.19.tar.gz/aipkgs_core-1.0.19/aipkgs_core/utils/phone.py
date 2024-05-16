be048ec65558279b8bf9e3648875a7d26a3c48ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 17:04:26 +0000

    Add package aipkgs-core/1.0.19/aipkgs_core-1.0.19.tar.gz

---
+++ packages/aipkgs-core/aipkgs_core-1.0.19.tar.gz/aipkgs_core-1.0.19/aipkgs_core/utils/phone.py
@@ -0,0 +1,32 @@
+import phonenumbers
+from phonenumbers import PhoneNumber
+
+from aipkgs_core.utils import utils
+
+
+def is_number_format_valid(number: str) -> (bool, str):
+    try:
+        # if utils.contains_space(number):
+        #     return False, number
+
+        parsed_number: PhoneNumber = phonenumbers.parse(number, None)
+        return True, parsed_number
+    except:
+        return False, number
+
+
+def is_valid_full_number(number: str) -> (bool, PhoneNumber):
+    is_valid, phone_number = is_number_format_valid(number)
+    if not is_valid:
+        return False, phone_number
+
+    is_phone_valid = phonenumbers.is_valid_number(phone_number)
+    if not is_phone_valid:
+        return False, phone_number
+
+    return is_phone_valid, phone_number
+
+
+def is_valid_number_with_country_code(country_code: str, national_number: str) -> (bool, PhoneNumber):
+    full_number = f"+{country_code}{national_number}"
+    return is_valid_full_number(full_number)
