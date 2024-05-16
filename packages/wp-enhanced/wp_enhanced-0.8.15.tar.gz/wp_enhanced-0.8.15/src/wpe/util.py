bf730f75f07b050926db868586cd58ed1d57a1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 17:04:33 +0000

    Add package wp-enhanced/0.8.15/wp_enhanced-0.8.15.tar.gz

---
+++ packages/wp-enhanced/wp_enhanced-0.8.15.tar.gz/wp_enhanced-0.8.15/src/wpe/util.py
@@ -0,0 +1,41 @@
+import re
+import tomllib
+import os.path as osp
+from distutils.dir_util import copy_tree
+from distutils.file_util import copy_file
+from pathlib import Path
+
+__all__ = [
+    'overwrite_copy',
+    'path_is_under',
+    'load_toml',
+    'replace_in_basename',
+    'remove_ansi_color'
+]
+
+
+def overwrite_copy(src: str, dst: str):
+    if osp.isfile(src):
+        copy_file(src, dst)
+
+    if osp.isdir(src):
+        copy_tree(src, dst)
+
+
+def path_is_under(child: str, parent: str) -> bool:
+    return Path(parent) in Path(child).parents
+
+
+def load_toml(path):
+    with open(path, 'rb') as f:
+        toml_content = tomllib.load(f)
+    return toml_content
+
+
+def replace_in_basename(path: str, old: str, new: str):
+    return osp.join(osp.dirname(path), osp.basename(path).replace(old, new))
+
+
+def remove_ansi_color(text):
+    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+    return ansi_escape.sub('', text)
