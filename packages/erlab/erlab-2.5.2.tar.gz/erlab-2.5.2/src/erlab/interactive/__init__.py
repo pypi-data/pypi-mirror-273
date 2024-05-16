8ac04a90777509e619b80d14e594a881ad6e1058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 15:05:10 +0000

    Add package erlab/2.5.2/erlab-2.5.2.tar.gz

---
+++ packages/erlab/erlab-2.5.2.tar.gz/erlab-2.5.2/src/erlab/interactive/__init__.py
@@ -0,0 +1,29 @@
+"""
+Interactive plotting based on Qt and pyqtgraph.
+
+.. currentmodule:: erlab.interactive
+
+Interactive tools
+=================
+
+.. autosummary::
+   :toctree: generated
+
+   imagetool
+   bzplot
+   colors
+   curvefittingtool
+   fermiedge
+   kspace
+   masktool
+   derivative
+   utilities
+
+"""
+
+__all__ = ["dtool", "goldtool", "itool", "ktool"]
+
+from erlab.interactive.derivative import dtool
+from erlab.interactive.fermiedge import goldtool
+from erlab.interactive.imagetool import itool
+from erlab.interactive.kspace import ktool
