ff3f2e56eab0dac50e69f21c4ea7960179e1c29a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 15:05:14 +0000

    Add package msiaLex/0.3.9.19/msiaLex-0.3.9.19.tar.gz

---
+++ packages/msiaLex/msiaLex-0.3.9.19.tar.gz/msiaLex-0.3.9.19/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='msiaLex',
+    packages=find_packages(include=['msiaLex']),
+    version='0.3.9.19',
+    description='Malaysia Lexicon',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Khor Yuen Kei',
+    author_email='khoryk1104@gmail.com',
+    url='https://github.com/yuenkeiwac/msialex',
+    license='MIT',
+    install_requires=[], 
+    setup_requires=['pytest-runner'], 
+    tests_require=['pytest==6.2.5'],
+    test_suite='tests'
+)
