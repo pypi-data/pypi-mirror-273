a876fa3eafbd52501e07be8c98478505a63cf9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 14:04:43 +0000

    Add package tsugu/1.0.5/tsugu-1.0.5.tar.gz

---
+++ packages/tsugu/tsugu-1.0.5.tar.gz/tsugu-1.0.5/tsugu/router/__init__.py
@@ -0,0 +1,66 @@
+from ..utils import server_exists, text_response, config, get_user
+from ..command_matcher import match_command
+from . import remote
+from . import universal
+from . import bandoristation
+from ..command_matcher import match_command, MC
+
+
+def handler(message: str, user_id: str, platform: str, channel_id: str):
+    # 进行 api 命令匹配
+    result = universal_api_handler(user_id, message, platform, channel_id)
+    if result:
+        return result
+
+    # 远程命令
+    result = remote_api_handler(user_id, message, platform, channel_id)
+    if result:
+        return result
+
+    result = bandoristation_api_handler(user_id, message, platform, channel_id)
+    if result:
+        return result
+
+    # 开始匹配 help
+    if res := match_command(message, commands=['help']):
+        return help_command(res)
+
+
+def help_command(res: MC):
+    if not res.args:
+        command_list = list(config._help_doc_dict.keys())
+        command_list.sort()
+        return text_response(f'当前支持的命令有：\n{" ".join(command_list)}\n请使用"help 命令名"来查看命令的详细帮助')
+    if help_text := config. _help_doc_dict.get(res.args[0], None):
+        return text_response(help_text)
+    return None
+
+
+def bandoristation_api_handler(user_id: str, message: str,  platform: str, channel_id: str):
+    for i in config.bandoristation_commands:
+        if res := match_command(message, commands=i['command_name']):
+            api = i['api']
+            user = get_user(user_id, platform)
+            return bandoristation.api_handler(user, res, api, platform, channel_id)
+    return None
+
+
+def universal_api_handler(user_id: str, message: str,  platform: str, channel_id: str):
+    for i in config.commands:
+        if res := match_command(message, commands=i['command_name']):
+            api = i['api']
+            user = get_user(user_id, platform)
+            return universal.api_handler(user, res, api, platform, channel_id)
+    return None
+
+
+def remote_api_handler(user_id: str, message: str,  platform: str, channel_id: str):
+    for i in config.user_commands:
+        if res := match_command(message, commands=i['command_name']):
+            api = i['api']
+            user = get_user(user_id, platform)
+            return remote.api_handler(user, res, api, platform, channel_id)
+    return None
+
+
+
---
+++ packages/tsugu/tsugu-1.0.5.tar.gz/tsugu-1.0.5/tsugu/router/bandoristation/rooms_forward.py
@@ -0,0 +1,58 @@
+import tsugu_api
+
+from ...utils import config
+import re
+from ...utils import User, text_response
+from ...command_matcher import MC
+from ...utils import get_user
+from tsugu_api.exception import RoomSubmitFailure
+
+
+def handler(user: User, res: MC, platform: str, channel_id: str):
+    message = res.text
+    # 检查car_config['car']中的关键字
+    for keyword in config._car_config["car"]:
+        if str(keyword) in message:
+            break
+    else:
+        return []
+
+    # 检查car_config['fake']中的关键字
+    for keyword in config._car_config["fake"]:
+        if str(keyword) in message:
+            return []
+
+    pattern = r"^\d{5}(\D|$)|^\d{6}(\D|$)"
+    if not re.match(pattern, message):
+        return []
+
+    user = get_user(user.user_id, platform)
+
+    # 获取用户数据
+    try:
+        if platform:
+            if not user.car:
+                return []
+    except Exception as e:
+        # 默认不开启关闭车牌，继续提交
+        pass
+
+    try:
+        car_id = message[:6]
+        if not car_id.isdigit() and car_id[:5].isdigit():
+            car_id = car_id[:5]
+        if user.user_id.isdigit():
+            car_user_id = user.user_id
+        else:
+            car_user_id = '3889000770'
+        tsugu_api.submit_room_number(number=int(car_id), user_id=car_user_id, raw_message=message, source=config.token_name, token=config.bandori_station_token)
+        return []
+
+    except RoomSubmitFailure as e:
+        return []
+
+    except Exception as e:
+        return []
+
+
+
