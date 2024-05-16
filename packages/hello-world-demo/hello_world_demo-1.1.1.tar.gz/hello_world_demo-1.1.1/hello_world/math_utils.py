0dc5c22a0381770d2f8f849d871d5cf1b40b734f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 16:08:47 +0000

    Add package hello-world-demo/1.1.1/hello_world_demo-1.1.1.tar.gz

---
+++ packages/hello-world-demo/hello_world_demo-1.1.1.tar.gz/hello_world_demo-1.1.1/hello_world/math_utils.py
@@ -0,0 +1,9 @@
+# my_package/math_utils.py
+class MathUtils:
+    @staticmethod
+    def add(x, y):
+        return x + y
+
+    @staticmethod
+    def subtract(x, y):
+        return x - y
---
+++ packages/hello-world-demo/hello_world_demo-1.1.1.tar.gz/hello_world_demo-1.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hello_world_demo",
+    version="1.1.1",
+    description="A simple hello world package",
+    author="Cornélius vincent",
+    packages=find_packages(),
+)
