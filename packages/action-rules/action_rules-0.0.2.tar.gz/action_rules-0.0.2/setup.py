a6ca63de41d63e3e639455136f1ff07adb95d35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 14:04:28 +0000

    Add package action-rules/0.0.2/action_rules-0.0.2.tar.gz

---
+++ packages/action-rules/action_rules-0.0.2.tar.gz/action_rules-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from os import path
+working_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(working_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='action_rules',
+    version='0.0.2',
+    url='https://github.com/lukassykora/actionrules',
+    author='Lukas Sykora',
+    author_email='lukas.sykora@vse.cz',
+    description='Action Rules Mining Tool.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['pandas~=2.2.2'],
+)
