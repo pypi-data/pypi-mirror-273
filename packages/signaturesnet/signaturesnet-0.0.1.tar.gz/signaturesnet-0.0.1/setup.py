6dddd7488cf5fe187d02c37f4cc08f45f5a7e5c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 05:04:38 +0000

    Add package signaturesnet/0.0.1/signaturesnet-0.0.1.tar.gz

---
+++ packages/signaturesnet/signaturesnet-0.0.1.tar.gz/signaturesnet-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(name='signaturesnet',
+      version='0.0.1',
+      description="Package to manipulate mutational processes.",
+      url='https://github.com/weghornlab/SigNet',
+      packages=find_packages(),
+      install_requires=[
+            'torch==1.11',
+            'scipy',
+            'numpy==1.23.3',
+            'matplotlib==3.3.4',
+            'pandas',
+            'seaborn==0.11.1',
+            'scikit_optimize==0.8.1',
+            'tqdm==4.59.0',
+            'pyparsing==2.4.7',
+            # 'gaussian_process==0.0.14',
+            'PyYAML==6.0',
+            'scikit_learn',
+            'openpyxl',
+            'tensorboard',
+            'wandb',
+      ])
