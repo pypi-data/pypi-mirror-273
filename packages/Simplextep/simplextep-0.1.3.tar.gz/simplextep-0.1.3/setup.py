0363a52e3fa0df00623d9600069c7475f49edb4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 16:08:01 +0000

    Add package Simplextep/0.1.3/simplextep-0.1.3.tar.gz

---
+++ packages/Simplextep/simplextep-0.1.3.tar.gz/simplextep-0.1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Simplextep',
+    version='0.1.3',
+    packages=find_packages(),
+    description='Simplex implementation with steps.',
+    author='Keivan Jamali',
+    author_email='K1Jamali01@gmail.com',
+    url='https://github.com/KeivanJamali/simplextep',
+    install_requires=["numpy", "pandas", "tabulate", "matplotlib"],
+)
