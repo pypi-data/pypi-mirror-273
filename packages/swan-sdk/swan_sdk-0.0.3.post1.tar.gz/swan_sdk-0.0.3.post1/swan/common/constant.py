94218aea16274de5db514ea19abd0d0b2dcf047f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 04:04:36 +0000

    Add package swan-sdk/0.0.3.post1/swan_sdk-0.0.3.post1.tar.gz

---
+++ packages/swan-sdk/swan_sdk-0.0.3.post1.tar.gz/swan_sdk-0.0.3.post1/swan/common/constant.py
@@ -0,0 +1,34 @@
+# ./swan/common/constant.py
+
+SWAN_API = "https://orchestrator-api.swanchain.io"
+
+# Swan API
+SWAN_APIKEY_LOGIN = "/login_by_api_key"
+DEPLOY_TASK = "/v1/space_deployment"
+DEPLOYMENT_INFO = "/v1/space_deployment/"
+DEPLOYED_URL = "/v1/space_deploymnet/job_real_url"
+GET_CP_CONFIG = "/cp/machines"
+PROVIDER_PAYMENTS = "/provider/payments"
+CREATE_TASK = "/v2/task_deployment"
+GET_CONTRACT_INFO = "/contract_info"
+GET_ABI_VERSION = "/abi_version"
+GET_SOURCE_URI = "/v2/get_source_uri"
+
+# API Syntax
+REST_API_VERSION = "v1"
+GET = "GET"
+PUT = "PUT"
+POST = "POST"
+DELETE = "DELETE"
+
+# Contract
+PAYMENT_CONTRACT_ABI = "PaymentContract.json"
+SWAN_TOKEN_ABI = "SwanToken.json"
+CLIENT_CONTRACT_ABI = "ClientPayment.json"
+
+# Other
+CONTRACT_TIMEOUT = 300
+STORAGE_LAGRANGE: str = "lagrange"
+ORCHESTRATOR_API = "orchestrator-api.swanchain.io"
+MAX_DURATION = 1209600
+ORCHESTRATOR_PUBLIC_ADDRESS = "0x29eD49c8E973696D07E7927f748F6E5Eacd5516D"
