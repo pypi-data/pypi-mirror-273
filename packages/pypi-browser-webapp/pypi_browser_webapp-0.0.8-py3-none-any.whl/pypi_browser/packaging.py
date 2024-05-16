12306786c45f8e743e4dd792a0eff0f852431ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 05:04:28 +0000

    Add package pypi-browser-webapp/0.0.8/pypi_browser_webapp-0.0.8-py3-none-any.whl

---
+++ packages/pypi-browser-webapp/pypi_browser_webapp-0.0.8-py3-none-any.whl/pypi_browser/packaging.py
@@ -0,0 +1,148 @@
+import asyncio
+import base64
+import contextlib
+import enum
+import os.path
+import re
+import stat
+import tarfile
+import typing
+import zipfile
+from dataclasses import dataclass
+from types import TracebackType
+
+
+def pep426_normalize(package_name: str) -> str:
+    return re.sub(r'[-_.]+', '-', package_name.strip()).lower()
+
+
+class UnsupportedPackageType(Exception):
+    pass
+
+
+class PackageType(enum.Enum):
+    SDIST = enum.auto()
+    WHEEL = enum.auto()
+    EGG = enum.auto()
+
+
+class PackageFormat(enum.Enum):
+    ZIPFILE = enum.auto()
+    TARBALL = enum.auto()
+
+
+@dataclass(frozen=True)
+class PackageEntry:
+    path: str
+    mode: str
+    size: int
+
+
+def _package_entries_from_zipfile(path: str) -> typing.Set[PackageEntry]:
+    with zipfile.ZipFile(path) as zf:
+        return {
+            PackageEntry(
+                path=entry.filename,
+                size=entry.file_size,
+                mode=stat.filemode(entry.external_attr >> 16),
+            )
+            for entry in zf.infolist()
+            if not entry.is_dir()
+        }
+
+
+def _package_entries_from_tarball(path: str) -> typing.Set[PackageEntry]:
+    with tarfile.open(path) as tf:
+        return {
+            PackageEntry(
+                path=entry.name,
+                size=entry.size,
+                mode=stat.filemode(entry.mode),
+            )
+            for entry in tf.getmembers()
+            if not entry.isdir()
+        }
+
+
+class AsyncArchiveFile:
+
+    file_: typing.IO[bytes]
+
+    def __init__(self, file_: typing.IO[bytes]) -> None:
+        self.file_ = file_
+
+    async def __aenter__(self) -> 'AsyncArchiveFile':
+        return self
+
+    async def __aexit__(
+        self,
+        exc_t: typing.Optional[typing.Type[BaseException]],
+        exc_v: typing.Optional[BaseException],
+        exc_tb: typing.Optional[TracebackType],
+    ) -> None:
+        await asyncio.to_thread(self.file_.close)
+
+    async def read(self, n_bytes: int = -1) -> bytes:
+        return await asyncio.to_thread(self.file_.read, n_bytes)
+
+
+@dataclass(frozen=True)
+class Package:
+    package_type: PackageType
+    package_format: PackageFormat
+    path: str
+
+    @classmethod
+    def from_path(cls, path: str) -> 'Package':
+        name = base64.b64decode(os.path.basename(path).encode('ascii')).decode('utf8')
+
+        if name.endswith('.whl'):
+            package_type = PackageType.WHEEL
+            package_format = PackageFormat.ZIPFILE
+        elif name.endswith('.zip'):
+            package_type = PackageType.SDIST
+            package_format = PackageFormat.ZIPFILE
+        elif name.endswith('.egg'):
+            package_type = PackageType.EGG
+            package_format = PackageFormat.ZIPFILE
+        elif name.endswith(('.tar', '.tar.gz', '.tgz', '.tar.bz2')):
+            package_type = PackageType.SDIST
+            package_format = PackageFormat.TARBALL
+        else:
+            raise UnsupportedPackageType(name)
+
+        return cls(
+            package_type=package_type,
+            package_format=package_format,
+            path=path,
+        )
+
+    async def entries(self) -> typing.Set[PackageEntry]:
+        if self.package_format is PackageFormat.ZIPFILE:
+            return await asyncio.to_thread(_package_entries_from_zipfile, self.path)
+        elif self.package_format is PackageFormat.TARBALL:
+            return await asyncio.to_thread(_package_entries_from_tarball, self.path)
+        else:
+            raise AssertionError(self.package_format)
+
+    @contextlib.asynccontextmanager
+    async def open_from_archive(self, path: str) -> typing.AsyncIterator[AsyncArchiveFile]:
+        if self.package_format is PackageFormat.ZIPFILE:
+            zf = await asyncio.to_thread(zipfile.ZipFile, self.path)
+            try:
+                zip_archive_file = await asyncio.to_thread(zf.open, path)
+                async with AsyncArchiveFile(zip_archive_file) as wrapped:
+                    yield wrapped
+            finally:
+                await asyncio.to_thread(zf.close)
+        elif self.package_format is PackageFormat.TARBALL:
+            tf = await asyncio.to_thread(tarfile.open, self.path)
+            try:
+                tar_archive_file = await asyncio.to_thread(tf.extractfile, path)
+                assert tar_archive_file is not None, path
+                async with AsyncArchiveFile(tar_archive_file) as wrapped:
+                    yield wrapped
+            finally:
+                await asyncio.to_thread(tf.close)
+        else:
+            raise AssertionError(self.package_format)
---
+++ packages/pypi-browser-webapp/pypi_browser_webapp-0.0.8-py3-none-any.whl/pypi_browser/pypi.py
@@ -0,0 +1,105 @@
+import base64
+import contextlib
+import dataclasses
+import itertools
+import os.path
+import typing
+import urllib.parse
+
+import aiofiles.os
+import httpx
+
+
+@dataclasses.dataclass(frozen=True)
+class PyPIConfig:
+    cache_path: str
+    pypi_url: str
+
+
+class PackageDoesNotExist(Exception):
+    pass
+
+
+async def package_metadata(
+    config: PyPIConfig,
+    client: httpx.AsyncClient,
+    package: str,
+) -> typing.Dict[typing.Any, typing.Any]:
+    resp = await client.get(f'{config.pypi_url}/pypi/{package}/json')
+    if resp.status_code == 404:
+        raise PackageDoesNotExist(package)
+    resp.raise_for_status()
+    return resp.json()
+
+
+async def files_for_package(config: PyPIConfig, package: str) -> typing.Dict[str, typing.Set[str]]:
+    async with httpx.AsyncClient() as client:
+        metadata = await package_metadata(config, client, package)
+
+    return {
+        version: {file_['filename'] for file_ in files}
+        for version, files in metadata['releases'].items()
+        if len(files) > 0
+    }
+
+
+class CannotFindFileError(Exception):
+    pass
+
+
+def _storage_path(config: PyPIConfig, package: str, filename: str) -> str:
+    return os.path.join(
+        config.cache_path,
+        # Base64-encoding the names to calculate the storage path just to be
+        # extra sure to avoid any path traversal vulnerabilities.
+        base64.urlsafe_b64encode(package.encode('utf8')).decode('ascii'),
+        base64.urlsafe_b64encode(filename.encode('utf8')).decode('ascii'),
+    )
+
+
+@contextlib.asynccontextmanager
+async def _atomic_file(path: str) -> typing.AsyncIterator[aiofiles.threadpool.binary.AsyncBufferedIOBase]:
+    async with aiofiles.tempfile.NamedTemporaryFile('wb', dir=os.path.dirname(path), delete=False) as f:
+        tmp_path = typing.cast(str, f.name)
+        try:
+            yield f
+        except BaseException:
+            await aiofiles.os.remove(tmp_path)
+            raise
+        else:
+            # This is atomic since the temporary file was created in the same directory.
+            await aiofiles.os.rename(tmp_path, path)
+
+
+async def downloaded_file_path(config: PyPIConfig, package: str, filename: str) -> str:
+    """Return path on filesystem to downloaded PyPI file.
+
+    May be instant if the file is already cached; otherwise it will download
+    it and may take a while.
+    """
+    stored_path = _storage_path(config, package, filename)
+    if await aiofiles.os.path.exists(stored_path):
+        return stored_path
+
+    async with httpx.AsyncClient() as client:
+        metadata = await package_metadata(config, client, package)
+
+        # Parsing versions from non-wheel Python packages isn't perfectly
+        # reliable, so just search through all releases until we find a
+        # matching file.
+        for file_ in itertools.chain.from_iterable(metadata['releases'].values()):
+            if file_['filename'] == filename:
+                url = urllib.parse.urljoin(config.pypi_url, file_['url'])
+                break
+        else:
+            raise CannotFindFileError(package, filename)
+
+        await aiofiles.os.makedirs(os.path.dirname(stored_path), exist_ok=True)
+
+        async with _atomic_file(stored_path) as f:
+            async with client.stream('GET', url) as resp:
+                resp.raise_for_status()
+                async for chunk in resp.aiter_bytes():
+                    await f.write(chunk)
+
+        return stored_path
