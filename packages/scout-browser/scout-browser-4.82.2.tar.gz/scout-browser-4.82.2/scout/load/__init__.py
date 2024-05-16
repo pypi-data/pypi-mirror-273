b8e38c6352f36091e66380325edfd7a418bed9a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 03:04:05 +0000

    Add package scout-browser/4.82.2/scout-browser-4.82.2.tar.gz

---
+++ packages/scout-browser/scout-browser-4.82.2.tar.gz/scout-browser-4.82.2/scout/load/__init__.py
@@ -0,0 +1,7 @@
+from .all import load_scout
+from .case import load_case
+from .cytoband import load_cytobands
+from .exon import load_exons
+from .hgnc_gene import load_hgnc_genes
+from .institute import load_institute
+from .transcript import load_transcripts
