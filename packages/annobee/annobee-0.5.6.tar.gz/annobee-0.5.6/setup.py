93ef9fdbea6268e2d31e16974a71f685a210a944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 04:04:34 +0000

    Add package annobee/0.5.6/annobee-0.5.6.tar.gz

---
+++ packages/annobee/annobee-0.5.6.tar.gz/annobee-0.5.6/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='annobee',
+    version='0.5.6',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'annobee=annobee.cli:main',
+        ],
+    },
+    install_requires=[
+        'numpy',
+        'pandas',
+        'tqdm',
+        'dnspython',
+        'pymongo',
+        'requests',
+        'six',
+        'tzdata',
+        'python-dateutil',
+        'pytz',
+        'pool',
+    ],
+    include_package_data=True,
+    description='Annovar SDK for variant interpretation',
+    url='https://github.com/variantAnnotation/annobee-sdk',  # Update with your repository URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
