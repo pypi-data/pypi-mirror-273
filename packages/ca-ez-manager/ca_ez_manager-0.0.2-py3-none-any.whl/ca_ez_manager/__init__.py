3d9ac9fcd4e5c51bc7c4c38f99d2e800bfe8a058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 17:04:33 +0000

    Add package ca-ez-manager/0.0.2/ca_ez_manager-0.0.2-py3-none-any.whl

---
+++ packages/ca-ez-manager/ca_ez_manager-0.0.2-py3-none-any.whl/ca_ez_manager/__init__.py
@@ -0,0 +1,20 @@
+__app_name__ = "ca-ez-manager"
+__version__ = "0.0.2"
+
+(
+    SUCCESS,
+    DIR_ERROR,
+    FILE_ERROR,
+    DB_READ_ERROR,
+    DB_WRITE_ERROR,
+    JSON_ERROR,
+    ID_ERROR,
+) = range(7)
+
+ERRORS = {
+    DIR_ERROR: "config directory error",
+    FILE_ERROR: "config file error",
+    DB_READ_ERROR: "database read error",
+    DB_WRITE_ERROR: "database write error",
+    ID_ERROR: "to-do id error",
+}
