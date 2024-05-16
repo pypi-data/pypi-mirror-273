254964193c17a3c58b3376d6db94579aa3821a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 04:05:00 +0000

    Add package appmap/1.22.0/appmap-1.22.0.tar.gz

---
+++ packages/appmap/appmap-1.22.0.tar.gz/appmap-1.22.0/_appmap/__init__.py
@@ -0,0 +1,19 @@
+from . import configuration
+from . import env as appmapenv
+from . import event, importer, metadata, recorder, recording, web_framework
+from .py_version_check import check_py_version
+
+
+def initialize(**kwargs):
+    check_py_version()
+    appmapenv.initialize(**kwargs)
+    event.initialize()
+    importer.initialize()
+    recorder.initialize()
+    configuration.initialize()  # needs to be initialized after recorder
+    metadata.initialize()
+    web_framework.initialize()
+    recording.initialize()
+
+
+initialize()
