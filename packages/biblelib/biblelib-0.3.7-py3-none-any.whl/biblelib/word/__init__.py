4868c635f6908747685703b166ffab8064160587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 06:04:43 +0000

    Add package biblelib/0.3.7/biblelib-0.3.7-py3-none-any.whl

---
+++ packages/biblelib/biblelib-0.3.7-py3-none-any.whl/biblelib/word/__init__.py
@@ -0,0 +1,46 @@
+"""This package provides utilities for working with word-level data from the Hebrew Bible and Greek New Testament.
+
+The Hebrew and Greek source files are available from
+
+* [Clear-Bible/macula-hebrew](https://github.com/Clear-Bible/macula-hebrew):
+  Syntax trees, morphology, and linguistic annotations for the Hebrew
+  Bible
+
+* [Clear-Bible/macula-greek](https://github.com/Clear-Bible/macula-greek):
+  Syntax trees, morphology, and linguistic annotations for the Greek
+  New Testament
+
+"""
+
+from .bcvwpid import (
+    BID,
+    BCID,
+    BCVID,
+    BCVIDRange,
+    BCVWPID,
+    fromlogos,
+    fromname,
+    fromosis,
+    fromusfm,
+    fromubs,
+    reftypes,
+    simplify,
+    to_bcv,
+)
+
+__all__ = [
+    # bcvwpid
+    "BID",
+    "BCID",
+    "BCVID",
+    "BCVIDRange",
+    "BCVWPID",
+    "fromlogos",
+    "fromname",
+    "fromosis",
+    "fromusfm",
+    "fromubs",
+    "reftypes",
+    "simplify",
+    "to_bcv",
+]
