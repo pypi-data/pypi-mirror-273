dc5c3ca7bfd89c364eec25bcf048fb8424b10014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 15:05:49 +0000

    Add package TsingPig-Lab/0.1.0/TsingPig_Lab-0.1.0.tar.gz

---
+++ packages/TsingPig-Lab/TsingPig_Lab-0.1.0.tar.gz/TsingPig_Lab-0.1.0/setup.py
@@ -0,0 +1,25 @@
+
+import setuptools
+
+with open("README.md",'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "TsingPig_Lab",
+    version = "0.1.0",
+    author = "tsingpig",
+    author_email = "1114196607@qq.com",
+    description = "TsingPig_Lab is a package for algorithm.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url="https://gitlab.com/tsingpig-code/tsingpig_lab",
+    packages=setuptools.find_packages(),
+    install_requires = [],
+    # add any additional packages that needs to be installed along with SSAP package.
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
