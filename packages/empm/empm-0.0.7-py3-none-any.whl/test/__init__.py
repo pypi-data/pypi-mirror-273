410b4111fab2f50ce35b2e461add9c1395607363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 16:08:02 +0000

    Add package empm/0.0.7/empm-0.0.7-py3-none-any.whl

---
+++ packages/empm/empm-0.0.7-py3-none-any.whl/test/__init__.py
@@ -0,0 +1,17 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import empm
+
+log = logging.getLogger("rich")
+log.setLevel(logging.DEBUG)
+
+log_handle = RotatingFileHandler("debug.log", mode="w", backupCount=5, encoding="utf-8")
+formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)-8s - %(name)s - %(funcName)s - %(message)s "
+)
+log_handle.setFormatter(formatter)
+
+log.addHandler(log_handle)
+
+import os
+os.makedirs('lib', exist_ok=True)
