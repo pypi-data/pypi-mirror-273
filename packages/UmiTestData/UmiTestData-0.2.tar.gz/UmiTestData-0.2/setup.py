8197bf9c083fbc89a538c7b13e3627ebc34592ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 06:04:43 +0000

    Add package UmiTestData/0.2/UmiTestData-0.2.tar.gz

---
+++ packages/UmiTestData/UmiTestData-0.2.tar.gz/UmiTestData-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='UmiTestData',
+    version='0.2',
+    packages=find_packages(),
+    description='A simple tool to generate random test data',
+    author='UmiCore - Kazuya',
+    url='https://github.com/kazuya-2006-26',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
