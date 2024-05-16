dc400de82a16790bc7200a6d46e463fbd9b08dba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-16 16:08:49 +0000

    Add package fastinference-llm/0.0.4/fastinference_llm-0.0.4-py3-none-any.whl

---
+++ packages/fastinference-llm/fastinference_llm-0.0.4-py3-none-any.whl/fastinference/data_processing/__init__.py
@@ -0,0 +1,7 @@
+from fastinference.data_processing.data_processor import DataProcessor
+from fastinference.data_processing.datablock import DataBlock
+
+__all__ = [
+    'DataProcessor',
+    'DataBlock'
+]
---
+++ packages/fastinference-llm/fastinference_llm-0.0.4-py3-none-any.whl/fastinference/managers/__init__.py
@@ -0,0 +1,7 @@
+from fastinference.managers.task_manager import TasksManager
+from fastinference.managers.llm_manager import LLMManager
+
+__all__ = [
+    'TasksManager',
+    'LLMManager'
+]
