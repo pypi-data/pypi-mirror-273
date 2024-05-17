1de1c2ef73e5eff3e48a3229fc8c3e85bad0557c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 16:04:56 +0000

    Add package feather-extract/0.1.14/feather_extract-0.1.14.tar.gz

---
+++ packages/feather-extract/feather_extract-0.1.14.tar.gz/feather_extract-0.1.14/feather_extract/forms.py
@@ -0,0 +1,31 @@
+import os
+
+def get_form(filled_out=False, output_path=None):
+    """ Download the standardized PDF form.
+
+    Args:
+        filled_out (bool, optional): If True, download the filled-out version of the form. Default is False (blank form).
+        output_path (str, optional): The path where the PDF file should be saved. If not provided, the file will be saved in the current working directory.
+
+    Returns:
+        str: The path to the downloaded PDF file.
+    """
+    package_dir = os.path.dirname(os.path.abspath(__file__))
+    data_dir = os.path.join(package_dir, 'data')
+
+    if filled_out:
+        form_file = 'standard-form-filled-out.pdf'
+    else:
+        form_file = 'standard-form.pdf'
+
+    pdf_file = os.path.join(data_dir, form_file)
+
+    if output_path is None:
+        output_path = os.path.join(os.getcwd(), form_file)
+    else:
+        output_path = os.path.join(output_path, form_file)
+
+    with open(pdf_file, 'rb') as src, open(output_path, 'wb') as dst:
+        dst.write(src.read())
+
+    return output_path
