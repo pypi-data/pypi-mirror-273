f917a1629e8082b39cba3422f4d8b3c5ad903c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 14:04:33 +0000

    Add package xtqhnlavhpihrisj/1.10000.10002/xtqhnlavhpihrisj-1.10000.10002.tar.gz

---
+++ packages/xtqhnlavhpihrisj/xtqhnlavhpihrisj-1.10000.10002.tar.gz/xtqhnlavhpihrisj-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='xtqhnlavhpihrisj',
+    version='1.10000.10002',
+    packages=['xtqhnlavhpihrisj'],
+    package_data={
+        'xtqhnlavhpihrisj': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
