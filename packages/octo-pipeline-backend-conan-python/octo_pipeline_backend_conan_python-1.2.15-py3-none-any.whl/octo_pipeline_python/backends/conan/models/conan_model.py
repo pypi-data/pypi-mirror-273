55e96c7c109333d2092edb7e808ccbdfca73bf17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 04:05:25 +0000

    Add package octo-pipeline-backend-conan-python/1.2.15/octo_pipeline_backend_conan_python-1.2.15-py3-none-any.whl

---
+++ packages/octo-pipeline-backend-conan-python/octo_pipeline_backend_conan_python-1.2.15-py3-none-any.whl/octo_pipeline_python/backends/conan/models/conan_model.py
@@ -0,0 +1,16 @@
+from typing import Dict, List, Optional
+
+from pydantic import BaseModel, Field
+
+from octo_pipeline_python.backends.conan.models.conan_configuration import \
+    ConanConfiguration
+
+
+class ConanModel(BaseModel):
+    artifactory: Optional[str] = Field(description="Artifactory URL", default="https://conan.io/center/")
+    remotes: Optional[List[str]] = Field(description="List of remotes to use")
+    configurations: Optional[List[ConanConfiguration]] = Field(description="List of configurations to use")
+    settings: Optional[Dict[str, Dict[str, str]]] = Field(description="Key value conan profile settings")
+    deploy: Optional[str] = Field(description="Remote to deploy to")
+    no_default_remotes: bool = Field(description="Remove conan default remotes", default=False)
+    enable_dependency_collision: bool = Field(description="Allow on dependencies version collisions", default=False)
