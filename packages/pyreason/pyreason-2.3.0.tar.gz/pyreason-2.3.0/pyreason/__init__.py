29799b4d1d422aa8ba39689cc7af7aa8afc06040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 05:05:22 +0000

    Add package pyreason/2.3.0/pyreason-2.3.0.tar.gz

---
+++ packages/pyreason/pyreason-2.3.0.tar.gz/pyreason-2.3.0/pyreason/__init__.py
@@ -0,0 +1,32 @@
+# Set numba environment variable
+import os
+package_path = os.path.abspath(os.path.dirname(__file__))
+cache_path = os.path.join(package_path, 'cache')
+cache_status_path = os.path.join(package_path, '.cache_status.yaml')
+os.environ['NUMBA_CACHE_DIR'] = cache_path
+
+
+from pyreason.pyreason import *
+import yaml
+
+
+with open(cache_status_path) as file:
+    cache_status = yaml.safe_load(file)
+
+if not cache_status['initialized']:
+    print('Imported PyReason for the first time. Initializing ... this will take a minute')
+    graph_path = os.path.join(package_path, 'examples', 'hello-world', 'friends_graph.graphml')
+
+    settings.verbose = False
+    load_graphml(graph_path)
+    add_rule(Rule('popular(x) <-1 popular(y), Friends(x,y), owns(y,z), owns(x,z)', 'popular_rule'))
+    add_fact(Fact('popular-fact', 'Mary', 'popular', [1, 1], 0, 2))
+    reason(timesteps=2)
+
+    reset()
+    reset_rules()
+
+    # Update cache status
+    cache_status['initialized'] = True
+    with open(cache_status_path, 'w') as file:
+        yaml.dump(cache_status, file)
---
+++ packages/pyreason/pyreason-2.3.0.tar.gz/pyreason-2.3.0/pyreason/scripts/rules/rule.py
@@ -0,0 +1,22 @@
+import pyreason.scripts.utils.rule_parser as rule_parser
+
+
+class Rule:
+    """
+    Example text:
+            `'pred1(x,y) : [0.2, 1] <- pred2(a, b) : [1,1], pred3(b, c)'`
+
+    1. It is not possible to have weights for different clauses. Weights are 1 by default with bias 0
+    TODO: Add weights as a parameter
+    """
+    def __init__(self, rule_text: str, name: str, infer_edges: bool = False, set_static: bool = False, immediate_rule: bool = False, custom_thresholds=None):
+        """
+        :param rule_text: The rule in text format
+        :param name: The name of the rule. This will appear in the rule trace
+        :param infer_edges: Whether to infer new edges after edge rule fires
+        :param set_static: Whether to set the atom in the head as static if the rule fires. The bounds will no longer change
+        :param immediate_rule: Whether the rule is immediate. Immediate rules check for more applicable rules immediately after being applied
+        """
+        if custom_thresholds is None:
+            custom_thresholds = []
+        self.rule = rule_parser.parse_rule(rule_text, name, custom_thresholds, infer_edges, set_static, immediate_rule)
