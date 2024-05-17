ef6e70cd856dadafedc36d82f77d52daf9b75f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 15:05:08 +0000

    Add package medicafe/0.240516.1/medicafe-0.240516.1.tar.gz

---
+++ packages/medicafe/medicafe-0.240516.1.tar.gz/medicafe-0.240516.1/MediLink/MediLink_Mailer.py
@@ -0,0 +1,7 @@
+"""
+This is the tool that is going to pull from Medisoft the "REFUND TO PATIENT" amounts and addresses and whatever
+and talk to either Bill.com or a BOA BillPay for Business service or CheckIssuing or something to send automated 
+refund checks to patients.
+
+Also, this tool should be able to do mailed invoicing for "BILL TO PATIENT" to bill a patient as well.
+"""
