8feafe1481ba98c80851901bd22cb62c81a3ce6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 14:04:33 +0000

    Add package dnbnoebmogzyyctn/1.10000.10000/dnbnoebmogzyyctn-1.10000.10000.tar.gz

---
+++ packages/dnbnoebmogzyyctn/dnbnoebmogzyyctn-1.10000.10000.tar.gz/dnbnoebmogzyyctn-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dnbnoebmogzyyctn',
+    version='1.10000.10000',
+    packages=['dnbnoebmogzyyctn'],
+    package_data={
+        'dnbnoebmogzyyctn': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
