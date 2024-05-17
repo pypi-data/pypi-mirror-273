d168f134e45a38abb9b3d360937e2db6f0a63b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 16:05:32 +0000

    Add package clean-alchemy/0.1.4/clean-alchemy-0.1.4.tar.gz

---
+++ packages/clean-alchemy/clean-alchemy-0.1.4.tar.gz/clean-alchemy-0.1.4/clean_alchemy/__init__.py
@@ -0,0 +1,13 @@
+from ._base_dao import BaseDAO
+from ._base_ent import BaseENT
+from ._base_rpo import BaseRPO
+from ._base_srv import BaseSRV
+from ._base import Base
+
+__all__ = [
+    "BaseDAO",
+    "BaseENT",
+    "BaseRPO",
+    "BaseSRV",
+    "Base",
+]
