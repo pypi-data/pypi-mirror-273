9018fe2e4c4ea734f3edc529de359f294d142840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 05:04:56 +0000

    Add package cloudcomponents.cdk-codepipeline-anchore-inline-scan-action/2.3.0/cloudcomponents.cdk-codepipeline-anchore-inline-scan-action-2.3.0.tar.gz

---
+++ packages/cloudcomponents.cdk-codepipeline-anchore-inline-scan-action/cloudcomponents.cdk-codepipeline-anchore-inline-scan-action-2.3.0.tar.gz/cloudcomponents.cdk-codepipeline-anchore-inline-scan-action-2.3.0/setup.py
@@ -0,0 +1,64 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cloudcomponents.cdk-codepipeline-anchore-inline-scan-action",
+    "version": "2.3.0",
+    "description": "CodePipeline action to integrate Anchore Engine into your pipeline",
+    "license": "MIT",
+    "url": "https://github.com/cloudcomponents/cdk-components",
+    "long_description_content_type": "text/markdown",
+    "author": "hupe1980",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/cloudcomponents/cdk-components.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cloudcomponents.cdk_codepipeline_anchore_inline_scan_action",
+        "cloudcomponents.cdk_codepipeline_anchore_inline_scan_action._jsii"
+    ],
+    "package_data": {
+        "cloudcomponents.cdk_codepipeline_anchore_inline_scan_action._jsii": [
+            "cdk-codepipeline-anchore-inline-scan-action@2.3.0.jsii.tgz"
+        ],
+        "cloudcomponents.cdk_codepipeline_anchore_inline_scan_action": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.8",
+    "install_requires": [
+        "aws-cdk-lib>=2.141.0, <3.0.0",
+        "constructs>=10.3.0, <11.0.0",
+        "jsii>=1.95.0, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard~=2.13.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
