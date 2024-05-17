c7bb402ecc0aa92444702aed0e1a7c28893e2a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 05:05:06 +0000

    Add package pythonix/1.0.8/pythonix-1.0.8-py3-none-any.whl

---
+++ packages/pythonix/pythonix-1.0.8-py3-none-any.whl/pythonix/pair.py
@@ -0,0 +1,14 @@
+"""
+Functions for creating and mutating key value pairs in a safe and immutable way.
+Pairs well with the `Bind` wrapper type.
+"""
+from pythonix.internals.pair import (
+    Pair,
+    Pairs,
+    new,
+    get_key,
+    get_value,
+    set_key,
+    set_value,
+    map,
+)
