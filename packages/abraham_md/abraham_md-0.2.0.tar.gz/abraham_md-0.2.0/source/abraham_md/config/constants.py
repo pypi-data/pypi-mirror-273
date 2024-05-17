80f5f5c92d3bebddb39b3574cd557aba568de77b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 06:04:09 +0000

    Add package abraham_md/0.2.0/abraham_md-0.2.0.tar.gz

---
+++ packages/abraham_md/abraham_md-0.2.0.tar.gz/abraham_md-0.2.0/source/abraham_md/config/constants.py
@@ -0,0 +1,52 @@
+"""constants.py: Constants for the application."""
+
+from dotenv import load_dotenv  # TODO: Decide whether to use .env file or not
+from os import getenv
+
+
+class Constants:
+    """
+    Constants for the application.
+
+    Notes
+    -----
+    This class contains constants used throughout the application.
+    By storing constants in a single location, it is easier to
+    manage and update them. Constants should be defined as class
+    attributes and should be named in uppercase with underscores
+    separating words.
+    """
+
+    load_dotenv()
+
+    # Logging constants
+    LOGGING_LEVEL_DEFAULT = "INFO"
+    LOGGING_FORMAT = "%(message)s"
+    LOGGING_DATE_FORMAT = "[%X]"
+    LOGGING_TRACEBACKS = True
+
+    # Conversion constants
+    TO_CONVERT_EXTENSION = ".pdf"  # TODO: Add support for other input file types
+    MATHPIX_APP_ID = getenv("MATHPIX_APP_ID")
+    MATHPIX_APP_KEY = getenv("MATHPIX_APP_KEY")
+    MATHPIX_CONVERSION_SUFFIX = ".md"
+    MATHPIX_ENDPOINT = "https://api.mathpix.com/v3/pdf"
+    MATHPIX_OPTIONS = {
+        "math_inline_delimiters": ["$", "$"],
+        "math_display_delimiters": ["$$", "$$"],
+        "idomatic_eqn_arrays": True,
+        "numbers_default_to_math": True,
+        "enable_spell_check": True,
+    }
+
+    # LLM constants
+    OPENAI_API_KEY = getenv("OPENAI_API_KEY")
+    FETCH_SYSTEM_PROMPT = (
+        "You are a program that fetches the {information} from the provided markdown. "
+        "Provide the information in the following format: '{information}: Information'."
+        "If you cannot find the information, return '{information}: None'."
+        "{additional_prompting}"
+    )
+
+    # File constants
+    TEMPLATE_EXTENSION = ".j2"
---
+++ packages/abraham_md/abraham_md-0.2.0.tar.gz/abraham_md-0.2.0/source/abraham_md/interface/command_line.py
@@ -0,0 +1,75 @@
+"""command_line.py: Command line interface for the application."""
+
+from pathlib import Path
+
+from ..config.dialogue import Dialogue
+from ..logs.setup_logging import setup_logging
+from ..validation.path_validation import validate_input_path
+
+cli_interface_logger = setup_logging()
+
+
+def intro_text() -> None:
+    """
+    Display the introduction text for the application.
+
+    Notes
+    -----
+    This function displays the introduction text for the application.
+    """
+    cli_interface_logger.info(Dialogue.INTRO_TEXT)
+
+
+def fetch_input_path() -> Path:
+    """
+    Fetch the input directory path from the user.
+
+    Returns
+    -------
+    Path
+        The input directory path.
+
+    Exceptions
+    ----------
+    FileNotFoundError
+        Raised when the input path is invalid.
+
+    Notes
+    -----
+    This function prompts the user to enter the input directory path.
+    The input path is then validated to ensure it exists and contains
+    the correct directory structure.
+    """
+    cli_interface_logger.info(Dialogue.SETUP_TEXT)
+    cli_interface_logger.info(Dialogue.INPUT_PATH_TEXT)
+    input_directory = input()
+
+    try:
+        input_path = Path(input_directory)
+        input_path_validity = validate_input_path(input_path)
+
+        if input_path_validity is True:
+            return input_path
+        else:
+            raise FileNotFoundError
+    except FileNotFoundError:
+        cli_interface_logger.error(Dialogue.INVALID_PATH_ERROR)
+        return fetch_input_path()
+
+
+def fetch_output_path() -> Path:
+    """
+    Fetch the output directory path from the user.
+
+    Returns
+    -------
+    Path
+        The output directory path.
+
+    Notes
+    -----
+    This function prompts the user to enter the output directory path.
+    """
+    cli_interface_logger.info(Dialogue.OUTPUT_PATH_TEXT)
+    output_directory = input()
+    return Path(output_directory)
