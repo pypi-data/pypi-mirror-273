67514b5ea8ef3682e1d24a25af0a17199b1fcc0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 16:04:49 +0000

    Add package fake-bpy-module/20240517/fake_bpy_module-20240517.tar.gz

---
+++ packages/fake-bpy-module/fake_bpy_module-20240517.tar.gz/fake_bpy_module-20240517/console_shell/__init__.pyi
@@ -0,0 +1,28 @@
+import typing
+
+GenericType = typing.TypeVar("GenericType")
+
+def add_scrollback(text, text_type):
+    """ """
+
+    ...
+
+def autocomplete(_context):
+    """ """
+
+    ...
+
+def banner(context):
+    """ """
+
+    ...
+
+def execute(context, _is_interactive):
+    """ """
+
+    ...
+
+def shell_run(text):
+    """ """
+
+    ...
