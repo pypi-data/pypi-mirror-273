99681d38288a745d37481d8eae225b048407f300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 17:04:45 +0000

    Add package cookieplone/0.6.1/cookieplone-0.6.1.tar.gz

---
+++ packages/cookieplone/cookieplone-0.6.1.tar.gz/cookieplone-0.6.1/tests/conftest.py
@@ -0,0 +1,36 @@
+import random
+import string
+from pathlib import Path
+
+import pytest
+from git import Repo
+
+
+@pytest.fixture()
+def tmp_repo(tmp_path):
+    repo = Repo.init(tmp_path)
+    repo.index.add(tmp_path)
+    repo.index.commit("test commit")
+
+    return tmp_path
+
+
+@pytest.fixture()
+def no_repo(tmp_path):
+    sub_path = "".join(random.choice(string.ascii_lowercase) for _ in range(20))  # noQA:S311
+    path = tmp_path / sub_path
+    path.mkdir(parents=True)
+    return path
+
+
+@pytest.fixture()
+def read_data_file():
+    def func(filepath: str) -> str:
+        data = ""
+        cwd = Path.cwd()
+        path = (cwd / "tests" / "_resources" / filepath).resolve()
+        if path.exists():
+            data = path.read_text()
+        return data
+
+    return func
