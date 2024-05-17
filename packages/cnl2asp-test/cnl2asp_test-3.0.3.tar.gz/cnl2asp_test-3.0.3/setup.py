df8e5b8e12ab39f90061af906f6959c46ae29932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 14:04:57 +0000

    Add package cnl2asp-test/3.0.3/cnl2asp_test-3.0.3.tar.gz

---
+++ packages/cnl2asp-test/cnl2asp_test-3.0.3.tar.gz/cnl2asp_test-3.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cnl2asp-test',
+    version='3.0.3',
+    description='Hello',
+    long_description='World',
+    url='https://github.com/simocaruso/test',
+    license='Apache License',
+    author='Simone Caruso',
+    author_email='simone.caruso@edu.unige.it',
+    maintainer='Simone Caruso',
+    maintainer_email='simone.caruso@edu.unige.it',
+    package_dir={'': 'src'},
+    packages=find_packages('src', exclude=['tests*']),
+    entry_points={
+        'console_scripts': ['printer = printer.main:main'],
+    },
+    python_requires=">=3.10"
+)
