e24a27681caf78e2ac87d3e163a880275a308310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 04:05:35 +0000

    Add package wisest/0.9.0/wisest-0.9.0-py3-none-any.whl

---
+++ packages/wisest/wisest-0.9.0-py3-none-any.whl/wise/cli.py
@@ -0,0 +1,46 @@
+from itertools import product
+
+import click
+from tqdm import tqdm
+
+from .cost import print_cost
+from .price import query_price
+
+
+@click.command()
+@click.argument("source-currency", type=click.STRING)
+@click.argument("target-amount", type=click.STRING)
+@click.argument("target-currency", type=click.STRING)
+@click.option("-i", "--pay-in-method", type=click.STRING, default="GOOGLE_PAY")
+@click.option("-o", "--pay-out-method", type=click.STRING, default="BALANCE")
+@click.option("--new", is_flag=True, type=click.BOOL)
+def cli(
+    source_currency: str,
+    target_amount: str,
+    target_currency: str,
+    pay_in_method: str,
+    pay_out_method: str,
+    new: bool,
+) -> None:
+    sources = source_currency.split(",")
+    amounts = [float(x) for x in target_amount.split(",")]
+    targets = target_currency.split(",")
+
+    prices = [
+        query_price(
+            source_currency=source,
+            target_amount=float(amount),
+            target_currency=target,
+            pay_in_method=pay_in_method,
+            pay_out_method=pay_out_method,
+            price_set_id=2593 if new else 2586,
+        )
+        for source, amount, target in tqdm(list(product(sources, amounts, targets)))
+    ]
+
+    # sort by total fee rate
+    prices = sorted(prices, key=lambda p: p.variable_fee_percent)
+
+    # print costs
+    for price in prices:
+        print_cost(price)
