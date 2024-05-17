27855d161dc812c527ae59a3cd2d5ad83632caba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 04:04:52 +0000

    Add package openclip-client/0.1.1/openclip_client-0.1.1-py3-none-any.whl

---
+++ packages/openclip-client/openclip_client-0.1.1-py3-none-any.whl/openclip_client/client_common.py
@@ -0,0 +1,344 @@
+from __future__ import annotations
+
+import logging
+import os
+import statistics
+import time
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+from typing import Any, Callable, Dict, Optional, Union
+
+import imutils
+import numpy as np
+import yaml
+
+from era_5g_client.client import NetAppClient
+from era_5g_client.client_base import NetAppClientBase
+from era_5g_client.dataclasses import MiddlewareInfo
+from era_5g_interface.channels import CallbackInfoClient, ChannelType
+from era_5g_interface.interface_helpers import HEARTBEAT_CLIENT_EVENT
+from era_5g_interface.measuring import Measuring
+
+logger = logging.getLogger(__name__)
+
+image_storage: Dict[int, np.ndarray] = dict()
+
+DEBUG_PRINT_WARNING = True  # Prints score.
+DEBUG_PRINT_DELAY = True  # Prints the delay between capturing image and receiving the results.
+
+# URL of the CLIP service.
+NETAPP_ADDRESS = str(os.getenv("NETAPP_ADDRESS", "http://localhost:5896"))
+HEARTBEAT_ADDRESS = str(os.getenv("HEARTBEAT_ADDRESS", "http://localhost:5898"))
+
+
+class ResultsReader:
+    """Default class for processing CLIP results."""
+
+    def __init__(self, extended_measuring: bool = False) -> None:
+        """Constructor.
+
+        Args:
+            extended_measuring (bool): Enable logging of measuring.
+        """
+
+        self.delays = []
+        self.delays_recv = []
+        self.delays_send = []
+        self.delays_process = []
+        self.timestamps = [
+            [
+                "start_timestamp_ns",
+                "recv_timestamp_ns",
+                "send_timestamp_ns",
+                "end_timestamp_ns",
+                "timestamp_before_process",
+                "timestamp_after_process",
+            ]
+        ]
+
+        measuring_items = {
+            "key_timestamp": 0,
+            "final_timestamp": 0,
+            "worker_recv_timestamp": 0,
+            "worker_before_process_timestamp": 0,
+            "worker_after_process_timestamp": 0,
+            "worker_send_timestamp": 0,
+        }
+        prefix = f"client-final"
+        self.measuring = Measuring(measuring_items, enabled=extended_measuring, filename_prefix=prefix)
+
+    def stats(self, send_frames_count: int) -> None:
+        """Print timestamps stats and can write them to CSV file.
+
+        Args:
+            send_frames_count (int): Number of send frames.
+        """
+
+        logger.info(f"-----")
+        if len(self.delays) < 1 or len(self.delays_recv) < 1 or len(self.delays_send) < 1:
+            logger.warning(f"No results data received")
+        else:
+            logger.info(f"Send frames: {send_frames_count}, dropped frames: {send_frames_count - len(self.delays)}")
+            logger.info(
+                f"Delay median:                 {statistics.median(self.delays) * 1.0e-9:.3f}s "
+                f"mean: {statistics.mean(self.delays) * 1.0e-9:.3f}s "
+                f"min: {min(self.delays) * 1.0e-9:.3f}s "
+                f"max: {max(self.delays) * 1.0e-9:.3f}s"
+            )
+            logger.info(
+                f"Delay service recv median:    {statistics.median(self.delays_recv) * 1.0e-9:.3f}s "
+                f"mean: {statistics.mean(self.delays_recv) * 1.0e-9:.3f}s "
+                f"min: {min(self.delays_recv) * 1.0e-9:.3f}s "
+                f"max: {max(self.delays_recv) * 1.0e-9:.3f}s"
+            )
+            logger.info(
+                f"Delay service send median:    {statistics.median(self.delays_send) * 1.0e-9:.3f}s "
+                f"mean: {statistics.mean(self.delays_send) * 1.0e-9:.3f}s "
+                f"min: {min(self.delays_send) * 1.0e-9:.3f}s "
+                f"max: {max(self.delays_send) * 1.0e-9:.3f}s"
+            )
+            logger.info(
+                f"Delay service process median: {statistics.median(self.delays_process) * 1.0e-9:.3f}s "
+                f"mean: {statistics.mean(self.delays_process) * 1.0e-9:.3f}s "
+                f"min: {min(self.delays_process) * 1.0e-9:.3f}s "
+                f"max: {max(self.delays_process) * 1.0e-9:.3f}s"
+            )
+
+    def get_results(self, results: Dict[str, Any]) -> None:
+        """Callback which process the results from the CLIP service.
+
+        Args:
+            results (Dict[str, Any]): The results in JSON format.
+        """
+
+        results_timestamp = time.perf_counter_ns()
+
+        if "texts" in results:
+            logger.info(f"texts {results['texts']}")
+        if "probs" in results:
+            logger.info(f"probs {results['probs']}")
+
+        # Process timestamps.
+        if "timestamp" in results:
+            key_timestamp = results.get("timestamp")
+            recv_timestamp = results.get("recv_timestamp", 0)
+            send_timestamp = results.get("send_timestamp", 0)
+            timestamp_before_process = results.get("timestamp_before_process", 0)
+            timestamp_after_process = results.get("timestamp_after_process", 0)
+
+            # Log the final recv time of this node
+            self.measuring.log_measuring(key_timestamp, "final_timestamp", results_timestamp)
+
+            # Log other misc timestamps from the received message
+            self.measuring.log_measuring(key_timestamp, "worker_recv_timestamp", recv_timestamp)
+            self.measuring.log_measuring(
+                key_timestamp,
+                "worker_before_process_timestamp",
+                timestamp_before_process,
+            )
+            self.measuring.log_measuring(
+                key_timestamp,
+                "worker_after_process_timestamp",
+                timestamp_after_process,
+            )
+            self.measuring.log_measuring(key_timestamp, "worker_send_timestamp", send_timestamp)
+
+            self.measuring.store_measuring(key_timestamp)
+
+            if DEBUG_PRINT_DELAY:
+                logger.info(
+                    f"Result number {len(self.timestamps)}"
+                    f", delay: {(results_timestamp - key_timestamp) * 1.0e-9:.3f}s"
+                    # f", recv frame delay: {(recv_timestamp - key_timestamp) * 1.0e-9:.3f}s"
+                )
+                self.delays.append((results_timestamp - key_timestamp))
+                self.delays_recv.append((recv_timestamp - key_timestamp))
+                self.delays_send.append((send_timestamp - key_timestamp))
+                self.delays_process.append((timestamp_after_process - timestamp_before_process))
+
+            self.timestamps.append(
+                [
+                    key_timestamp,
+                    recv_timestamp,
+                    send_timestamp,
+                    results_timestamp,
+                    timestamp_before_process,
+                    timestamp_after_process,
+                ]
+            )
+
+
+class StreamType(Enum):
+    """Class for stream types."""
+
+    JPEG = 1
+    H264 = 2
+    HEVC = 3
+
+
+@dataclass
+class MiddlewareAllInfo:
+    """MiddlewareInfo with task ID, robot ID and resource lock values."""
+
+    middleware_info: MiddlewareInfo
+    task_id: str
+    robot_id: str
+    resource_lock = False
+
+
+class CLIPClient:
+    """Wrapper class for CLIP client."""
+
+    def __init__(
+        self,
+        config: Path,
+        netapp_info: Union[str, MiddlewareAllInfo] = NETAPP_ADDRESS,
+        results_callback: Optional[Callable] = None,
+        stream_type: Optional[StreamType] = StreamType.H264,
+        stats: bool = False,
+        extended_measuring: bool = False,
+    ) -> None:
+        """Constructor.
+
+        Args:
+            config (Path): Path to CLIP configuration file.
+            netapp_info (Union[str, MiddlewareAllInfo]): The URI and port of the CLIP service interface (default taken
+                from environment variables NETAPP_ADDRESS) or MiddlewareInfo with task ID, robot ID and resource lock
+                values.
+            results_callback (Callable, optional): Callback for receiving results. Default to ResultsReader.get_results.
+            stream_type (StreamType, optional): Stream type JPEG or H264 or HEVC. Default to H264.
+            stats (bool): Store output data sizes.
+            extended_measuring (bool): Enable logging of measuring.
+        """
+
+        logger.info("Loading configuration file {cfg}".format(cfg=config))
+        self.config_dict = yaml.safe_load(config.open())
+
+        self.results_callback = results_callback
+        if self.results_callback is None:
+            self.results_viewer = ResultsReader(extended_measuring=extended_measuring)
+            self.results_callback = self.results_viewer.get_results
+        self.stream_type = stream_type
+        self.frame_id = 0
+
+        # Test heartbeat module
+        self.heartbeat_client = NetAppClientBase(
+            {},
+            logging_level=logging.getLogger().level,
+            stats=stats,
+            extended_measuring=False,
+        )
+
+        logger.info(f"Register heartbeat client: {HEARTBEAT_ADDRESS}")
+        # Register heartbeat client.
+        try:
+            self.heartbeat_client.register(HEARTBEAT_ADDRESS)
+            logger.info(f"Heartbeat client registered")
+
+            logger.info(
+                self.heartbeat_client.send_data("GET_BEST_MIDDLEWARE_ADDRESS", HEARTBEAT_CLIENT_EVENT, blocking=True)
+            )
+        except Exception as ex:
+            self.heartbeat_client.disconnect()
+            logger.warning(f"Cannot connect to heartbeat module")
+            # raise ex
+
+        # Create CLIP client.
+        if isinstance(netapp_info, MiddlewareAllInfo):
+            self.client = NetAppClient(
+                {"results": CallbackInfoClient(ChannelType.JSON, self.results_callback)},
+                logging_level=logging.getLogger().level,
+                stats=stats,
+                extended_measuring=extended_measuring,
+            )
+            logger.info(f"Register with netapp_info: {netapp_info}")
+            self.client.connect_to_middleware(netapp_info.middleware_info)
+            # Register client.
+            try:
+                self.client.run_task(
+                    task_id=netapp_info.task_id,
+                    robot_id=netapp_info.robot_id,
+                    resource_lock=False,
+                    args={
+                        "config": self.config_dict,
+                    },
+                )
+            except Exception as ex:
+                self.client.disconnect()
+                raise ex
+            logger.info(f"Client registered")
+        else:
+            self.client = NetAppClientBase(
+                {"results": CallbackInfoClient(ChannelType.JSON, self.results_callback)},
+                logging_level=logging.getLogger().level,
+                stats=stats,
+                extended_measuring=extended_measuring,
+            )
+            logger.info(f"Register with netapp_info: {netapp_info}")
+            # Register client.
+            try:
+                self.client.register(
+                    netapp_info,
+                    args={
+                        "config": self.config_dict,
+                    },
+                )
+            except Exception as ex:
+                self.client.disconnect()
+                raise ex
+            logger.info(f"Client registered")
+
+    def info_callback(self, data: Dict[str, Any]) -> None:
+        logger.info(data)
+
+    def send_image(self, frame: np.ndarray, metadata: Dict, timestamp: Optional[int] = None) -> None:
+        """Send image to CLIP service including resizing.
+
+        Args:
+            frame (np.ndarray): Image in numpy array format ("bgr24").
+            metadata (Dict): Metadata with model and texts.
+            timestamp (int, optional): Timestamp for frame and results synchronization. Default to current time
+                (time.perf_counter_ns()).
+        """
+
+        if self.client is not None:
+            self.frame_id += 1
+            if "reduced_image_height" in self.config_dict:
+                frame = imutils.resize(frame, height=self.config_dict.get("reduced_image_height"))
+            if not timestamp:
+                timestamp = time.perf_counter_ns()
+            if self.stream_type is StreamType.H264:
+                self.client.send_image(
+                    frame,
+                    "image_h264",
+                    ChannelType.H264,
+                    timestamp,
+                    metadata=metadata,
+                )
+            elif self.stream_type is StreamType.HEVC:
+                self.client.send_image(
+                    frame,
+                    "image_hevc",
+                    ChannelType.HEVC,
+                    timestamp,
+                    metadata=metadata,
+                )
+            elif self.stream_type is StreamType.JPEG:
+                self.client.send_image(
+                    frame,
+                    "image_jpeg",
+                    ChannelType.JPEG,
+                    timestamp,
+                    metadata=metadata,
+                )
+
+    def stop(self) -> None:
+        """Print stats and disconnect from CLIP service."""
+
+        logger.info("CLIP client stopping")
+
+        if hasattr(self, "results_viewer") and self.results_viewer is not None:
+            self.results_viewer.stats(self.frame_id)
+        if self.client is not None:
+            self.client.disconnect()
---
+++ packages/openclip-client/openclip_client-0.1.1-py3-none-any.whl/openclip_client_python/client_python.py
@@ -0,0 +1,159 @@
+from __future__ import annotations
+
+import logging
+import signal
+import sys
+import time
+import traceback
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import Optional
+import random
+
+import cv2
+
+# CLIP and 5G-ERA stuff.
+from era_5g_client.exceptions import FailedToConnect
+from era_5g_interface.exceptions import BackPressureException
+from era_5g_interface.utils.rate_timer import RateTimer
+from openclip_client.client_common import CLIPClient, StreamType
+
+# Set logging.
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+logger = logging.getLogger("CLIP client python")
+
+# Testing video file.
+# TEST_VIDEO_FILE = str("../../../videos/video3.mp4")
+TEST_VIDEO_FILE = str("../../../videos/bringauto_2023-03-20_short.mp4")
+
+# Testing configuration of the algorithm.
+CONFIG_FILE = Path("../../../config/config.yaml")
+
+# stopped flag for SIGTERM handler (stopping video frames sending).
+stopped = False
+clip_client: Optional[CLIPClient] = None
+
+
+def signal_handler(sig: int, *_) -> None:
+    """Signal handler for SIGTERM and SIGINT."""
+
+    logger.info(f"Terminating ({signal.Signals(sig).name}) ...")
+    global stopped
+    if stopped:
+        if clip_client is not None:
+            clip_client.stop()
+        sys.exit(1)
+    stopped = True
+
+
+signal.signal(signal.SIGTERM, signal_handler)
+signal.signal(signal.SIGINT, signal_handler)
+
+
+def main() -> None:
+    """Example of CLIP client."""
+
+    # Parse arguments.
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-s", "--stream_type", type=int, help="StreamType: 1 = JPEG, 2 = H.264, 3 = HEVC", default=StreamType.H264
+    )
+    parser.add_argument("-c", "--config", type=Path, help="Collision warning config", default=CONFIG_FILE)
+    parser.add_argument("-t", "--play_time", type=int, help="Video play time in seconds", default=5000)
+    parser.add_argument("source_video", type=str, help="Video stream (file or url)", nargs="?", default=TEST_VIDEO_FILE)
+    parser.add_argument("-m", "--measuring", type=bool, help="Enable extended measuring logs", default=True)
+    parser.add_argument("--stats", type=bool, help="Store output data sizes", default=True)
+    args = parser.parse_args()
+
+    global clip_client
+
+    try:
+        # Create a video capture to pass images to the 5G-ERA Network Application.
+        logger.info(f"Opening video capture {args.source_video}")
+        cap = cv2.VideoCapture(args.source_video)
+        if not cap.isOpened():
+            raise Exception("Cannot open video capture")
+
+        # Check bad loaded FPS
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        if fps > 60:
+            logger.warning(f"FPS {fps} is strangely high, newly set to 30")
+            fps = 30
+
+        # Get width, height and number of frames.
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        # Number of frames can be wrong.
+        frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+        logger.info(f"Video {width}x{height}, {fps} FPS, " f"{frames} frames, " f"{frames / fps} seconds")
+        logger.info(f"Stream type: {StreamType(args.stream_type)}")
+
+        # Create collision warning client with given parameters,
+        # width and height for client registration is loaded from camera config.
+        clip_client = CLIPClient(
+            config=args.config,
+            stream_type=StreamType(args.stream_type),
+            stats=args.stats,
+            extended_measuring=args.measuring,
+        )
+
+        # Testing texts.
+        texts = [
+            ["big car", "small car", "bicycle", "person"],
+            ["car on the right", "car on the left", "car in front"],
+            ["one person", "no person", "more than 5 people"],
+            ["colored cars", "black and white cars"],
+            ["the sun shines from the right side", "the sun shines from the left side"],
+            ["stone road", "asphalt road", "concrete road", "dirt road"],
+            ["I drive in the city", "I drive in the forest", "I drive outside the city"],
+            [
+                "there is a traffic light in front of me",
+                "there is no traffic light in front of me",
+                "there is a traffic sign in front of me",
+                "no sign or traffic light",
+            ],
+            ["the traffic light is red", "the traffic light is green", "the traffic light is orange"],
+            ["it's cloudy", "it's clear", "it's raining"],
+        ]
+
+        # Rate timer for control the speed of a loop (fps).
+        rate_timer = RateTimer(rate=fps, time_function=time.perf_counter, iteration_miss_warning=True)
+        # Start time
+        start_time = time.perf_counter_ns()
+        # Check elapsed time or stopped flag.
+        # Play time in ns
+        play_time_ns = args.play_time * 1.0e9
+        # Main processing loop
+        logger.info("Start sending images ...")
+        while time.perf_counter_ns() - start_time < play_time_ns and not stopped:
+            # Read single frame from a stream.
+            ret, frame = cap.read()
+            if not ret:
+                break
+            # Send to CLIP service for processing.
+            try:
+                metadata = {"model_name": "ViT-B-32", "texts": random.choice(texts)}
+                clip_client.send_image(frame, metadata=metadata)
+            except BackPressureException as e:
+                logger.warning(f"BackPressureException raised while sending: {e}")
+            # Sleep until next frame should be sent (with given fps).
+            rate_timer.sleep()
+        end_time = time.perf_counter_ns()
+        logger.info(f"Total streaming time: {(end_time - start_time) * 1.0e-9:.3f}s")
+        cap.release()
+
+    except FailedToConnect as ex:
+        logger.error(f"Failed to connect to server: {ex}")
+    except KeyboardInterrupt:
+        logger.info("Terminating ...")
+    except Exception as ex:
+        traceback.print_exc()
+        logger.error(f"Exception: {repr(ex)}")
+    finally:
+        if clip_client is not None:
+            clip_client.stop()
+
+
+if __name__ == "__main__":
+    main()
