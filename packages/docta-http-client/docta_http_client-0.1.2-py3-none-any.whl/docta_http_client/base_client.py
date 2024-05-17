23402c4e2d75ac4c5142a5c84a9738ddeeaf0dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 14:04:34 +0000

    Add package docta-http-client/0.1.2/docta_http_client-0.1.2-py3-none-any.whl

---
+++ packages/docta-http-client/docta_http_client-0.1.2-py3-none-any.whl/docta_http_client/base_client.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from .utils import parse_json_response, check_status_code
+
+class APIClient:
+    def __init__(self, api_key, user_id):
+        self.api_key = api_key
+        self.base_url = 'https://staging.api.docta.ai/api-key-'
+        self.user_id = user_id
+
+    def get(self, model, params=None):
+        headers = {
+            'apiKey': self.api_key, 
+            'userId': self.user_id
+        }
+        payload = {
+            "message": params
+        }
+        response = requests.get(f'{self.base_url + model}', headers=headers, params=payload)
+        check_status_code(response)
+        return parse_json_response(response)
+
+    def post(self, model, data=None):
+        headers = {
+            'apiKey': self.api_key, 
+            'userId': self.user_id
+        }
+        payload = {
+            "message": json.dumps(data)
+        }
+        response = requests.post(f'{self.base_url + model}', headers=headers, json=payload)
+        check_status_code(response)
+        return parse_json_response(response)
