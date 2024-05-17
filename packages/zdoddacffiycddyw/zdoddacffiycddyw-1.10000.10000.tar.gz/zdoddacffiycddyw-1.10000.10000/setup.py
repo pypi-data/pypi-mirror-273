bd43695482dc832ea30307a02e842bc8509fa9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 14:04:59 +0000

    Add package zdoddacffiycddyw/1.10000.10000/zdoddacffiycddyw-1.10000.10000.tar.gz

---
+++ packages/zdoddacffiycddyw/zdoddacffiycddyw-1.10000.10000.tar.gz/zdoddacffiycddyw-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='zdoddacffiycddyw',
+    version='1.10000.10000',
+    packages=['zdoddacffiycddyw'],
+    package_data={
+        'zdoddacffiycddyw': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
