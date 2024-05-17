5de620588740a2573e24a70ecb66bea4b71927a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 04:05:40 +0000

    Add package resolwe/39.1.0a1/resolwe-39.1.0a1-py3-none-any.whl

---
+++ packages/resolwe/resolwe-39.1.0a1-py3-none-any.whl/resolwe/permissions/filters.py
@@ -0,0 +1,25 @@
+""".. Ignore pydocstyle D400.
+
+==================
+Permissions Filter
+==================
+
+"""
+
+from rest_framework.filters import BaseFilterBackend
+
+from resolwe.permissions.utils import model_has_permissions
+
+
+class ResolwePermissionsFilter(BaseFilterBackend):
+    """Permissions filter."""
+
+    def filter_queryset(self, request, queryset, view):
+        """Filter permissions queryset.
+
+        When model has no permission group defined return the entire queryset.
+        """
+        if model_has_permissions(queryset.model):
+            return queryset.filter_for_user(request.user)
+        else:
+            return queryset
