744500f122ca7b60e5ec8b173758698e1a25adb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 15:05:02 +0000

    Add package zorch/0.0.0/zorch-0.0.0.tar.gz

---
+++ packages/zorch/zorch-0.0.0.tar.gz/zorch-0.0.0/setup.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zorch',
+    version='0.0.0',
+    author='zwhy',
+    author_email='zwhy2025@gamil.com',
+    description='A brief description of the package',
+    #long_description=open('README.md').read(),
+    #long_description_content_type='text/markdown',
+   # url='https://github.com/yourusername/your_package_name',
+    packages=find_packages(),
+    install_requires=[
+        # 依赖列表
+    ],
+    classifiers=[
+        # 分类器列表
+    ],
+    python_requires='>=3.9',
+)
