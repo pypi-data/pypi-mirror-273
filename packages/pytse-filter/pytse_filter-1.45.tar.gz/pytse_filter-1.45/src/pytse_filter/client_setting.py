2bb117a34762f5e34a71e26de7ac72ac03e16eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 05:05:00 +0000

    Add package pytse-filter/1.45/pytse_filter-1.45.tar.gz

---
+++ packages/pytse-filter/pytse_filter-1.45.tar.gz/pytse_filter-1.45/src/pytse_filter/client_setting.py
@@ -0,0 +1,51 @@
+# This module defines the settings for client-specific calculations.
+# It contains a dictionary named 'calculations' which outlines various financial metrics
+# and their corresponding calculation methods and periods.
+
+calculations = {
+    "sell_per_capita": [
+        {
+            "columns": [
+                "sell_per_capita_avg10"
+            ],
+            "args": {
+                "method": "mean",
+                "period": 10
+            }
+        }
+    ],
+    "buy_per_capita": [
+        {
+            "columns": [
+                "buy_per_capita_avg10"
+            ],
+            "args": {
+                "method": "mean",
+                "period": 10
+            }
+        }
+    ],
+    "power": [
+        {
+            "columns": [
+                "power_avg10"
+            ],
+            "args": {
+                "method": "mean",
+                "period": 10
+            }
+        }
+    ],
+    "money_flow": [
+        {
+            "columns": [
+                "money_flow_total10"
+            ],
+            "args": {
+                "method": "sum",
+                "period": 10
+            }
+        }
+    ],
+
+}
