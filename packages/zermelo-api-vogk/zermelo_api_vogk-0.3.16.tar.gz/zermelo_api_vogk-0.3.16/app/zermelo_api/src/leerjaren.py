acf0c643c88afb96ff512d33c7d476c290d9e137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 06:04:32 +0000

    Add package zermelo-api-vogk/0.3.16/zermelo_api_vogk-0.3.16.tar.gz

---
+++ packages/zermelo-api-vogk/zermelo_api_vogk-0.3.16.tar.gz/zermelo_api_vogk-0.3.16/app/zermelo_api/src/leerjaren.py
@@ -0,0 +1,46 @@
+from .zermelo_api import ZermeloCollection
+from dataclasses import dataclass, InitVar
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def getLeerjaarNaam(string):
+    names = {"LG1": "klas 1", "LG2": "klas 2", "LG3": "klas 3"}
+    for name, result in names.items():
+        if name in string:
+            return result
+    else:
+        return string
+
+
+@dataclass
+class Leerjaar:
+    id: int
+    code: str
+    yearOfEducation: int
+    branchOfSchool: int
+    teacherTeams: list[int]
+    mainGroupAuthority: str
+    educationType: str
+    weekTimeTable: int
+    teachingLevel: str
+    educations: list[int]
+    schoolInSchoolYear: int
+    branchOfSchoolCode: str
+    schoolInSchoolYearName: str
+    profiles: list[int]
+    schoolInSchoolYearId: int
+    name: str = ""
+
+    def __post_init__(self):
+        self.name = getLeerjaarNaam(self.code.upper())
+
+
+@dataclass
+class Leerjaren(ZermeloCollection, list[Leerjaar]):
+    schoolinschoolyear: InitVar
+
+    def __post_init__(self, schoolinschoolyear: int):
+        query = f"departmentsofbranches?schoolInSchoolYear={schoolinschoolyear}"
+        self.load_collection(query, Leerjaar)
