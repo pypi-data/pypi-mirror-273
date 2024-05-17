98618f14dcd94800e7fe3acd40585cfc65fd3c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 16:04:44 +0000

    Add package ntnx-prism-py-client/4.0.1b1/ntnx_prism_py_client-4.0.1b1-py3-none-any.whl

---
+++ packages/ntnx-prism-py-client/ntnx_prism_py_client-4.0.1b1-py3-none-any.whl/ntnx_prism_py_client/api/__init__.py
@@ -0,0 +1,9 @@
+
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ntnx_prism_py_client.api.batches_api import BatchesApi
+from ntnx_prism_py_client.api.categories_api import CategoriesApi
+from ntnx_prism_py_client.api.tasks_api import TasksApi
