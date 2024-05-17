729248c794afa0eff93c23c576c10df441709e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 05:04:44 +0000

    Add package don-cv/0.5.0/don_cv-0.5.0.tar.gz

---
+++ packages/don-cv/don_cv-0.5.0.tar.gz/don_cv-0.5.0/setup.py
@@ -0,0 +1,30 @@
+
+from setuptools import setup, find_packages
+
+setup(
+        name="don-cv",
+        version="0.5.0",
+        packages=find_packages(),
+        entry_points={
+            'console_scripts': [
+                'don-cv=don_cv.cli:main',
+            ],
+        },
+        install_requires=[
+            'rich',
+            'PyYAML',
+        ],
+        author="Don Johnson",
+        author_email="dj@codetestcode.io",
+        description="A terminal GUI displaying a configurable resume",
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        url="https://github.com/copyleftdev/don",
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+        ],
+        python_requires='>=3.6',
+    )
+    
