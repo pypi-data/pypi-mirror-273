cd7c495bd828d584cb17bdc34282cd6daba17828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 04:05:26 +0000

    Add package callable-registry/1.0.0/callable-registry-1.0.0.tar.gz

---
+++ packages/callable-registry/callable-registry-1.0.0.tar.gz/callable-registry-1.0.0/registry/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import importlib.metadata
+
+from .registry import RegisteredFunction, Registry, bind_relevant_kwargs, has_var_kwargs, iterate_arg_names
+
+
+__version__ = importlib.metadata.version("registry")
+
+__all__ = ["Registry", "RegisteredFunction", "iterate_arg_names", "has_var_kwargs", "bind_relevant_kwargs"]
