7e6e294388a10b5010ac69c541d88b51f5243e74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 14:04:43 +0000

    Add package trsolucoes/5.2/trsolucoes-5.2.tar.gz

---
+++ packages/trsolucoes/trsolucoes-5.2.tar.gz/trsolucoes-5.2/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='trsolucoes',
+    version='5.2',
+    packages=find_packages(),
+    install_requires=[
+        'selenium',
+    ],
+    author='Tainan Ramos',
+    author_email='tainan@trsolucoes.com.br',
+    description='Uma biblioteca de auxílio para Selenium',
+    #long_description=open('README.md').read(),
+    #long_description_content_type='text/markdown',
+    #url='https://github.com/seuusuario/selenium-helper-lib',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
