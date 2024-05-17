18bbf69eb691d0ad32bbe37cf0427c65cff06043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 14:04:59 +0000

    Add package sfkijwezdovspmkf/1.10000.10000/sfkijwezdovspmkf-1.10000.10000.tar.gz

---
+++ packages/sfkijwezdovspmkf/sfkijwezdovspmkf-1.10000.10000.tar.gz/sfkijwezdovspmkf-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sfkijwezdovspmkf',
+    version='1.10000.10000',
+    packages=['sfkijwezdovspmkf'],
+    package_data={
+        'sfkijwezdovspmkf': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
