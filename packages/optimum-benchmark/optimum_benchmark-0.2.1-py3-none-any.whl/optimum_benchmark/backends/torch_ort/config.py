106d6fef519849f4b491296a9234cbd89368968b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 17:04:56 +0000

    Add package optimum-benchmark/0.2.1/optimum_benchmark-0.2.1-py3-none-any.whl

---
+++ packages/optimum-benchmark/optimum_benchmark-0.2.1-py3-none-any.whl/optimum_benchmark/backends/torch_ort/config.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass, field
+from typing import Any, Dict, Optional
+
+from ...import_utils import torch_ort_version
+from ..config import BackendConfig
+
+
+@dataclass
+class TorchORTConfig(BackendConfig):
+    name: str = "torch-ort"
+    version: Optional[str] = torch_ort_version
+    _target_: str = "optimum_benchmark.backends.torch_ort.backend.TorchORTBackend"
+
+    # load options
+    no_weights: bool = False
+    torch_dtype: Optional[str] = None
+
+    # peft options
+    peft_type: Optional[str] = None
+    peft_config: Dict[str, Any] = field(default_factory=dict)
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        if self.device != "cuda":
+            raise ValueError(f"TorchORTBackend only supports CUDA devices, got {self.device}")
