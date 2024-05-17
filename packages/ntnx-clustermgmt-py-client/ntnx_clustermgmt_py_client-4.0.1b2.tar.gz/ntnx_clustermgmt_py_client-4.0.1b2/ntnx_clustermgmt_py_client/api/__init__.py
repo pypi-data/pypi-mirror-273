092f74b7cac71ea0706d501b11112c2abc4943c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 16:04:45 +0000

    Add package ntnx-clustermgmt-py-client/4.0.1b2/ntnx_clustermgmt_py_client-4.0.1b2.tar.gz

---
+++ packages/ntnx-clustermgmt-py-client/ntnx_clustermgmt_py_client-4.0.1b2.tar.gz/ntnx_clustermgmt_py_client-4.0.1b2/ntnx_clustermgmt_py_client/api/__init__.py
@@ -0,0 +1,10 @@
+
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ntnx_clustermgmt_py_client.api.clusters_api import ClustersApi
+from ntnx_clustermgmt_py_client.api.disks_api import DisksApi
+from ntnx_clustermgmt_py_client.api.storage_containers_api import StorageContainersApi
+from ntnx_clustermgmt_py_client.api.vcenter_extensions_api import VcenterExtensionsApi
