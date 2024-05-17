5a1a5d2ac1faca1a15e30e7c4f5e710f17a1d292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 04:04:52 +0000

    Add package recom/0.0.3/recom-0.0.3-py3-none-any.whl

---
+++ packages/recom/recom-0.0.3-py3-none-any.whl/recom/__init__.py
@@ -0,0 +1,6 @@
+from recom.device import RecomDevice, RecomDeviceException
+from recom.interface import RecomInterface
+
+__version__ = "0.0.3"   # This will be replaced by setuptools_scm during installation
+
+__all__ = ["RecomDevice", "RecomInterface", "RecomDeviceException"]
