538904e059ed288d2e2dbcee9b3f5c3427367cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 17:04:51 +0000

    Add package mkdocs-roamlinks-with-pdf-plugin/1.0.2/mkdocs-roamlinks-with-pdf-plugin-1.0.2.tar.gz

---
+++ packages/mkdocs-roamlinks-with-pdf-plugin/mkdocs-roamlinks-with-pdf-plugin-1.0.2.tar.gz/mkdocs-roamlinks-with-pdf-plugin-1.0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mkdocs-roamlinks-with-pdf-plugin',
+    version='1.0.2',
+    description='An MkDocs plugin',
+    long_description='An MkDocs plugin that automagically generates relative links and convert roamlike links for foam and obsidian between markdown pages',
+    keywords='mkdocs',
+    url= 'https://github.com/jokulab/mkdocs-roamlinks-plugin',
+    author='jokulab',
+    author_email='166611085+jokudev@users.noreply.github.com',
+    license='MIT',
+    python_requires='>=3.6',
+    install_requires=[
+        'mkdocs>=1.0.4',
+    ],
+    extras_require={
+        'dev': [ 'pytest']
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    packages=find_packages(),
+    entry_points={
+        'mkdocs.plugins': [
+            'roamlinks-with-pdf = mkdocs_roamlinks_with_pdf_plugin.plugin:RoamLinksPlugin',
+        ]
+    }
+)
