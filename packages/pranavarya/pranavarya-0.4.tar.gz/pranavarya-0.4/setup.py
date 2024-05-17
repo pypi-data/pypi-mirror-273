cd1f4b47995b17bb3aeccdcdb2c6e7fe3bd91d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 14:04:49 +0000

    Add package pranavarya/0.4/pranavarya-0.4.tar.gz

---
+++ packages/pranavarya/pranavarya-0.4.tar.gz/pranavarya-0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pranavarya',
+    version='0.4',
+    author='Pranav Arya',
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'pranavarya=pranavarya:hello',
+        ],
+    },
+)
