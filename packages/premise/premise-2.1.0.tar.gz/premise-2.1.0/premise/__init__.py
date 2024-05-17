246e5b6be29d33fa0d090cbde58181730b015e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 14:05:00 +0000

    Add package premise/2.1.0/premise-2.1.0.tar.gz

---
+++ packages/premise/premise-2.1.0.tar.gz/premise-2.1.0/premise/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ("NewDatabase", "clear_cache", "get_regions_definition")
+__version__ = (2, 1, 0)
+
+
+from premise.new_database import NewDatabase
+from premise.utils import clear_cache, get_regions_definition
