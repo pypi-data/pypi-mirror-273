172518be09cc9eb19961737922b78520c737e563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 16:04:53 +0000

    Add package scalellm/0.0.10/scalellm-0.0.10-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/scalellm/scalellm-0.0.10-cp310-cp310-manylinux_2_28_x86_64.whl/scalellm/__init__.py
@@ -0,0 +1,38 @@
+__version__ = "0.0.10"
+from scalellm._C import (
+    LLMHandler,
+    Message,
+    Priority,
+    RequestOutput,
+    SamplingParams,
+    SequenceOutput,
+    Status,
+    StatusCode,
+    Usage,
+    get_metrics,
+)
+from scalellm.llm import LLM
+from scalellm.llm_engine import (
+    AsyncLLMEngine,
+    OutputAsyncStream,
+    OutputStream,
+    ValidationError,
+)
+
+__all__ = [
+    "Message",
+    "LLM",
+    "AsyncLLMEngine",
+    "OutputAsyncStream",
+    "OutputStream",
+    "ValidationError",
+    "Priority",
+    "RequestOutput",
+    "SamplingParams",
+    "SequenceOutput",
+    "Status",
+    "StatusCode",
+    "Usage",
+    "LLMHandler",
+    "get_metrics",
+]
