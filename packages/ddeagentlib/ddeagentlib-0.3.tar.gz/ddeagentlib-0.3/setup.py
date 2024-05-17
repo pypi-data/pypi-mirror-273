db88f1d277c2c4e8ff50b5f56b961a00bbad20ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 16:05:01 +0000

    Add package ddeagentlib/0.3/ddeagentlib-0.3.tar.gz

---
+++ packages/ddeagentlib/ddeagentlib-0.3.tar.gz/ddeagentlib-0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ddeagentlib',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    # 其他元数据
+    author='geogpt',
+    author_email='zhuquezhitu@zhejianglab.com',
+    description='dde agent library',
+    long_description='dde agent library',
+    long_description_content_type='text/markdown',
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
