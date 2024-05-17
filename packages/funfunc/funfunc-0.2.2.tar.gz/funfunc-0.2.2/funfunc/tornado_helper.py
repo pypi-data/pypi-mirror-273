f38f6494a7e4c3c5e4d96cc1787c4bee3fd133ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 17:04:44 +0000

    Add package funfunc/0.2.2/funfunc-0.2.2.tar.gz

---
+++ packages/funfunc/funfunc-0.2.2.tar.gz/funfunc-0.2.2/funfunc/tornado_helper.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# AUTHOR  : wjia
+# TIME    : 2022/6/21 17:38
+# FILE    : tornado
+# PROJECT : funfunc
+# IDE     : PyCharm
+__all__ = ['BaseTornadoWebHandler']
+
+import tornado.web
+
+
+class BaseTornadoWebHandler(tornado.web.RequestHandler):
+    def data_received(self, chunk):
+        pass
+
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "*")
+        self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
+        self.set_header("Access-Control-Max-Age", "3600")
+
+    def options(self):
+        pass
