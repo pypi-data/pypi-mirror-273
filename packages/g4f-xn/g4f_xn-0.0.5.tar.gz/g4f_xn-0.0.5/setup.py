f069c006ccc119ff663df8079c2783f94b8dbd95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 06:04:13 +0000

    Add package g4f-xn/0.0.5/g4f_xn-0.0.5.tar.gz

---
+++ packages/g4f-xn/g4f_xn-0.0.5.tar.gz/g4f_xn-0.0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.5'
+DESCRIPTION = 'library for easy access to GPT models based on g4f'
+LONG_DESCRIPTION = 'long description. Will change in the future'
+
+# Setting up
+setup(
+    name="g4f_xn",
+    version=VERSION,
+    author="Xandr0v",
+    author_email="<olegalexandrov468@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['g4f', 'pyaudio', 'pydub', 'colorama', 'ffmpeg-downloader'],
+    keywords=['python', 'g4f', 'gpt'],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
