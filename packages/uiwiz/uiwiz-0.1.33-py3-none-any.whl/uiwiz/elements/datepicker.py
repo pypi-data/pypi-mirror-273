5b9133e0d61257a875534e096d616201ae8ed4c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 05:05:08 +0000

    Add package uiwiz/0.1.33/uiwiz-0.1.33-py3-none-any.whl

---
+++ packages/uiwiz/uiwiz-0.1.33-py3-none-any.whl/uiwiz/elements/datepicker.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from typing import Optional
+
+from uiwiz.element import Element
+
+
+class Datepicker(Element):
+    def __init__(self, name: str, default_date: Optional[datetime] = None) -> None:
+        super().__init__("input")
+        self.attributes["name"] = name
+        self.attributes["type"] = "date"
+        if default_date:
+            self.attributes["value"] = default_date.strftime("%Y-%m-%d")
