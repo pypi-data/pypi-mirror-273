6a537a8411c3f222486633818beb2e44ebcff63a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 06:04:37 +0000

    Add package ipyspeck/0.6.2/ipyspeck-0.6.2-py2.py3-none-any.whl

---
+++ packages/ipyspeck/ipyspeck-0.6.2-py2.py3-none-any.whl/ipyspeck/stspeck/frontend/build/precache-manifest.1d6326f1ac1b84bfa5bc465350420187.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb2623b5208370bd014bb15a9a8bf32",
+    "url": "./index.html"
+  },
+  {
+    "revision": "40f7ca59c5f48d302563",
+    "url": "./static/js/2.106d2ed7.chunk.js"
+  },
+  {
+    "revision": "cf9ab535e5099157adce312871dd4996",
+    "url": "./static/js/2.106d2ed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e286e95b02488a911d",
+    "url": "./static/js/main.77915833.chunk.js"
+  },
+  {
+    "revision": "759b9ca7b9c36f738779",
+    "url": "./static/js/runtime-main.388eb054.js"
+  }
+]);
