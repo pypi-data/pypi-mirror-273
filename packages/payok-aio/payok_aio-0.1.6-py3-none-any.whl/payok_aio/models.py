3f76fb7b6c00dfb54fdf29ca4b2d6db006541dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 03:05:04 +0000

    Add package payok-aio/0.1.6/payok_aio-0.1.6-py3-none-any.whl

---
+++ packages/payok-aio/payok_aio-0.1.6-py3-none-any.whl/payok_aio/models.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+
+class Balance(BaseModel):
+    '''Payok API balance model'''
+
+    balance: float
+    ref_balance: float
+
+class Transaction(BaseModel):
+    '''Payok API transaction model'''
+
+    transaction: int
+    email: str
+    amount: float
+    currency: str
+    currency_amount: float
+    comission_percent: float
+    comission_fixed: float
+    amount_profit: float
+    method: str | None
+    payment_id: int | str
+    description: str
+    date: str
+    pay_date: str
+    transaction_status: int
+    custom_fields: str
+    webhook_status: int
+    webhook_amount: int
