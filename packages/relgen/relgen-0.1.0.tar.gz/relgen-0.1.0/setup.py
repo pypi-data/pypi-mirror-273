87ceeac637d434bdfe18c31925601f4ef06dcea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 17:04:58 +0000

    Add package relgen/0.1.0/relgen-0.1.0.tar.gz

---
+++ packages/relgen/relgen-0.1.0.tar.gz/relgen-0.1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="relgen",
+    version="0.1.0",
+    author="ruc-datalab",
+    author_email="ltyzzz@ruc.edu.cn",
+    description="A tool for relational data generation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ruc-datalab/RelGen",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    license="Apache-2.0",
+)
