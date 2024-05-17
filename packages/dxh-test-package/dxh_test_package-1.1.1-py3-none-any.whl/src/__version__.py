26a31ef10731958fccd96709e225094926dd6781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 04:04:42 +0000

    Add package dxh-test-package/1.1.1/dxh_test_package-1.1.1-py3-none-any.whl

---
+++ packages/dxh-test-package/dxh_test_package-1.1.1-py3-none-any.whl/src/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "dxh-test-package"
+__version__ = "1.1.1"
+__description__ = "This package is utilized for testing purposes in the learning process."
+__author__ = "Mehedi Hasan"
+__license__ = "MIT"
+__keywords__ = [
+    "test"
+]
