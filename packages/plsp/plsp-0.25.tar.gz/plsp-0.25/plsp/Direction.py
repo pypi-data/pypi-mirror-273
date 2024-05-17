a881baf820e1b8d4b5530d50c9b4a21a8d49199c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 04:05:26 +0000

    Add package plsp/0.25/plsp-0.25.tar.gz

---
+++ packages/plsp/plsp-0.25.tar.gz/plsp-0.25/plsp/Direction.py
@@ -0,0 +1,11 @@
+import dataclasses
+
+@dataclasses.dataclass
+class IODirection:
+	do_encode: bool
+	file_handle: int|None
+	file_path: str|None
+
+	def validate(self):
+		if self.file_handle and self.file_path:
+			raise ValueError("Cannot have both file handle and file path set.")
