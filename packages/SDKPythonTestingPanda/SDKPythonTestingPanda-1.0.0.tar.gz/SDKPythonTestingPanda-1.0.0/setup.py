41b850f0c68ca58e6439e028e6e5858fe1548b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 03:05:38 +0000

    Add package SDKPythonTestingPanda/1.0.0/SDKPythonTestingPanda-1.0.0.tar.gz

---
+++ packages/SDKPythonTestingPanda/SDKPythonTestingPanda-1.0.0.tar.gz/SDKPythonTestingPanda-1.0.0/setup.py
@@ -0,0 +1,13 @@
+"""
+Python SDL external package setup
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='SDKPythonTestingPanda',
+    version='1.0.0',
+    description='Python SDK for Panda Server',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
