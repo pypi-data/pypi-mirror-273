08e0afe261889c75e308b3a72fc4d8089fedc6dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 17:04:51 +0000

    Add package iguana/0.1.5/iguana-0.1.5.tar.gz

---
+++ packages/iguana/iguana-0.1.5.tar.gz/iguana-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='iguana',
+    version='0.1.5',
+    description='A network scanning tool for Kali Linux',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Uveis Smajli',
+    author_email='smajliuveis@yahoo.com',
+    url='https://github.com/uveissmjl/iguana',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'iguana=iguana.iguana:main',
+        ],
+    },
+    install_requires=[
+        'scapy',
+        'dnspython',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
