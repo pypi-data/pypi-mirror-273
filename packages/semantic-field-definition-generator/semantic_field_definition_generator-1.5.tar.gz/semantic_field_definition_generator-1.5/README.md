bc97ea7a0358a18e306611d5f0aa36c58d27fce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 04:05:16 +0000

    Add package semantic-field-definition-generator/1.5/semantic_field_definition_generator-1.5.tar.gz

---
+++ packages/semantic-field-definition-generator/semantic_field_definition_generator-1.5.tar.gz/semantic_field_definition_generator-1.5/README.md
@@ -0,0 +1,154 @@
+# Semantic Field Definition Generator
+
+A generator for [Metaphacts](https://metaphacts.com/) / [ResearchSpace](https://researchspace.org/) semantic field definitions. 
+
+For background information see:
+- ResearchSpace: https://documentation.researchspace.org/resource/Help:SemanticForm
+- Metaphacts: https://help.metaphacts.com/resource/Help:SemanticForm
+
+Based on https://github.com/swiss-art-research-net/sari-field-definitions-generator
+
+## Installation
+
+install using pip
+
+```sh
+pip install semantic-field-definition-generator
+```
+
+## Usage
+
+### Command line tool
+
+For more details on the use of the command line tool run `semantic-field-util -h`:
+
+```
+usage: semantic-field-util [-h] [--version] [-f {RS,MP,UNI,JSON,INLINE}] -y YAML_FILE
+                           [-u SPARQL_URI] [--sparql-repository SPARQL_REPOSITORY]
+                           [--sparql-auth-user SPARQL_USER] [--sparql-auth-password SPARQL_PASS]
+                           [-t TRIG_FILE] [--field-id-prefix FIELD_PREFIX] [--split-fields]
+                           [--add-ns-prefix ADD_NS_PREFIX] [-l {INFO,DEBUG,ERROR}]
+                           {read,write}
+
+Utility to convert ResarchSpace/Metaphacts semantic field definitions.
+
+positional arguments:
+  {read,write}          Action: read=read semantic field definitions in RDF (SPARQL store or file)
+                        and write YAML file, write=read YAML file and write semantic field
+                        definitions to RDF TriG file(s)
+
+options:
+  -h, --help            show this help message and exit
+  --version             show program's version number and exit
+  -f {RS,MP,UNI,JSON,INLINE}, --flavor {RS,MP,UNI,JSON,INLINE}
+                        Flavor of RDF field definitions: RS=ResearchSpace, MP=Metaphacts,
+                        UNI=universal, JSON=JSON, INLINE=inline, default=RS
+  -y YAML_FILE, --yaml YAML_FILE
+                        YAML file (can be directory containing *.yml files) with field definitions
+                        to read or write
+  -u SPARQL_URI, --sparql-uri SPARQL_URI
+                        SPARQL endpoint URI, e.g. http://localhost:8081/sparql
+  --sparql-repository SPARQL_REPOSITORY
+                        Optional SPARQL repository parameter, default=assets
+  --sparql-auth-user SPARQL_USER
+                        Optional SPARQL auth username, default=admin
+  --sparql-auth-password SPARQL_PASS
+                        Optional SPARQL auth password, default=admin
+  -t TRIG_FILE, --trig TRIG_FILE
+                        RDF TriG file (can be directory containing *.trig files) to read or write
+  --field-id-prefix FIELD_PREFIX
+                        Optional URL prefix for field ids
+  --split-fields        Optional split TriG/YAML output into one file per field (file name = field
+                        id)
+  --add-ns-prefix ADD_NS_PREFIX
+                        Optional additional namespace prefix e.g.
+                        skos=http://www.w3.org/2004/02/skos/core# (multiple separated by comma)
+  -l {INFO,DEBUG,ERROR}, --log {INFO,DEBUG,ERROR}
+                        Log level.
+```
+
+### Create field definitions
+
+Define field definitions as a Python dict or in an external YAML file:
+
+```yaml
+prefix: http://rs.swissartresearch.net/instances/fields/
+
+fields:
+
+    - id: {unique identifier}
+      label: {label}
+      description: {description}
+      dataType: {datatype}
+      domain: {domain}
+      range: {range}
+      minOccurs: #
+      maxOccurs: #
+      queries:
+        - ask: '{ask query}'
+        - delete: '{delete query}'
+        - insert: '{insert query}'
+        - select: '{select query}'
+        - valueSet: '{value set query}'
+          
+    - ...
+```
+
+Then, load and compile it using the the `write` action of the command line tool `semantic-field-util`
+
+```
+semantic-field-util -f RS -y ./fieldDefinitions.yml write -t ../ldp/assets/fieldDefinitions.trig
+```
+
+This will read the YAML file `fieldDefinitions.yml` and create ResearchSpace-flavor (`-f RS`) field definitions in the TriG file `../ldp/assets/fieldDefinitions.trig`.
+
+You can also use the generator library in your Python program
+
+```python
+from SemanticFieldDefinitionGenerator import generator
+
+inputFile = './fieldDefinitions.yml'
+outputFile = '../ldp/assets/fieldDefinitions.trig'
+
+model = generator.loadSourceFromFile(inputFile)
+
+output = generator.generate(model, generator.METAPHACTS)
+
+with open(outputFile, 'w') as f:
+    f.write(output)
+```
+
+Available templates are:
+- `generator.METAPHACTS` for Metaphacts Open Source Platform (command line flavor `MP`)
+- `generator.RESEARCHSPACE` for ResearchSpace (command line flavor `RS`)
+- `generator.UNIVERSAL` for both platforms (command line flavor `UNI`)
+- `generator.JSON` for a JSON representation (command line flavor `JSON`)
+- `generator.INLINE` for a Backend Template version (command line flavor `INLINE`)
+
+### Read field definitions
+
+You can read semantic field definitions in RDF from a SPARQL endpoint or TriG files and create a YAML file in the format shown above using the `read` action of the command line tool `semantic-field-util`
+
+```
+semantic-field-util -f RS read -u http://localhost:8280/sparql -y ./fieldDefinitions.yml
+```
+
+This will read ResearchSpace-flavor field definitions from the SPARQL endpoint `http://localhost:8080/sparql` and create the YAML file `./fieldDefinitions.yml`.
+
+You can also use the parser library in your Python program
+
+```python
+from SemanticFieldDefinitionGenerator import parser
+
+sparql_uri = 'http://localhost:8080/sparql'
+outputFile = './fieldDefinitions.yml'
+
+store = parser.open_sparql_store(sparql_uri, repository='assets', auth_user='admin', auth_pass='admin')
+fields = parser.read_fields(store, parser.RESEARCHSPACE)
+parser.write_fields_yaml(fields, outputfile)
+```
+
+## Limitations
+
+- The parser currently doesn't support "Tree Patterns".
+- The generator currently doesn't support multiple values for "Domains", "Ranges" and "Default Values".
---
+++ packages/semantic-field-definition-generator/semantic_field_definition_generator-1.5.tar.gz/semantic_field_definition_generator-1.5/src/SemanticFieldDefinitionGenerator/semantic_field_util.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+from SemanticFieldDefinitionGenerator import generator, parser
+from pathlib import Path
+import urllib
+import argparse
+import logging
+import sys
+
+__version__ = '1.5'
+
+def main():
+    ## 
+    ## main
+    ##
+    argp = argparse.ArgumentParser(description='Utility to convert ResarchSpace/Metaphacts semantic field definitions.')
+    argp.add_argument('--version', action='version', version='%(prog)s ' + __version__)
+    argp.add_argument('action', choices=['read', 'write'],
+                      help='Action: read=read semantic field definitions in RDF (SPARQL store or file) and write YAML file, '
+                      + 'write=read YAML file and write semantic field definitions to RDF TriG file(s)')
+    argp.add_argument('-f', '--flavor', dest='flavor', choices=['RS', 'MP', 'UNI', 'JSON', 'INLINE'],
+                      default='RS',
+                      help='Flavor of RDF field definitions: RS=ResearchSpace, MP=Metaphacts, UNI=universal, '
+                      + 'JSON=JSON, INLINE=inline, default=RS')
+    argp.add_argument('-y', '--yaml', required=True, dest='yaml_file',
+                      help='YAML file (can be directory containing *.yml files) with field definitions to read or write')
+    argp.add_argument('-u', '--sparql-uri', dest='sparql_uri',
+                      help='SPARQL endpoint URI, e.g. http://localhost:8081/sparql')
+    argp.add_argument('--sparql-repository', dest='sparql_repository', default='assets',
+                      help='Optional SPARQL repository parameter, default=assets')
+    argp.add_argument('--sparql-auth-user', dest='sparql_user', default='admin',
+                      help='Optional SPARQL auth username, default=admin')
+    argp.add_argument('--sparql-auth-password', dest='sparql_pass', default='admin',
+                      help='Optional SPARQL auth password, default=admin')
+    argp.add_argument('-t', '--trig', dest='trig_file',
+                      help='RDF TriG file (can be directory containing *.trig files) to read or write')
+    argp.add_argument('--field-id-prefix', dest='field_prefix',
+                      help='Optional URL prefix for field ids')
+    argp.add_argument('--split-fields', dest='split_fields', action='store_true',
+                      help='Optional split TriG/YAML output into one file per field (file name = field id)')
+    argp.add_argument('--add-ns-prefix', dest='add_ns_prefix',
+                      help='Optional additional namespace prefix e.g. skos=http://www.w3.org/2004/02/skos/core# (multiple separated by comma)')
+    argp.add_argument('-l', '--log', dest='loglevel', choices=['INFO', 'DEBUG', 'ERROR'], default='INFO', 
+                      help='Log level.')
+    args = argp.parse_args()
+    
+    logging.basicConfig(level=args.loglevel)
+    
+    ##
+    ## write action
+    ##
+    if args.action == 'write':
+        if args.flavor == 'RS':
+            flavor = generator.RESEARCHSPACE
+        elif args.flavor == 'MP':
+            flavor = generator.METAPHACTS
+        elif args.flavor == 'UNI':
+            flavor = generator.UNIVERSAL
+        elif args.flavor == 'JSON':
+            flavor = generator.JSON
+        elif args.flavor == 'INLINE':
+            flavor = generator.INLINE
+            
+        if not args.trig_file:
+            sys.exit(f"ERROR: action 'write' requires TRIG_FILE!")
+            
+        if args.add_ns_prefix:
+            add_ns_prefix = { key: val for key, val in [prefs.split('=') for prefs in args.add_ns_prefix.split(',')]}
+        else:
+            add_ns_prefix = None
+    
+        logging.info(f"reading field definitions from YAML file {args.yaml_file}")
+        model = generator.loadSourceFromFile(args.yaml_file)
+        logging.info(f"  read {len(model['fields'])} field definitions")
+        if args.split_fields:
+            # generate split field list of ids and outputs
+            outputs = generator.generate(model, flavor, splitFields=True, add_ns_prefix=add_ns_prefix)
+            p = Path(args.trig_file)
+            if not p.is_dir():
+                sys.exit(f"ERROR: TRIG_FILE {p} must be directory for option split_fields!")
+                
+            logging.info(f"writing field definitions to RDF trig files in directory {args.trig_file} in flavor {args.flavor}")
+            cnt = 0
+            for field_id, output in outputs:
+                filename = urllib.parse.quote_plus(field_id) + '.trig'
+                with open(p / filename, 'w') as f:
+                    logging.debug(f"writing trig file {filename}")
+                    f.write(output)
+                    cnt += 1
+                
+            logging.info(f"  wrote {cnt} trig files")
+
+        else:
+            output = generator.generate(model, flavor, splitFields=False, add_ns_prefix=add_ns_prefix)
+            with open(args.trig_file, 'w') as f:
+                logging.info(f"writing field definitions to RDF trig file {args.trig_file} in flavor {args.flavor}")
+                f.write(output)
+
+    ##
+    ## read action
+    ##        
+    elif args.action == 'read':
+        if args.flavor == 'RS':
+            flavor = parser.RESEARCHSPACE
+        elif args.flavor == 'MP':
+            flavor = parser.METAPHACTS
+        else:
+            sys.exit(f"ERROR: action 'read' does not support flavor {args.flavor}!")
+            
+        if args.add_ns_prefix:
+            # create dict from add_ns_prefix string
+            add_ns_prefix = { key: val for key, val in [prefs.split('=') for prefs in args.add_ns_prefix.split(',')]}
+        else:
+            add_ns_prefix = None
+    
+        if args.sparql_uri:
+            store = parser.open_sparql_store(args.sparql_uri, repository=args.sparql_repository, 
+                                      auth_user=args.sparql_user, auth_pass=args.sparql_pass)
+        elif args.trig_file:
+            store = parser.read_trig_store(args.trig_file)
+        else:
+            sys.exit(f"ERROR: action 'read' requires SPARQL_URI or TRIG_FILE!")
+    
+        fields = parser.read_fields(store, flavor, field_id_prefix=args.field_prefix, add_ns_prefix=add_ns_prefix)
+        logging.info(f"  found {len(fields)} fields (flavor={args.flavor})")
+        parser.write_fields_yaml(fields, args.yaml_file, field_id_prefix=args.field_prefix, splitFields=args.split_fields)
+        if args.split_fields:
+            logging.info(f"  wrote {len(fields)} fields to YAML directory {args.yaml_file}")
+        else:
+            logging.info(f"  wrote {len(fields)} fields to YAML file {args.yaml_file}")
