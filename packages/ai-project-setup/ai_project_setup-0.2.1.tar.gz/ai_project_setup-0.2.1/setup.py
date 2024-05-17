f3b7db3d11e2097dca16f91cf7c2bff4d76a9380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 14:04:41 +0000

    Add package ai-project-setup/0.2.1/ai_project_setup-0.2.1.tar.gz

---
+++ packages/ai-project-setup/ai_project_setup-0.2.1.tar.gz/ai_project_setup-0.2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ai_project_setup',
+    version='0.2.1',
+    packages=find_packages(),
+    description='A versatile utility package designed to streamline the setup of AI project structures while seamlessly integrating with Git repositories and Google Colab, simplifying the process of building collaborative AI projects.',
+    author='Patrick Ogbuitepu',
+    author_email='pat2echo@gmail.com',
+    keywords=['AI', 'utility package', 'project structure', 'Git integration', 'Google Colab integration', 'collaborative projects', 'ease of use', 'streamlined workflow', 'automation', 'project management', 'project', 'setup', 'structure', 'ai', 'artificial intelligence', 'research', 'data science', 'machine learning'],
+    install_requires=[
+        # Any dependencies the package might have, e.g.,
+        # 'numpy', 'pandas'
+    ]
+)
