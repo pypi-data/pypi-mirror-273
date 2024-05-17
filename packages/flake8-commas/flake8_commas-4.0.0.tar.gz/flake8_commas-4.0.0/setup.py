2a933579b20a1256fbb69d54019729877a878f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 05:05:25 +0000

    Add package flake8-commas/4.0.0/flake8_commas-4.0.0.tar.gz

---
+++ packages/flake8-commas/flake8_commas-4.0.0.tar.gz/flake8_commas-4.0.0/setup.py
@@ -0,0 +1,59 @@
+import io
+import os
+
+from setuptools import setup
+
+__dir__ = os.path.dirname(__file__)
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+
+readme = read('README.rst')
+history = read('CHANGES.rst').replace('.. :changelog:', '')
+
+
+setup(
+    name='flake8-commas',
+    author="Trevor Creech",
+    author_email="trevor@trevorcreech.com",
+    maintainer="Peter Law",
+    maintainer_email="PeterJCLaw@gmail.com",
+    version='4.0.0',
+    install_requires=['flake8>=5'],
+    python_requires='>=3.8',
+    url='https://github.com/PyCQA/flake8-commas/',
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/x-rst',
+    description='Flake8 lint for trailing commas.',
+    packages=['flake8_commas'],
+    test_suite='test',
+    include_package_data=True,
+    entry_points={
+        'flake8.extension': [
+            'C81 = flake8_commas:CommaChecker',
+        ],
+    },
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+        'Framework :: Flake8',
+    ],
+)
