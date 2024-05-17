ee5175388f046a58c09765da5e383b361b88c7d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 14:04:32 +0000

    Add package sojhldqcwnerwxdk/1.10000.10001/sojhldqcwnerwxdk-1.10000.10001.tar.gz

---
+++ packages/sojhldqcwnerwxdk/sojhldqcwnerwxdk-1.10000.10001.tar.gz/sojhldqcwnerwxdk-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sojhldqcwnerwxdk',
+    version='1.10000.10001',
+    packages=['sojhldqcwnerwxdk'],
+    package_data={
+        'sojhldqcwnerwxdk': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
