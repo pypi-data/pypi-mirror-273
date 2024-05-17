b1d894ff8c33ac743a7a5d700bfadde8ca23ac65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 04:05:00 +0000

    Add package url-remote/0.0.93/url_remote-0.0.93.tar.gz

---
+++ packages/url-remote/url_remote-0.0.93.tar.gz/url_remote-0.0.93/url_remote/src/authentiction_api_version_dict.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+# TODO Another option, which option we prefer?
+AUTHENTICATION_API_VERSION_DICT = {
+    "play1": 1,
+    "dvlp1": 2
+}
+
+
+class AuthenticationApiVersion(Enum):
+    AUTHENTICATION_API_VERSION_DICT = {
+        "play1": 1,
+        "dvlp1": 2
+    }
