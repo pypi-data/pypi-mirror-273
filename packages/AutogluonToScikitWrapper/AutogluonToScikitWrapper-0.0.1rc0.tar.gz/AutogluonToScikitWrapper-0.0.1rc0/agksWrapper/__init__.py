e7a820d2118708a458afb913d7257ca05fc4cc52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 03:05:40 +0000

    Add package AutogluonToScikitWrapper/0.0.1rc0/AutogluonToScikitWrapper-0.0.1rc0.tar.gz

---
+++ packages/AutogluonToScikitWrapper/AutogluonToScikitWrapper-0.0.1rc0.tar.gz/AutogluonToScikitWrapper-0.0.1rc0/agksWrapper/__init__.py
@@ -0,0 +1,1 @@
+from agsk_wrapper import *
---
+++ packages/AutogluonToScikitWrapper/AutogluonToScikitWrapper-0.0.1rc0.tar.gz/AutogluonToScikitWrapper-0.0.1rc0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1c'
+DESCRIPTION = 'Autogluon to Scikit Wrapper'
+LONG_DESCRIPTION = (
+    'A wrapper which converts Autogluon models to Scikit-Learn models, '
+    'thus breaking any limitation faced by user when implementing Scikit-learn methods to Autogluon Models'
+)
+
+# Setting up
+setup(
+    name="AutogluonToScikitWrapper",
+    version=VERSION,
+    author="Arijit Singh",
+    author_email="arijit@easydata.ai",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["autogluon"],
+    keywords=['Autogluon', 'Scikit-learn', 'wrapper'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
