bfe3f18db778b72b97a8690bcfd27be8151166fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 17:04:40 +0000

    Add package popoll-backend/0.0.54/popoll_backend-0.0.54-py3-none-any.whl

---
+++ packages/popoll-backend/popoll_backend-0.0.54-py3-none-any.whl/popoll_backend/query/get_instruments.py
@@ -0,0 +1,21 @@
+import sqlite3
+from typing import List
+
+from popoll_backend.model import Payload
+from popoll_backend.model.db.instrument import Instrument
+from popoll_backend.model.payload.instruments_payload import InstrumentsPayload
+from popoll_backend.query import Query
+
+
+class GetInstruments(Query):
+    
+    instruments: List[Instrument]
+    
+    def __init__(self, poll: str):
+        super().__init__(poll)
+    
+    def process(self, cursor: sqlite3.Cursor):
+        self.instruments = [Instrument(row) for row in cursor.execute('SELECT * from instruments.instruments').fetchall()]
+        
+    def buildResponse(self, cursor: sqlite3.Cursor) -> Payload:
+        return InstrumentsPayload(self.instruments)
