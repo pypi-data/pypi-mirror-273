47d665d0b1336a51f80f6c13d0660f77539d1a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 16:06:02 +0000

    Add package RetrieverVectorDB/0.1/RetrieverVectorDB-0.1.tar.gz

---
+++ packages/RetrieverVectorDB/RetrieverVectorDB-0.1.tar.gz/RetrieverVectorDB-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RetrieverVectorDB',
+    version='0.1',
+    description='A simple Python library for retreiving documents from vectorDB.',
+    author='Nitya Shah',
+    packages=find_packages(),
+)
