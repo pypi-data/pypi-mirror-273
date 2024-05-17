856005fdaf08951d72c23a724923fae3119fc31d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 16:04:47 +0000

    Add package ntnx-monitoring-py-client/4.0.1b1/ntnx_monitoring_py_client-4.0.1b1.tar.gz

---
+++ packages/ntnx-monitoring-py-client/ntnx_monitoring_py_client-4.0.1b1.tar.gz/ntnx_monitoring_py_client-4.0.1b1/ntnx_monitoring_py_client/api/__init__.py
@@ -0,0 +1,12 @@
+
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ntnx_monitoring_py_client.api.alert_email_configuration_api import AlertEmailConfigurationApi
+from ntnx_monitoring_py_client.api.alerts_api import AlertsApi
+from ntnx_monitoring_py_client.api.audits_api import AuditsApi
+from ntnx_monitoring_py_client.api.events_api import EventsApi
+from ntnx_monitoring_py_client.api.manage_alerts_api import ManageAlertsApi
+from ntnx_monitoring_py_client.api.user_defined_policies_api import UserDefinedPoliciesApi
