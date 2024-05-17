a78f004efde2a0d2c42df128189a25fe36cd4a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 04:04:44 +0000

    Add package dxh/0.1.3/dxh-0.1.3.tar.gz

---
+++ packages/dxh/dxh-0.1.3.tar.gz/dxh-0.1.3/dxh/__version__.py
@@ -0,0 +1,9 @@
+__title__ = "dxh"
+__version__ = "0.1.3"
+__description__ = "dxh is Convert python objects to camel case and snake case keys using this library."
+__author__ = "Mohammad Foysal"
+__url__ = "https://github.com/iamfoysal/dxh"
+__license__ = "MIT"
+__keywords__ = [
+    "python", "object", "convert", "camel", "snake", "case", "keys", "to", "camel", "case",
+]
