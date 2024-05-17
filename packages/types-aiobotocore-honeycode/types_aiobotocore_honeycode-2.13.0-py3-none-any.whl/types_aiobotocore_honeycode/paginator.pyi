259cfe301b4bd80672f31b29c2eb2289421d0d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 15:04:54 +0000

    Add package types-aiobotocore-honeycode/2.13.0/types_aiobotocore_honeycode-2.13.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-honeycode/types_aiobotocore_honeycode-2.13.0-py3-none-any.whl/types_aiobotocore_honeycode/paginator.pyi
@@ -0,0 +1,123 @@
+"""
+Type annotations for honeycode service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_honeycode/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_honeycode.client import HoneycodeClient
+    from types_aiobotocore_honeycode.paginator import (
+        ListTableColumnsPaginator,
+        ListTableRowsPaginator,
+        ListTablesPaginator,
+        QueryTableRowsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("honeycode") as client:
+        client: HoneycodeClient
+
+        list_table_columns_paginator: ListTableColumnsPaginator = client.get_paginator("list_table_columns")
+        list_table_rows_paginator: ListTableRowsPaginator = client.get_paginator("list_table_rows")
+        list_tables_paginator: ListTablesPaginator = client.get_paginator("list_tables")
+        query_table_rows_paginator: QueryTableRowsPaginator = client.get_paginator("query_table_rows")
+    ```
+"""
+
+from typing import AsyncIterator, Generic, Iterator, Sequence, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import (
+    FilterTypeDef,
+    ListTableColumnsResultTypeDef,
+    ListTableRowsResultTypeDef,
+    ListTablesResultTypeDef,
+    PaginatorConfigTypeDef,
+    QueryTableRowsResultTypeDef,
+)
+
+__all__ = (
+    "ListTableColumnsPaginator",
+    "ListTableRowsPaginator",
+    "ListTablesPaginator",
+    "QueryTableRowsPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class ListTableColumnsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/honeycode.html#Honeycode.Paginator.ListTableColumns)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_honeycode/paginators/#listtablecolumnspaginator)
+    """
+
+    def paginate(
+        self, *, workbookId: str, tableId: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListTableColumnsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/honeycode.html#Honeycode.Paginator.ListTableColumns.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_honeycode/paginators/#listtablecolumnspaginator)
+        """
+
+class ListTableRowsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/honeycode.html#Honeycode.Paginator.ListTableRows)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_honeycode/paginators/#listtablerowspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        workbookId: str,
+        tableId: str,
+        rowIds: Sequence[str] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListTableRowsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/honeycode.html#Honeycode.Paginator.ListTableRows.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_honeycode/paginators/#listtablerowspaginator)
+        """
+
+class ListTablesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/honeycode.html#Honeycode.Paginator.ListTables)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_honeycode/paginators/#listtablespaginator)
+    """
+
+    def paginate(
+        self, *, workbookId: str, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListTablesResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/honeycode.html#Honeycode.Paginator.ListTables.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_honeycode/paginators/#listtablespaginator)
+        """
+
+class QueryTableRowsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/honeycode.html#Honeycode.Paginator.QueryTableRows)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_honeycode/paginators/#querytablerowspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        workbookId: str,
+        tableId: str,
+        filterFormula: FilterTypeDef,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[QueryTableRowsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/honeycode.html#Honeycode.Paginator.QueryTableRows.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_honeycode/paginators/#querytablerowspaginator)
+        """
