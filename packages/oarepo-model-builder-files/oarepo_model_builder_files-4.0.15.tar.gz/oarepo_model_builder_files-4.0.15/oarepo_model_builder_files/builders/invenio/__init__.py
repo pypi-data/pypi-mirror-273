211a70bc2beee5915587a5f5a459b2272fa791d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 03:05:43 +0000

    Add package oarepo-model-builder-files/4.0.15/oarepo_model_builder_files-4.0.15.tar.gz

---
+++ packages/oarepo-model-builder-files/oarepo_model_builder_files-4.0.15.tar.gz/oarepo_model_builder_files-4.0.15/oarepo_model_builder_files/builders/invenio/__init__.py
@@ -0,0 +1,5 @@
+TEMPLATES = {
+    "files-record": "templates/invenio_files_record.py.jinja2",
+    "files-record-metadata": "templates/invenio_files_record_metadata.py.jinja2",
+    "files-permissions": "templates/invenio_files_record_permissions.py.jinja2",
+}
