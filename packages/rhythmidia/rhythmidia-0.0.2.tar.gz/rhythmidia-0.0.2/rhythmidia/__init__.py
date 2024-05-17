1535dfbae8e660b03426216b207d9df709cd5ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 06:04:16 +0000

    Add package rhythmidia/0.0.2/rhythmidia-0.0.2.tar.gz

---
+++ packages/rhythmidia/rhythmidia-0.0.2.tar.gz/rhythmidia-0.0.2/rhythmidia/__init__.py
@@ -0,0 +1,7 @@
+"""Race tube image analysis and period elucidation in Python."""
+
+# Add imports here
+from .rhythmidia_gui import *
+
+
+from ._version import __version__
---
+++ packages/rhythmidia/rhythmidia-0.0.2.tar.gz/rhythmidia-0.0.2/rhythmidia/rhythmidia.py
@@ -0,0 +1,9 @@
+from rhythmidia.rhythmidia_gui import openAndRun
+from argparse import ArgumentParser
+
+def run():
+    openAndRun()
+
+
+if __name__ == "__main__":
+    run()
