32df5da8a68ca2999136864cd35566b70a137286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 14:04:58 +0000

    Add package flytekit/1.12.1b1/flytekit-1.12.1b1-py3-none-any.whl

---
+++ packages/flytekit/flytekit-1.12.1b1-py3-none-any.whl/flytekit/types/iterator/__init__.py
@@ -0,0 +1,13 @@
+"""
+Flytekit Iterator Type
+==========================================================
+.. currentmodule:: flytekit.types.iterator
+
+.. autosummary::
+   :nosignatures:
+   :toctree: generated/
+
+   FlyteIterator
+"""
+
+from .iterator import FlyteIterator
