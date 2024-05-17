48359a21633780e08ee22fe0904a698016f52344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 04:05:00 +0000

    Add package wxxxxxxx/1.0.0/wxxxxxxx-1.0.0.tar.gz

---
+++ packages/wxxxxxxx/wxxxxxxx-1.0.0.tar.gz/wxxxxxxx-1.0.0/setup.py
@@ -0,0 +1,11 @@
+"""
+Python SDL external package setup
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='wxxxxxxx',
+    version='1.0.0',
+    description='Python SDK for Panda Server',
+    packages=find_packages()
+)
