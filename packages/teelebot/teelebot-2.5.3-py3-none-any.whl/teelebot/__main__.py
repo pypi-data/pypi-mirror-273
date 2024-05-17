8dfac49faec9cffc7abd6d2ba05f7213ed6ada03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 03:05:37 +0000

    Add package teelebot/2.5.3/teelebot-2.5.3-py3-none-any.whl

---
+++ packages/teelebot/teelebot-2.5.3-py3-none-any.whl/teelebot/__main__.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+'''
+@creation date: 2019-8-23
+@last modification: 2023-05-09
+'''
+
+if __name__ == "__main__":
+    import teelebot
+
+    teelebot.main()
---
+++ packages/teelebot/teelebot-2.5.3-py3-none-any.whl/teelebot/version.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+"""
+@description: A Python-based Telegram Bot framework
+@creation date: 2019-11-15
+@last modification: 2024-05-16
+@author: Pluto (github:plutobell)
+"""
+
+__version__ = "2.5.3"
+
+__author__ = "Pluto"
+__email__ = "hi@ojoll.com"
+__blog__ = "https://ojoll.com"
+__github__ = "https://github.com/plutobell/teelebot"
+__description__ = "teelebot is a Python-based Telegram Bot framework with a plugin system that supports hot reload and hot loading."
+
+
