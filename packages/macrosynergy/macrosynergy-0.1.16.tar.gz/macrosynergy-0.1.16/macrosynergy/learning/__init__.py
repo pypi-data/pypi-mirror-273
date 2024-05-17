c08c802bf90c6559acb1ab99112908c435c2fbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 17:05:00 +0000

    Add package macrosynergy/0.1.16/macrosynergy-0.1.16.tar.gz

---
+++ packages/macrosynergy/macrosynergy-0.1.16.tar.gz/macrosynergy-0.1.16/macrosynergy/learning/__init__.py
@@ -0,0 +1,70 @@
+from .panel_time_series_split import (
+    ExpandingKFoldPanelSplit,
+    RollingKFoldPanelSplit,
+    ExpandingIncrementPanelSplit,
+    BasePanelSplit,
+)
+from .cv_tools import panel_cv_scores
+from .transformers import (
+    LassoSelector,
+    MapSelector,
+    ENetSelector,
+    ZnScoreAverager,
+    PanelMinMaxScaler,
+    PanelStandardScaler,
+    FeatureAverager,
+)
+from .metrics import (
+    panel_significance_probability,
+    sharpe_ratio,
+    sortino_ratio,
+    regression_accuracy,
+    regression_balanced_accuracy,
+)
+from .signal_optimizer import SignalOptimizer
+
+from .predictors import (
+    NaivePredictor,
+    BaseWeightedRegressor,
+    WeightedLinearRegression,
+    SignWeightedLinearRegression,
+    TimeWeightedLinearRegression,
+    WeightedLADRegressor,
+    SignWeightedLADRegressor,
+    TimeWeightedLADRegressor,
+    LADRegressor,
+)
+
+__all__ = [
+    # panel_time_series_split
+    "ExpandingKFoldPanelSplit",
+    "RollingKFoldPanelSplit",
+    "ExpandingIncrementPanelSplit",
+    "BasePanelSplit",
+    # cv_tools
+    "panel_cv_scores",
+    # transformers   
+    "FeatureAverager",     
+    "LassoSelector",
+    "MapSelector",
+    "ENetSelector",
+    "PanelMinMaxScaler",
+    "PanelStandardScaler",
+    "ZnScoreAverager",
+    # metrics
+    "panel_significance_probability",
+    "sharpe_ratio",
+    "sortino_ratio",
+    "regression_accuracy",
+    "regression_balanced_accuracy",
+    # signal_optimizer
+    "SignalOptimizer",
+    # predictors
+    "LADRegressor",
+    "NaivePredictor",
+    "SignWeightedLADRegressor",
+    "TimeWeightedLADRegressor",
+    "SignWeightedLinearRegression",
+    "TimeWeightedLinearRegression",
+]
+ 
