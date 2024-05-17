5c5f0dfa5359ec09b6c5f6d8006dec4b043c0a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 16:04:46 +0000

    Add package ntnx-licensing-py-client/4.0.1a1/ntnx_licensing_py_client-4.0.1a1-py3-none-any.whl

---
+++ packages/ntnx-licensing-py-client/ntnx_licensing_py_client-4.0.1a1-py3-none-any.whl/ntnx_licensing_py_client/api/__init__.py
@@ -0,0 +1,8 @@
+
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ntnx_licensing_py_client.api.end_user_license_agreements_api import EndUserLicenseAgreementsApi
+from ntnx_licensing_py_client.api.licenses_api import LicensesApi
