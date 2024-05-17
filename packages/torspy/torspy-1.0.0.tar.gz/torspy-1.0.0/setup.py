b2ba71d37aa1f3df6d1578229d4e14a497801664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 17:04:55 +0000

    Add package torspy/1.0.0/torspy-1.0.0.tar.gz

---
+++ packages/torspy/torspy-1.0.0.tar.gz/torspy-1.0.0/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='torspy',
+    version='1.0.0',
+    author='Fidal',
+    author_email='mrfidal@proton.me',
+    description='Tor onion site scraping tool',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mr-fidal/torspy',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'torspy = torspy.cli:main'
+        ]
+    },
+    install_requires=[
+        'requests',
+        'beautifulsoup4'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    keywords=[
+        'mrfidal',
+        'tor',
+        'DarkWeb',
+        'onion',
+        'torspy',
+        'scraping',
+        'dark web',
+        'web scraping',
+        'hidden services',
+        'privacy',
+        'security',
+        'beautifulsoup4',
+        'requests',
+        'python'
+    ],
+)
