63d0878cd7ed5740ae6a850112c58873e98a9695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 05:05:12 +0000

    Add package FinToolsAP/1.7.7/fintoolsap-1.7.7.tar.gz

---
+++ packages/FinToolsAP/fintoolsap-1.7.7.tar.gz/fintoolsap-1.7.7/tests/test_dbp.py
@@ -0,0 +1,13 @@
+class RCI:
+
+    TABLE = 'RCI'
+
+    DEFAULT_ID = None
+    DEFAULT_DATE = None
+
+    DEFAULT_VARS = []
+
+    VARS_DATA_TYPE = {
+                      '?': object, 'IDRSSD': object, 'RCFN2133': object,
+                      'date': object
+                     }
