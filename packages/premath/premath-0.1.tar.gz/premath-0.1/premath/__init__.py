f7f49f19ae890201f1d888269ed5e6e6114237f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-17 14:04:49 +0000

    Add package premath/0.1/premath-0.1.tar.gz

---
+++ packages/premath/premath-0.1.tar.gz/premath-0.1/premath/__init__.py
@@ -0,0 +1,1 @@
+from premath.linearr import LinearR
---
+++ packages/premath/premath-0.1.tar.gz/premath-0.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'premath',
+  packages = ['premath'],
+  version = '0.1',
+  license='MIT',
+  description = 'This is a library for predictive math.',
+  long_description = 'Easy library for Linear Regression, made specially for stadistic students.',
+  author = 'Iñaki Salcedo',
+  author_email = 'isalcedodurston@gmail.com',
+  url = 'https://github.com/inakisalcedo/premath',
+  download_url = 'https://github.com/inakisalcedo/premath/archive/refs/tags/v_01.tar.gz',
+  keywords = ['MATH', 'REGRESSION', 'PREDICTION'],
+  install_requires=[
+          'numpy',
+          'sklearn',
+          'scipy',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+  ],
+)
