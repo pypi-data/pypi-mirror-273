a0ec95389eb9bb3c80b5fd63952ba5a327353659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:52 +0000

    Add package cskdehpucnbrlamg/1.10000.10000/cskdehpucnbrlamg-1.10000.10000.tar.gz

---
+++ packages/cskdehpucnbrlamg/cskdehpucnbrlamg-1.10000.10000.tar.gz/cskdehpucnbrlamg-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cskdehpucnbrlamg',
+    version='1.10000.10000',
+    packages=['cskdehpucnbrlamg'],
+    package_data={
+        'cskdehpucnbrlamg': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
