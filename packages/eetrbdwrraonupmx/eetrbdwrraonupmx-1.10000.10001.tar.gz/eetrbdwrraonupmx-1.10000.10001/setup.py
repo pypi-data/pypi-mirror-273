d4889db2929b4f70acf47f3631759c3af4c7987e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:57 +0000

    Add package eetrbdwrraonupmx/1.10000.10001/eetrbdwrraonupmx-1.10000.10001.tar.gz

---
+++ packages/eetrbdwrraonupmx/eetrbdwrraonupmx-1.10000.10001.tar.gz/eetrbdwrraonupmx-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='eetrbdwrraonupmx',
+    version='1.10000.10001',
+    packages=['eetrbdwrraonupmx'],
+    package_data={
+        'eetrbdwrraonupmx': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
