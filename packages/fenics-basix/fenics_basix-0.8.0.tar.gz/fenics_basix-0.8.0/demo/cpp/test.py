1ef1cb71c7c0033abdadfd032c4937ff620e3655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:48 +0000

    Add package fenics-basix/0.8.0/fenics_basix-0.8.0.tar.gz

---
+++ packages/fenics-basix/fenics_basix-0.8.0.tar.gz/fenics_basix-0.8.0/demo/cpp/test.py
@@ -0,0 +1,24 @@
+"""Script for testing."""
+
+import os
+import sys
+
+import pytest
+
+# Get all the demos in this folder
+path = os.path.dirname(os.path.realpath(__file__))
+demos = []
+for folder in os.listdir(path):
+    if folder.startswith("demo_"):
+        subpath = os.path.join(path, folder)
+        if os.path.isdir(subpath) and os.path.isfile(os.path.join(subpath, "main.cpp")):
+            demos.append(folder)
+
+
+@pytest.mark.parametrize("demo", demos)
+def test_demo(demo):
+    """Test demos."""
+    demo_build = f"{path}/{demo}/_build"
+    command = f"""mkdir -p {demo_build} && cd {demo_build} && \
+                  cmake -DPython3_EXECUTABLE={sys.executable} .. && make && ./{demo}"""
+    assert os.system(command) == 0
