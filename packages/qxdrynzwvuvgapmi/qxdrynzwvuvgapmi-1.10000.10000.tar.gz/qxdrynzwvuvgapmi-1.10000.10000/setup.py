00cc6224060a0e8309b47b5f53e0ad039120de4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:18 +0000

    Add package qxdrynzwvuvgapmi/1.10000.10000/qxdrynzwvuvgapmi-1.10000.10000.tar.gz

---
+++ packages/qxdrynzwvuvgapmi/qxdrynzwvuvgapmi-1.10000.10000.tar.gz/qxdrynzwvuvgapmi-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='qxdrynzwvuvgapmi',
+    version='1.10000.10000',
+    packages=['qxdrynzwvuvgapmi'],
+    package_data={
+        'qxdrynzwvuvgapmi': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
