b5bf55d62c72076e3c81c94bceadc3121cb6e3c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:18 +0000

    Add package pmiaujcahkxzffye/1.10000.10002/pmiaujcahkxzffye-1.10000.10002.tar.gz

---
+++ packages/pmiaujcahkxzffye/pmiaujcahkxzffye-1.10000.10002.tar.gz/pmiaujcahkxzffye-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pmiaujcahkxzffye',
+    version='1.10000.10002',
+    packages=['pmiaujcahkxzffye'],
+    package_data={
+        'pmiaujcahkxzffye': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
