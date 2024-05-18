26bbf9d83418cf942610636d963e5604917b02e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:24 +0000

    Add package jxrdnpixnfglftul/1.10000.10000/jxrdnpixnfglftul-1.10000.10000.tar.gz

---
+++ packages/jxrdnpixnfglftul/jxrdnpixnfglftul-1.10000.10000.tar.gz/jxrdnpixnfglftul-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='jxrdnpixnfglftul',
+    version='1.10000.10000',
+    packages=['jxrdnpixnfglftul'],
+    package_data={
+        'jxrdnpixnfglftul': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
