3a37e13416e66071a2bc18853998be8271f5b16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:39 +0000

    Add package stielztpomdeoopf/1.10000.10000/stielztpomdeoopf-1.10000.10000.tar.gz

---
+++ packages/stielztpomdeoopf/stielztpomdeoopf-1.10000.10000.tar.gz/stielztpomdeoopf-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='stielztpomdeoopf',
+    version='1.10000.10000',
+    packages=['stielztpomdeoopf'],
+    package_data={
+        'stielztpomdeoopf': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
