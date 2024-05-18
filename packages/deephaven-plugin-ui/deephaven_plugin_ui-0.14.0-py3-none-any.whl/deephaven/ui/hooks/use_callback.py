e36ee3817b1c6d9a06ef5d781b1d83a3cad19fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:05:02 +0000

    Add package deephaven-plugin-ui/0.14.0/deephaven_plugin_ui-0.14.0-py3-none-any.whl

---
+++ packages/deephaven-plugin-ui/deephaven_plugin_ui-0.14.0-py3-none-any.whl/deephaven/ui/hooks/use_callback.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import Callable, Any, Sequence
+
+from .use_ref import use_ref, Ref
+from ..types import Dependencies
+
+
+def use_callback(func: Callable, dependencies: Dependencies) -> Callable:
+    """
+    Create a stable handle for a callback function. The callback will only be recreated if the dependencies change.
+
+    Args:
+        func: The function to create a stable handle to.
+        dependencies: The dependencies to check for changes.
+
+    Returns:
+        The stable handle to the callback function.
+    """
+    deps_ref: Ref[set[Any] | Sequence[Any] | None] = use_ref(None)
+    callback_ref = use_ref(lambda: None)
+    stable_callback_ref = use_ref(
+        lambda *args, **kwargs: callback_ref.current(*args, **kwargs)
+    )
+
+    if deps_ref.current != dependencies:
+        callback_ref.current = func
+        deps_ref.current = dependencies
+
+    return stable_callback_ref.current
