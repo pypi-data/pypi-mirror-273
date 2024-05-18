862eec91b4d2b5109414088e88e2a92c2cb4d490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:28 +0000

    Add package axpwcntpmgjzmach/1.10000.10001/axpwcntpmgjzmach-1.10000.10001.tar.gz

---
+++ packages/axpwcntpmgjzmach/axpwcntpmgjzmach-1.10000.10001.tar.gz/axpwcntpmgjzmach-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='axpwcntpmgjzmach',
+    version='1.10000.10001',
+    packages=['axpwcntpmgjzmach'],
+    package_data={
+        'axpwcntpmgjzmach': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
