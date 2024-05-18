c79fa0040a4695d2ffeb0c2cf429e93b1aeba0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:09:22 +0000

    Add package odlabel/0.7.26.9/odlabel-0.7.26.9.tar.gz

---
+++ packages/odlabel/odlabel-0.7.26.9.tar.gz/odlabel-0.7.26.9/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="odlabel",
+    version="0.7.26.9",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "setuptools==69.5.1",
+        "wheel==0.43.0",
+        "customtkinter",
+        "ultralytics",
+        "matplotlib",
+        "numpy",
+        "opencv-python",
+        "torch>=1.7.0",
+        "torchvision>=0.8.0",
+        "clip-for-odlabel",
+    ],
+    entry_points={
+        "console_scripts": [
+            "odlabel=app.main:launch_GUI",
+        ],
+    },
+    author="Ziad-Algrafi",
+    author_email="ZiadAlgrafi@gmail.com",
+    description="A tool for object detection, labeling and visualization using PyTorch",
+    long_description=open("../README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ziad-Algrafi/odlabel",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.9",
+    setup_requires=["setuptools>=69.5.1", "wheel>=0.43.0"],
+)
