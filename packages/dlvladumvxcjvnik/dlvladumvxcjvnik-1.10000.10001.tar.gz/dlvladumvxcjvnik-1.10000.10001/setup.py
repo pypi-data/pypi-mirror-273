b69d24c67ae265526e3abc83fe0ddf7cb2ebc28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:31 +0000

    Add package dlvladumvxcjvnik/1.10000.10001/dlvladumvxcjvnik-1.10000.10001.tar.gz

---
+++ packages/dlvladumvxcjvnik/dlvladumvxcjvnik-1.10000.10001.tar.gz/dlvladumvxcjvnik-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dlvladumvxcjvnik',
+    version='1.10000.10001',
+    packages=['dlvladumvxcjvnik'],
+    package_data={
+        'dlvladumvxcjvnik': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
