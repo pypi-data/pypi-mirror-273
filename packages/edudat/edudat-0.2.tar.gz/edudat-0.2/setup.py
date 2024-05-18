df01daebce37ec6fbb9563efc586b7096bce647f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:21 +0000

    Add package edudat/0.2/edudat-0.2.tar.gz

---
+++ packages/edudat/edudat-0.2.tar.gz/edudat-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="edudat",
+    version="0.002",
+    packages=["edudat"],
+    install_requires=[
+        "pandas",
+        "requests",
+    ],
+    author="Oliver Dürr",
+    description="A package for educational datasets",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/tensorchiefs/data",
+)
