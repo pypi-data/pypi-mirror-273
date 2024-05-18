fdb7e578defcb91ccc26549ed87f16c25b1294ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:37 +0000

    Add package vdafuiivmqxdzfds/1.10000.10002/vdafuiivmqxdzfds-1.10000.10002.tar.gz

---
+++ packages/vdafuiivmqxdzfds/vdafuiivmqxdzfds-1.10000.10002.tar.gz/vdafuiivmqxdzfds-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='vdafuiivmqxdzfds',
+    version='1.10000.10002',
+    packages=['vdafuiivmqxdzfds'],
+    package_data={
+        'vdafuiivmqxdzfds': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
