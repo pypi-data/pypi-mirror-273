f4183684c7d9e3f77333bd84dadfceef228b6f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:49 +0000

    Add package cfspopcon/5.0.0/cfspopcon-5.0.0.tar.gz

---
+++ packages/cfspopcon/cfspopcon-5.0.0.tar.gz/cfspopcon-5.0.0/cfspopcon/named_options.py
@@ -0,0 +1,66 @@
+"""Enumerators to constrain options for functions."""
+from enum import Enum, auto
+
+
+class ProfileForm(Enum):
+    """Methods to calculate nT profiles."""
+
+    analytic = auto()
+    prf = auto()
+
+
+class RadiationMethod(Enum):
+    """Methods to calculate radiation losses."""
+
+    Inherent = "Bremsstrahlung and synchrotron radiation only"
+    PostJensen = "Impurity radiation, using a coronal equilibrium model from Post & Jensen 1977"
+    MavrinCoronal = "Impurity radiation, using a coronal equilibrium model from Mavrin 2018"
+    MavrinNoncoronal = "Impurity radiation, using a non-coronal model from Mavrin 2017"
+    Radas = "Impurity line and bremsstrahlung radiation, using coronal Lz curves from Radas"
+
+
+class AtomicSpecies(Enum):
+    """Enum of possible atomic species.
+
+    The enum value represents the species atomic number (Z).
+    """
+
+    Hydrogen = 1
+    Helium = 2
+    Lithium = 3
+    Beryllium = 4
+    Carbon = 6
+    Nitrogen = 7
+    Oxygen = 8
+    Neon = 10
+    Argon = 18
+    Krypton = 36
+    Xenon = 54
+    Tungsten = 74
+
+    def __lt__(self, other: "AtomicSpecies") -> bool:
+        """Implements '<' to allow sorting."""
+        return self.value < other.value
+
+    def __gt__(self, other: "AtomicSpecies") -> bool:
+        """Implements '>' to allow sorting."""
+        return self.value > other.value
+
+
+class MomentumLossFunction(Enum):
+    """Select which SOL momentum loss function to use."""
+
+    KotovReiter = auto()
+    Sang = auto()
+    Jarvinen = auto()
+    Moulton = auto()
+    PerezH = auto()
+    PerezL = auto()
+
+
+class LambdaQScaling(Enum):
+    """Options for heat flux decay length scaling."""
+
+    Brunner = auto()
+    EichRegression14 = auto()
+    EichRegression15 = auto()
