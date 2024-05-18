1d540e0064cd8655ff1a53572f59303a116cea33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:31 +0000

    Add package tgcxyixvqbfvkvnz/1.10000.10002/tgcxyixvqbfvkvnz-1.10000.10002.tar.gz

---
+++ packages/tgcxyixvqbfvkvnz/tgcxyixvqbfvkvnz-1.10000.10002.tar.gz/tgcxyixvqbfvkvnz-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tgcxyixvqbfvkvnz',
+    version='1.10000.10002',
+    packages=['tgcxyixvqbfvkvnz'],
+    package_data={
+        'tgcxyixvqbfvkvnz': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
