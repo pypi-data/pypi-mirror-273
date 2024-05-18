cf64bbe94d06d869c06a05b46423ca5198b73bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:20 +0000

    Add package ybzbrdcyczzugckf/1.10000.10001/ybzbrdcyczzugckf-1.10000.10001.tar.gz

---
+++ packages/ybzbrdcyczzugckf/ybzbrdcyczzugckf-1.10000.10001.tar.gz/ybzbrdcyczzugckf-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ybzbrdcyczzugckf',
+    version='1.10000.10001',
+    packages=['ybzbrdcyczzugckf'],
+    package_data={
+        'ybzbrdcyczzugckf': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
