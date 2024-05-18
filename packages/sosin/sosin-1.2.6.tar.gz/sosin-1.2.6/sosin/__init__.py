8dae4ad93be3c1fad28a8e292a486cc20f84a744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:28 +0000

    Add package sosin/1.2.6/sosin-1.2.6.tar.gz

---
+++ packages/sosin/sosin-1.2.6.tar.gz/sosin-1.2.6/sosin/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '1.2.6'
+
+from .rpa.email_mgr import EmailManager
+from .rpa.sms_mgr import AligoManager
+
+from .web.session import SessionManager
+from .web.virtual import VirtualDriver
+
+from .utils.secret import read_config
+from .utils.log import logging
+from .utils.progress import progressBar
