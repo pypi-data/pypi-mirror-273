f13db78342465b5d101b48acf3b9641aec753594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:09:24 +0000

    Add package vectorizer/0.3.0/vectorizer-0.3.0.tar.gz

---
+++ packages/vectorizer/vectorizer-0.3.0.tar.gz/vectorizer-0.3.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vectorizer",
+    version="0.3.0",
+    author="Simean Hamado",
+    author_email="fatihamtech@gmail.com",
+    description="This package aims to facilitate natural languange processing",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="http://example.com/mypackage",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        # "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        # Add your dependencies here
+        #'numpy',
+        # 'requests',
+    ],
+    include_package_data=True,
+    package_data={
+        # Include any package data files here
+        # '': ['*.txt', '*.md'],
+    },
+)
