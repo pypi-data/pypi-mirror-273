1a6c6225f8348ff411a9c66a2ac4807be2caf4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:58 +0000

    Add package rkuabkyhogkrbaby/1.10000.10001/rkuabkyhogkrbaby-1.10000.10001.tar.gz

---
+++ packages/rkuabkyhogkrbaby/rkuabkyhogkrbaby-1.10000.10001.tar.gz/rkuabkyhogkrbaby-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rkuabkyhogkrbaby',
+    version='1.10000.10001',
+    packages=['rkuabkyhogkrbaby'],
+    package_data={
+        'rkuabkyhogkrbaby': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
