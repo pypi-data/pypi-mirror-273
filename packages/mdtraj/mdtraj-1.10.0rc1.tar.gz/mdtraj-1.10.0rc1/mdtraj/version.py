40f4cc28018340755e4bcbf54dc701038ff85dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:20 +0000

    Add package mdtraj/1.10.0rc1/mdtraj-1.10.0rc1.tar.gz

---
+++ packages/mdtraj/mdtraj-1.10.0rc1.tar.gz/mdtraj-1.10.0rc1/mdtraj/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '1.10.0rc1'
+short_version = '1.10.0rc1'
+full_version = '1.10.0rc1+4c9bb6e8bc7d6e86890ff0d57814c3c76f7cf792'
+git_revision = '4c9bb6e8bc7d6e86890ff0d57814c3c76f7cf792'
+release = False
