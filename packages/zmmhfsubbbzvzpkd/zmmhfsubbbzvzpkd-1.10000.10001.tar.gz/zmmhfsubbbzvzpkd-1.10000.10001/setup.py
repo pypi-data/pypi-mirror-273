296e0e84338b9e8aeb695b221033e606b59c9890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:37 +0000

    Add package zmmhfsubbbzvzpkd/1.10000.10001/zmmhfsubbbzvzpkd-1.10000.10001.tar.gz

---
+++ packages/zmmhfsubbbzvzpkd/zmmhfsubbbzvzpkd-1.10000.10001.tar.gz/zmmhfsubbbzvzpkd-1.10000.10001/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='zmmhfsubbbzvzpkd',
+    version='1.10000.10001',
+    packages=['zmmhfsubbbzvzpkd'],
+    package_data={
+        'zmmhfsubbbzvzpkd': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
