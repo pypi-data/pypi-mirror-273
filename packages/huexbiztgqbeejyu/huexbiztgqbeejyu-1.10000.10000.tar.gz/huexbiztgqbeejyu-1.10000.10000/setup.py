eb32b74fe0204522428efd9b622663b8ebd5a922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:37 +0000

    Add package huexbiztgqbeejyu/1.10000.10000/huexbiztgqbeejyu-1.10000.10000.tar.gz

---
+++ packages/huexbiztgqbeejyu/huexbiztgqbeejyu-1.10000.10000.tar.gz/huexbiztgqbeejyu-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='huexbiztgqbeejyu',
+    version='1.10000.10000',
+    packages=['huexbiztgqbeejyu'],
+    package_data={
+        'huexbiztgqbeejyu': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
