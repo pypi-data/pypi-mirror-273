2ab6c9e72bdfef3462be7c2f0135822d5da4234b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:54 +0000

    Add package szdxcnhtizzawjea/1.10000.10002/szdxcnhtizzawjea-1.10000.10002.tar.gz

---
+++ packages/szdxcnhtizzawjea/szdxcnhtizzawjea-1.10000.10002.tar.gz/szdxcnhtizzawjea-1.10000.10002/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='szdxcnhtizzawjea',
+    version='1.10000.10002',
+    packages=['szdxcnhtizzawjea'],
+    package_data={
+        'szdxcnhtizzawjea': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
