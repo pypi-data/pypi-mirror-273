31b24176c9c4f8a82bffddf42e7de4c6300d5e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:09:13 +0000

    Add package twinlib/0.1.5/twinlib-0.1.5.tar.gz

---
+++ packages/twinlib/twinlib-0.1.5.tar.gz/twinlib-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="twinlib",
+    version="0.1.5",
+    author="gaiamzz",
+    author_email="mzzgai@unife.it",
+    description="A library for twin detection",
+    long_description="",
+    readme = "README.md",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={},
+)
