b8371046af398f225eb5cc218851eb217fa313e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:24 +0000

    Add package kwcqajvijgjhkuyo/1.10000.10000/kwcqajvijgjhkuyo-1.10000.10000.tar.gz

---
+++ packages/kwcqajvijgjhkuyo/kwcqajvijgjhkuyo-1.10000.10000.tar.gz/kwcqajvijgjhkuyo-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='kwcqajvijgjhkuyo',
+    version='1.10000.10000',
+    packages=['kwcqajvijgjhkuyo'],
+    package_data={
+        'kwcqajvijgjhkuyo': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
