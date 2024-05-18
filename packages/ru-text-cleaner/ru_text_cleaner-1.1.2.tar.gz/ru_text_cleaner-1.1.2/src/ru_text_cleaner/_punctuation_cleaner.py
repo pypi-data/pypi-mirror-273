4ffdd4930c2b9e985955ccc0e57da1d44cb9efe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:09:17 +0000

    Add package ru-text-cleaner/1.1.2/ru_text_cleaner-1.1.2.tar.gz

---
+++ packages/ru-text-cleaner/ru_text_cleaner-1.1.2.tar.gz/ru_text_cleaner-1.1.2/src/ru_text_cleaner/_punctuation_cleaner.py
@@ -0,0 +1,11 @@
+import string
+
+
+def clean_punctuation(self, text: str) -> str:
+    """
+    Метод clean_puctuation убирает все знаки припинания из текста
+
+    return ''.join(i for i in text if i not in string.punctuation) - возвращает текст без пунктуационных знаков 
+    """
+    return ''.join(i for i in text if i not in string.punctuation)
+
