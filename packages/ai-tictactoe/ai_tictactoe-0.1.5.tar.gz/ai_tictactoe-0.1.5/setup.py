24d7e8e65adecaf79ed76ebc7182b14e9994fcbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:30 +0000

    Add package ai-tictactoe/0.1.5/ai_tictactoe-0.1.5.tar.gz

---
+++ packages/ai-tictactoe/ai_tictactoe-0.1.5.tar.gz/ai_tictactoe-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ai_tictactoe',
+    version='0.1.5',
+    packages=find_packages(),
+    description='A Tic-Tac-Toe game with AI opponent',
+    author='Benjamin M',
+    author_email='bzmarkovits@yahoo.com',
+    url='https://github.com/bzm10/ai_tictactoe',   
+    long_description="# TicTacToe AI\n TicTacToe AI is a Python package that lets you play Tic-Tac-Toe against the computer. It's like the classic game, but with a smart computer opponent that makes the game more interesting. Have fun playing and see if you can beat the AI!",
+    python_requires='>=3.6',
+)
