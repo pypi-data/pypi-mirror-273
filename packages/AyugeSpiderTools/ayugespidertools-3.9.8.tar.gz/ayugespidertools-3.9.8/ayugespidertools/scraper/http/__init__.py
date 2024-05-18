56fb5b6fd0dcf18c6e518cf0f1cf6891f5a758f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:09:32 +0000

    Add package AyugeSpiderTools/3.9.8/ayugespidertools-3.9.8.tar.gz

---
+++ packages/AyugeSpiderTools/ayugespidertools-3.9.8.tar.gz/ayugespidertools-3.9.8/ayugespidertools/scraper/http/__init__.py
@@ -0,0 +1,7 @@
+from ayugespidertools.scraper.http.request import AiohttpRequest
+from ayugespidertools.scraper.http.request.form import AiohttpFormRequest
+
+__all__ = [
+    "AiohttpRequest",
+    "AiohttpFormRequest",
+]
