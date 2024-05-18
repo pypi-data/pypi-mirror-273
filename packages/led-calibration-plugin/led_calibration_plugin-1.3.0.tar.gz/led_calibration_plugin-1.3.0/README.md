1ac2a3a5ebca8a6e30aaababd36d38cb83ff9bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:09:20 +0000

    Add package led-calibration-plugin/1.3.0/led_calibration_plugin-1.3.0.tar.gz

---
+++ packages/led-calibration-plugin/led_calibration_plugin-1.3.0.tar.gz/led_calibration_plugin-1.3.0/README.md
@@ -0,0 +1,74 @@
+
+## Pioreactor LED calibration plugin
+
+![CI tests](https://github.com/pioreactor/pioreactor-led-calibration-plugin/actions/workflows/ci.yaml/badge.svg)
+
+The LED automations available on the Pioreactor are limited: light intensity percentages are based on the power supplied to the LED wires. For specific research and for comparing results between Pioreactors, this plugin can be used to determine the exact LED intensity values.
+
+This plugin offers the ability to calibrate your LEDs using an **external light probe**. It functions in two parts:
+1) a command line calibration that creates a line-of-best-fit and
+2) a calibrated light/dark cycle automation available on the Pioreactor web interface.
+
+## Installation instructions
+
+Install from the command line.
+
+```
+pio install-plugin led-calibration-plugin  ## to install on a single Pioreactor
+
+## OR, on the command line of the leader Pioreactor
+
+pios install-plugin led-calibration-plugin ## to install on all Pioreactors in a cluster
+```
+
+This plugin is also available on the Pioreactor web interface, in the _Plugins_ tab. Downloading from the web interface will install on all Pioreactors in a cluster.
+
+## Run your calibration
+
+Type into your command line:
+
+```
+pio run led_calibration
+```
+
+To perform this calibration, insert your vial containing media into the Pioreactor and submerge your light probe. Follow the prompts on the command line. The plugin will increase the light intensity, and prompt you to record the readings from your light probe. A calibration line of best fit will be generated based on your light probe readings.
+
+## Use the calibration on the UI
+
+An automation will become available on the web interface. To use this automation, use two LED cables in each of channels C and D, and insert the bulbs into the X2 and X3 pockets on the Pioreactor vial holder. **Calibrations for LEDs in channels "C" and "D" must exist.**
+
+In the _Pioreactors_ tab, under _Manage_, you can _Start_ an _LED automation_. A new option becomes available in the drop-down menu called "Calibrated Light/Dark Cycle". Input your desired light intensity in AU (ex. 1000 AU). The automation will set the percent light intensity such that an output of 1000 AU occurs on **both** LEDs.
+
+## Subcommands
+
+Run a subcommand by typing the following into the command line:
+```
+pio run led_calibration <SUBCOMMAND>
+```
+The following subcommands are available:
+
+### **list**
+Prints a table with all existing calibrations stored on the leader. Headings include unique names, timestamps, and channels.
+
+| Name | Timestamp | Channel | Currently in use? |
+|------|----------|---------|-------------------|
+| Algae_C_2022 | 2022-08-29T20:12:00.400000Z | C | ✅ |
+| Algae_B_2022 | 2022-08-29T20:13:00.400000Z | B | ✅ |
+| Algae_B_2021 | 2021-08-29T20:15:00.400000Z | B |  |
+
+### **display**
+Displays the graph and data for the current calibration for each channel A, B, C, and D, if it exists. For example, for the data above, the current calibrations for Algae_C_2022 and Algae_B_2022 will be displayed.
+
+### **change_current**
+If you would like to change a current calibration to a previous one, use `change_current "<UNIQUE NAME>"`. These changes are based on the channel assigned to the calibration.
+
+For example:
+`pio run led_calibration change_current "Algae_B_2021"` would replace Algae_B_2022, since only one calibation is active per channel.
+
+## When to perform an LED calibration
+
+Calibrations should be performed on a case-by-case basis. A new calibration must be performed per channel, and/or for new LED cables, and with any change in media that can alter the light intensity within the vial.
+
+## Plugin documentation
+
+Documentation for plugins can be found on the [Pioreactor wiki](https://docs.pioreactor.com/developer-guide/intro-plugins).
---
+++ packages/led-calibration-plugin/led_calibration_plugin-1.3.0.tar.gz/led_calibration_plugin-1.3.0/led_calibration_plugin/test_led_calibration.py
@@ -0,0 +1,226 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+import time
+
+import pytest
+from msgspec.json import encode
+from pioreactor.background_jobs.led_control import LEDController
+from pioreactor.exc import CalibrationError
+from pioreactor.utils import local_intermittent_storage
+from pioreactor.utils import local_persistant_storage
+from pioreactor.utils.timing import current_utc_timestamp
+from pioreactor.whoami import get_unit_name
+
+from .led_calibration import LEDCalibration
+
+
+def pause(n=1) -> None:
+    time.sleep(n * 0.5)
+
+
+def test_led_fails_if_calibration_not_present():
+    experiment = "test_led_fails_if_calibration_not_present"
+    unit = get_unit_name()
+
+    with local_persistant_storage("current_led_calibration") as cache:
+        if "C" in cache:
+            del cache["C"]
+        if "D" in cache:
+            del cache["D"]
+
+    with pytest.raises(CalibrationError):
+
+        with LEDController(
+            "calibrated_light_dark_cycle",
+            duration=0.01,
+            light_intensity=-1,
+            light_duration_hours=16,
+            dark_duration_hours=8,
+            unit=unit,
+            experiment=experiment,
+        ):
+
+            pause(8)
+
+
+def test_set_intensity_au_above_max() -> None:
+    experiment = "test_set_intensity_au_above_max"
+    unit = get_unit_name()
+
+    with local_persistant_storage("current_led_calibration") as cache:
+        cache["C"] = encode(
+            LEDCalibration(
+                created_at=current_utc_timestamp(),
+                pioreactor_unit=unit,
+                name=experiment,
+                max_intensity=100,
+                min_intensity=0,
+                min_lightprobe_readings=0,
+                max_lightprobe_readings=1000,
+                curve_data_=[1, 0],
+                curve_type="poly",
+                lightprobe_readings=[],
+                led_intensities=[],
+                channel="C",
+            )
+        )
+
+        cache["D"] = encode(
+            LEDCalibration(
+                created_at=current_utc_timestamp(),
+                pioreactor_unit=unit,
+                name=experiment,
+                max_intensity=100,
+                min_intensity=0,
+                min_lightprobe_readings=0,
+                max_lightprobe_readings=1000,
+                curve_data_=[1, 0],
+                curve_type="poly",
+                lightprobe_readings=[],
+                led_intensities=[],
+                channel="D",
+            )
+        )
+
+    with LEDController(
+        "calibrated_light_dark_cycle",
+        duration=0.01,
+        light_intensity=1500,
+        light_duration_hours=16,
+        dark_duration_hours=8,
+        unit=unit,
+        experiment=experiment,
+    ) as lc:
+
+        assert lc.automation_job.light_intensity == 1500  # test returns light_intensity (au)
+
+        lc.automation_job.set_light_intensity(2000)
+
+        assert lc.automation_job.light_intensity == 2000
+
+
+def test_set_intensity_au_negative() -> None:
+    experiment = "test_set_intensity_au_negative"
+    unit = get_unit_name()
+
+    with local_persistant_storage("current_led_calibration") as cache:
+        cache["C"] = encode(
+            LEDCalibration(
+                created_at=current_utc_timestamp(),
+                pioreactor_unit=unit,
+                name=experiment,
+                max_intensity=100,
+                min_intensity=0,
+                min_lightprobe_readings=0,
+                max_lightprobe_readings=1000,
+                curve_data_=[1, 0],
+                curve_type="poly",
+                lightprobe_readings=[],
+                led_intensities=[],
+                channel="C",
+            )
+        )
+
+        cache["D"] = encode(
+            LEDCalibration(
+                created_at=current_utc_timestamp(),
+                pioreactor_unit=unit,
+                name=experiment,
+                max_intensity=100,
+                min_intensity=0,
+                min_lightprobe_readings=0,
+                max_lightprobe_readings=1000,
+                curve_data_=[10, 0],
+                curve_type="poly",
+                lightprobe_readings=[],
+                led_intensities=[],
+                channel="D",
+            )
+        )
+
+    with LEDController(
+        "calibrated_light_dark_cycle",
+        duration=0.01,
+        light_intensity=-1,
+        light_duration_hours=16,
+        dark_duration_hours=8,
+        unit=unit,
+        experiment=experiment,
+    ) as lc:
+
+        assert lc.automation_job.light_intensity == -1
+        pause(8)
+
+        with local_intermittent_storage("leds") as led_cache:
+            assert float(led_cache["C"]) == 0.0
+            assert float(led_cache["D"]) == 0.0
+
+
+def test_set_curve_data_negative() -> None:
+    experiment = "test_set_curve_data_negative"
+    unit = get_unit_name()
+
+    with local_persistant_storage("current_led_calibration") as cache:
+        cache["C"] = encode(
+            LEDCalibration(
+                created_at=current_utc_timestamp(),
+                pioreactor_unit=unit,
+                name=experiment,
+                max_intensity=100,
+                min_intensity=0,
+                min_lightprobe_readings=0,
+                max_lightprobe_readings=1000,
+                curve_data_=[1, -4],
+                curve_type="poly",
+                lightprobe_readings=[],
+                led_intensities=[],
+                channel="C",
+            )
+        )
+
+    with local_persistant_storage("current_led_calibration") as cache:
+        cache["D"] = encode(
+            LEDCalibration(
+                created_at=current_utc_timestamp(),
+                pioreactor_unit=unit,
+                name=experiment,
+                max_intensity=100,
+                min_intensity=0,
+                min_lightprobe_readings=0,
+                max_lightprobe_readings=1000,
+                curve_data_=[1, -4],
+                curve_type="poly",
+                lightprobe_readings=[],
+                led_intensities=[],
+                channel="D",
+            )
+        )
+
+    with LEDController(
+        "calibrated_light_dark_cycle",
+        duration=60,
+        light_intensity=1,
+        light_duration_hours=16,
+        dark_duration_hours=8,
+        unit=unit,
+        experiment=experiment,
+    ) as lc:
+        assert lc.automation_job.light_intensity == 1
+        pause(8)
+
+        with local_intermittent_storage("leds") as led_cache:
+            assert float(led_cache["C"]) == 5.0
+            assert float(led_cache["D"]) == 5.0
+
+        lc.automation_job.set_light_intensity(150)
+
+        with local_intermittent_storage("leds") as led_cache:
+            assert float(led_cache["C"]) == 100
+            assert float(led_cache["D"]) == 100
+
+        lc.automation_job.set_light_intensity(-5)
+
+        with local_intermittent_storage("leds") as led_cache:
+            assert float(led_cache["C"]) == 0
+            assert float(led_cache["D"]) == 0
