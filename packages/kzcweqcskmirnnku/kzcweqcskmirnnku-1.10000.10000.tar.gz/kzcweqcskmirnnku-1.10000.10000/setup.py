6f9ece74840f6e24ce47daf017f1da6603eed4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:35 +0000

    Add package kzcweqcskmirnnku/1.10000.10000/kzcweqcskmirnnku-1.10000.10000.tar.gz

---
+++ packages/kzcweqcskmirnnku/kzcweqcskmirnnku-1.10000.10000.tar.gz/kzcweqcskmirnnku-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='kzcweqcskmirnnku',
+    version='1.10000.10000',
+    packages=['kzcweqcskmirnnku'],
+    package_data={
+        'kzcweqcskmirnnku': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
