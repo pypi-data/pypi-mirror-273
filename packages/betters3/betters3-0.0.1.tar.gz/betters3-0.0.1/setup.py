9c2e2d1143727b02c5d699470bc0746efbf475ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:20 +0000

    Add package betters3/0.0.1/betters3-0.0.1.tar.gz

---
+++ packages/betters3/betters3-0.0.1.tar.gz/betters3-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+  long_description = fh.read()
+
+setup(
+  name="betters3",
+  author="matgasp",
+  version='0.0.1',
+  url = "https://github.com/matgasp/betters3",
+  description="A wrapper of AWS S3 (boto3) that facilitate some operations. ",
+  install_requires=['boto3', 'botocore'],
+  
+  long_description_content_type="text/markdown",
+  long_description=long_description,
+  packages=find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+  ]
+)
