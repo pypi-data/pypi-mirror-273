4e806874a91b765b5c4ca7d868097bee6c8114a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:30 +0000

    Add package desalsim/1.0.3/desalsim-1.0.3.tar.gz

---
+++ packages/desalsim/desalsim-1.0.3.tar.gz/desalsim-1.0.3/tests/__init__.py
@@ -0,0 +1,32 @@
+# __init__.py
+from Desalsim import density_calc
+from Desalsim import constants
+
+from Desalsim.nanofiltration_unit_f import NFMass
+from Desalsim.nanofiltration_unit_f import OsmoticPressure
+from Desalsim.nanofiltration_unit_f import NfEnergy
+
+from Desalsim.med_unit_f import MEDCalculator
+
+from Desalsim.thermal_cryst_f import thermal_calc
+from Desalsim.thermal_cryst_f import conc_cal
+from Desalsim.thermal_cryst_f import calculate_energy
+
+from Desalsim.mfpfr_unit_f import MFPFRCALC
+from Desalsim.mfpfr_unit_f import HClAddition
+from Desalsim.mfpfr_unit_f import energycons
+
+from Desalsim.ed_unit_f import ElectrodialysisCalc
+
+from Desalsim.edbm_unit_f import EDBMCalc
+
+from Desalsim.economic_f import econom
+from Desalsim.economic_f import revenue
+
+
+
+
+
+
+
+
