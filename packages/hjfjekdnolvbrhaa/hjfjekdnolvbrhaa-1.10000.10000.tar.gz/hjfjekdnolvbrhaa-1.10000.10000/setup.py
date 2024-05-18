55a4d21ee31eea788172791638ada0c14be1c932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:36 +0000

    Add package hjfjekdnolvbrhaa/1.10000.10000/hjfjekdnolvbrhaa-1.10000.10000.tar.gz

---
+++ packages/hjfjekdnolvbrhaa/hjfjekdnolvbrhaa-1.10000.10000.tar.gz/hjfjekdnolvbrhaa-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hjfjekdnolvbrhaa',
+    version='1.10000.10000',
+    packages=['hjfjekdnolvbrhaa'],
+    package_data={
+        'hjfjekdnolvbrhaa': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
