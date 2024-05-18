1204fef9b95fc1a899dcf9851733313384fa128d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:05:04 +0000

    Add package kalimera/2.0.0/kalimera-2.0.0.tar.gz

---
+++ packages/kalimera/kalimera-2.0.0.tar.gz/kalimera-2.0.0/README.md
@@ -0,0 +1,14 @@
+# hello-from-chris
+
+A simple Python package to demonstrate package creation and publishing to PyPI.
+
+## Description
+
+`kalimera` is a basic Python package designed to showcase how to create and distribute a Python package. 
+
+## Installation
+
+You can install this package using `pip`:
+
+```bash
+pip install kalimera
---
+++ packages/kalimera/kalimera-2.0.0.tar.gz/kalimera-2.0.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Read the long description from README.md
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '2.0.0'
+DESCRIPTION = 'Hello world'
+
+# Setting up
+setup(
+    name="kalimera",
+    version=VERSION,
+    author="Chris (Christoforos Eseroglou)",
+    author_email="chriseseroglou@gmail.com",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
