340d8a2ea831b2c55d6c1f207657867b0da959f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:09:18 +0000

    Add package yc-aws-wrapper/0.0.0/yc_aws_wrapper-0.0.0.tar.gz

---
+++ packages/yc-aws-wrapper/yc_aws_wrapper-0.0.0.tar.gz/yc_aws_wrapper-0.0.0/setup.py
@@ -0,0 +1,80 @@
+#####################################################################################
+#
+#  Copyright (C) MCode GmbH
+#
+#  Unless a separate license agreement exists between you and MCode GmbH (e.g. you
+#  have purchased a commercial license), the license terms below apply.
+#
+#  Should you enter into a separate license agreement after having received a copy of
+#  this software, then the terms of such license agreement replace the terms below at
+#  the time at which such license agreement becomes effective.
+#
+#  In case a separate license agreement ends, and such agreement ends without being
+#  replaced by another separate license agreement, the license terms below apply
+#  from the time at which said agreement ends.
+#
+#  LICENSE TERMS
+#
+#  This program is free software: you can redistribute it and/or modify it under the
+#  terms of the GNU Affero General Public License, version 3, as published by the
+#  Free Software Foundation. This program is distributed in the hope that it will be
+#  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  See the GNU Affero General Public License Version 3 for more details.
+#
+#  You should have received a copy of the GNU Affero General Public license along
+#  with this program. If not, see <http://www.gnu.org/licenses/agpl-3.0.en.html>.
+#
+#####################################################################################
+
+from __future__ import absolute_import
+import os
+from setuptools import setup, find_packages
+
+
+base_dir = os.path.dirname(__file__)
+about = {}
+if base_dir:
+    os.chdir(base_dir)
+with open(os.path.join(base_dir, "__about__.py")) as f:
+    exec(f.read(), about)
+
+with open(os.path.join(base_dir, "README.md")) as f:
+    long_description = f.read()
+
+
+setup(
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__summary__"],
+    long_description=long_description,
+    license=about["__license__"],
+    url=about["__uri__"],
+    author=about["__author__"],
+    author_email=about["__email__"],
+    platforms=('Any'),
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    data_files=[('.', ['LICENSE', 'COPYRIGHT'])],
+    zip_safe=False,
+    classifiers=[
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Development Status :: 1 - Planning",
+        "Environment :: No Input/Output (Daemon)",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Internet",
+        "Topic :: Communications",
+        "Topic :: Database",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: System :: Networking"
+    ],
+    keywords='WAMP crossbar beanstalkd boom'
+)
