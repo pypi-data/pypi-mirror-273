54b862cb7c80a2c247ca0cc04d959f4c192d5b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:30 +0000

    Add package ml-project-tracker/0.0.4/ml_project_tracker-0.0.4.tar.gz

---
+++ packages/ml-project-tracker/ml_project_tracker-0.0.4.tar.gz/ml_project_tracker-0.0.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ml-project-tracker',
+    version='0.0.4',
+    description='A package for tracking the ML projects',
+    author='Lilia MAHDID',
+    author_email='jl_mahdid@esi.dz',
+    packages=find_packages(exclude=['requirements.sh', 'build.sh', 'test.py', 'update_version.sh', 'test/']),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/mahdid-lilia/ML-Project-Tracker',
+    keywords=['Deep Learning', 'Machine Learning', 'Wandb', 'Neptune'],
+    install_requires=[
+        'wandb',
+        'numpy',
+        'torchvision',
+        'torch', 
+        "neptune"
+        # Add any other dependencies here
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+
+)
