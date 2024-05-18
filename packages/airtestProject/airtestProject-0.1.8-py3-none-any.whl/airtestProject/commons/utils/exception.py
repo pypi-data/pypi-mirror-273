65bf86a4fcd9ca4854a1d326682e55f5d7e7a1f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:50 +0000

    Add package airtestProject/0.1.8/airtestProject-0.1.8-py3-none-any.whl

---
+++ packages/airtestProject/airtestProject-0.1.8-py3-none-any.whl/airtestProject/commons/utils/exception.py
@@ -0,0 +1,50 @@
+"""
+自定义异常类
+"""
+
+class LRunException(Exception):
+
+    pass
+
+
+class NoSuchNodeException(LRunException):
+    """
+    This is NoSuchNodeException
+    """
+    pass
+
+
+class InvalidOperationException(LRunException):
+    """
+    This is InvalidOperationException
+    """
+    pass
+
+
+class TargetTimeout(LRunException):
+    """
+    This is TargetTimeout
+    """
+    pass
+
+
+class ConnectError(LRunException):
+    """
+    This is ConnectError
+    """
+    pass
+
+
+class AdbError(LRunException):
+    """
+    This is AdbError
+    """
+    pass
+
+
+class InvalidParamError(LRunException):
+    """
+    This is InvalidParamError
+    """
+    pass
+
