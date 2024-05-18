7ca03078a03686856e091309ec9263030352ced5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:26 +0000

    Add package bia-bob/0.14.1/bia_bob-0.14.1.tar.gz

---
+++ packages/bia-bob/bia_bob-0.14.1.tar.gz/bia_bob-0.14.1/src/bia_bob/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.14.1"
+
+__all__ = (
+)
+
+from ._machinery import bob, init_assistant, enable_plugins
+from ._accelerate import acc
+from ._bug_fixing import fix
+from ._document import doc
+from ._utilities import available_models
+
+bob.initialize = init_assistant
+bob.__version__ = __version__
+bob.enable_plugins = enable_plugins
