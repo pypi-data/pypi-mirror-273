a74941ed8a8e1d0305a3aa0fb7947a43e32137d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:16 +0000

    Add package rylmzwavefcuxiaz/1.10000.10000/rylmzwavefcuxiaz-1.10000.10000.tar.gz

---
+++ packages/rylmzwavefcuxiaz/rylmzwavefcuxiaz-1.10000.10000.tar.gz/rylmzwavefcuxiaz-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rylmzwavefcuxiaz',
+    version='1.10000.10000',
+    packages=['rylmzwavefcuxiaz'],
+    package_data={
+        'rylmzwavefcuxiaz': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
