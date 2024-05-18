06d88dd1bb1bf70263ff65faa149127322844eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:09:17 +0000

    Add package django-persian-pdf/0.1.3/django_persian_pdf-0.1.3-py3-none-any.whl

---
+++ packages/django-persian-pdf/django_persian_pdf-0.1.3-py3-none-any.whl/django_persian_pdf/apps.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from django.apps import AppConfig
+
+
+class DjangoPersianPDF(AppConfig):
+    name = 'django_persian_pdf'
+    verbose_name = 'django-persian-pdf'
