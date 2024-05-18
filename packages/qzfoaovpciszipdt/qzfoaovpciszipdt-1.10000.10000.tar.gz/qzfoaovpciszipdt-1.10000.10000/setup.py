4f09004bdf9dfebd96e95966b62d6c10c93b9280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 03:04:34 +0000

    Add package qzfoaovpciszipdt/1.10000.10000/qzfoaovpciszipdt-1.10000.10000.tar.gz

---
+++ packages/qzfoaovpciszipdt/qzfoaovpciszipdt-1.10000.10000.tar.gz/qzfoaovpciszipdt-1.10000.10000/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='qzfoaovpciszipdt',
+    version='1.10000.10000',
+    packages=['qzfoaovpciszipdt'],
+    package_data={
+        'qzfoaovpciszipdt': ['*.so']
+    },
+    python_requires='>=3',
+    platforms=["all"]
+)
+
