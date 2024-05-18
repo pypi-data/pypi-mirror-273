fa8c2b5e82ba3d8b3e2f1a2adaeceb176fea3c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-18 04:09:30 +0000

    Add package wsppchem/0.1/wsppchem-0.1.tar.gz

---
+++ packages/wsppchem/wsppchem-0.1.tar.gz/wsppchem-0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name="wsppchem",
+    version="0.1",
+    author="Cossard Lucas and Enzo Venancio",
+    author_email="lucas.cossard@epfl.ch and enzo.venancio@epfl.ch",
+    description="Water Solubility Prediction Project",
+    long_description=open('README.md').read() if os.path.exists('README.md') else '',
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nohalyan/WSPP_Projectppchem",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        "pandas",
+        "numpy",
+        "rdkit",
+        "tqdm",
+        "lightgbm",
+        "requests"
+    ],
+)
